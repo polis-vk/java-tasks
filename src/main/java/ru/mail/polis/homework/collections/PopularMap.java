010a2a7c1b44c2646cce414049c8edfbb53736bb
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-11-26 12:23:56 +0300

    refactoring

--- src/main/java/ru/mail/polis/homework/collections/PopularMap.java
+++ src/main/java/ru/mail/polis/homework/collections/PopularMap.java
@@ -5,11 +5,9 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
