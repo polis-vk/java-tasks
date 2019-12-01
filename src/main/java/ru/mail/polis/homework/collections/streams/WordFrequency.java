d276eb0c7dab5005ec485d1993678bfbc761e0b6
Vlad Zybkin <zybkin11@gmail.com>
2019-12-01 23:37:24 +0300

    added impl of WordFrequency task

--- src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
@@ -1,6 +1,11 @@
 package ru.mail.polis.homework.collections.streams;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -21,7 +26,18 @@ public class WordFrequency {
      * Если будут использоваться условные операторы, то оценка максимальная оценка 2 балла.
      */
     public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
+
+      return lines
+              .flatMap(line -> Arrays.stream(line.split("[ .,!:-?;]+")))
+              .map(String::toLowerCase)
+              .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+              .entrySet().stream()
+              .sorted(Comparator.comparingLong(Map.Entry<String, Long>::getValue).reversed()
+                      .thenComparing(Map.Entry::getKey))
+              .limit(10)
+              .map(Map.Entry::getKey)
+              .collect(Collectors.toList());
+
     }
 
 
