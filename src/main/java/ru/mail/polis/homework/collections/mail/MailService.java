9c4b9ef38811e3ed9679112e8eb003ec10e8d35d
Nekobitlz <jokers.bit@gmail.com>
2019-12-05 17:13:51 +0300

    Implement MailService

--- src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
@@ -1,9 +1,14 @@
 package ru.mail.polis.homework.collections.mail;
 
 
+import ru.mail.polis.homework.collections.PopularMap;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * Нужно создать сервис, который умеет обрабатывать письма и зарплату.
@@ -12,42 +17,75 @@ import java.util.function.Consumer;
  *
  * В реализации нигде не должно быть классов Object и коллекций без типа. Используйте дженерики.
  */
-public class MailService implements Consumer {
+public class MailService implements Consumer<Mail> {
+
+    private PopularMap<String, List<Mail>> mailBox = new PopularMap<>();
 
     /**
      * С помощью этого метода почтовый сервис обрабатывает письма и зарплаты
      * 1 балл
      */
     @Override
-    public void accept(Object o) {
-
+    public void accept(Mail mail) {
+        mailBox.compute(mail.getReceiver(), (key, value) -> {
+            List<Mail> list = mailBox.getOrDefault(key, new ArrayList<>());
+            list.add(mail);
+            return list;
+        });
     }
 
     /**
      * Метод возвращает мапу получатель -> все объекты которые пришли к этому получателю через данный почтовый сервис
      */
-    public Map<String, List> getMailBox() {
-        return null;
+    public Map<String, List<Mail>> getMailBox() {
+        return mailBox;
     }
 
     /**
      * Возвращает самого популярного отправителя
      */
     public String getPopularSender() {
-        return null;
+       return mailBox
+                .entrySet()
+                .stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.groupingBy(Mail::getSender, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry<String, Long>::getValue))
+                .orElse(new EmptyEntry())
+                .getKey();
     }
 
     /**
      * Возвращает самого популярного получателя
      */
     public String getPopularRecipient() {
-        return null;
+        return mailBox.getPopularKey();
     }
 
     /**
      * Метод должен заставить обработать service все mails.
      */
-    public static void process(MailService service, List mails) {
+    public static void process(MailService service, List<Mail> mails) {
+        mails.forEach(service);
+    }
 
+    /* Prevention NullPointerException for getPopularSender() method */
+    private static class EmptyEntry implements Map.Entry<String, Long> {
+        @Override
+        public String getKey() {
+            return null;
+        }
+
+        @Override
+        public Long getValue() {
+            return null;
+        }
+
+        @Override
+        public Long setValue(Long aLong) {
+            return null;
+        }
     }
 }
--- src/main/java/ru/mail/polis/homework/collections/mail/Salary.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/Salary.java
@@ -1,4 +1,15 @@
 package ru.mail.polis.homework.collections.mail;
 
-public class Salary {
+public class Salary extends Mail {
+
+    private int amount;
+
+    public Salary(String sender, String receiver, int amount) {
+        super(sender, receiver);
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
