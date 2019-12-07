9d26e6e89016e8ad92563ba544eeb66a28c52083
Nekobitlz <jokers.bit@gmail.com>
2019-12-07 23:47:25 +0300

    Fix access modifiers for mail class

--- src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
@@ -1,22 +1,20 @@
 package ru.mail.polis.homework.collections.mail;
 
-abstract public class Mail {
+@SuppressWarnings("WeakerAccess")
+public class Mail {
     private String sender;
     private String receiver;
 
-    /* package-private */
-    Mail(String sender, String receiver) {
+    protected Mail(String sender, String receiver) {
         this.sender = sender;
         this.receiver = receiver;
     }
 
-    /* package-private */
-    String getSender() {
+    protected String getSender() {
         return sender;
     }
 
-    /* package-private */
-    String getReceiver() {
+    protected String getReceiver() {
         return receiver;
     }
 }
