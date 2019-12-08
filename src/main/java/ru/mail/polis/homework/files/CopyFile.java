7d7662d3629369c52390e448115a68aca9db134d
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-12-08 23:33:13 +0300

    added first impls task 2

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,13 +1,52 @@
 package ru.mail.polis.homework.files;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class CopyFile {
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      */
-    public static String copySmallFiles(String pathFrom, String pathTo) {
+    public static String copySmallFiles(final String pathFrom, final String pathTo) throws IOException {
+        File fileFrom = new File(pathFrom);
+        File fileTo = new File(pathTo);
+        if (fileFrom.exists() && fileFrom.isDirectory() && !fileTo.exists()) {
+            fileTo.mkdirs();
+            for (final String fileName : fileFrom.list()) {
+                String subFileFrom = fileFrom.getAbsolutePath() + File.separator + fileName;
+                String subFileTo = fileTo.getAbsolutePath() + File.separator + fileName;
+                if (Files.isDirectory(Paths.get(subFileFrom))) {
+                    copySmallFiles(subFileFrom, subFileTo);
+                } else {
+                    copyFile(subFileFrom, subFileTo);
+                }
+            }
+            return pathTo;
+        }
         return null;
     }
 
+    private static void copyFile(final String fileFrom, final String pathTo) throws IOException {
+        final File from = new File(fileFrom);
+        final File to = new File(pathTo);
+        if (from.exists() && from.isFile()) {
+            try (InputStream inputStream = new FileInputStream(from);
+                 OutputStream outputStream = new FileOutputStream(to)) {
+
+                byte[] buffer = new byte[1024];
+                int bytes;
+                if ((bytes = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, bytes);
+                }
+            }
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,12 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Directories {
 
 
@@ -10,13 +17,44 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        if (!file.exists()) {
+            return 0;
+        }
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        }
+        int count = 0;
+        for (File subFile : file.listFiles()) {
+            count += removeWithFile(subFile.getAbsolutePath());
+        }
+        file.delete();
+        return count + 1;
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path) throws IOException {
+        Path file = Paths.get(path);
+        if (!Files.exists(file)) {
+            return 0;
+        }
+        if (file.toFile().isFile()) {
+            Files.delete(file);
+            return 1;
+        }
+
+        AtomicInteger count = new AtomicInteger(0);
+        Files.list(file).forEach(tempPath -> {
+            try {
+                count.addAndGet(removeWithPath(tempPath.toString()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        Files.delete(file);
+        return count.get() + 1;
     }
 }
