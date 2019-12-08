b842fcb592790a54fa9d0a0ca499ee278c1683b9
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-12-08 23:18:53 +0300

    Created streams task

--- src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
@@ -1,6 +1,11 @@
 package ru.mail.polis.homework.collections.streams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -20,9 +25,24 @@ public class WordFrequency {
      * Задачу можно решить без единого условного оператора, только с помощью стримов.
      * Если будут использоваться условные операторы, то оценка максимальная оценка 2 балла.
      */
-    public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
-    }
 
+
+    public static List<String> wordFrequency(Stream<String> lines) {
+        List<String> list = lines
+                .flatMap((str) -> Arrays.stream(str.split("[.,!:-?; ]+")))
+                .collect(Collectors.toList());
+
+        list.forEach(s -> s = s.toLowerCase());
+
+        return  list
+                .stream()
+                .collect(Collectors.toMap(Function.identity(), v -> 1, Integer::sum))
+                .entrySet()
+                .stream()
+                .sorted((t1, t2) -> t2.getValue().compareTo(t1.getValue()))
+                .map(Map.Entry::getKey)
+                .limit(10)
+                .collect(Collectors.toList());
+    }
 
 }
