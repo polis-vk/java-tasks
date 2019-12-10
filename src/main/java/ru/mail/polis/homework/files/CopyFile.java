d4503052dee5b42b4f77a2fc510967ff6a0e0359
suhova <dasuhovada@yandex.ru>
2019-12-10 15:19:58 +0300

    hw2 (files)

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+
 public class CopyFile {
 
     /**
@@ -7,7 +9,43 @@ public class CopyFile {
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
+        File dirFrom = new File(pathFrom);
+        File dirTo = new File(pathTo);
+        if (dirFrom.isDirectory() && dirFrom.exists())
+            if (!dirTo.exists()) {
+                dirTo.mkdirs();
+            }
+        for (String file : dirFrom.list()) {
+            File fileFrom = new File(dirFrom + File.separator + file);
+            File fileTo = new File(dirTo + File.separator + file);
+            if (fileFrom.isDirectory()) {
+                copySmallFiles(fileFrom.getAbsolutePath(), fileTo.getAbsolutePath());
+            } else {
+                InputStream inputStream = null;
+                OutputStream outputStream = null;
+                try {
+                    inputStream = new FileInputStream(fileFrom);
+                    outputStream = new FileOutputStream(fileTo);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buffer)) > 0) {
+                        outputStream.write(buffer, 0, length);
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        inputStream.close();
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return pathTo;
+        }
         return null;
     }
-
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,22 +1,47 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Directories {
 
 
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
-     * Метод должен возвращать количество удаленных файла и директорий.
+     * Метод должен возвращать количество удаленных файлов и директорий.
      * Если директории по существующему пути нет, то возвращаем 0.
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        int count = 0;
+        File dir = new File(path);
+        if (dir.exists()) {
+            if (dir.isDirectory()) {
+                for (File file : dir.listFiles()) {
+                    count += removeWithFile(file.getPath());
+                }
+            }
+            dir.delete();
+            count++;
+        }
+        return count;
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path) throws IOException {
+        int count = 0;
+        Path dir = Path.of(path);
+        if (Files.exists(dir)) {
+            if (Files.isDirectory(dir)) {
+                count = Files.list(dir).mapToInt(dirPath -> removeWithFile(dirPath.toString())).sum();
+            }
+            Files.delete(dir);
+            count++;
+        }
+        return count;
     }
 }
