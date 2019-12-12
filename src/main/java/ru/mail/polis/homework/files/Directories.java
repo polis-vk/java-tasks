12b60a3d35a8c2a406ed838cfc4f2a512f1629f7
Nekobitlz <jokers.bit@gmail.com>
2019-12-12 22:59:26 -0100

    Implement removeWithFile & removeWithPath tasks

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,7 +1,13 @@
 package ru.mail.polis.homework.files;
 
-public class Directories {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class Directories {
 
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
@@ -10,13 +16,55 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        int count = 0;
+
+        if (file.exists() && file.isDirectory()) {
+            count += purgeDirectory(file);
+            file.delete();
+            count++;
+        } else {
+            if (file.exists() && file.isFile()) {
+                file.delete();
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static int purgeDirectory(File file) {
+        int count = 0;
+
+        for (File child : file.listFiles()) {
+            if (child.isDirectory()) {
+                count += purgeDirectory(child);
+            }
+            child.delete();
+            count++;
+        }
+
+        return count;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        AtomicInteger count = new AtomicInteger();
+
+        try {
+            Files.walk(Paths.get(path))
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(file -> {
+                        file.delete();
+                        count.getAndIncrement();
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return count.get();
     }
 }
