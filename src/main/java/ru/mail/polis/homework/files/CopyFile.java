71862f273e9e3b3b9b4a5bfc039081ecbacad852
Nekobitlz <jokers.bit@gmail.com>
2019-12-12 22:59:03 -0100

    Implement copySmallFiles task

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+
 public class CopyFile {
 
     /**
@@ -7,7 +9,57 @@ public class CopyFile {
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
+        final File from = new File(pathFrom);
+        final File to = new File(pathTo);
+
+        if (from.exists() && from.isDirectory()) {
+            if (!to.exists()) {
+                to.mkdirs();
+            }
+            for (File file : from.listFiles()) {
+                String childFrom = file.getAbsolutePath();
+                String childTo = to.getAbsolutePath() + File.separator + file.getName();
+
+                if (file.isDirectory()) {
+                    copySmallFiles(childFrom, childTo);
+                } else {
+                    try {
+                        copyFile(childFrom, childTo);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            if (from.exists() && from.isFile()) {
+                try {
+                    if (!to.exists()) {
+                        to.mkdirs();
+                    }
+                    copyFile(pathFrom, pathTo);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return pathTo;
     }
 
+    private static void copyFile(String pathFrom, String pathTo) throws IOException {
+        final File from = new File(pathFrom);
+        final File to = new File(pathTo);
+
+        try (InputStream in = new BufferedInputStream(new FileInputStream(from));
+             OutputStream out = new BufferedOutputStream(new FileOutputStream(to))
+        ) {
+            byte[] buffer = new byte[1024];
+            int lengthRead;
+
+            while ((lengthRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, lengthRead);
+                out.flush();
+            }
+        }
+    }
 }
