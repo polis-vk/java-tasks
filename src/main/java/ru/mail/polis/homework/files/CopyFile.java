f5f601573a38bbb327857fc2abcdbdda24078132
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-12-14 21:09:18 +0300

    some updates

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -2,6 +2,7 @@ package ru.mail.polis.homework.files;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,12 +16,17 @@ public class CopyFile {
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      */
-    public static String copySmallFiles(final String pathFrom, final String pathTo) throws IOException {
+    public static String copySmallFiles(final String pathFrom, final String pathTo) {
         File fileFrom = new File(pathFrom);
         File fileTo = new File(pathTo);
-        if (fileFrom.exists() && !fileTo.exists()) {
+        if (fileFrom.exists()) {
             if (fileFrom.isDirectory()) {
-                fileTo.mkdirs();
+                if (!fileTo.exists()) {
+                    fileTo.mkdirs();
+                } else if (fileTo.isFile()) {
+                    return null;
+                }
+
                 for (final File fileName : fileFrom.listFiles()) {
                     String subFileTo = fileTo.getAbsolutePath() + File.separator + fileName.getName();
                     if (Files.isDirectory(Paths.get(fileName.getAbsolutePath()))) {
@@ -39,16 +45,21 @@ public class CopyFile {
         return null;
     }
 
-    private static void copyFile(final File from, final File to) throws IOException {
+    private static void copyFile(final File from, final File to) {
         if (from.exists() && from.isFile()) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(from));
-                 BufferedWriter writer = new BufferedWriter(new FileWriter(to))) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(from))) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(to))) {
 
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    writer.write(line);
-                    writer.newLine();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        writer.write(line);
+                        writer.newLine();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
