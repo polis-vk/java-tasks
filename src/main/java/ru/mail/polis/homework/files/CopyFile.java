de5402067ef6ebe955ecfe6c9bc62286398ea79d
Marashov_Alexander <marashov.as@edu.spbstu.ru>
2019-12-14 16:55:16 +0300

    Task 2

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,5 +1,10 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+
 public class CopyFile {
 
     /**
@@ -7,7 +12,70 @@ public class CopyFile {
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
+        final Path source = Paths.get(pathFrom);
+        source.normalize();
+
+        final Path dest = Paths.get(pathTo);
+        dest.normalize();
+
+        if (!source.toFile().exists()) {
+            return null;
+        }
+
+        if (source.toFile().isDirectory()) {
+            if (!dest.toFile().mkdirs()) {
+                return null;
+            }
+        }  else {
+            if (!dest.subpath(0, dest.getNameCount() - 1).toFile().mkdirs()) {
+                return null;
+            }
+        }
+
+        try {
+            Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    Objects.requireNonNull(dir);
+                    Objects.requireNonNull(attrs);
+
+                    final Path relativePath = source.relativize(dir);
+                    dest.resolve(relativePath).toFile().mkdir();
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Objects.requireNonNull(file);
+                    Objects.requireNonNull(attrs);
+
+                    final Path relativePath = source.relativize(file);
+                    copyFile(file, dest, relativePath);
+
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return dest.toString();
+    }
+
+    private static void copyFile(final Path source, final Path dest, final Path relativePath) throws IOException {
+        final File sourceFile = source.toFile();
+        final File destinationFile = dest.resolve(relativePath).toFile();
+        try (
+                InputStream in = new FileInputStream(sourceFile);
+                OutputStream out = new FileOutputStream(destinationFile)
+        ) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+        }
     }
 
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,7 +1,13 @@
 package ru.mail.polis.homework.files;
 
-public class Directories {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class Directories {
 
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
@@ -10,13 +16,62 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        final File directory;
+        if (path == null // метод removeWithFile() не бросает исключения, а new File() бросит, если path==null
+                || !(directory = new File(path)).exists()/* || directory.isFile()*/) {
+            return 0;
+        }
+        // В задании сказано "Реализовать рекурсивное удаление всех файлов и дерикторий из ДИРЕКТОРИИ по заданному пути",
+        // но в тесте filesOneFile() считается, что при аргументе-файле функция должна удалить его и вернуть 1.
+        // Подогнал под тесты, но здесь явно противоречие заданию.
+        return directory.isDirectory()
+                ? recursivelyRemove(directory)
+                : (directory.delete() ? 1 : 0);
+        // return recursivelyRemove(directory);
+    }
+
+    // дополнительный метод сделан для избежания постоянных проверок на null
+    private static int recursivelyRemove(final File directory) {
+        int result = 0;
+        for (final File file : directory.listFiles()) {
+            result += file.isDirectory() ? recursivelyRemove(file) : 0;
+            result += file.delete() ? 1 : 0;
+        }
+        result += directory.delete() ? 1 : 0;
+        return result;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        final AtomicInteger count = new AtomicInteger(0);
+        // считается ли использование статического метода walkFileTree() как "рекурсивный обход ручками"?)
+        try {
+            final Path filePath = Paths.get(path);
+            Files.walkFileTree(filePath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Objects.requireNonNull(file);
+                    Objects.requireNonNull(attrs);
+                    Files.delete(file);
+                    count.incrementAndGet();
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Objects.requireNonNull(dir);
+                    if (exc != null) {
+                        throw exc;
+                    } else {
+                        Files.delete(dir);
+                        count.incrementAndGet();
+                        return FileVisitResult.CONTINUE;
+                    }
+                }
+            });
+        } catch (IOException | NullPointerException ignored) { }
+        return count.intValue();
     }
 }
