0642f82dc2660925899a20e4700a5fd194a6a3a1
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2019-12-17 01:17:45 +0300

    Delete MailMessage.java

--- src/main/java/ru/mail/polis/homework/collections/mail/MailMessage.java
+++
@@ -1,8 +0,0 @@
-package ru.mail.polis.homework.collections.mail;
-
-public class MailMessage extends Mail<String>{
-
-    public MailMessage(String sender, String addressee, String post) {
-        super(sender, addressee, post);
-    }
-}
