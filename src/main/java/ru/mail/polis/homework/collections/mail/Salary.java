6095bd2d8a158c659ec58411ada3111e1ded31bb
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2019-12-17 01:18:05 +0300

    Delete Salary.java

--- src/main/java/ru/mail/polis/homework/collections/mail/Salary.java
+++
@@ -1,8 +0,0 @@
-package ru.mail.polis.homework.collections.mail;
-
-public class Salary extends Mail<Integer> {
-
-    public Salary(String sender, String addressee, Integer post) {
-        super(sender, addressee, post);
-    }
-}
