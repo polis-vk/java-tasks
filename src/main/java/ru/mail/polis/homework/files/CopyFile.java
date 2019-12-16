1af968e3a24095b798249e8221b81943fe492845
Badma <48029071+0b010101@users.noreply.github.com>
2019-12-16 21:58:53 +0300

    hw2

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,13 +1,48 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+import java.nio.file.*;
+
 public class CopyFile {
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      */
-    public static String copySmallFiles(String pathFrom, String pathTo) {
+    public static String copySmallFiles(String pathFrom, String pathTo) throws IOException {
+        File copyFrom = Paths.get(pathFrom).toFile();
+        File copyTo = Paths.get(pathTo).toFile();
+
+        if (!copyFrom.exists() && !copyFrom.isDirectory() && copyTo.isFile()) return null;
+        if (!copyTo.exists()) copyTo.mkdir();
+
+        BufferedReader reader;
+        BufferedWriter writer;
+
+        File[] files = copyFrom.listFiles();
+
+        for (File f : files) {
+            if (f.isFile()) {
+                String buff;
+                try {
+                    reader = new BufferedReader(new FileReader(f));
+                    writer = new BufferedWriter(new FileWriter(copyTo));
+                    while ((buff = reader.readLine()) != null) writer.write(buff);
+                    reader.close();
+                    writer.close();
+                } catch (IOException e) {}
+            } else if (f.isDirectory()) {
+                String nameDir = f.getName();
+                File dirTo = new File(copyTo.getAbsolutePath() + File.separator + nameDir);
+                copySmallFiles(dirTo.getParent(), dirTo.getAbsolutePath());
+            }
+        }
         return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        copySmallFiles("C:\\Users\\null\\Desktop\\java-tasks\\src\\main\\java\\ru\\mail\\polis\\homework\\files\\cp", "C:\\Users\\null\\Desktop\\java-tasks\\src\\main\\java\\ru\\mail\\polis\\homework\\files\\cpTo");
+
     }
 
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +18,47 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+
+        if (file == null || !file.exists()) return 0;
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        }
+
+        int result = 0;
+        for (File f : file.listFiles()) {
+            result += removeWithFile(f.getPath());
+        }
+
+        file.delete();
+        return result + 1;
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(Path path) throws IOException {
+        if (!Files.exists(path)) return 0;
+        if (Files.isRegularFile(path)) Files.delete(path);
+
+        AtomicInteger result = new AtomicInteger();
+        try (Stream<Path> stream = Files.list(path)) {
+            stream.forEach(file -> {
+                try {
+                    removeWithPath(file);
+                    result.getAndIncrement();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        Files.delete(path);
+        return result.get() + 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Path p = Paths.get("C:\\Users\\null\\Desktop\\java-tasks\\src\\main\\java\\ru\\mail\\polis\\homework\\files\\test\\test.java");
+        removeWithPath(p);
     }
 }
