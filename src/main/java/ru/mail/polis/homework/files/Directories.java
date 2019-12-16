d456afd87e6438692186b0319e1b140046b883a8
Vlad Zybkin <zybkin11@gmail.com>
2019-12-16 18:06:31 +0300

    remove files task implementation added

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +18,56 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+      File file = new File(path);
+      if (file.exists()) {
+        if (file.isDirectory()) {
+          int amount = 1;
+          for (File innerFile : file.listFiles()) {
+            amount += removeWithFile(innerFile.getAbsolutePath());
+          }
+          file.delete();
+          return amount;
+        } else {
+          file.delete();
+          return 1;
+        }
+      }
+      return 0;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
+      Path file = Paths.get(path);
+      if (Files.exists(file)) {
+        AtomicInteger amount = new AtomicInteger(1);
+        if (Files.isDirectory(file)) {
+          try (Stream<Path> paths = Files.list(file)) {
+            paths.forEach(nextPath -> {
+              try {
+                if (Files.isDirectory(nextPath)) {
+                  amount.addAndGet(removeWithPath(nextPath.toString()));
+                } else {
+                  amount.incrementAndGet();
+                }
+                Files.deleteIfExists(nextPath);
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+            });
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+        try {
+          Files.delete(file);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        return amount.get();
+      } else {
         return 0;
+      }
     }
 }
