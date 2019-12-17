dbcdf9f9a0cfff9d65e4891b57a727405304a74c
KoDim97 <kodim97@yandex.ru>
2019-12-17 13:11:53 +0300

    Directories

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +18,51 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        int count = 0;
+        if(file.isDirectory()){
+            count += clearDirectory(file);
+            file.delete();
+            count++;
+        }
+        else{
+            if (file.isFile()){
+                file.delete();
+                count++;
+            }
+        }
+        return count;
     }
 
+    private static  int clearDirectory(File file){
+        int count = 0;
+        for (File cur :
+                file.listFiles()) {
+            count += clearDirectory(cur);
+            cur.delete();
+            count++;
+        }
+        return count;
+    }
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path){
+        AtomicInteger count = new AtomicInteger(0);
+        try (Stream<Path> pathStream = Files.walk(Paths.get(path))) {
+            pathStream.sorted(Comparator.reverseOrder()).forEach(
+                    curPath -> {
+                        try {
+                            Files.delete(curPath);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        count.getAndIncrement();
+                    }
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count.get();
     }
 }
