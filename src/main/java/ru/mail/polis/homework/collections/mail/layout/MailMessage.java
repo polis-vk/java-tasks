ae94144711d5edd34f1a422ed7abbdeb3049e934
Artem Gerasimov <39194008+art241111@users.noreply.github.com>
2019-12-17 16:40:54 +0300

    Load second homework

--- src/main/java/ru/mail/polis/homework/collections/mail/layout/MailMessage.java
+++
@@ -1,7 +0,0 @@
-package ru.mail.polis.homework.collections.mail.layout;
-
-public class MailMessage extends MailLayout<String> {
-    public MailMessage(String sender, String recipient, String text) {
-        super(sender, recipient, text);
-    }
-}
--- src/main/java/ru/mail/polis/homework/collections/mail/layout/Salary.java
+++
@@ -1,7 +0,0 @@
-package ru.mail.polis.homework.collections.mail.layout;
-
-public class Salary extends MailLayout<Integer> {
-    public Salary(String sender, String recipient, Integer salary) {
-        super(sender, recipient, salary);
-    }
-}
