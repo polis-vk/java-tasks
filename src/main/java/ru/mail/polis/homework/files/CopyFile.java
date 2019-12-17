776ec9ea2dd53373259927e5b5f4cec4e77fc6de
Artem Gerasimov <39194008+art241111@users.noreply.github.com>
2019-12-17 16:38:47 +0300

    Load second homework

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,13 +1,70 @@
 package ru.mail.polis.homework.files;
 
-public class CopyFile {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+public class CopyFile {
+    final static String SEPARATOR = File.separator;
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
+        File dirFrom = new File(pathFrom);
+        File dirTo = new File(pathTo);
+
+        if (dirFrom.exists()) {
+            File[] files = dirFrom.listFiles();
+            if (files != null) {
+                if (!dirTo.exists()) {
+                    dirTo.mkdirs();
+                }
+
+                for (File fileFrom : files) {
+                    File fileTo = new File(dirTo + SEPARATOR + fileFrom.getName());
+                    copySmallFiles(fileFrom.getAbsolutePath(), fileTo.getAbsolutePath());
+                }
+
+                return pathTo;
+            } else {
+                try {
+                    createFile(dirTo);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                copyFile(dirFrom, dirTo);
+            }
+        }
         return null;
     }
 
+    private static void createFile(File file) throws IOException {
+        File parent = file.getParentFile();
+
+        if (!parent.exists()) {
+            parent.mkdirs();
+            file.createNewFile();
+        } else if (file.exists()) {
+            file.createNewFile();
+        }
+    }
+
+    private static void copyFile(File fileFrom, File fileTo) {
+        try (BufferedReader from = Files.newBufferedReader(fileFrom.toPath())){
+            try(BufferedWriter to = Files.newBufferedWriter(fileTo.toPath())) {
+                from.lines()
+                        .forEach(str -> {
+                            try {
+                                to.write(str);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+            }
+        } catch (IOException ignored) {
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,22 +1,58 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
-     * Метод должен возвращать количество удаленных файла и директорий.
+     * Метод должен возвращать количество удаленных файлов и директорий.
      * Если директории по существующему пути нет, то возвращаем 0.
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        int countFilesDelete = 0;
+        File dir = new File(path);
+
+        if (dir.exists()) {
+            if (dir.isDirectory()) {
+                for (File file : dir.listFiles()) {
+                    countFilesDelete += removeWithFile(file.getPath());
+                }
+            }
+            dir.delete();
+            countFilesDelete++;
+        }
+        return countFilesDelete;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        AtomicInteger countFilesDelete = new AtomicInteger(0);
+
+        try (Stream<Path> tree = Files.walk(Paths.get(path))) {
+            tree.sorted(Comparator.reverseOrder())
+                    .forEach(p -> {
+                        try {
+                            Files.deleteIfExists(p);
+                            countFilesDelete.getAndIncrement();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return countFilesDelete.get();
     }
 }
