56df3ebf5e50ace8265f328e62eef967c6da9267
Makary <makary.b@gmail.com>
2019-12-18 22:25:06 +0300

    Исправляет неточности по последним замечаниям к pull-request

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -10,6 +10,8 @@ import java.util.stream.Collectors;
 
 public class CopyFile {
 
+    private static Logger logger = Logger.getLogger(CopyFile.class.getName());
+
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
@@ -17,20 +19,21 @@ public class CopyFile {
      * modified by БорискинМА
      * 14.12.19
      */
-    public static String copySmallFiles(String pathFrom, String pathTo) {
+    public static String copySmallFiles(final String pathFrom, final String pathTo) {
         try {
             return dirs(pathFrom, pathTo);
         } catch (Exception e) {
-            Logger.getLogger(CopyFile.class.getName()).log(Level.SEVERE, null, e);
+            logger.log(Level.SEVERE, null, e);
         }
         return null;
     }
 
-    private static String dirs(String pathFrom, String pathTo) throws IOException {
+    private static String dirs(final String pathFrom, final String pathTo) throws IOException {
         File inputFolder = new File(pathFrom);
         if (!inputFolder.exists()) {
             return null;
         }
+        String newPathTo = pathTo;
         File outputFolder = new File(pathTo);
         if (outputFolder.exists()) {
             if (outputFolder.isFile() && inputFolder.isFile()) {
@@ -45,25 +48,25 @@ public class CopyFile {
                 if (!parentPath.exists()) {
                     parentPath.mkdirs();
                 }
-                pathTo = parentPath.toString();
+                newPathTo = parentPath.toString();
             } else {
                 outputFolder.mkdirs();
             }
         }
         System.out.println("Начинаем копирование...");
-        System.out.println((int) Files.walk(Paths.get(pathFrom)).count());
+        logger.log(Level.INFO, String.valueOf((int) Files.walk(Paths.get(pathFrom)).count()));
         File[] sourceArray = new File[1];
         sourceArray[0] = inputFolder;
-        copyFilesAndDirs(sourceArray, pathTo, inputFolder.getAbsolutePath());
-        System.out.println((int) Files.walk(Paths.get(pathTo)).count());
+        copyFilesAndDirs(sourceArray, newPathTo, inputFolder.getAbsolutePath());
+        logger.log(Level.INFO, String.valueOf((int) Files.walk(Paths.get(pathTo)).count()));
         return pathFrom;
     }
 
-    private static boolean isFolderName(File name, String inputDir) {
+    private static boolean isFolderName(File name, final String inputDir) {
         return name.getAbsolutePath().equals(inputDir);
     }
 
-    private static void copyFilesAndDirs(File[] sourceArray, String parentDir, String inputDir) {
+    private static void copyFilesAndDirs(File[] sourceArray, String parentDir, final String inputDir) {
         for (int i = 0; i <= sourceArray.length - 1; i++) {
             File outputFolder = new File(parentDir + "/" + sourceArray[i].getName());
             if (sourceArray[i].isDirectory()) {
@@ -74,26 +77,28 @@ public class CopyFile {
                 }
                 File[] newPaths = sourceArray[i].listFiles();
                 copyFilesAndDirs(newPaths, outputFolder.getAbsolutePath(), inputDir);
-            }
-            else if (sourceArray[i].isFile()) {
+            } else if (sourceArray[i].isFile()) {
                 copyFile(sourceArray[i].getAbsolutePath(), outputFolder.getAbsolutePath());
-            }
-            else {
+            } else {
                 System.out.println("Ни такой папки, ни такого файла здесь нет: " + sourceArray[i].getAbsolutePath());
             }
         }
     }
 
-    private static void copyFile(String pathFrom, String pathTo) {
-        try (BufferedInputStream bufferedSource = new BufferedInputStream(new FileInputStream(new File(pathFrom)), 8192);
-             BufferedOutputStream bufferedDestination = new BufferedOutputStream(new FileOutputStream(new File(pathTo)), 8192)) {
-            int transfer;
-            while ((transfer = bufferedSource.read()) != -1) {
-                bufferedDestination.write(transfer);
+    private static void copyFile(final String pathFrom, final String pathTo) {
+        try (BufferedInputStream bufferedSource = new BufferedInputStream(new FileInputStream(new File(pathFrom)), 8192);) {
+            try (BufferedOutputStream bufferedDestination = new BufferedOutputStream(new FileOutputStream(new File(pathTo)), 8192)) {
+                int transfer;
+                while ((transfer = bufferedSource.read()) != -1) {
+                    bufferedDestination.write(transfer);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.log(Level.SEVERE, "output error", e);
             }
         } catch (IOException e) {
             e.printStackTrace();
-            Logger.getLogger(CopyFile.class.getName()).log(Level.SEVERE, null, e);
+            logger.log(Level.SEVERE, "input error", e);
         }
     }
 
@@ -104,7 +109,7 @@ public class CopyFile {
                             "src/main/java/ru/mail/polis/homework/files/sample_folder.txt",
                             "src/main/java/ru/mail/polis/homework/sample_folder.txt"));
         } catch (Exception e) {
-            Logger.getLogger(CopyFile.class.getName()).log(Level.SEVERE, null, e);
+            logger.log(Level.SEVERE, null, e);
         }
     }
 }
