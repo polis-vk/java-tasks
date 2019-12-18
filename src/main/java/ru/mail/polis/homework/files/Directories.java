b5038c9c75086b300127973b7021b4c91393eef0
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-12-19 01:14:02 +0300

    Made Directories with path Completed HM2

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,12 +1,13 @@
 package ru.mail.polis.homework.files;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Directories {
-    public static void main(String[] args) {
-        int a = Directories.removeWithFile("C:\\Users\\maksim\\Desktop\\q\\qwe");
-        System.out.println(a);
-    }
 
     private static int deleted = 0;
     /**
@@ -16,6 +17,7 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
+        deleted = 0;
         File file = new File(path);
         if (!file.exists()) return 0;
 
@@ -40,18 +42,44 @@ public class Directories {
             }
         }
 
-        if(file.delete()) deleted++;
+        if (file.delete()) deleted++;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        deleted = 0;
+        Path p = Paths.get(path);
+        if (Files.notExists(p)) return 0;
+
+        try {
+            if (Files.isRegularFile(p)) {
+                Files.delete(p);
+                deleted++;
+            } else if (Files.isDirectory(p))
+                removeWithPath(p);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return deleted;
     }
 
-    private static void removeWithPath(File file) {
+    private static void removeWithPath(Path p) throws IOException {
+            DirectoryStream<Path> ds = Files.newDirectoryStream(p);
+            for (Path child : ds) {
+                if (Files.isRegularFile(child)) {
+                    Files.delete(child);
+                    deleted++;
+                } else if (Files.isDirectory(child)) {
+                    removeWithPath(child);
+                }
+            }
+            ds.close();
 
+            Files.delete(p);
+            deleted++;
     }
 
 }
