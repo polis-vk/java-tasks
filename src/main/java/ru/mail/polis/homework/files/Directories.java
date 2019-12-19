03b4ecf4f2386948d63aecd389aa8eb5399c638d
root <almick17933@gmail.com>
2019-12-19 15:27:34 +0300

    Directories done

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,11 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Stack;
+
 public class Directories {
 
 
@@ -10,13 +16,66 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        return removeFile(file);
+    }
+
+    private static int removeFile(File file) {
+        if (file == null || !file.exists()) {
+            return 0;
+        }
+
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        }
+
+        int count = 0;
+        for (File temp : file.listFiles()) {
+            count += removeFile(temp);
+        }
+        file.delete();
+
+        return count + 1;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        Path filePath = Paths.get(path);
+        if (!Files.exists(filePath)) {
+            return 0;
+        }
+
+        Stack<Path> stack = new Stack<>();
+        int count = 0;
+
+        SimpleFileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                stack.push(dir);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                stack.push(file);
+                return FileVisitResult.CONTINUE;
+            }
+        };
+
+        try {
+            Files.walkFileTree(filePath, visitor);
+
+            while (!stack.empty()) {
+                Files.delete(stack.pop());
+                count++;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return count;
     }
 }
