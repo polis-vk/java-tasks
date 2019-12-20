e992548ec048798ccccc5d7c469121270e3f67dc
Ekaterina Moreva <moreva.ei@edu.spbstu.ru>
2019-12-20 19:14:28 +0300

    Homework implementation was added

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+import java.util.Objects;
+
 public class CopyFile {
 
     /**
@@ -7,7 +10,55 @@ public class CopyFile {
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
+        final File copyDir = new File(pathFrom);
+        final File pastDir = new File(pathTo);
+
+        if (!copyDir.exists()) {
+            return null;
+        }
+
+        if (copyDir.isDirectory()) {
+
+            if (!pastDir.exists()) {
+                pastDir.mkdirs();
+            } else if (pastDir.isFile()) {
+                return null;
+            }
+
+            for (File nextFile : Objects.requireNonNull(copyDir.listFiles())) {
+                if (nextFile.isDirectory()) {
+                    copySmallFiles(nextFile.getAbsolutePath(),
+                            pastDir + File.separator + nextFile.getName());
+                } else {
+                    copyFile(nextFile.getAbsolutePath(),
+                            pastDir + File.separator + nextFile.getName());
+                }
+            }
+
+        } else {
+
+            if (!pastDir.exists()) {
+                new File(pastDir.getParent()).mkdirs();
+            }
+            copyFile(pathFrom, pathTo);
+        }
+
+        return pathTo;
+    }
+
+    private static void copyFile(final String pathFrom, final String pathTo) {
+        try (BufferedReader in = new BufferedReader(new FileReader(new File(pathFrom)))) {
+            try (BufferedWriter out = new BufferedWriter(new FileWriter(pathTo))) {
+                String next;
+
+                while ((next = in.readLine()) != null) {
+                    out.write(next);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +18,54 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+         File file = new File(path);
+
+        if (!file.exists()) {
+            return 0;
+        }
+
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        } else {
+            int counter = 1;
+            for (File infile : Objects.requireNonNull(file.listFiles())) {
+                counter += removeWithFile(infile.getAbsolutePath());
+            }
+            file.delete();
+            return counter;
+        }
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
-    }
+    public static int removeWithPath(String path) throws IOException {
+        Path file = Paths.get(path);
+
+        if (!Files.exists(file)) {
+            return 0;
+        } else {
+            if (Files.isRegularFile(file)) {
+                Files.delete(file);
+                return 1;
+            }
+            AtomicInteger counter = new AtomicInteger(1);
+            try (Stream<Path> paths = Files.list(file)) {
+                paths.forEach(nextPath -> {
+                    try {
+                        counter.addAndGet(removeWithPath(nextPath.toString()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+            try {
+                Files.delete(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return counter.get();
+        }
+    }   
 }
