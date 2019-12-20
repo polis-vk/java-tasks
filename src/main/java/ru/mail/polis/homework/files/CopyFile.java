7ff16edee03931db22007bfea15ee7968c01d514
nik27090 <nik27090@mail.ru>
2019-12-21 02:04:50 +0300

    fixed

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,13 +1,70 @@
 package ru.mail.polis.homework.files;
 
-public class CopyFile {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Comparator;
 
+public class CopyFile {
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
+        File srcFile = new File(pathFrom);
+        File dstFile = new File(pathTo);
+        if (!srcFile.exists() || dstFile.exists()){
+            return null;
+        }
+        copyDir(srcFile,dstFile);
         return null;
     }
 
+    private static void copyDir(File srcFile, File dstFile){
+        if (srcFile.isDirectory()){
+            dstFile.mkdirs();
+            Arrays.stream(srcFile.listFiles())
+                    .sorted(Comparator.reverseOrder())
+                    .forEachOrdered(file -> {
+                        File newDstFile = new File(dstFile.getAbsolutePath() + File.separator + file.getName());
+                        if (file.isDirectory()) {
+                            copyDir(file, newDstFile);
+                        } else {
+                            copyFile(file, newDstFile);
+                        }
+                    });
+        } else {
+            File dstParentFile = dstFile.getParentFile();
+            if(!dstParentFile.exists()){
+                dstParentFile.mkdirs();
+            }
+            copyFile(srcFile,dstFile);
+        }
+    }
+
+    private static void copyFile(File srcFile, File dstFile) {
+        if (!srcFile.exists() && !srcFile.isFile() && dstFile.exists()){
+            return;
+        }
+        try (BufferedReader readFile = Files.newBufferedReader(srcFile.toPath())){
+           try (BufferedWriter writeFile = Files.newBufferedWriter(dstFile.toPath())){
+            readFile
+                    .lines()
+                    .forEachOrdered(line -> {
+                        try{
+                            writeFile.write(line + "\n");
+                        } catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,8 +1,15 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
-
-
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
      * Метод должен возвращать количество удаленных файла и директорий.
@@ -10,13 +17,45 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        return removeFiles(file);
+    }
+
+    private static int removeFiles(File file) {
+        if (file==null || !file.exists()){
+            return 0;
+        }
+        if (file.isFile()){
+            file.delete();
+            return 1;
+        }
+        int result =0;
+        for (File temp : file.listFiles()){
+            result+=removeFiles(temp);
+        }
+        file.delete();
+        return result + 1;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        AtomicInteger atomicCount = new AtomicInteger();
+        try(Stream<Path> walk = Files.walk(Paths.get(path))){
+            walk
+                    .sorted(Comparator.reverseOrder())
+                    .forEachOrdered(file -> {
+                        atomicCount.incrementAndGet();
+                        try {
+                            Files.deleteIfExists(file);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return atomicCount.get();
     }
 }
