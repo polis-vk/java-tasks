acc73a107930b211420d71fce454aa993c83d72e
jhoysbou <neeroll21Wgmail.com>
2019-12-21 01:01:33 +0300

    Directories problem is done

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,14 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +19,42 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File filePath = new File(path);
+        int result = 0;
+
+        if (filePath.isDirectory()) {
+            for (File subfile : filePath.listFiles()) {
+                result += removeWithFile(subfile.getPath());
+            }
+        }
+
+        if (filePath.delete()) {
+            result++;
+        }
+
+        return result;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        AtomicInteger result = new AtomicInteger(0);
+
+        try (Stream<Path> paths = Files.walk(Paths.get(path))) {
+            paths.sorted(Comparator.reverseOrder()).forEach(subFile ->{
+                try {
+                    Files.delete(subFile);
+                    result.getAndIncrement();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.get();
     }
 }
