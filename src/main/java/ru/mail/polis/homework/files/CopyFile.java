5a86b2d6a688318726f54a04bbb10cbf96edf554
vegtamr <barekov.am.v@gmail.com>
2019-12-21 21:19:43 +0300

    CopyFile first impl.

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,13 +1,70 @@
 package ru.mail.polis.homework.files;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CopyFile {
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      */
-    public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
+    public static String copySmallFiles(final String pathFrom, final String pathTo) {
+        File from = new File(pathFrom);
+        File to = new File(pathTo);
+
+        if (!from.exists()) {
+            return null;
+        }
+
+        if (from.isFile()) {
+            File parent = to.getParentFile();
+            parent.mkdirs();
+            copyFile(from, to);
+
+            return pathTo;
+        }
+
+        if (!to.exists()) {
+            to.mkdirs();
+        }
+
+        for (final File file : from.listFiles()) {
+            File sub = new File(to.getAbsolutePath() + File.separator + file.getName());
+
+            if (sub.isFile()) {
+                copyFile(file, sub);
+            } else {
+                copySmallFiles(file.getAbsolutePath(), sub.getAbsolutePath());
+            }
+        }
+        
+        return pathTo;
+    }
+
+    private static void copyFile(final File from, final File to) {
+        if (!from.exists() || !from.isFile()) {
+            return;
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(from));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(to));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line+"\n");
+            }
+
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
