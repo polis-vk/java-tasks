7517d1ff3957a704c0368f99171b1b8b78c8fada
Kadyrov Kamil <43938114+Kopeyka885@users.noreply.github.com>
2019-12-21 23:11:33 +0300

    Update Directories.java

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,12 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -9,14 +16,60 @@ public class Directories {
      * Если директории по существующему пути нет, то возвращаем 0.
      * Написать двумя способами. С использованием File
      */
+
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        int count = 0;
+        if (!file.exists()){
+            return 0;
+        }
+        if (file.isDirectory()) {
+            count+=enterDirectory(file);
+        }
+        file.delete();
+        count++;
+        return count;
+    }
+
+    private static int enterDirectory(File directory) {
+        int count = 0;
+        for (File subFile : directory.listFiles()) {
+            if (subFile.isDirectory()) {
+                count += enterDirectory(subFile);
+            }
+            subFile.delete();
+            count++;
+        }
+        return count;
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path) throws IOException {
+        AtomicInteger count = new AtomicInteger();
+        Path file = Paths.get(path);
+        if (Files.notExists(file)){
+            return 0;
+        }
+        if (Files.isRegularFile(file)){
+            Files.delete(file);
+            return 1;
+        }
+        try (Stream<Path> paths = Files.list(file)){
+            paths.forEach(subFile -> {
+                try{
+                    count.addAndGet(removeWithPath(subFile.toString()));
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        Files.delete(file);
+        return count.get() + 1;
     }
 }
