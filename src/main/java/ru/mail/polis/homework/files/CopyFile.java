3da9bebf966589f052a983df108fd17e9dafb279
3r23f43 <danil.antonov.96@list.ru>
2019-12-21 17:55:55 +0300

    2/3 задач(пока)

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,12 +1,27 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class CopyFile {
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      */
-    public static String copySmallFiles(String pathFrom, String pathTo) {
+    public static String copySmallFiles(String pathFrom, String pathTo) throws IOException {
+        File folder = new File(pathFrom);
+
+        File[] listOfFiles = folder.listFiles();
+
+        Path destDir = Paths.get(pathTo);
+        if (listOfFiles != null)
+            for (File file : listOfFiles)
+                Files.copy(file.toPath(), destDir.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
         return null;
     }
 
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+
 public class Directories {
 
 
@@ -9,8 +11,22 @@ public class Directories {
      * Если директории по существующему пути нет, то возвращаем 0.
      * Написать двумя способами. С использованием File
      */
+
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        if(!file.exists())
+            return 0; //если пусто то false
+        if(file.isDirectory()) //если имеем директорию, то рекурсивно идем в ее конец, чтобы удалять файлы
+        {
+            for(File f : file.listFiles())
+                removeWithFile(f.getAbsolutePath());
+            file.delete(); // удалям пустую директорию
+        }
+        else // если имеем файл, то удаляем
+        {
+            file.delete();
+        }
+        return 1;
     }
 
     /**
