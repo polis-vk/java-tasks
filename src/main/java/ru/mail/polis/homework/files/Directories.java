e592faca518ddd7ed8bcc91ee9cf9f9aae9b55e7
vegtamr <barekov.am.v@gmail.com>
2019-12-21 22:51:22 +0300

    First impl of directories task

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,22 +1,85 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
-
     /**
-     * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
-     * Метод должен возвращать количество удаленных файла и директорий.
-     * Если директории по существующему пути нет, то возвращаем 0.
+     * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по
+     * заданному пути. Метод должен возвращать количество удаленных файла и
+     * директорий. Если директории по существующему пути нет, то возвращаем 0.
      * Написать двумя способами. С использованием File
      */
-    public static int removeWithFile(String path) {
-        return 0;
+    public static int removeWithFile(final String path) {
+        final File file = new File(path);
+
+        if (!file.exists()) {
+            return 0;
+        }
+
+        if (file.isFile()) {
+            file.delete();
+
+            return 1;
+        }
+
+        int counter = 0;
+
+        for (final File f : file.listFiles()) {
+            counter += removeWithFile(f.getAbsolutePath());
+        }
+
+        file.delete();
+        ++counter;
+
+        return counter;
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(final String path) {
+        Path filePath = Paths.get(path);
+
+        if (!Files.exists(filePath)) {
+            return 0;
+        }
+
+        if (Files.isRegularFile(filePath)) {
+            try {
+                Files.delete(filePath);
+
+                return 1;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        try {
+            Stream<Path> paths = Files.list(filePath);
+
+            paths.forEach(
+                p -> {
+                    counter.addAndGet(removeWithPath(p.toString()));
+                }
+            );
+
+            paths.close();
+            Files.delete(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        counter.incrementAndGet();
+
+        return counter.get();
     }
 }
