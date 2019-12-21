dffa0df1d757de986e8136aa43e817acbfb85c86
Ilya <gusarov2000@gmail.com>
2019-12-21 21:38:17 +0300

    adding workable Directories, working on copy

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+
 public class CopyFile {
 
     /**
@@ -7,7 +9,40 @@ public class CopyFile {
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
+        File from = new File(pathFrom);
+        File to = new File(pathTo);
+
+        if (from.isDirectory()) {
+            if (!to.exists() && to.isDirectory()) {
+                to.mkdirs();
+            } else {
+                for (File file: from.listFiles()) {
+                    if (file.isDirectory()) {
+                        File newTo = new File(to + File.pathSeparator + from.getName());
+                        copySmallFiles(from.getAbsolutePath(), newTo.getAbsolutePath());
+                    }
+
+                    copyFile(file.getAbsolutePath(), to.getAbsolutePath());
+                }
+            }
+
+        }
+
+        return pathTo;
     }
 
+    private static void copyFile(String from, String to) {
+        try (InputStream in = new BufferedInputStream(new FileInputStream(from));
+             OutputStream out = new BufferedOutputStream(new FileOutputStream(to))) {
+
+            byte[] buffer = new byte[1024];
+            int lengthRead;
+            while ((lengthRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, lengthRead);
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,14 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +19,55 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File forDelete = new File(path);
+
+        if (forDelete.isFile()) {
+            forDelete.delete();
+            return 1;
+        }
+
+        int res = 0;
+
+        if (forDelete.isDirectory()) {
+            for (File item: forDelete.listFiles()) {
+                res += removeWithFile(item.getPath());
+            }
+        } else {
+            return 0;
+        }
+
+        forDelete.delete();
+        return res + 1;
+
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path) throws IOException {
+        Path forDelete = Paths.get(path);
+
+        if (Files.isRegularFile(forDelete)) {
+            Files.delete(forDelete);
+            return 1;
+        }
+
+        AtomicInteger res = new AtomicInteger();
+
+        if (Files.isDirectory(forDelete)) {
+            try (Stream<Path> walk = Files.walk(forDelete)) {
+                walk.sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(x -> {
+                            x.delete();
+                            res.incrementAndGet();
+                        });
+            }
+        }else {
+            return 0;
+        }
+
+        return res.get();
+
     }
 }
