1053a39c778aeb15a167c22f52362349f9aa3c1c
kuzo-liza <kuzo-liza@yandex.ru>
2019-12-21 23:26:34 +0300

    delete task implemented

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,11 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Directories {
 
 
@@ -10,13 +16,59 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        return remove(file);
+    }
+
+    private static int remove(File file){
+        int result = 0;
+        if(file == null || !file.exists()){
+            return -1;
+        }
+
+        if (file.isFile()){
+            file.delete();
+            return 1;
+        }
+
+        for(File t : file.listFiles()){
+            result += remove(t);
+        }
+
+        file.delete();
+        return result + 1;
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path) throws IOException {
+        Path filePath = Paths.get(path);
+        return pathRemove(filePath);
+    }
+
+    private static int pathRemove(Path path) throws IOException {
+        if (!Files.exists(path)) {
+            return 0;
+        }
+
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                atomicInteger.incrementAndGet();
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                atomicInteger.incrementAndGet();
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return atomicInteger.get();
     }
 }
