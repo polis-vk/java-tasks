7e9df2df575a2c441dca455377fc523eed09c2b0
Alexey <medvedev.lasta@yandex.ru>
2019-12-22 04:02:59 +0700

    removing implemented

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +18,53 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        return remove(new File(path));
+    }
+
+    private static int remove(File file) {
+        if(file == null || !file.exists()) {
+            return 0;
+        }
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        }
+        int result = 0;
+        for (File tmp: file.listFiles()) {
+            result += remove(tmp);
+        }
+        file.delete();
+        return result + 1;
     }
 
     /**
      * С использованием Path
      */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path) throws IOException {
+        Path file = Paths.get(path);
+
+        if(!Files.exists(file)) {
+            return 0;
+        }
+
+        if (Files.isRegularFile(file)) {
+            Files.delete(file);
+            return 1;
+        }
+        AtomicInteger counter = new AtomicInteger(0);
+        try (Stream<Path> paths = Files.list(file)) {
+            paths.forEach(nextPath -> {
+                try {
+                    counter.addAndGet(removeWithPath(nextPath.toString()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        Files.delete(file);
+        return counter.get() + 1;
+
     }
+
 }
