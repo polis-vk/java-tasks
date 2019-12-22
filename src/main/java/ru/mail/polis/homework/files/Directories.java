588274c3543fa53d7174974b61db40087d5e62d0
s3ponia <slava.shlyaga@gmail.com>
2019-12-22 15:44:18 +0300

    solve Directories

--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 public class Directories {
 
 
@@ -10,13 +18,45 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        if (!file.exists()) {
+            return 0;
+        }
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        }
+        int count = 0;
+        for (File removeFile : file.listFiles()) {
+            count += removeWithFile(removeFile.getPath());
+        }
+        file.delete();
+        return count + 1;
     }
 
-    /**
-     * С использованием Path
-     */
-    public static int removeWithPath(String path) {
-        return 0;
+    public static int removeWithPath(String path) throws IOException {
+        Path file = Paths.get(path)
+                ;
+        if (!Files.exists(file)) {
+            return 0;
+        }
+
+        if (Files.isRegularFile(file)) {
+            Files.delete(file);
+            return 1;
+        }
+
+        AtomicInteger count = new AtomicInteger();
+        try (Stream<Path> stream = Files.list(file)) {
+            stream.forEach(curFile -> {
+                try {
+                    count.addAndGet(removeWithPath(curFile.toString()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        Files.delete(file);
+        return count.get() + 1;
     }
 }
