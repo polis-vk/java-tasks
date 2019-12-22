444d02714fe80781eb895e4eeeb81cd6dd4432b3
v.ivlev <nevidd@yandex.ru>
2019-12-22 13:25:15 +0300

    Create task 2

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,5 +1,11 @@
 package ru.mail.polis.homework.files;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class CopyFile {
 
     /**
@@ -7,7 +13,60 @@ public class CopyFile {
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
+        File file = new File(pathFrom);
+        File dir = new File(pathTo);
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                for (File fileFrom : files) {
+                    File fileTo = new File(dir + File.separator + fileFrom.getName());
+                    copySmallFiles(fileFrom.getAbsolutePath(), fileTo.getAbsolutePath());
+                }
+                return pathTo;
+            } else {
+                try {
+                    createFile(dir);
+                    copyFile(file, dir);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
     }
 
+    private static void createFile(File file) throws IOException {
+        File f = file.getParentFile();
+        if (!f.exists()) {
+            f.mkdirs();
+            file.createNewFile();
+        } else if (file.exists()) {
+            file.createNewFile();
+        }
+    }
+
+    private static void copyFile(File fileFrom, File fileTo) {
+        try (BufferedReader from = Files.newBufferedReader(fileFrom.toPath())) {
+            try (BufferedWriter to = Files.newBufferedWriter(fileTo.toPath())) {
+                from.lines().forEach(str -> {
+                    write(to, str);
+                });
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void write(BufferedWriter to, String str) {
+        try {
+            to.write(str);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,7 +1,15 @@
 package ru.mail.polis.homework.files;
 
-public class Directories {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
+public class Directories {
 
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
@@ -10,13 +18,38 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        int count = 0;
+        File file = new File(path);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                for (File f : file.listFiles()) {
+                    count += removeWithFile(f.getPath());
+                }
+            }
+            file.delete();
+            count++;
+        }
+        return count;
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        AtomicInteger count = new AtomicInteger();
+        try (Stream<Path> tree = Files.walk(Paths.get(path))) {
+            tree.sorted(Comparator.reverseOrder())
+                    .forEach(p -> {
+                        try {
+                            Files.deleteIfExists(p);
+                            count.getAndIncrement();
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    });
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return count.get();
     }
 }
