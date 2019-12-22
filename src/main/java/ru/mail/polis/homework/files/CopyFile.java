79fa91a386e966ba399f1f227d781619af6a1f15
Kirill <kir.kungurov@gmail.com>
2019-12-22 13:59:08 +0300

    done CopyFile

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,5 +1,10 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class CopyFile {
 
     /**
@@ -7,7 +12,57 @@ public class CopyFile {
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
+        File from = new File(pathFrom);
+        File to = new File(pathTo);
+
+        if (pathFrom.equals(pathTo) || !from.exists()) {
+            return null;
+        }
+
+        if (from.isDirectory()) {
+            if (!to.exists()) {
+                to.mkdirs();
+            } else if (to.isFile()) {
+                return null;
+            }
+            Arrays.stream(Objects.requireNonNull(from.listFiles()))
+                    .forEach(file -> {
+                        String pathOfNewFile = Paths.get(to.getAbsolutePath() + File.separator + file.getName()).toString();
+                        File newFile = new File(pathOfNewFile);
+                        if (file.isDirectory()) {
+                            copySmallFiles(file.getAbsolutePath(), newFile.getAbsolutePath());
+                        } else {
+                            copyFiles(file, newFile);
+                        }
+                    });
+
+        } else {
+            if (!to.exists()) {
+                new File(to.getParent()).mkdirs();
+            }
+            copyFiles(from, to);
+        }
+
+        return pathTo;
+    }
+
+    private static void copyFiles(File from, File to) {
+        if (from.exists() && from.isFile()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(from))) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(to))) {
+                    reader.lines()
+                            .forEach(line -> {
+                                try {
+                                    writer.write(line + "\n");
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
