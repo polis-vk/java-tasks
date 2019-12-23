1676d289aaffa43b26bbc3bc662686a70c1f09ab
NastyPill <DJPonnYash.Vinyl@gmail.com>
2019-12-23 12:42:43 +0300

    1st task haven't done yet, 2nd task done Directories

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,13 +1,25 @@
 package ru.mail.polis.homework.files;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class CopyFile {
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      */
-    public static String copySmallFiles(String pathFrom, String pathTo) {
-        return null;
-    }
+//    public static String copySmallFiles(String pathFrom, String pathTo) {
+//        Files.walkFileTree(pathFrom, );
+//    }
+//
+//    private static boolean copyFile(Path from, Path to) throws IOException {
+//        FileInputStream fileInputStream = new FileInputStream(from.toFile());
+//        FileOutputStream fileOutputStream = new FileOutputStream(to.toFile());
+//        fileOutputStream.write(fileInputStream.readAllBytes());
+//        fileInputStream.close();
+//        fileOutputStream.close();
+//    }
 
 }
--- src/main/java/ru/mail/polis/homework/files/Directories.java
+++ src/main/java/ru/mail/polis/homework/files/Directories.java
@@ -1,7 +1,37 @@
 package ru.mail.polis.homework.files;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+
 public class Directories {
 
+    private static class MyFileVisitor extends SimpleFileVisitor<Path> {
+
+        int count = 0;
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.delete(file);
+            count++;
+            return FileVisitResult.CONTINUE;
+        }
+
+
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Files.delete(dir);
+            count++;
+            return FileVisitResult.CONTINUE;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
 
     /**
      * Реализовать рекурсивное удаление всех файлов и дерикторий из директороии по заданному пути.
@@ -10,13 +40,48 @@ public class Directories {
      * Написать двумя способами. С использованием File
      */
     public static int removeWithFile(String path) {
-        return 0;
+        return remove(new File(path));
+    }
+
+    private static int remove(File file) {
+        int count = 0;
+        try {
+            for (File f : file.listFiles()) {
+                if (f.isDirectory()) {
+                    count += remove(f);
+                } else {
+                    f.delete();
+                    count++;
+                }
+            }
+            if (file.delete())
+                count++;
+            return count;
+        } catch (NullPointerException ex) {
+            try {
+                if (file.delete())
+                    count++;
+                return count;
+            } catch (SecurityException e) {
+                return 0;
+            }
+        }
     }
 
     /**
      * С использованием Path
      */
     public static int removeWithPath(String path) {
-        return 0;
+        try {
+            return remove(Paths.get(path));
+        } catch (IOException e) {
+            return 0;
+        }
+    }
+
+    private static int remove(Path path) throws IOException {
+        MyFileVisitor fileVisitor = new MyFileVisitor();
+        Files.walkFileTree(path, fileVisitor);
+        return fileVisitor.getCount();
     }
 }
