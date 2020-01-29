bc442fa87bf35296fffde4470f58ff345965833a
Stanislav Milke <stanislav.milke@corp.mail.ru>
2020-01-30 02:09:51 +0300

    copyfile done

--- src/main/java/ru/mail/polis/homework/files/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/files/CopyFile.java
@@ -1,5 +1,12 @@
 package ru.mail.polis.homework.files;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CopyFile {
 
     /**
@@ -7,7 +14,67 @@ public class CopyFile {
      * Файлы копировать ручками через стримы.
      */
     public static String copySmallFiles(String pathFrom, String pathTo) {
+        if (pathFrom.equals(pathTo)) {
+            return null;
+        }
+
+        Path from = Paths.get(pathFrom);
+        Path to = Paths.get(pathTo);
+
+        try {
+            return copySmallFilesWithPaths(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String copySmallFilesWithPaths(final Path pathFrom, final Path pathTo) throws IOException {
+        if (Files.exists(pathFrom) && Files.isReadable(pathFrom)) {
+            if (Files.isRegularFile(pathFrom)) {
+                if (!Files.exists(pathTo.getParent())) {
+                    Files.createDirectories(pathTo.getParent());
+                }
+                copyFile(pathFrom, pathTo);
+                return pathTo.toString();
+            } else if (Files.isDirectory(pathFrom)) {
+                if (!Files.exists(pathTo)) {
+                    Files.createDirectories(pathTo);
+                }
+                Files.list(pathFrom).forEach(oldPath -> {
+                    Path newPath = Paths.get(pathTo.toString(), oldPath.getFileName().toString());
+                    if (Files.isDirectory(oldPath)) {
+                        try {
+                            copySmallFilesWithPaths(oldPath, newPath);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else if (Files.isRegularFile(oldPath)) {
+                        try {
+                            copyFile(oldPath, newPath);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                return pathTo.toString();
+            }
+        }
+
         return null;
     }
 
+    private static void copyFile(final Path from, final Path to) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(from)) {
+            try (BufferedWriter writer = Files.newBufferedWriter(to)) {
+                reader.lines().forEach(line -> {
+                    try {
+                        writer.write(line + "\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        }
+    }
 }
