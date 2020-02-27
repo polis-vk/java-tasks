810d266265dddc93f0dd0c50842e0824eeaa92e7
Andrei Kukhto <andrei060656@mail.ru>
2020-02-27 14:54:01 +0300

    commit1

--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -41,6 +41,10 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result*=i;
+        }
+        return result;
     }
 }
