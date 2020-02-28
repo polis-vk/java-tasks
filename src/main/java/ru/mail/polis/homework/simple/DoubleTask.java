c2ce3612c630be91ba1df7c2b54f6bbf63efc7f1
Технополис <LamaMen>
2020-02-28 20:25:15 +0300

    Все задачи из папки simple решены, все тесты проходят

--- src/main/java/ru/mail/polis/homework/simple/DoubleTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleTask.java
@@ -14,16 +14,9 @@ public class DoubleTask {
      * Пример: (1, 5, 4) -> "-1.0, -4.0"
      */
     public static String equation(int a, int b, int c) {
-        double x1 = 0;
-        double x2 = 0;
         double d = b * b - 4 * a * c;
-        if (d == 0) {
-            x1 = (-b * 1.0) / (2 * a);
-            x2 = x1;
-        } else {
-            x1 = (-b + Math.sqrt(d)) / (2 * a);
-            x2 = (-b - Math.sqrt(d)) / (2 * a);
-        }
+        double x1 = (-b + Math.sqrt(d)) / (2 * a);
+        double x2 = (-b - Math.sqrt(d)) / (2 * a);
         if (x1 < x2){
             double temp = x1;
             x1 = x2;
