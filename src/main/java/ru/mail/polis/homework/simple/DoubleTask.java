1c4265e5cd70df6984cc063b7fa5e9dcea38157a
Daniil Drobyshevskiy <nolsenus@mail.ru>
2020-03-02 18:36:34 +0300

    Completed all tasks

--- src/main/java/ru/mail/polis/homework/simple/DoubleTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleTask.java
@@ -6,16 +6,30 @@ package ru.mail.polis.homework.simple;
  * Чтобы увидеть все методы класса Math, достаточно написать Math. и среда вам сама покажет возможные методы.
  * Для просмотра подробной документации по выбранному методу нажмите Ctrl + q
  */
-public class DoubleTask {
+public class DoubleTask
+{
 
     /**
      * Вывести два корня квадратного уравнения через запятую: a * x ^ 2 + b * x + c = 0;
      * Вывод менять не нужно, надо только посчитать x1 и x2, где x1 > x2
      * Пример: (1, 5, 4) -> "-1.0, -4.0"
      */
-    public static String equation(int a, int b, int c) {
-        double x1 = 0;
-        double x2 = 0;
+    public static String equation(int a, int b, int c)
+    {
+        double x1;
+        double x2;
+        int D = b * b - 4 * a * c;
+        double sqrtD = Math.sqrt(D);
+        if(a > 0)
+        {
+            x1 = (-b + sqrtD) / (2 * a);
+            x2 = (-b - sqrtD) / (2 * a);
+        }
+        else
+        {
+            x1 = (-b - sqrtD) / (2 * a);
+            x2 = (-b + sqrtD) / (2 * a);
+        }
         return x1 + ", " + x2;
     }
 
@@ -23,7 +37,8 @@ public class DoubleTask {
      * Нужно посчитать расстояние, между двумя точками (x1, y1) и (x2, y2)
      * Пример: (0, 0, 3, 4) -> 5.0
      */
-    public static float length(double x1, double y1, double x2, double y2) {
-        return 0;
+    public static float length(double x1, double y1, double x2, double y2)
+    {
+        return (float)Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     }
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -7,14 +7,21 @@ package ru.mail.polis.homework.simple;
  * Чтобы увидеть все методы класса Math, достаточно написать Math. и среда вам сама покажет возможные методы.
  * Для просмотра подробной документации по выбранному методу нажмите Ctrl + q
  */
-public class IntegerTask {
+public class IntegerTask
+{
 
     /**
      * Сумма чисел от 1 до n (1 + 2 + 3 + ... + n)
      * Пример: (5) -> 15
      */
-    public static int sum(int n) {
-        return 0;
+    public static int sum(int n)
+    {
+        int res = n;
+        for(int i = 1; i < n; i++)
+        {
+            res += i;
+        }
+        return res;
     }
 
     /**
@@ -23,16 +30,33 @@ public class IntegerTask {
      * Верните число Integer.MAX_VALUE;
      * Пример: (10, 3, 2) -> 8
      */
-    public static int snake(int height, int top, int bottom) {
-        return 0;
+    public static int snake(int height, int top, int bottom)
+    {
+        if(bottom >= top && height > top)
+        {
+            return Integer.MAX_VALUE;
+        }
+        int level = top;
+        int res = 1;
+        while(level < height)
+        {
+            res++;
+            level += (top - bottom);
+        }
+        return res;
     }
 
     /**
      * Дано число n и номер разряда order. Выведите цифру стояющую на нужном разряде
      * Пример: (454355, 3) -> 3
      */
-    public static int kDecimal(int n, int order) {
-        return 0;
+    public static int kDecimal(int n, int order)
+    {
+        for(int i = 1; i < order; i++)
+        {
+            n /= 10;
+        }
+        return Math.abs(n % 10);
     }
 
 
@@ -40,7 +64,13 @@ public class IntegerTask {
      * Выведите факториал от числа n
      * Пример: (5) -> 120
      */
-    public static long factorial(byte n) {
-        return 0;
+    public static long factorial(byte n)
+    {
+        long res = 1;
+        for(int i = 2; i <= n; i++)
+        {
+            res *= i;
+        }
+        return res;
     }
 }
