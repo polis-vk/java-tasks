8d0557ab84a6fe9e67dbad1c388110e2a2de7632
Grabar <grabar.og@edu.spbstu.ru>
2020-03-03 15:57:28 +0300

    В задании maxNumber переменная countOfDigits объявляется в цикле, потому что она используется не только в области видимости цикла (в return'e).

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -10,8 +10,15 @@ public class HomeworkTask {
      * Считаем, что функция определена на всем пространстве от a до b
      */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        function.applyAsDouble(4d);
-        return 0;
+        double integral = 0;
+        double currentX = a;
+
+        while (currentX < b) {
+            integral += function.applyAsDouble(currentX) * delta;
+            currentX += delta;
+        }
+
+        return integral;
     }
 
     /**
@@ -19,7 +26,21 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
+        long maxDigit = 0;
+        long numberOfMaxDigit = 0;
+        long currentDigit;
+        long countOfDigits;
+
+        for (countOfDigits = 1; a > 0; countOfDigits++) {
+            currentDigit = a % 10;
+            a = a / 10;
+            if (currentDigit >= maxDigit) {
+                maxDigit = currentDigit;
+                numberOfMaxDigit = countOfDigits;
+            }
+        }
+
+        return (byte) (countOfDigits - numberOfMaxDigit);
     }
 
 
@@ -28,16 +49,30 @@ public class HomeworkTask {
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        return (double)((x2 * y1 - x1 * y2) - (y1 - y2) * x3) / (x2 - x1);
     }
 
     /**
      * Даны 4 точки в пространстве A(x1, y1), B(x2, y2), C(x3, y3), D(x4, y4). Найдите площадь выпуклого
      * четырехуголька ABCD.
      * Это дополнительное задание, необязательное для выполнения
+     *
+     * Четырехугольник разбивается на два треугольника, площади каждого из которых считается по формуле Герона, а затем
+     * они суммируются.
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+        double ab = Math.sqrt( (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) );
+        double bc = Math.sqrt( (x2 - x3)*(x2 - x3) + (y2 - y3)*(y2 - y3) );
+        double cd = Math.sqrt( (x3 - x4) * (x3 - x4) + (y3 - y4) * (y3 - y4) );
+        double da = Math.sqrt( (x4 - x1)*(x4 - x1) + (y4 - y1) * (y4 - y1) );
+        double ac = Math.sqrt( (x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3) );
+
+        double p1 = (ab + bc + ac) / 2;
+        double p2 = (ac + cd + da) / 2;
+        double s1 = Math.sqrt(p1 * (p1 - ab) * (p1 - bc) * (p1 - ac));
+        double s2 = Math.sqrt(p2 * (p2 - ac) * (p2 - cd) * (p2 - da));
+
+        return s1 + s2;
     }
 
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -14,7 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -24,7 +28,12 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        if (height <= top) {
+            return 1;
+        } else if (top <= bottom){
+            return Integer.MAX_VALUE;
+        }
+        return (int) Math.ceil( (double)(height - top) / (top - bottom)) + 1;
     }
 
     /**
@@ -32,7 +41,7 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        return Math.abs((int) (n / Math.pow(10., (double) order - 1) % 10));
     }
 
 
@@ -41,6 +50,16 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        long fact = 1;
+        for (byte i = 2; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
     }
+
 }
