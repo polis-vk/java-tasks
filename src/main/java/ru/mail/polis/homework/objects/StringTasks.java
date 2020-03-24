70b27a181b36b659eda6fca24fff7ff3acc35045
Valeriy <Chugaynov.v300100@mail.ru>
2020-03-24 20:21:48 +0500

    убрал лишние импорты

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -1,7 +1,7 @@
 package ru.mail.polis.homework.objects;
 
 public class StringTasks {
-
+    
     /**
      * Убрать все лишние символы из строки и вернуть получившееся число.
      * Разрешенные символы: цифры, '-', '.', 'e'
@@ -13,7 +13,109 @@ public class StringTasks {
      * Работайте со строкой, НЕ надо ее переводить в массив байт (это можно использовать только для цикла)
      * У класса Character есть полезные методы, например Character.isDigit()
      */
+    
     public static Number valueOf(String str) {
-        return null;
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        String numericString = getNumericString(str);
+        if (numericString == null || numericString.isEmpty()) {
+            return null;
+        }
+        if (isReal(numericString)) {
+            return getDouble(numericString);
+        }
+        
+        long number = getLong(numericString);
+        if (number > Integer.MAX_VALUE || number < Integer.MIN_VALUE) {
+            return number;
+        }
+        return (int) number;
+    }
+    
+    private enum SymbolType {
+        DIGIT,
+        E,
+        DOT,
+        MINUS,
+        DEFAULT
+    }
+    
+    private static String getNumericString(String str) {
+        boolean dotIsHere = false;
+        boolean eIsHere = false;
+        SymbolType prevSymbolType = SymbolType.DEFAULT;
+        StringBuilder numericStringBuilder = new StringBuilder();
+        
+        for (int i = 0; i < str.length(); i++) {
+            char symbol = str.charAt(i);
+            
+            if (symbol == '.') {
+                if (dotIsHere || eIsHere || prevSymbolType != SymbolType.DIGIT) {
+                    return null;
+                }
+                dotIsHere = true;
+                prevSymbolType = SymbolType.DOT;
+                numericStringBuilder.append(symbol);
+                
+            } else if (symbol == 'e') {
+                if (eIsHere || prevSymbolType != SymbolType.DIGIT) {
+                    return null;
+                }
+                eIsHere = true;
+                prevSymbolType = SymbolType.E;
+                numericStringBuilder.append(symbol);
+                
+            } else if (symbol == '-') {
+                if (prevSymbolType != SymbolType.DEFAULT && prevSymbolType != SymbolType.E) {
+                    return null;
+                }
+                prevSymbolType = SymbolType.MINUS;
+                numericStringBuilder.append(symbol);
+                
+            } else if (Character.isDigit(symbol)) {
+                prevSymbolType = SymbolType.DIGIT;
+                numericStringBuilder.append(symbol);
+            }
+        }
+        
+        if (prevSymbolType != SymbolType.DIGIT) {
+            return null;
+        }
+        
+        return numericStringBuilder.toString();
+    }
+    
+    private static long getLong(String str) {
+        long number = 0;
+        int length = str.length();
+        
+        for (int i = length - 1; i > 0; i--) {
+            number += Character.digit(str.charAt(i), 10) * Math.pow(10, length - i - 1);
+        }
+        if (str.charAt(0) == '-') {
+            number = -number;
+        } else {
+            number += Character.digit(str.charAt(0), 10) * Math.pow(10, length - 1);
+        }
+        return number;
+    }
+    
+    private static boolean isReal(String str) {
+        return str.contains("e") || str.contains(".");
+    }
+    
+    private static double getDouble(String str) {
+        if (str.contains("e")) {
+            String[] partsOfNumber = str.split("e");
+            double number = partsOfNumber[0].contains(".") ? numberWithDotParse(partsOfNumber[0]) : getLong(partsOfNumber[0]);
+            return number * Math.pow(10, getLong(partsOfNumber[1]));
+        }
+        return numberWithDotParse(str);
+    }
+    
+    private static double numberWithDotParse(String numberWithDot) {
+        String[] partsOfNumber = numberWithDot.split("\\.");
+        return getLong(partsOfNumber[0]) + getLong(partsOfNumber[1]) * Math.pow(10, -partsOfNumber[1].length());
     }
 }
