d38beba9ab356531055e8566ed0646a311be3f09
Stanislav Koptsev <stanislavrecovery@gmail.com>
2020-04-10 15:26:21 +0300

    еще правки...

--- src/main/java/ru/mail/polis/homework/analyzer/CustomAnalyzer.java
+++ src/main/java/ru/mail/polis/homework/analyzer/CustomAnalyzer.java
@@ -4,13 +4,13 @@ public class CustomAnalyzer implements TextAnalyzer {
 
     @Override
     public FilterType analyze(String txt) {
-        txt = txt.replaceAll(" |[0-9]|\\p{Punct}", ""); //отрезаем все кроме букв
-        if (txt.length() <= 1) {
+        String tmpTxt = txt.replaceAll(" |[0-9]|\\p{Punct}", ""); //отрезаем все кроме букв
+        if (tmpTxt.length() <= 1) {
             return FilterType.GOOD;
         }
-        for (int k = 1; k < txt.length(); k++) {                           //проходим по строке
-            if (checkSize(txt.charAt(k), txt.charAt(k - 1))) {             //если символы одного регистра
-                return FilterType.GOOD;                                    //это уже не ЗаБоРчИк
+        for (int k = 1; k < tmpTxt.length(); k++) {                             //проходим по строке
+            if (checkSize(tmpTxt.charAt(k), tmpTxt.charAt(k - 1))) {            //если символы одного регистра
+                return FilterType.GOOD;                                         //это уже не ЗаБоРчИк
             }
         }
         return FilterType.CUSTOM;
