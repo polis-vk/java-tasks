516e108eb843427948027a506bd7b9fe3da18e35
Valeriy <Chugaynov.v300100@mail.ru>
2020-04-25 13:50:31 +0500

    Добавил тип для BasicTransfer в MailService

--- src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
@@ -15,24 +15,24 @@ import java.util.function.Consumer;
  *
  * В реализации нигде не должно быть классов Object и коллекций без типа. Используйте дженерики.
  */
-public class MailService implements Consumer<BasicTransfer> {
+public class MailService implements Consumer<BasicTransfer<?>> {
     
-    private PopularMap<String, List<BasicTransfer>> senders = new PopularMap<>();
-    private PopularMap<String, List<BasicTransfer>> recipients = new PopularMap<>();
+    private PopularMap<String, List<BasicTransfer<?>>> senders = new PopularMap<>();
+    private PopularMap<String, List<BasicTransfer<?>>> recipients = new PopularMap<>();
     
     /**
      * С помощью этого метода почтовый сервис обрабатывает письма и зарплаты
      * 1 балл
      */
     @Override
-    public void accept(BasicTransfer basicTransfer) {
+    public void accept(BasicTransfer<?> basicTransfer) {
         recipients.computeIfAbsent(basicTransfer.getRecipient(), k -> new ArrayList<>()).add(basicTransfer);
         senders.computeIfAbsent(basicTransfer.getSender(), k -> new ArrayList<>()).add(basicTransfer);
     }
     /**
      * Метод возвращает мапу получатель -> все объекты которые пришли к этому получателю через данный почтовый сервис
      */
-    public Map<String, List<BasicTransfer>> getMailBox() {
+    public Map<String, List<BasicTransfer<?>>> getMailBox() {
         return recipients;
     }
 
@@ -53,7 +53,7 @@ public class MailService implements Consumer<BasicTransfer> {
     /**
      * Метод должен заставить обработать service все mails.
      */
-    public static void process(MailService service, List<BasicTransfer> mails) {
+    public static void process(MailService service, List<BasicTransfer<?>> mails) {
         mails.forEach(service);
     }
 }
