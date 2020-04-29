7a76edf959004d5dc1eede6d369b6fd668d0f243
Aleksandr Galkin <alexander.galkin@corp.mail.ru>
2020-04-29 10:45:45 +0300

    added hometask 5

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,12 +1,15 @@
-package ru.mail.polis.homework.files;
+package ru.mail.polis.homework.io;
 
 public class CopyFile {
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
+     * В тесте для создания нужных файлов для первого запуска надо расскоментировать код в setUp()
+     *
+     * 6 баллов
      */
-    public static String copySmallFiles(String pathFrom, String pathTo) {
+    public static String copyFiles(String pathFrom, String pathTo) {
         return null;
     }
 
