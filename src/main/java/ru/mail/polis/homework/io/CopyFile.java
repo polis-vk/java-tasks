d8d223a868807d03ccb1a42b4a8e0099d42ff9d8
Ekaterina <zheleznikova.katya@gmail.com>
2020-05-06 23:02:44 +0300

    5Homework, без сериализации

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,16 +1,54 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.*;
+
+
 public class CopyFile {
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы.
      * В тесте для создания нужных файлов для первого запуска надо расскоментировать код в setUp()
-     *
+     * <p>
      * 6 баллов
      */
     public static String copyFiles(String pathFrom, String pathTo) {
-        return null;
+        Path fromDirectory = Paths.get(pathFrom);
+        Path toDirectory = Paths.get(pathTo);
+        if (Files.notExists(fromDirectory)) {
+            return pathTo;
+        }
+        if (Files.isRegularFile(fromDirectory)) {
+            copyFile(fromDirectory, toDirectory);
+            return pathTo;
+        }
+        try {
+            Files.createDirectories(toDirectory.getParent());
+            DirectoryStream<Path> files = Files.newDirectoryStream(fromDirectory);
+            for (Path file : files) {
+                copyFiles(file.toString(), toDirectory.resolve(file.getFileName()).toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return pathTo;
     }
 
+
+    private static void copyFile(Path fileFrom, Path fileTo) {
+        try (InputStream inputStream = Files.newInputStream(fileFrom)) {
+            try (OutputStream outputStream = Files.newOutputStream(fileTo)) {
+                byte[] buffer = new byte[1024];
+                int lengthRead;
+                while ((lengthRead = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, lengthRead);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,11 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Directories {
 
@@ -13,7 +18,24 @@ public class Directories {
      * 2 балла
      */
     public static int removeWithFile(String path) {
-        return 0;
+
+        File directory = new File(path);
+        int count = 0;
+        if (!directory.exists()) {
+            return count;
+        }
+        if (directory.isFile()) {
+            return deleteFile(directory, 0);
+        }
+
+        for (File file : directory.listFiles()) {
+            count += removeWithFile(file.toString());
+        }
+        return deleteFile(directory, count);
+    }
+
+    private static int deleteFile(File file, int count) {
+        return file.delete() ? count + 1 : count;
     }
 
     /**
@@ -21,6 +43,21 @@ public class Directories {
      * 2 балла
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path directoryPath = Paths.get(path);
+        if (Files.notExists(directoryPath)) {
+            return 0;
+        }
+        if (Files.isRegularFile(directoryPath)) {
+            Files.delete(directoryPath);
+            return 1;
+        }
+        int count = 0;
+        try (DirectoryStream<Path> entries = Files.newDirectoryStream(directoryPath)) {
+            for (Path entry : entries) {
+                count += removeWithPath(entry.toString());
+            }
+        }
+        Files.delete(directoryPath);
+        return count + 1;
     }
 }
