49a15dffd501edf73de022210059d3f1ee50e15e
Aleksandr Galkin <alexander.galkin@corp.mail.ru>
2020-10-18 19:56:51 +0300

    added homework

---
+++ src/main/java/ru/mail/polis/homework/collections/streams/account/Account.java
@@ -0,0 +1,12 @@
+package ru.mail.polis.homework.collections.streams.account;
+
+/**
+ * Реализуйте класс Account с полями:
+ * id
+ * список всех транзакций с аккаунта (входящие и исходящие)
+ * баланс
+ * 1 балл
+ */
+public class Account {
+
+}
---
+++ src/main/java/ru/mail/polis/homework/functions/SimpleFunction.java
@@ -0,0 +1,63 @@
+package ru.mail.polis.homework.functions;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SimpleFunction {
+
+    /**
+     * Функция от трех аргументов. Не забудьте добавить дженерики.
+     * Функция должна походить на {@link java.util.function.BiFunction}
+     * 1 балл
+     */
+    interface TerFunction {
+
+    }
+
+    /**
+     * Реализуйте каррирование для функции от трех аргументов.
+     * Вам нужно правильно определить тип возращаемого значения и реализовать метод.
+     * Не забывайте использовать дженерики.
+     * 2 балла
+     */
+    static Object curring(TerFunction terFunction) {
+        return null;
+    }
+
+
+    /**
+     * Превращает список унарных операторов в один унарный оператор для списка чисел. Получившийся оператор
+     * берет каждый элемент из списка чисел и последовательно применяет все входящие операторы.
+     * Пример: multifunctionalMapper.apply([x -> x, x -> x + 1, x -> x * x]).apply([1, 2]) = [1, 2, 4, 2, 3, 9]
+     * 4 балла
+     */
+    public static final Function<List<IntUnaryOperator>, UnaryOperator<List<Integer>>> multifunctionalMapper =
+            list -> numbers -> numbers.stream()
+                    .map(number -> list.stream()
+                            .reduce(x -> x, (op1, op2) -> x -> op2.applyAsInt(op1.applyAsInt(x)))
+                            .applyAsInt(number))
+                    .collect(Collectors.toList());
+
+
+    /**
+     * Написать функцию, которая принимает начальное значение и преобразователь двух чисел в одно, возвращает функцию,
+     * которая на заданном интервале (входящие аргументы результирующей функции) считает преобразование всех целых чисел
+     * на заданном интервале.
+     *
+     * Пример хотим просуммировать числа от 2 до 10:
+     * reduceIntOperator.apply(начальное значение, (x,y) -> ...).apply(2, 10) = 54
+     * 3 балла
+     */
+    public static final BiFunction<Integer, IntBinaryOperator, IntBinaryOperator> reduceIntOperator = (seed, bi) ->
+            (a, b) -> IntStream.rangeClosed(a, b).reduce(seed, bi);
+
+    public static final IntBinaryOperator sumOperator = reduceIntOperator.apply(0, (a, b) -> a * b);
+}
