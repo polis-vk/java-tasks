6e01c3d5585b2fa6e1530beb137014f397337c15
Valeriy <chugajnov.va@edu.spbstu.ru>
2020-10-23 20:26:59 +0300

    SimpleFunction + SimpleStreams

--- src/main/java/ru/mail/polis/homework/collections/streams/SimpleStreams.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/SimpleStreams.java
@@ -1,43 +1,55 @@
 package ru.mail.polis.homework.collections.streams;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.DoubleUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 public class SimpleStreams {
-
+    
     /**
      * Реализуйте проверку на простоту входящего числа с помощью стримов.
      * Никаких циклов.
      * 1 балл
      */
     public static boolean isPrime(int n) {
-        return false;
+        return n > 1 && IntStream.rangeClosed(2, (int) Math.sqrt(n))
+                .noneMatch(i -> n % i == 0);
     }
-
+    
+    
     /**
      * Вам передается текст и список плохих слов. Вам нужно вернуть мапу (слово -> количество упоминаний в тексте)
      * Слово - набор символ между началом строки/ концом строки / пробелами / знаками препинания (.,;:!?)
      * 1 балл
      */
     public static Map<String, Integer> createBadWordsDetectingStream(String text, List<String> badWords) {
-        return Collections.emptyMap();
+        return Arrays.stream(text.split("[.,;:!?\\s]+"))
+                .filter(badWords::contains)
+                .collect(Collectors.groupingBy(String::toString, Collectors.summingInt(i -> 1)));
     }
-
-
+    
+    
     /**
      * Маленький мальчик кидает мячик n раз в поле силы тяжести под углом alpha к поверхности земли.
      * Так же известно, что мальчик устает с каждым броском все больше. Дана начальная скорость v - скорость
      * при первом броске и функция изменения скорости от номера броска - changeV
-     *
+     * <p>
      * Посчитать расстояние на которое улетит мячик.
      * Для расчета дальности броска можно пользоваться физическими формулами движения и законом сохранения энергии.
      * g = 9.8
-     *
+     * <p>
      * 3 балла
      */
     public static double calcDistance(double v, DoubleUnaryOperator changeV, double alpha, int n) {
-        return 0;
+        final double sin = Math.sin(2 * alpha);
+        final double g = 9.8;
+        return DoubleStream.iterate(v, changeV)
+                .limit(n)
+                .reduce(0.0,
+                        (fullDist, vPrev) -> fullDist + vPrev * vPrev * sin / g);
     }
 }
--- src/main/java/ru/mail/polis/homework/functions/SimpleFunction.java
+++ src/main/java/ru/mail/polis/homework/functions/SimpleFunction.java
@@ -1,8 +1,7 @@
 package ru.mail.polis.homework.functions;
 
-
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.IntBinaryOperator;
@@ -12,27 +11,32 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SimpleFunction {
-
     /**
      * Функция от трех аргументов. Не забудьте добавить дженерики.
      * Функция должна походить на {@link java.util.function.BiFunction}
      * 1 балл
      */
-    interface TerFunction {
-
+    @FunctionalInterface
+    interface TerFunction<T, U, V, R> {
+        R apply(T t, U u, V v);
+        
+        default <X> TerFunction<T, U, V, X> andThen(Function<? super R, ? extends X> after) {
+            Objects.requireNonNull(after);
+            return (T t, U u, V v) -> after.apply(apply(t, u, v));
+        }
     }
-
+    
     /**
      * Реализуйте каррирование для функции от трех аргументов.
      * Вам нужно правильно определить тип возращаемого значения и реализовать метод.
      * Не забывайте использовать дженерики.
      * 2 балла
      */
-    static Object curring(TerFunction terFunction) {
-        return null;
+    static <T, U, V, R> Function<T, Function<U, Function<V, R>>> curring(TerFunction<T, U, V, R> terFunction) {
+        return t -> u -> v -> terFunction.apply(t, u, v);
     }
-
-
+    
+    
     /**
      * Превращает список унарных операторов в один унарный оператор для списка чисел. Получившийся оператор
      * берет каждый элемент из списка чисел и последовательно применяет все входящие операторы.
@@ -40,17 +44,30 @@ public class SimpleFunction {
      * 4 балла
      */
     public static final Function<List<IntUnaryOperator>, UnaryOperator<List<Integer>>> multifunctionalMapper =
-            a -> null;
-
-
+            operators -> ints -> ints.stream()
+                    .map(number -> operators.stream()
+                            .mapToInt(operator -> operator.applyAsInt(number))
+                            .boxed()
+                            .collect(Collectors.toList()))
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
+    
+    public static final Function<List<IntUnaryOperator>, UnaryOperator<List<Integer>>> multifunctionalMapper1 =
+            operators -> ints -> ints.stream()
+                    .map(number -> operators.stream()
+                            .reduce(operator -> operator, IntUnaryOperator::andThen)
+                            .applyAsInt(number))
+                    .collect(Collectors.toList());
+    
     /**
      * Написать функцию, которая принимает начальное значение и преобразователь двух чисел в одно, возвращает функцию,
      * которая на заданном интервале (входящие аргументы результирующей функции) считает преобразование всех целых чисел
      * на заданном интервале.
-     *
+     * <p>
      * Пример хотим просуммировать числа от 2 до 10:
      * reduceIntOperator.apply(начальное значение, (x,y) -> ...).apply(2, 10) = 54
      * 3 балла
      */
-    public static final BiFunction<Integer, IntBinaryOperator, IntBinaryOperator> reduceIntOperator = (a, b) -> null;
+    public static final BiFunction<Integer, IntBinaryOperator, IntBinaryOperator> reduceIntOperator
+            = (initNumber, function) -> (startInc, endInc) -> IntStream.rangeClosed(startInc, endInc).reduce(initNumber, function);
 }
