537ae8740252a0d7f2491e71647874b23727c985
Valeriy <chugajnov.va@edu.spbstu.ru>
2020-10-24 00:24:29 +0300

    Account + Transaction + Task

--- src/main/java/ru/mail/polis/homework/collections/streams/account/Account.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/account/Account.java
@@ -1,5 +1,11 @@
 package ru.mail.polis.homework.collections.streams.account;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Реализуйте класс Account с полями:
  * id
@@ -8,5 +14,53 @@ package ru.mail.polis.homework.collections.streams.account;
  * 1 балл
  */
 public class Account {
-
+    private final long id;
+    private final List<Transaction> transactionList = new ArrayList<>();
+    private final Map<Date, Long> balanceOnDate = new TreeMap<>();
+    private long curBalance = 0L;
+    private static long idCounter = 1;
+    
+    
+    public Account() {
+        this.id = idCounter++;
+        balanceOnDate.put(new Date(0L), 0L);
+    }
+    
+    public void addTransaction(Transaction transaction) {
+        if (transactionList.isEmpty()
+                || transactionList.get(transactionList.size() - 1).getId() < transaction.getId()) {
+            boolean isDest = transaction.getDestAcc() == this;
+            boolean isSrc = transaction.getSrcAcc() == this;
+            if (!isSrc && !isDest) {
+                return;
+            } else if (isDest && !isSrc) {
+                curBalance += transaction.getSum();
+            } else {
+                curBalance -= transaction.getSum();
+            }
+            transactionList.add(transaction);
+            balanceOnDate.put(transaction.getDate(), curBalance);
+        }
+    }
+    
+    public long getBalanceOnDate(Date date) {
+        return balanceOnDate.entrySet().stream()
+                .filter(e -> e.getKey().compareTo(date) <= 0)
+                .reduce(0L, (first, second) -> second.getValue(), (x, y) -> y);
+    }
+    
+    public long getCurBalance() {
+        return curBalance;
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                '}';
+    }
 }
--- src/main/java/ru/mail/polis/homework/collections/streams/account/Transaction.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/account/Transaction.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.collections.streams.account;
 
+import java.util.Date;
+
 /**
  * Реализуйте класс Transaction с полями:
  * id
@@ -10,10 +12,44 @@ package ru.mail.polis.homework.collections.streams.account;
  * 1 балл
  */
 public class Transaction {
-    public Account getAccount() {
-        return null;
+    private final long id;
+    private final Date date;
+    private final Account srcAcc;
+    private final Account destAcc;
+    private final long sum;
+    private static long idCounter = 1;
+    
+    public Transaction(Date date, Account srcAcc, Account destAcc, long sum) {
+        this(idCounter++, date, srcAcc, destAcc, sum);
     }
+    
+    private Transaction(long id, Date date, Account srcAcc, Account destAcc, long sum) {
+        this.id = id;
+        this.date = date;
+        this.srcAcc = srcAcc;
+        this.destAcc = destAcc;
+        this.sum = sum;
+        srcAcc.addTransaction(this);
+        destAcc.addTransaction(this);
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    public Account getSrcAcc() {
+        return srcAcc;
+    }
+    
+    public Account getDestAcc() {
+        return destAcc;
+    }
+    
+    public Date getDate() {
+        return date;
+    }
+    
     public Long getSum() {
-        return null;
+        return sum;
     }
 }
