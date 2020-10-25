19d8ab253dabe4ca6a0c2dfaeb959f8afba8d725
OGSegu <seguisreal@gmail.com>
2020-10-25 16:01:46 +0300

    Account package with some tasks from streams, function has been done

--- src/main/java/ru/mail/polis/homework/collections/streams/SimpleStreams.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/SimpleStreams.java
@@ -1,11 +1,17 @@
 package ru.mail.polis.homework.collections.streams;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.DoubleUnaryOperator;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SimpleStreams {
+    public static void main(String[] args) {
+        System.out.println(createBadWordsDetectingStream("Hello, my name is Vadim! And I am, is 20 years. Old", Arrays.asList("is", "Old")));
+    }
 
     /**
      * Реализуйте проверку на простоту входящего числа с помощью стримов.
@@ -13,7 +19,9 @@ public class SimpleStreams {
      * 1 балл
      */
     public static boolean isPrime(int n) {
-        return false;
+        return IntStream
+                .rangeClosed(2, (int) Math.sqrt(n))
+                .noneMatch(i -> n % i == 0);
     }
 
     /**
@@ -22,7 +30,16 @@ public class SimpleStreams {
      * 1 балл
      */
     public static Map<String, Integer> createBadWordsDetectingStream(String text, List<String> badWords) {
-        return Collections.emptyMap();
+        List<String> wordsList = new ArrayList<>();
+        Matcher matcher = Pattern.compile("[aA-zZ]+")
+                .matcher(text);
+        while (matcher.find()) {
+            wordsList.add(matcher.group());
+        }
+        return wordsList.stream()
+                .map(String::toLowerCase)
+                .filter(badWords::contains)
+                .collect(Collectors.toMap(Function.identity(), e -> 1, Integer::sum));
     }
 
 
@@ -30,11 +47,11 @@ public class SimpleStreams {
      * Маленький мальчик кидает мячик n раз в поле силы тяжести под углом alpha к поверхности земли.
      * Так же известно, что мальчик устает с каждым броском все больше. Дана начальная скорость v - скорость
      * при первом броске и функция изменения скорости от номера броска - changeV
-     *
+     * <p>
      * Посчитать расстояние на которое улетит мячик.
      * Для расчета дальности броска можно пользоваться физическими формулами движения и законом сохранения энергии.
      * g = 9.8
-     *
+     * <p>
      * 3 балла
      */
     public static double calcDistance(double v, DoubleUnaryOperator changeV, double alpha, int n) {
