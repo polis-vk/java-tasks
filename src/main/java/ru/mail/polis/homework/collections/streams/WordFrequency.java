3356fbb2ebbcc633225178041f59d2dd13450c3d
OGSegu <seguisreal@gmail.com>
2020-10-26 20:13:43 +0300

    Deleted useless imports

--- src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
@@ -1,10 +1,8 @@
 package ru.mail.polis.homework.collections.streams;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
--- src/main/java/ru/mail/polis/homework/collections/streams/account/Task.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/account/Task.java
@@ -1,8 +1,8 @@
 package ru.mail.polis.homework.collections.streams.account;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Function;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Task {
