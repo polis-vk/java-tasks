db2202623fa0e5f6068a778659ff19acdf7a2f0e
IgorSamohin <igor04012001@yandex.ru>
2020-10-27 23:16:12 +0300

    transaction implementing

--- src/main/java/ru/mail/polis/homework/collections/streams/account/Transaction.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/account/Transaction.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.collections.streams.account;
 
+import java.util.Date;
+
 /**
  * Реализуйте класс Transaction с полями:
  * id
@@ -10,10 +12,37 @@ package ru.mail.polis.homework.collections.streams.account;
  * 1 балл
  */
 public class Transaction {
-    public Account getAccount() {
-        return null;
+    private final String id;
+    private final Date date;
+    private final Account sender;
+    private final Account receiver;
+    private final Long sum;
+
+    public Transaction(String id, Date date, Account sender, Account receiver, Long sum) {
+        this.id = id;
+        this.date = date;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.sum = sum;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Account getSender() {
+        return sender;
+    }
+
+    public Account getReceiver() {
+        return receiver;
+    }
+
     public Long getSum() {
-        return null;
+        return sum;
     }
 }
