d4048780f45ade3b55aefe14ccbccb52919792d8
Maxim-Byte <maxim5gromov@gmail.com>
2020-12-03 17:35:12 +0300

    Changes

--- src/main/java/ru/mail/polis/homework/collections/streams/account/Transaction.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/account/Transaction.java
@@ -1,7 +1,5 @@
 package ru.mail.polis.homework.collections.streams.account;
 
-import java.util.Date;
-
 /**
  * Реализуйте класс Transaction с полями:
  * id
@@ -12,39 +10,10 @@ import java.util.Date;
  * 1 балл
  */
 public class Transaction {
-
-    private final long id;
-    private final Date date;
-    private final Account recipientAccount;
-    private final Account senderAccount;
-    private final Long cost;
-    private static long counter = 1;
-
-    public Transaction(Date date, Account recipientAccount, Account senderAccount, Long cost) {
-        this.id = counter++;
-        this.date = date;
-        this.recipientAccount = recipientAccount;
-        this.senderAccount = senderAccount;
-        this.cost = cost;
+    public Account getAccount() {
+        return null;
     }
-
-    public long getId() {
-        return id;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public Account getRecipientAccount() {
-        return recipientAccount;
-    }
-
-    public Account getSenderAccount() {
-        return senderAccount;
-    }
-
-    public Long getCost() {
-        return cost;
+    public Long getSum() {
+        return null;
     }
 }
