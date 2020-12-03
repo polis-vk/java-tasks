647fc8eb41388935b792ea59b03ebd9bf948885e
Shabinsky Dmitry <61068119+sdimosik@users.noreply.github.com>
2020-12-03 22:03:13 +0700

    Update SimpleStreams.java

--- src/main/java/ru/mail/polis/homework/collections/streams/SimpleStreams.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/SimpleStreams.java
@@ -1,16 +1,9 @@
 package ru.mail.polis.homework.collections.streams;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.DoubleUnaryOperator;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
 
 public class SimpleStreams {
 
@@ -20,8 +13,7 @@ public class SimpleStreams {
      * 1 балл
      */
     public static boolean isPrime(int n) {
-        return IntStream.rangeClosed(2, n / 2)
-                .noneMatch(value -> n % value == 0);
+        return false;
     }
 
     /**
@@ -30,9 +22,7 @@ public class SimpleStreams {
      * 1 балл
      */
     public static Map<String, Integer> createBadWordsDetectingStream(String text, List<String> badWords) {
-        return Arrays.stream(text.split("[.,;:!? ]+"))
-                .filter(badWords::contains)
-                .collect(Collectors.toMap(Function.identity(), e -> 1, Integer::sum));
+        return Collections.emptyMap();
     }
 
 
@@ -40,16 +30,14 @@ public class SimpleStreams {
      * Маленький мальчик кидает мячик n раз в поле силы тяжести под углом alpha к поверхности земли.
      * Так же известно, что мальчик устает с каждым броском все больше. Дана начальная скорость v - скорость
      * при первом броске и функция изменения скорости от номера броска - changeV
-     * <p>
+     *
      * Посчитать расстояние на которое улетит мячик.
      * Для расчета дальности броска можно пользоваться физическими формулами движения и законом сохранения энергии.
      * g = 9.8
-     * <p>
+     *
      * 3 балла
      */
     public static double calcDistance(double v, DoubleUnaryOperator changeV, double alpha, int n) {
-        return DoubleStream.iterate(v, changeV)
-                .limit(n)
-                .reduce(0.0, (s, prev) -> s + prev * prev * Math.sin(2 * alpha) / 9.8);
+        return 0;
     }
 }
