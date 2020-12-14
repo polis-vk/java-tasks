422449bf53d385f07d131c09472e426bbf58c98b
IlyaAAAA <ilya-sachuk@mail.ru>
2020-12-14 14:18:58 +0300

    calc container

--- src/main/java/ru/mail/polis/homework/concurrency/state/CalculateContainer.java
+++ src/main/java/ru/mail/polis/homework/concurrency/state/CalculateContainer.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.concurrency.state;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
@@ -8,32 +11,37 @@ import java.util.function.UnaryOperator;
  * Потокобезопасный контейнер для вычислений. Контейнер создается с некторым дэфолтным значеним.
  * Далее значение инициализируется, вычисляется и отдается к потребителю. В каждом методе контейнер меняет состояние
  * и делает некоторое вычисление (которое передано ему в определенный метод)
- *
+ * <p>
  * Последовательность переходов из состояния в состояние строго определена:
  * START -> INIT -> RUN -> FINISH
  * Из состояния FINISH можно перейти или в состояние INIT или в состояние CLOSE.
  * CLOSE - конченое состояние.
- *
+ * <p>
  * Если какой-либо метод вызывается после перехода в состояние CLOSE
  * он должен написать ошибку (НЕ бросить) и сразу выйти.
  * Если вызван метод, который не соответствует текущему состоянию - он ждет,
  * пока состояние не станет подходящим для него (или ждет состояние CLOSE, чтобы написать ошибку и выйти)
- *
- *
+ * <p>
+ * <p>
  * Есть три варианта решения этой задачи.
  * 1) через методы wait and notify - 5 баллов
  * 2) через Lock and Condition - 5 баллов
  * 3) через операцию compareAndSet на Atomic классах - 9 баллов
  * Баллы за методы не суммируются, берется наибольший балл из всех методов. (то есть, если вы сделали 1 метод на 4 балла,
  * и 3 метод на 3 балла, я поставлю баллы только 4 балла)
- *
+ * <p>
  * Max 8 баллов
  */
 public class CalculateContainer<T> {
 
-    private State state = State.START;
+    private volatile State state = State.START;
 
-    private T result;
+    private volatile T result;
+
+    private final Lock lock = new ReentrantLock();
+    private final Condition stateWaitingCondition = lock.newCondition();
+
+    private final String errorMessage = "ERROR";
 
     public CalculateContainer(T result) {
         this.result = result;
@@ -43,6 +51,23 @@ public class CalculateContainer<T> {
      * Инициализирует результат и переводит контейнер в состояние INIT (Возможно только из состояния START и FINISH)
      */
     public void init(UnaryOperator<T> initOperator) {
+        lock.lock();
+        try {
+            while (state != State.START && state != State.FINISH) {
+                if (state == State.CLOSE) {
+                    System.out.println(errorMessage);
+                    return;
+                }
+                stateWaitingCondition.await();
+            }
+            result = initOperator.apply(result);
+            state = State.INIT;
+            stateWaitingCondition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
 
     }
 
@@ -50,7 +75,25 @@ public class CalculateContainer<T> {
      * Вычисляет результат и переводит контейнер в состояние RUN (Возможно только из состояния INIT)
      */
     public void run(BinaryOperator<T> runOperator, T value) {
+        lock.lock();
 
+        try {
+            while (state != State.INIT) {
+                if (state == State.CLOSE) {
+                    System.out.println(errorMessage);
+                    return;
+                }
+                stateWaitingCondition.await();
+            }
+
+            result = runOperator.apply(result, value);
+            state = State.RUN;
+            stateWaitingCondition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
     }
 
 
@@ -58,7 +101,23 @@ public class CalculateContainer<T> {
      * Передает результат потребителю и переводит контейнер в состояние FINISH (Возможно только из состояния RUN)
      */
     public void finish(Consumer<T> finishConsumer) {
-
+        lock.lock();
+        try {
+            while (state != State.RUN) {
+                if (state == State.CLOSE) {
+                    System.out.println(errorMessage);
+                    return;
+                }
+                stateWaitingCondition.await();
+            }
+            finishConsumer.accept(result);
+            state = State.FINISH;
+            stateWaitingCondition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
     }
 
 
@@ -67,7 +126,25 @@ public class CalculateContainer<T> {
      * (Возможно только из состояния FINISH)
      */
     public void close(Consumer<T> closeConsumer) {
+        lock.lock();
 
+        try {
+            while (state != State.FINISH) {
+                if (state == State.CLOSE) {
+                    System.out.println(errorMessage);
+                    return;
+                }
+                stateWaitingCondition.await();
+            }
+
+            closeConsumer.accept(result);
+            state = State.CLOSE;
+            stateWaitingCondition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
     }
 
 
