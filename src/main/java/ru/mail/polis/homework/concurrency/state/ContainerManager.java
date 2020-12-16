0dc4d981a076f698e5471df2d8fa9a20c10f9f31
IgorSamohin <igor04012001@yandex.ru>
2020-12-16 17:23:27 +0300

    Add not thread-safe container manager

--- src/main/java/ru/mail/polis/homework/concurrency/state/ContainerManager.java
+++ src/main/java/ru/mail/polis/homework/concurrency/state/ContainerManager.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.homework.concurrency.state;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.function.UnaryOperator;
@@ -8,7 +9,7 @@ import java.util.function.UnaryOperator;
 /**
  * Класс, который упраялвет контейнерами и умеет их инициализировать, запускать, финишировать и закрывать.
  * Делает все это параллельно, то есть может использоваться из многих потоков.
- *
+ * <p>
  * Это задание на 2 балла за каждый метод. Конструктор на 1 балл
  * Max 11 баллов
  */
@@ -20,7 +21,10 @@ public class ContainerManager {
      * Создайте список из непустых контейнеров
      */
     public ContainerManager(int containersCount) {
-        this.calculateContainers = null;
+        this.calculateContainers = new ArrayList<>();
+        for (int i = 0; i < containersCount; i++) {
+            calculateContainers.add(new CalculateContainer<>(5d));
+        }
     }
 
 
@@ -28,11 +32,13 @@ public class ContainerManager {
      * Используйте executor c расширяемым количеством потоков,
      * который будет инициировать все контейнеры, какой-нибудь математической операцией 1_000_000 раз.
      * (для этого используйте вспомогательный метод operation)
-     *
+     * <p>
      * Каждый контейнер надо исполнять отдельно.
      */
     public void initContainers() {
-
+        for (CalculateContainer<Double> c : calculateContainers) {
+            c.init(Math::sqrt);
+        }
     }
 
 
@@ -40,55 +46,61 @@ public class ContainerManager {
      * Используйте executor c 2 потоками (общий с операцией finish),
      * который будет запускать все контейнеры какой-нибудь математической операцией 1_000_000 раз
      * (для этого используйте вспомогательный метод operation)
-     *
+     * <p>
      * Каждый контейнер надо исполнять отдельно.
      */
     public void runContainers() {
-
+        for (CalculateContainer<Double> c : calculateContainers) {
+            c.run((start, param) -> start + param, 1d);
+        }
     }
 
 
     /**
      * Используйте executor c 2 потоками (общий с операцией run), который будет принимать
      * элемент из контейнеров и печатать их с соответствующим текстом об совершенных операциях
-     *
+     * <p>
      * Каждый контейнер надо исполнять отдельно.
      */
     public void finishContainers() {
-
+        for (CalculateContainer<Double> c : calculateContainers) {
+            c.finish(value -> System.out.println("finish " + value));
+        }
     }
 
 
     /**
      * Используйте executor c 1 потоком, который будет принимать элемент из контейнеров
      * и печатать их с соответствующим текстом о закртыии.
-     *
+     * <p>
      * Каждый контейнер надо исполнять отдельно.
-     *
+     * <p>
      * Так как этот метод переводит контейнер в закрытое состояине,
      * то нужно добавить некоторую синхронизацию, которая разблокируется,
      * как только закроются все 10 контейеров
      */
     public void closeContainers() {
-
+        for (CalculateContainer<Double> c : calculateContainers) {
+            c.close(value -> System.out.println("close " + value));
+        }
     }
 
     /**
      * Этот метод должен ждать, пока все контейнеры не закроются или пока не закончится время.
      * Если вышло время, то метод должен вернуть false, иначе true.
-     *
+     * <p>
      * Почти все методы ожидания, которые реализованы в Java умеют ждать с таймаутом.
      * Учтите, что время передается в милисекундах.
      */
     public boolean await(long timeoutMillis) throws Exception {
-        return false;
+       return false;
     }
 
     public List<CalculateContainer<Double>> getCalculateContainers() {
         return calculateContainers;
     }
 
-    private  <T> UnaryOperator<T> operation(UnaryOperator<T> operator) {
+    private <T> UnaryOperator<T> operation(UnaryOperator<T> operator) {
         return param -> {
             T result = param;
 
