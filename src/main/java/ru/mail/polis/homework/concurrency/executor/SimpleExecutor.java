5e7d543a9b5794b7c603af1a70e925b9a7da3d43
Shabinsky Dmitry <sdimosikvip@gmail.com>
2020-12-17 03:10:38 +0700

    Done Exec and TestsExec

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,18 +1,20 @@
 package ru.mail.polis.homework.concurrency.executor;
 
-import java.util.concurrent.Executor;
+import java.util.HashSet;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Нужно сделать свой экзекьютор с линивой инициализацией потоков до какого-то заданного предела.
  * Линивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 баллов
- *
+ * <p>
  * Напишите 3 теста (2 балла за тест)
  * 1) запуск 1 задачи несколько раз с интервалом (должен создаться только 1 поток)
  * 2) запуск параллельно n - 1 задач несколько раз (должно создаться n - 1 потоков) и задачи должны завершится
@@ -22,15 +24,124 @@ import java.util.concurrent.Executor;
  * Max 6 баллов
  */
 public class SimpleExecutor implements Executor {
+    private final BlockingQueue<Runnable> queue = new LinkedBlockingDeque<>();
+    private final HashSet<Worker> pool = new HashSet<>();
+
+    private final AtomicInteger countLiveThreads = new AtomicInteger(0);
+    private final int capacity;
+    private volatile boolean running;
+
+    private final long TIMEOUT = 40;
+    private final TimeUnit SECONDS = TimeUnit.SECONDS;
+    private final Thread.State WAITING = Thread.State.WAITING;
+
+    public SimpleExecutor(int capacity) {
+        if (capacity < 1) {
+            throw new IllegalArgumentException("capacity < 1");
+        }
+        this.capacity = capacity;
+        running = true;
+    }
+
+    public void off() {
+        running = false;
+    }
+
     @Override
     public void execute(Runnable command) {
+        if (running) {
+            if (command == null) {
+                throw new NullPointerException();
+            }
 
+            queue.add(command);
+            Worker w = getEmptyWorker();
+
+            if (w == null) {
+                addWorker();
+            } else {
+                w.notifyThreads();
+            }
+        }
     }
 
-    /**
-     * Должен возвращать количество созданных потоков.
-     */
+    private void addWorker() {
+        if (countLiveThreads.get() < capacity) {
+            Worker w = new Worker();
+            pool.add(w);
+            w.start();
+        }
+    }
+
+    private Worker getEmptyWorker() {
+        for (Worker worker : pool) {
+            if (worker.getState() == WAITING) {
+                return worker;
+            }
+        }
+        return null;
+    }
+
+    private class Worker extends Thread {
+        private final int num;
+
+        Worker() {
+            num = countLiveThreads.incrementAndGet();
+        }
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                try {
+                    while (running) {
+                        if (queue.isEmpty()) {
+                            wait();
+                        } else {
+                            queue.take().run();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public synchronized void notifyThreads() {
+            notifyAll();
+        }
+    }
+
     public int getLiveThreadsCount() {
-        return 0;
+        return countLiveThreads.get();
     }
 }
+
+/*class Task implements Runnable {
+
+    private final String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.print("Start: " + name + " ");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finish: " + name);
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        SimpleExecutor simpleExecutor = new SimpleExecutor(3);
+        simpleExecutor.execute(new Task("1"));
+        simpleExecutor.execute(new Task("2"));
+        simpleExecutor.execute(new Task("3"));
+        simpleExecutor.execute(new Task("4"));
+    }
+}*/
---
+++ src/test/java/ru/mail/polis/homework/executor/SimpleExecutorTest.java
@@ -0,0 +1,132 @@
+package ru.mail.polis.homework.executor;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.mail.polis.homework.concurrency.executor.SimpleExecutor;
+
+public class SimpleExecutorTest {
+
+    @Test
+    public void Test1() {
+        SimpleExecutor simpleExecutor = new SimpleExecutor(4);
+
+        Runnable task = () -> {
+            System.out.print("Starting");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("Complete");
+        };
+
+        Assert.assertEquals(0, simpleExecutor.getLiveThreadsCount());
+        simpleExecutor.execute(task);
+        Assert.assertEquals(1, simpleExecutor.getLiveThreadsCount());
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(1, simpleExecutor.getLiveThreadsCount());
+        simpleExecutor.execute(task);
+        Assert.assertEquals(1, simpleExecutor.getLiveThreadsCount());
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(1, simpleExecutor.getLiveThreadsCount());
+        simpleExecutor.off();
+    }
+
+    @Test
+    public void Task2() {
+
+        final int N = 10;
+        SimpleExecutor simpleExecutor = new SimpleExecutor(N);
+
+        //450ms
+        Runnable task = () -> {
+            try {
+                Thread.sleep(450);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Assert.assertEquals(0, simpleExecutor.getLiveThreadsCount());
+
+        for (int i = 0; i < N; i++) {
+            simpleExecutor.execute(task);
+        }
+
+        Assert.assertEquals(N, simpleExecutor.getLiveThreadsCount());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(N, simpleExecutor.getLiveThreadsCount());
+
+        for (int i = 0; i < N; i++) {
+            simpleExecutor.execute(task);
+        }
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(N, simpleExecutor.getLiveThreadsCount());
+        simpleExecutor.off();
+    }
+
+    @Test
+    public void Task3() {
+        final int N = 30;
+        final int M = 15;
+
+        SimpleExecutor simpleExecutor = new SimpleExecutor(N);
+
+        //100ms
+        Runnable task = () -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Assert.assertEquals(0, simpleExecutor.getLiveThreadsCount());
+
+        for (int i = 0; i < N + M; i++) {
+            simpleExecutor.execute(task);
+        }
+        Assert.assertEquals(N, simpleExecutor.getLiveThreadsCount());
+        try {
+            Thread.sleep(6500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(N, simpleExecutor.getLiveThreadsCount());
+
+        for (int i = 0; i < N + M; i++) {
+            simpleExecutor.execute(task);
+        }
+        Assert.assertEquals(N, simpleExecutor.getLiveThreadsCount());
+        try {
+            Thread.sleep(6500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(N, simpleExecutor.getLiveThreadsCount());
+    }
+}
