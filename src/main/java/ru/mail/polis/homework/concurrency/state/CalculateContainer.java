7a10510e96532fa1145c9538d98963c883bd3ccf
IgorSamohin <igor04012001@yandex.ru>
2020-12-16 03:50:48 +0300

    Implement calculate contaiter

--- src/main/java/ru/mail/polis/homework/concurrency/state/CalculateContainer.java
+++ src/main/java/ru/mail/polis/homework/concurrency/state/CalculateContainer.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.homework.concurrency.state;
 
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
@@ -8,49 +9,77 @@ import java.util.function.UnaryOperator;
  * Потокобезопасный контейнер для вычислений. Контейнер создается с некторым дэфолтным значеним.
  * Далее значение инициализируется, вычисляется и отдается к потребителю. В каждом методе контейнер меняет состояние
  * и делает некоторое вычисление (которое передано ему в определенный метод)
- *
+ * <p>
  * Последовательность переходов из состояния в состояние строго определена:
  * START -> INIT -> RUN -> FINISH
  * Из состояния FINISH можно перейти или в состояние INIT или в состояние CLOSE.
  * CLOSE - конченое состояние.
- *
+ * <p>
  * Если какой-либо метод вызывается после перехода в состояние CLOSE
  * он должен написать ошибку (НЕ бросить) и сразу выйти.
  * Если вызван метод, который не соответствует текущему состоянию - он ждет,
  * пока состояние не станет подходящим для него (или ждет состояние CLOSE, чтобы написать ошибку и выйти)
- *
- *
+ * <p>
+ * <p>
  * Есть три варианта решения этой задачи.
  * 1) через методы wait and notify - 5 баллов
  * 2) через Lock and Condition - 5 баллов
  * 3) через операцию compareAndSet на Atomic классах - 9 баллов
  * Баллы за методы не суммируются, берется наибольший балл из всех методов. (то есть, если вы сделали 1 метод на 4 балла,
  * и 3 метод на 3 балла, я поставлю баллы только 4 балла)
- *
+ * <p>
  * Max 8 баллов
  */
 public class CalculateContainer<T> {
 
-    private State state = State.START;
+    private volatile AtomicReference<State> state = new AtomicReference<>();
 
     private T result;
 
     public CalculateContainer(T result) {
         this.result = result;
+        this.state.set(State.START);
     }
 
     /**
      * Инициализирует результат и переводит контейнер в состояние INIT (Возможно только из состояния START и FINISH)
      */
     public void init(UnaryOperator<T> initOperator) {
-
+        State cur;
+        do {
+            if (state.get() == State.CLOSE) {
+                System.out.println("Closed");
+                return;
+            }
+            cur = state.get();
+            if ((cur != State.START) && (cur != State.FINISH)) {
+                continue;
+            }
+        } while (state.compareAndSet(cur, State.INIT) ||
+                state.compareAndSet(cur, State.INIT));
+        synchronized (result) {
+            result = initOperator.apply(result);
+        }
     }
 
     /**
      * Вычисляет результат и переводит контейнер в состояние RUN (Возможно только из состояния INIT)
      */
     public void run(BinaryOperator<T> runOperator, T value) {
-
+        State cur;
+        do {
+            if (state.get() == State.CLOSE) {
+                System.out.println("Closed");
+                return;
+            }
+            cur = state.get();
+            if (cur != State.INIT) {
+                continue;
+            }
+        } while (state.compareAndSet(cur, State.RUN));
+        synchronized (result) {
+            result = runOperator.apply(result, value);
+        }
     }
 
 
@@ -58,7 +87,20 @@ public class CalculateContainer<T> {
      * Передает результат потребителю и переводит контейнер в состояние FINISH (Возможно только из состояния RUN)
      */
     public void finish(Consumer<T> finishConsumer) {
-
+        State cur;
+        do {
+            if (state.get() == State.CLOSE) {
+                System.out.println("Closed");
+                return;
+            }
+            cur = state.get();
+            if (cur != State.RUN) {
+                continue;
+            }
+        } while (state.compareAndSet(cur, State.FINISH));
+        synchronized (result) {
+            finishConsumer.accept(result);
+        }
     }
 
 
@@ -67,7 +109,20 @@ public class CalculateContainer<T> {
      * (Возможно только из состояния FINISH)
      */
     public void close(Consumer<T> closeConsumer) {
-
+        State cur;
+        do {
+            if (state.get() == State.CLOSE) {
+                System.out.println("Closed");
+                return;
+            }
+            cur = state.get();
+            if (cur != State.FINISH) {
+                continue;
+            }
+        } while (state.compareAndSet(cur, State.CLOSE));
+        synchronized (result) {
+            closeConsumer.accept(result);
+        }
     }
 
 
@@ -76,6 +131,6 @@ public class CalculateContainer<T> {
     }
 
     public State getState() {
-        return state;
+        return state.get();
     }
 }
