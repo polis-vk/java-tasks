f92ac196f7a5827d5e771d74770eb04212cd8085
pa-artem <pa.artem20@yandex.ru>
2020-12-16 22:18:34 +0300

    Implement container

--- src/main/java/ru/mail/polis/homework/concurrency/state/CalculateContainer.java
+++ src/main/java/ru/mail/polis/homework/concurrency/state/CalculateContainer.java
@@ -1,7 +1,9 @@
 package ru.mail.polis.homework.concurrency.state;
 
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 /**
@@ -30,27 +32,42 @@ import java.util.function.UnaryOperator;
  * Max 8 баллов
  */
 public class CalculateContainer<T> {
+    private final AtomicReference<State> state = new AtomicReference<>(State.START);
 
-    private State state = State.START;
-
-    private T result;
+    private final AtomicReference<T> result = new AtomicReference<>();
 
     public CalculateContainer(T result) {
-        this.result = result;
+        this.result.set(result);
     }
 
     /**
      * Инициализирует результат и переводит контейнер в состояние INIT (Возможно только из состояния START и FINISH)
      */
     public void init(UnaryOperator<T> initOperator) {
+        if (!waitForStateAndSet(State.INIT, state -> state == State.START || state == State.FINISH)) {
+            System.err.println("Error: Cannot proceed from state CLOSE to state INIT (inside `init` method).");
+            return;
+        }
 
+        T readResult = result.get();
+        while (!result.compareAndSet(readResult, initOperator.apply(readResult))) {
+            readResult = result.get();
+        }
     }
 
     /**
      * Вычисляет результат и переводит контейнер в состояние RUN (Возможно только из состояния INIT)
      */
     public void run(BinaryOperator<T> runOperator, T value) {
+        if (!waitForStateAndSet(State.RUN, state -> state == State.INIT)) {
+            System.err.println("Error: Cannot proceed from state CLOSE to state RUN (inside `run` method).");
+            return;
+        }
 
+        T readResult = result.get();
+        while (!result.compareAndSet(readResult, runOperator.apply(readResult, runOperator.apply(readResult, value)))) {
+            readResult = result.get();
+        }
     }
 
 
@@ -58,7 +75,12 @@ public class CalculateContainer<T> {
      * Передает результат потребителю и переводит контейнер в состояние FINISH (Возможно только из состояния RUN)
      */
     public void finish(Consumer<T> finishConsumer) {
+        if (!waitForStateAndSet(State.FINISH, state -> state == State.RUN)) {
+            System.err.println("Error: Cannot proceed from state CLOSE to state FINISH (inside `finish` method).");
+            return;
+        }
 
+        finishConsumer.accept(result.get());
     }
 
 
@@ -67,15 +89,36 @@ public class CalculateContainer<T> {
      * (Возможно только из состояния FINISH)
      */
     public void close(Consumer<T> closeConsumer) {
+        if (!waitForStateAndSet(State.CLOSE, state -> state == State.FINISH)) {
+            System.err.println("Error: Cannot perform close method twice.");
+            return;
+        }
 
+        closeConsumer.accept(result.get());
     }
 
 
     public T getResult() {
-        return result;
+        return result.get();
     }
 
     public State getState() {
-        return state;
+        return state.get();
+    }
+
+    private boolean waitForStateAndSet(State newState, Predicate<State> conditionToWait) {
+        State readState = state.get();
+        while (true) {
+            while (!conditionToWait.test(readState)) {
+                readState = state.get();
+                if (readState == State.CLOSE) {
+                    return false;
+                }
+            }
+            if (state.compareAndSet(readState, newState)) {
+                break;
+            }
+        }
+        return true;
     }
 }
