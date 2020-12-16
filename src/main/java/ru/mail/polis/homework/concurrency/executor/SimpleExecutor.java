8b20e8c8b616f2ac5beb53a72a0b080990046fc4
Vadim Ershov <vadim.ershov.01@mail.ru>
2020-12-16 19:47:49 +0500

    implemented SimpleExecutor

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,18 +1,22 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Нужно сделать свой экзекьютор с линивой инициализацией потоков до какого-то заданного предела.
  * Линивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 баллов
- *
+ * <p>
  * Напишите 3 теста (2 балла за тест)
  * 1) запуск 1 задачи несколько раз с интервалом (должен создаться только 1 поток)
  * 2) запуск параллельно n - 1 задач несколько раз (должно создаться n - 1 потоков) и задачи должны завершится
@@ -22,15 +26,79 @@ import java.util.concurrent.Executor;
  * Max 6 баллов
  */
 public class SimpleExecutor implements Executor {
+    private final BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>();
+    private final List<Worker> workers = new ArrayList<>();
+    private boolean isRunnable = true;
+    private final int size;
+
+    public SimpleExecutor() {
+        size = 10;
+    }
+
+    public SimpleExecutor(int n) {
+        this.size = n;
+    }
+
     @Override
     public void execute(Runnable command) {
+        workQueue.offer(command);
+        Worker freeWorker = getFreeWorker();
+        if (freeWorker == null && size > workers.size()) {
+            Worker newWorker = new Worker();
+            workers.add(newWorker);
+            newWorker.start();
+        } else {
+            while ((freeWorker = getFreeWorker()) != null) {
+                freeWorker.notifyWorker();
+            }
+        }
+    }
 
+    private Worker getFreeWorker() {
+        Worker free = null;
+        for (Worker worker : workers) {
+            if (worker.getState() == Thread.State.WAITING) {
+                free = worker;
+                break;
+            }
+        }
+        return free;
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        return workers.size();
+    }
+
+    public void shutdown() {
+        isRunnable = false;
+    }
+
+    private class Worker extends Thread {
+
+        @Override
+        public void run() {
+            while (isRunnable) {
+                try {
+                    synchronized (this) {
+                        Runnable nextTask ;
+                        while ((nextTask = workQueue.poll()) == null) {
+                            wait();
+                        }
+                        nextTask.run();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void notifyWorker() {
+            synchronized (this) {
+                this.notifyAll();
+            }
+        }
     }
 }
---
+++ src/test/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutorTest.java
@@ -0,0 +1,122 @@
+package ru.mail.polis.homework.concurrency.executor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleExecutorTest {
+
+    /**
+     * запуск 1 задачи несколько раз с интервалом (должен создаться только 1 поток)
+     */
+    @Test
+    public void oneTask() throws InterruptedException {
+        SimpleExecutor executor = new SimpleExecutor();
+        Assert.assertEquals(0, executor.getLiveThreadsCount());
+
+        Runnable task = () -> {
+            try {
+                System.out.println("start Task");
+                Thread.sleep(3000);
+                System.out.println("end Task");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        executor.execute(task);
+        Thread.sleep(4000);
+        Assert.assertEquals(1, executor.getLiveThreadsCount());
+
+        executor.execute(task);
+        Thread.sleep(4000);
+        Assert.assertEquals(1, executor.getLiveThreadsCount());
+
+        executor.execute(task);
+        Thread.sleep(4000);
+        Assert.assertEquals(1, executor.getLiveThreadsCount());
+        executor.shutdown();
+    }
+    /**
+     * запуск параллельно n - 1 задач несколько раз (должно создаться n - 1 потоков) и задачи должны завершится
+     * примерно одновременно
+     */
+    @Test
+    public void severalTask() throws InterruptedException {
+
+        SimpleExecutor executor = new SimpleExecutor();
+        Assert.assertEquals(0, executor.getLiveThreadsCount());
+
+        Runnable task = () -> {
+            try {
+                System.out.println("start Task");
+                Thread.sleep(3000);
+                System.out.println("end Task");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            executor.execute(task);
+        }
+        Thread.sleep(5000);
+        Assert.assertEquals(10, executor.getLiveThreadsCount());
+
+        for (int i = 0; i < n; i++) {
+            executor.execute(task);
+        }
+        Thread.sleep(5000);
+        Assert.assertEquals(10, executor.getLiveThreadsCount());
+        executor.shutdown();
+    }
+
+    /**
+     * запуск параллельно n + m задач несколько раз (должно создаться n потоков) и первые n задач должны завершится
+     * примерно одновременно, вторые m задач должны завершиться чуть позже первых n и тоже примерно одновременно
+     **/
+    @Test
+    public void severalTaskAtDifferentTimes() throws InterruptedException {
+        int n = 5;
+        int m = 3;
+        SimpleExecutor executor = new SimpleExecutor(n);
+        Assert.assertEquals(0, executor.getLiveThreadsCount());
+
+        Runnable task1 = () -> {
+            try {
+                System.out.println("start Task1");
+                Thread.sleep(2000);
+                System.out.println("end Task1");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Runnable task2 = () -> {
+            try {
+                System.out.println("start Task2");
+                Thread.sleep(2000);
+                System.out.println("end Task2");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        for (int i = 0; i < n; i++) {
+            executor.execute(task1);
+        }
+        Thread.sleep(1000);
+        for (int i = 0; i < m; i++) {
+            executor.execute(task2);
+        }
+        Thread.sleep(5000);
+        Assert.assertEquals(5, executor.getLiveThreadsCount());
+        for (int i = 0; i < n; i++) {
+            executor.execute(task1);
+        }
+        for (int i = 0; i < m; i++) {
+            executor.execute(task2);
+        }
+        Thread.sleep(5000);
+        Assert.assertEquals(5, executor.getLiveThreadsCount());
+        executor.shutdown();
+    }
+
+}
