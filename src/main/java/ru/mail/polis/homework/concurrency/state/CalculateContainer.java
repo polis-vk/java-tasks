1ea233815a64cecb28bb60a1496daaa632830f6c
Shabinsky Dmitry <sdimosikvip@gmail.com>
2020-12-17 08:47:20 +0700

    Add Container-1

--- src/main/java/ru/mail/polis/homework/concurrency/state/CalculateContainer.java
+++ src/main/java/ru/mail/polis/homework/concurrency/state/CalculateContainer.java
@@ -1,56 +1,82 @@
 package ru.mail.polis.homework.concurrency.state;
 
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
 
 /**
  * Потокобезопасный контейнер для вычислений. Контейнер создается с некторым дэфолтным значеним.
  * Далее значение инициализируется, вычисляется и отдается к потребителю. В каждом методе контейнер меняет состояние
  * и делает некоторое вычисление (которое передано ему в определенный метод)
- *
+ * <p>
  * Последовательность переходов из состояния в состояние строго определена:
  * START -> INIT -> RUN -> FINISH
  * Из состояния FINISH можно перейти или в состояние INIT или в состояние CLOSE.
  * CLOSE - конченое состояние.
- *
+ * <p>
  * Если какой-либо метод вызывается после перехода в состояние CLOSE
  * он должен написать ошибку (НЕ бросить) и сразу выйти.
  * Если вызван метод, который не соответствует текущему состоянию - он ждет,
  * пока состояние не станет подходящим для него (или ждет состояние CLOSE, чтобы написать ошибку и выйти)
- *
- *
+ * <p>
+ * <p>
  * Есть три варианта решения этой задачи.
  * 1) через методы wait and notify - 5 баллов
  * 2) через Lock and Condition - 5 баллов
  * 3) через операцию compareAndSet на Atomic классах - 9 баллов
  * Баллы за методы не суммируются, берется наибольший балл из всех методов. (то есть, если вы сделали 1 метод на 4 балла,
  * и 3 метод на 3 балла, я поставлю баллы только 4 балла)
- *
+ * <p>
  * Max 8 баллов
  */
+
 public class CalculateContainer<T> {
 
-    private State state = State.START;
+    //private volatile State state = State.START;
 
-    private T result;
+    //private volatile T result;
+
+    private final AtomicReference<T> result;
+    private final AtomicReference<State> state = new AtomicReference<>(State.START);
 
     public CalculateContainer(T result) {
-        this.result = result;
+        this.result = new AtomicReference<>(result);
     }
 
     /**
      * Инициализирует результат и переводит контейнер в состояние INIT (Возможно только из состояния START и FINISH)
      */
     public void init(UnaryOperator<T> initOperator) {
+        while (!state.compareAndSet(State.FINISH, State.INIT) && !state.compareAndSet(State.START, State.INIT)) {
+            if (state.compareAndSet(State.CLOSE, State.CLOSE)) {
+                System.err.println("'init' called when the state is 'CLOSE'");
+                return;
+            }
+        }
 
+        T r = result.get();
+        while (!result.compareAndSet(r, initOperator.apply(r))) {
+            r = result.get();
+        }
     }
 
     /**
      * Вычисляет результат и переводит контейнер в состояние RUN (Возможно только из состояния INIT)
      */
     public void run(BinaryOperator<T> runOperator, T value) {
+        while (!state.compareAndSet(State.INIT, State.RUN)) {
+            if (state.compareAndSet(State.CLOSE, State.CLOSE)) {
+                System.err.println("'run' called when the state is 'CLOSE'");
+                return;
+            }
+        }
 
+        T r = result.get();
+        while (!result.compareAndSet(r, runOperator.apply(r, runOperator.apply(r, value)))) {
+            r = result.get();
+        }
     }
 
 
@@ -58,7 +84,13 @@ public class CalculateContainer<T> {
      * Передает результат потребителю и переводит контейнер в состояние FINISH (Возможно только из состояния RUN)
      */
     public void finish(Consumer<T> finishConsumer) {
-
+        while (!state.compareAndSet(State.RUN, State.FINISH)) {
+            if (state.compareAndSet(State.CLOSE, State.CLOSE)) {
+                System.err.println("'finish' called when the state is 'CLOSE'");
+                return;
+            }
+        }
+        finishConsumer.accept(result.get());
     }
 
 
@@ -67,15 +99,21 @@ public class CalculateContainer<T> {
      * (Возможно только из состояния FINISH)
      */
     public void close(Consumer<T> closeConsumer) {
-
+        while (!state.compareAndSet(State.FINISH, State.CLOSE)) {
+            if (state.compareAndSet(State.CLOSE, State.CLOSE)) {
+                System.err.println("'close' called when the state is 'CLOSE'");
+                return;
+            }
+        }
+        closeConsumer.accept(result.get());
     }
 
 
     public T getResult() {
-        return result;
+        return result.get();
     }
 
     public State getState() {
-        return state;
+        return state.get();
     }
 }
