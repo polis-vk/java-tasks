5b7487f1f20b0ab4422d23aeda5be6b3fb8092c2
IgorSamohin <igor04012001@yandex.ru>
2020-12-23 20:18:20 +0300

    Implement second test

--- src/test/java/ru/mail/polis/homework/concurrent/executor/SimpleExecutorTest.java
+++ src/test/java/ru/mail/polis/homework/concurrent/executor/SimpleExecutorTest.java
@@ -1,5 +1,9 @@
 package ru.mail.polis.homework.concurrent.executor;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import ru.mail.polis.homework.concurrency.executor.SimpleExecutor;
@@ -23,4 +27,34 @@ public class SimpleExecutorTest {
         }
     }
 
+    @Test
+    public void runParallelTasks() throws InterruptedException {
+        int N = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(N);
+        SimpleExecutor executor = new SimpleExecutor(N);
+        for (int j = 0; j < 4; j++) {
+            System.out.println(j);
+            CountDownLatch countDownLatch = new CountDownLatch(N);
+            for (int i = 0; i < N; i++) {
+                executorService.execute(() -> {
+                    countDownLatch.countDown();
+                    try {
+                        countDownLatch.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    executor.execute(() -> {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("Some work results");
+                    });
+                });
+            }
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+            assertEquals(executor.getLiveThreadsCount(), N);
+        }
+    }
 }
