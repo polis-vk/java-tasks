15fc2f00541d4bcff30f790105211473b31913b6
Daniil Shilov <shilov.da2001@mail.ru>
2021-03-17 20:48:30 +0300

    Code style

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -2,64 +2,56 @@ package ru.mail.polis.homework.simple;
 
 import java.util.function.ToDoubleFunction;
 
-public class HomeworkTask
-{
-  /**
-   * Нужно численно посчитать интеграл от a до b с шагом delta от функции function
-   * Для того, что бы получить значение по Y для точки X, надо просто написать function.applyAsDouble(t)
-   * Считаем, что функция определена на всем пространстве от a до b
-   */
-  public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta)
-  {
-    double result = 0;
-    for (double i = a; i < b - (delta / 2); i += delta)
-    {
-      result += 0.5 * delta * (function.applyAsDouble(i) + function.applyAsDouble(i + delta));
+public class HomeworkTask {
+    /**
+     * Нужно численно посчитать интеграл от a до b с шагом delta от функции function
+     * Для того, что бы получить значение по Y для точки X, надо просто написать function.applyAsDouble(t)
+     * Считаем, что функция определена на всем пространстве от a до b
+     */
+    public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
+        double result = 0;
+        for (double i = a; i < b - (delta / 2); i += delta) {
+            result += 0.5 * delta * (function.applyAsDouble(i) + function.applyAsDouble(i + delta));
+        }
+
+        return result;
     }
 
-    return result;
-  }
-
-  /**
-   * Вывести номер максимальной цифры. Счет начинается слева направо,
-   * выводим номер первой максимальной цифры (если их несколько)
-   */
-  public static byte maxNumber(long a)
-  {
-    int max = 0;
-    int pos = 0;
-    String aStr = String.valueOf(a);
-    for (int i = 0; i < aStr.length(); ++i)
-    {
-      int number = Integer.parseInt(String.valueOf(aStr.charAt(i)));
-      if (max < number)
-      {
-        pos = i;
-        max = number;
-      }
+    /**
+     * Вывести номер максимальной цифры. Счет начинается слева направо,
+     * выводим номер первой максимальной цифры (если их несколько)
+     */
+    public static byte maxNumber(long a) {
+        int max = 0;
+        int pos = 0;
+        String aStr = String.valueOf(a);
+        for (int i = 0; i < aStr.length(); ++i) {
+            int number = Integer.parseInt(String.valueOf(aStr.charAt(i)));
+            if (max < number) {
+                pos = i;
+                max = number;
+            }
+        }
+        return (byte) (pos + 1);
     }
-    return (byte) (pos + 1);
-  }
 
 
-  /**
-   * Даны две точки в пространстве (x1, y1) и (x2, y2). Вам нужно найти Y координату третьей точки (x3, y3),
-   * которая находится на той же прямой что и первые две.
-   */
-  public static double lineFunction(int x1, int y1, int x2, int y2, int x3)
-  {
-    return (double) (x3 - x1) * (y2 - y1) / (x2 - x1) + y1;
-  }
+    /**
+     * Даны две точки в пространстве (x1, y1) и (x2, y2). Вам нужно найти Y координату третьей точки (x3, y3),
+     * которая находится на той же прямой что и первые две.
+     */
+    public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
+        return (double) (x3 - x1) * (y2 - y1) / (x2 - x1) + y1;
+    }
 
-  /**
-   * Даны 4 точки в пространстве A(x1, y1), B(x2, y2), C(x3, y3), D(x4, y4). Найдите площадь выпуклого
-   * четырехуголька ABCD.
-   * Это дополнительное задание, необязательное для выполнения
-   */
-  public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
-  {
-    return Math.abs((double) (x1 - x2) * (y3 - y2) - (y1 - y2) * (x3 - x2)) / 2 +
-            Math.abs((double) (x1 - x3) * (y4 - y3) - (y1 - y3) * (x4 - x3)) / 2;
-  }
+    /**
+     * Даны 4 точки в пространстве A(x1, y1), B(x2, y2), C(x3, y3), D(x4, y4). Найдите площадь выпуклого
+     * четырехуголька ABCD.
+     * Это дополнительное задание, необязательное для выполнения
+     */
+    public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+        return Math.abs((double) (x1 - x2) * (y3 - y2) - (y1 - y2) * (x3 - x2)) / 2 +
+                Math.abs((double) (x1 - x3) * (y4 - y3) - (y1 - y3) * (x4 - x3)) / 2;
+    }
 
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -7,61 +7,53 @@ package ru.mail.polis.homework.simple;
  * Чтобы увидеть все методы класса Math, достаточно написать Math. и среда вам сама покажет возможные методы.
  * Для просмотра подробной документации по выбранному методу нажмите Ctrl + q
  */
-public class IntegerTask
-{
-  /**
-   * Сумма чисел от 1 до n (1 + 2 + 3 + ... + n)
-   * Пример: (5) -> 15
-   */
-  public static int sum(int n)
-  {
-    return n * (n + 1) / 2;
-  }
-
-  /**
-   * Гусеница поднимается по стене длиной height на высоту top за день, ночью гусеница сползает на bottom.
-   * Сколько дней понадобится гусенице, чтобы доползти до потолка. Если она этого никогда не сможет сделать,
-   * Верните число Integer.MAX_VALUE;
-   * Пример: (10, 3, 2) -> 8
-   */
-  public static int snake(int height, int top, int bottom)
-  {
-    if (top >= height)
-    {
-      return 1;
+public class IntegerTask {
+    /**
+     * Сумма чисел от 1 до n (1 + 2 + 3 + ... + n)
+     * Пример: (5) -> 15
+     */
+    public static int sum(int n) {
+        return n * (n + 1) / 2;
     }
 
-    if (top <= bottom || height == Integer.MAX_VALUE)
-    {
-      return Integer.MAX_VALUE;
+    /**
+     * Гусеница поднимается по стене длиной height на высоту top за день, ночью гусеница сползает на bottom.
+     * Сколько дней понадобится гусенице, чтобы доползти до потолка. Если она этого никогда не сможет сделать,
+     * Верните число Integer.MAX_VALUE;
+     * Пример: (10, 3, 2) -> 8
+     */
+    public static int snake(int height, int top, int bottom) {
+        if (top >= height) {
+            return 1;
+        }
+
+        if (top <= bottom || height == Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        return (int) Math.ceil((double) (height - top) / (top - bottom)) + 1;
     }
 
-    return (int) Math.ceil((double) (height - top) / (top - bottom)) + 1;
-  }
-
-  /**
-   * Дано число n и номер разряда order. Выведите цифру стояющую на нужном разряде
-   * Пример: (454355, 3) -> 3
-   */
-  public static int kDecimal(int n, int order)
-  {
-    int temp = (int) Math.pow(10, order);
-    return Math.abs(n % temp / (temp / 10));
-  }
-
-
-  /**
-   * Выведите факториал от числа n
-   * Пример: (5) -> 120
-   */
-  public static long factorial(byte n)
-  {
-    long result = 1;
-    for (byte i = 2; i <= n; ++i)
-    {
-      result *= i;
+    /**
+     * Дано число n и номер разряда order. Выведите цифру стояющую на нужном разряде
+     * Пример: (454355, 3) -> 3
+     */
+    public static int kDecimal(int n, int order) {
+        int temp = (int) Math.pow(10, order);
+        return Math.abs(n % temp / (temp / 10));
     }
 
-    return result;
-  }
+
+    /**
+     * Выведите факториал от числа n
+     * Пример: (5) -> 120
+     */
+    public static long factorial(byte n) {
+        long result = 1;
+        for (byte i = 2; i <= n; ++i) {
+            result *= i;
+        }
+
+        return result;
+    }
 }
