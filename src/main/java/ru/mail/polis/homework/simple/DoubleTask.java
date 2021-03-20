ea6f5b3b1577ec98010e614c39261e8822f53267
MercilessYuna <64254254+MercilessYuna@users.noreply.github.com>
2021-03-20 13:15:05 +0300

    Home work 1 implemented

--- src/main/java/ru/mail/polis/homework/simple/DoubleTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleTask.java
@@ -16,6 +16,14 @@ public class DoubleTask {
     public static String equation(int a, int b, int c) {
         double x1 = 0;
         double x2 = 0;
+        double discriminant = b * b - 4 * a * c;
+        x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        if (x2 > x1) {
+            double temp = x1;
+            x1 = x2;
+            x2 = temp;
+        }
         return x1 + ", " + x2;
     }
 
@@ -24,6 +32,6 @@ public class DoubleTask {
      * Пример: (0, 0, 3, 4) -> 5.0
      */
     public static float length(double x1, double y1, double x2, double y2) {
-        return 0;
+        return (float) Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
     }
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -14,7 +14,7 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        return n * (n + 1) / 2;
     }
 
     /**
@@ -24,7 +24,15 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        if (top >= height) {
+            return 1;
+        }
+        double stepsPerDay = top - bottom;
+        if (stepsPerDay > 0) {
+            return (int) Math.ceil(((height - top) / stepsPerDay) + 1);
+        } else {
+            return Integer.MAX_VALUE;
+        }
     }
 
     /**
@@ -32,7 +40,11 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        for (int i = 1; i < order; i++) {
+            n = n / 10;
+        }
+        return Math.abs(n) % 10;
+
     }
 
 
@@ -41,6 +53,10 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        long result = 1;
+        for (byte i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
