54da3620699c7b84a7376ea1aeb9196907e51d7f
Nikita Dergunov <dergunov.ns@edu.spbstu.ru>
2021-03-21 01:21:28 +0300

    Delete extra casts, change int yo long, fixed CG, optimization snake.

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -4,47 +4,40 @@ import java.util.function.ToDoubleFunction;
 
 public class HomeworkTask {
 
+    /**
+     * Нужно численно посчитать интеграл от a до b с шагом delta от функции function
+     * Для того, что бы получить значение по Y для точки X, надо просто написать function.applyAsDouble(t)
+     * Считаем, что функция определена на всем пространстве от a до b
+     */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        double res = 0;
-
-        for (double x = a; x <= b; x += delta) {
-            res += delta * function.applyAsDouble(x);
-        }
-
-        return res;
+        function.applyAsDouble(4d);
+        return 0;
     }
 
-    public static final byte MAX_NUMERAL = 9;
-
+    /**
+     * Вывести номер максимальной цифры. Счет начинается слева направо,
+     * выводим номер первой максимальной цифры (если их несколько)
+     */
     public static byte maxNumber(long a) {
-        byte indexOfMaxNumeral = 0;
-        byte maxNumeral = 0;
-        char[] numberString = Long.toString(a).toCharArray();
-        for (byte i = 0; i < numberString.length; i++) {
-            byte nowNumeral = Byte.parseByte(String.valueOf(numberString[i]));
-            if (nowNumeral > maxNumeral) {
-                maxNumeral = nowNumeral;
-                indexOfMaxNumeral = i;
-                if (maxNumeral == MAX_NUMERAL) {
-                    return (byte) (indexOfMaxNumeral + 1);
-                }
-            }
-        }
-        return (byte) (indexOfMaxNumeral + 1);
+        return 0;
     }
 
+
+    /**
+     * Даны две точки в пространстве (x1, y1) и (x2, y2). Вам нужно найти Y координату третьей точки (x3, y3),
+     * которая находится на той же прямой что и первые две.
+     */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        double equationForX3 = ((double) x3 - (double) x1) / ((double) x2 - (double) x1);
-        return equationForX3 * ((double) y2 - (double) y1) + (double) y1;
+        return 0;
     }
 
+    /**
+     * Даны 4 точки в пространстве A(x1, y1), B(x2, y2), C(x3, y3), D(x4, y4). Найдите площадь выпуклого
+     * четырехуголька ABCD.
+     * Это дополнительное задание, необязательное для выполнения
+     */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        double firstTerm = x1 * y2 - y1 * x2;
-        double secondTerm = x2 * y3 - y2 * x3;
-        double thirdTerm = x3 * y4 - y3 * x4;
-        double forthTerm = x4 * y1 - y4 * x1;
-
-        return Math.abs(firstTerm + secondTerm + thirdTerm + forthTerm) / 2;
+        return 0;
     }
 
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -1,46 +1,46 @@
 package ru.mail.polis.homework.simple;
 
 
+/**
+ * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
+ * Math.sqrt(1.44)
+ * Чтобы увидеть все методы класса Math, достаточно написать Math. и среда вам сама покажет возможные методы.
+ * Для просмотра подробной документации по выбранному методу нажмите Ctrl + q
+ */
 public class IntegerTask {
 
+    /**
+     * Сумма чисел от 1 до n (1 + 2 + 3 + ... + n)
+     * Пример: (5) -> 15
+     */
     public static int sum(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+        return 0;
     }
 
+    /**
+     * Гусеница поднимается по стене длиной height на высоту top за день, ночью гусеница сползает на bottom.
+     * Сколько дней понадобится гусенице, чтобы доползти до потолка. Если она этого никогда не сможет сделать,
+     * Верните число Integer.MAX_VALUE;
+     * Пример: (10, 3, 2) -> 8
+     */
     public static int snake(int height, int top, int bottom) {
-        int days = 0;
-        int nowHeight = 0;
-        while (nowHeight < height) {
-            days++;
-            nowHeight += top;
-            if (nowHeight >= height) {
-                return days;
-            }
-            nowHeight -= bottom;
-            if (nowHeight <= 0) {
-                return Integer.MAX_VALUE;
-            }
-        }
-        return days;
+        return 0;
     }
 
+    /**
+     * Дано число n и номер разряда order. Выведите цифру стояющую на нужном разряде
+     * Пример: (454355, 3) -> 3
+     */
     public static int kDecimal(int n, int order) {
-        int forRemainder = (int) Math.pow(10, order);
-        int forExcretion = (int) Math.pow(10, order - 1);
-        return Math.abs(n) % forRemainder / forExcretion;
+        return 0;
     }
 
 
+    /**
+     * Выведите факториал от числа n
+     * Пример: (5) -> 120
+     */
     public static long factorial(byte n) {
-        long factorial = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial *= i;
-        }
-        return factorial;
+        return 0;
     }
-
 }
