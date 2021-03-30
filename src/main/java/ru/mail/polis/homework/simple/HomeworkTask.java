aab97ec521fb56706173c8c3434aa640fb6f3fb2
Denis <denis_gubrienko2002@mail.ru>
2021-03-30 13:22:33 +0300

    Сделал 1-ое домашнее задание

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -10,7 +10,11 @@ public class HomeworkTask {
      * Считаем, что функция определена на всем пространстве от a до b
      */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        return 0;
+        double sum = 0;
+        for (; a <= b; a += delta) {
+            sum += function.applyAsDouble(a) * (delta);
+        }
+        return sum;
     }
 
     /**
@@ -18,7 +22,20 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
+
+        int length = String.valueOf(Math.abs(a)).length();
+        byte degree_max_number = 1;
+        int number;
+        int top = 0;
+        for (byte i = 1; (length >= 0); length--) {
+            number = (int) (a / Math.pow(10, length - 1)) % 10;
+            if (number > top) {
+                top = number;
+                degree_max_number = i;
+            }
+            i++;
+        }
+        return degree_max_number;
     }
 
 
@@ -27,7 +44,7 @@ public class HomeworkTask {
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        return ((((x3 - x1) / (double) (x2 - x1)) * (y2 - y1)) + y1);
     }
 
     /**
@@ -36,7 +53,19 @@ public class HomeworkTask {
      * Это дополнительное задание, необязательное для выполнения
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
-    }
+        double side1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        double side2 = Math.sqrt(Math.pow((x3 - x2), 2) + Math.pow((y3 - y2), 2));
+        double side3 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+        double side4 = Math.sqrt(Math.pow((x1 - x4), 2) + Math.pow((y1 - y4), 2));
 
+        double diagonal = Math.sqrt(Math.pow((x4 - x2), 2) + Math.pow((y4 - y2), 2));
+
+        double half_meter_firstTriangle = (side1 + side4 + diagonal) * 0.5;
+        double half_meter_secondTriangle = (side3 + side2 + diagonal) * 0.5;
+
+        double area_firstTriangle = Math.sqrt(half_meter_firstTriangle * (half_meter_firstTriangle - side1) * (half_meter_firstTriangle - side4) * (half_meter_firstTriangle - diagonal));
+        double area_secondTriangle = Math.sqrt(half_meter_secondTriangle * (half_meter_secondTriangle - side3) * (half_meter_secondTriangle - side2) * (half_meter_secondTriangle - diagonal));
+
+        return (area_firstTriangle + area_secondTriangle);
+    }
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -1,6 +1,8 @@
 package ru.mail.polis.homework.simple;
 
 
+import org.w3c.dom.ls.LSOutput;
+
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
@@ -14,7 +16,12 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += i;
+            sum++;
+        }
+        return sum;
     }
 
     /**
@@ -24,7 +31,18 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        int i = 1;
+        int progress = top;
+        if ((bottom > height - top) || (top <= bottom)) {
+            if (top >= height) {
+                return i;
+            }
+            return Integer.MAX_VALUE;
+        }
+        for (; (height - progress) > 0; i++) {
+            progress = progress + top - bottom;
+        }
+        return i;
     }
 
     /**
@@ -32,15 +50,18 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        return (int) (Math.abs(n) / Math.pow(10, order - 1)) % 10;
     }
-
 
     /**
      * Выведите факториал от числа n
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        long top = 1;
+        for (int i = 1; i <= n; i++) {
+            top = top * i;
+        }
+        return top;
     }
 }
