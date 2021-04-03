6ebfc9c58dc16aafa51d79b2bee7932bbf265f66
mishka361 <elotomp@gmail.com>
2021-04-03 21:57:16 +0300

    Homework 2 full

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -9,10 +11,28 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копиии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        if (count > array.length) {
+            return null;
+        }
+        if (count == 0) {
+            return new int[0];
+        }
+        int[] result = new int[count];
+        Arrays.fill(result, Integer.MIN_VALUE);
+        for (int number : array) {
+            if (number > result[count - 1]) {
+                result[count - 1] = number;
+                for (int i = count - 1; i > 0; i--) {
+                    if (result[i] > result[i - 1]) {
+                        int temp = result[i];
+                        result[i] = result[i-1];
+                        result[i-1] = temp;
+                    }
+                }
+            }
+        }
+        return result;
     }
-
 }
--- src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
+++ src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
@@ -13,7 +13,26 @@ import java.util.Objects;
 public class RepeatingCharacters {
 
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
-        return new Pair<>('s', 4);
+        if (str == null || str.isEmpty()) {
+            return  null;
+        }
+        char[] symbols = str.toCharArray();
+        char maxFrequent = symbols[0];
+        int sequenceLength = 1;
+        int maxSequenceLength = 1;
+        for (int i = 1; i < symbols.length; i++) {
+            if (symbols[i] == symbols[i-1] ) {
+                sequenceLength++;
+            }
+            if (symbols[i] != symbols[i-1] || i == symbols.length - 1){
+                if (sequenceLength > maxSequenceLength) {
+                    maxSequenceLength = sequenceLength;
+                    maxFrequent = symbols[i-1];
+                }
+                sequenceLength = 1;
+            }
+        }
+        return new Pair<>(maxFrequent, maxSequenceLength);
     }
 
     public static class Pair<T, V> {
