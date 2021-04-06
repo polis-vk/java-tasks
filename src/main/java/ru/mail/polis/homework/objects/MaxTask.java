4da09b890f3f65258bceea8d77e12cd5538f498a
Malina2001 <emil_verdeiv@mail.ru>
2021-04-06 14:36:59 +0300

    Вердиев Эмиль дз 2

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.lang.reflect.Array;
+
 public class MaxTask {
 
     /**
@@ -8,11 +10,45 @@ public class MaxTask {
      * Если длина массива меньше count, то вернуть null
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
-     * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
+     * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копиии
      *
+     * @return
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
-    }
+        if (Array.getLength(array) < count) {
+            return null;
+        }
 
+        int[] result = new int[count];
+        if (count == 0) {
+            return result;
+        }
+
+        int[] index = new int[count];
+        for (int h = 0; h < count; h++) {
+            index[h] = -1;
+        }
+
+        int pos = 0;
+        while (pos != count) {
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < Array.getLength(array); i++) {
+                int check = 0;
+                if (array[i] > max) {
+                    for (int j = 0; j < count; j++) {
+                        if (index[j] == i) {
+                            check = 1;
+                        }
+                    }
+                    if (check == 0) {
+                        max = array[i];
+                        index[pos] = i;
+                    }
+                }
+                result[pos] = max;
+            }
+            pos++;
+        }
+        return result;
+    }
 }
