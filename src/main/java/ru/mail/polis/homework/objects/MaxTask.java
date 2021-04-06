f578ae49f0495424a77f1ca79172a47991823dd5
Владимир Лебедев <goldswanru@gmail.com>
2021-04-07 01:45:43 +0300

    Все задания сделаны, все тесты проходят, постарался соблюдать оформление

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -9,10 +9,39 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
-    }
+        if (array.length < count) {
+            return null;
+        }
 
+        int[] origin = array;
+        int maxValue;
+        int minValue;
+        int indMaxValue;
+        int[] result = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            maxValue = origin[0];
+            minValue = origin[0];
+            indMaxValue = 0;
+
+            for (int j = 1; j < origin.length; j++) {
+
+                if (origin[j] > maxValue) {
+                    maxValue = origin[j];
+                    indMaxValue = j;
+                }
+
+                if (origin[j] < minValue) {
+                    minValue = origin[j];
+                }
+            }
+
+            result[i] = maxValue;
+            origin[indMaxValue] = minValue;
+        }
+
+        return result;
+    }
 }
--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -14,6 +14,162 @@ public class StringTasks {
      * У класса Character есть полезные методы, например Character.isDigit()
      */
     public static Number valueOf(String str) {
-        return null;
+
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int dotCount = 0;
+        int minusCount = 0;
+        int eCount = 0;
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '.' || ch == '-' || ch == 'e' || Character.isDigit(ch)) {
+                switch (ch) {
+                    case '.':
+                        dotCount++;
+                        break;
+                    case '-':
+                        minusCount++;
+                        break;
+                    case 'e':
+                        eCount++;
+                        break;
+                    default:
+                        break;
+                }
+
+                sb.append(ch);
+            }
+        }
+
+        String numString = sb.toString();
+
+        if (dotCount > 1 || eCount > 1 || minusCount > 2 ||
+                numString.contains("--") || numString.contains("-e") || numString.endsWith("e") ||
+                numString.contains(".e") || numString.contains("e.") || numString.endsWith("-")) {
+            return null;
+        }
+
+        if (dotCount + eCount > 0) {
+            return parseDouble(numString);
+        }
+
+        long longNumber = parseLong(numString);
+
+        if (longNumber > Integer.MAX_VALUE || longNumber < Integer.MIN_VALUE) {
+            return longNumber;
+        }
+
+        return (int) longNumber;
+    }
+
+    private static long parseLong(String numString) {
+        long result = 0;
+        int num;
+        boolean minusFlag = false;
+
+        for (int i = 0; i < numString.length(); i++) {
+
+            num = parseChar(numString.charAt(i));
+
+            if (num >= 0) {
+                result += num * Math.pow(10, numString.length() - i - 1);
+            } else {
+                minusFlag = !minusFlag;
+            }
+        }
+
+        if (minusFlag) {
+            result *= -1;
+        }
+
+        return result;
+    }
+
+    private static double parseDouble(String numString) {
+        double result = 0;
+        int num;
+        boolean minusFlag = false;
+
+        String[] parts = numString.split("\\.|e");
+
+        for (int i = 0; i < parts[0].length(); i++) {
+            num = parseChar(parts[0].charAt(i));
+
+            if (num >= 0) {
+                result += num * Math.pow(10, parts[0].length() - i - 1);
+            } else {
+                minusFlag = !minusFlag;
+            }
+        }
+
+        if (minusFlag) {
+            result *= -1;
+        }
+
+        if (numString.contains(".")) {
+            for (int i = 0; i < parts[1].length(); i++) {
+                num = parseChar(parts[1].charAt(i));
+
+                if (num >= 0) {
+                    result += num * Math.pow(10, -i - 1);
+                }
+            }
+        }
+
+        if (numString.contains("e")) {
+            int pow = 0;
+            minusFlag = false;
+
+            for (int i = 0; i < parts[parts.length - 1].length(); i++) {
+                num = parseChar(parts[parts.length - 1].charAt(i));
+
+                if (num >= 0) {
+                    pow += num * Math.pow(10, parts[parts.length - 1].length() - i - 1);
+                } else {
+                    minusFlag = !minusFlag;
+                }
+            }
+
+            if (minusFlag) {
+                pow *= -1;
+            }
+
+            result *= Math.pow(10, pow);
+        }
+
+        return result;
+    }
+
+    private static int parseChar(char ch) {
+        switch (ch) {
+            case '-':
+                return -1;
+            case '0':
+                return 0;
+            case '1':
+                return 1;
+            case '2':
+                return 2;
+            case '3':
+                return 3;
+            case '4':
+                return 4;
+            case '5':
+                return 5;
+            case '6':
+                return 6;
+            case '7':
+                return 7;
+            case '8':
+                return 8;
+            case '9':
+                return 9;
+            default:
+                return -999;
+        }
     }
 }
