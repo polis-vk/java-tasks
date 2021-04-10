9b5b296f68aa05601478bd1fd761b03eeea8a38f
galievaregina <regina-galieva-1996@mail.ru>
2021-04-10 23:51:40 +0300

    Revert "done Integer and Double tasks"
    This reverts commit 95d83e06

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -12,29 +12,7 @@ public class MaxTask {
      *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        int[] inputArray = array;
-        if (inputArray.length == 0 || count == 0) {
-            return new int[]{};
-        }
-        if (inputArray.length < count) {
-            return null;
-        }
-        int[] maxArr = new int[count];
-        int minFromMax = Integer.MAX_VALUE;
-        for (int j = 0; j < maxArr.length; j++) {
-            int max = Integer.MIN_VALUE;
-            int indexMax = 0;
-            for (int i = 0; i < inputArray.length; i++) {
-                if (inputArray[i] > max && inputArray[i] <= minFromMax) {
-                    max = inputArray[i];
-                    indexMax = i;
-                }
-            }
-            maxArr[j] = max;
-            inputArray[indexMax] = 0;
-            minFromMax = max;
-        }
-        return maxArr;
+        return null;
     }
 
 }
