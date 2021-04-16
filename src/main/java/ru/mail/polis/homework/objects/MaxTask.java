5e01f305fd7aad72cb724f48efa6d96f7809bd4e
galievaregina <regina-galieva-1996@mail.ru>
2021-04-16 22:49:42 +0300

    Revert "create repository again and add hw2"
    This reverts commit 222210e4

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -9,30 +9,10 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
+     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        if (array.length == 0 || count == 0) {
-            return new int[]{};
-        }
-        if (array.length < count) {
-            return null;
-        }
-        int[] inputArray = array.clone();
-        int[] maxArr = new int[count];
-        int minFromMax = Integer.MAX_VALUE;
-        for (int j = 0; j < maxArr.length; j++) {
-            int max = Integer.MIN_VALUE;
-            int indexMax = 0;
-            for (int i = 0; i < inputArray.length; i++) {
-                if (inputArray[i] > max && inputArray[i] <= minFromMax) {
-                    max = inputArray[i];
-                    indexMax = i;
-                }
-            }
-            maxArr[j] = max;
-            inputArray[indexMax] = Integer.MIN_VALUE;
-            minFromMax = max;
-        }
-        return maxArr;
+        return null;
     }
+
 }
