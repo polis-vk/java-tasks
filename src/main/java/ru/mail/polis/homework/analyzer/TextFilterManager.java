82ffb2312d4a516701f0f9f107e54201bc3e44b3
Nikita Dergunov <dergunov.ns@edu.spbstu.ru>
2021-04-17 04:45:11 +0300

    Comeback comments

--- src/main/java/ru/mail/polis/homework/analyzer/TextFilterManager.java
+++ src/main/java/ru/mail/polis/homework/analyzer/TextFilterManager.java
@@ -19,17 +19,17 @@ import java.util.Arrays;
  * (SPAM, TOO_LONG, NEGATIVE_TEXT, CUSTOM - в таком порядке) и возвращать тип с максимальным приоритетом.
  * Отсортировать фильтра можно с помощью функции
  * Arrays.sort(filter, (filter1, filter2) -> {
- * if (filter1 < filter2) {
- * return -1;
- * } else if (filter1 == filter2) {
- * return 0;
- * }
- * return 1;
+ *     if (filter1 < filter2) {
+ *         return -1;
+ *     } else if (filter1 == filter2) {
+ *         return 0;
+ *     }
+ *     return 1;
  * }
  * где вместо сравнение самих фильтров должно быть стравнение каких-то количественных параметров фильтра
  * <p>
  * 2 балла ( + 2 балла за доп приоритет)
- * ого 15 баллов + 2 дополнительных
+ * Итого 15 баллов + 2 дополнительных
  */
 public class TextFilterManager {
 
