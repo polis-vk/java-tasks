6a2da5ff1c013c4e8a04f3eeaafb02173128c1e2
Владимир Лебедев <goldswanru@gmail.com>
2021-04-24 23:10:33 +0300

    Revert "Все задания сделаны, все тесты проходят, постарался соблюдать оформление"
    This reverts commit f578ae49

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,7 +1,5 @@
 package ru.mail.polis.homework.objects;
 
-import java.util.Arrays;
-
 public class MaxTask {
 
     /**
@@ -11,34 +9,10 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
+     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        if (array.length < count) {
-            return null;
-        }
-
-        int[] origin = Arrays.copyOf(array, array.length);
-        int maxValue;
-        int minValue = Integer.MIN_VALUE;
-        int indMaxValue;
-        int[] result = new int[count];
-
-        for (int i = 0; i < count; i++) {
-            maxValue = origin[0];
-            indMaxValue = 0;
-
-            for (int j = 1; j < origin.length; j++) {
-
-                if (origin[j] > maxValue) {
-                    maxValue = origin[j];
-                    indMaxValue = j;
-                }
-            }
-
-            result[i] = maxValue;
-            origin[indMaxValue] = minValue;
-        }
-
-        return result;
+        return null;
     }
+
 }
--- src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
+++ src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
@@ -13,33 +13,7 @@ import java.util.Objects;
 public class RepeatingCharacters {
 
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
-
-        if (str == null || str.length() == 0) {
-            return null;
-        }
-
-        char[] origin = str.toCharArray();
-        int counter = 1;
-        char currChar = origin[0];
-        int maxCounter = 1;
-        char maxChar = origin[0];
-
-        for (int i = 1; i < origin.length; i++) {
-            if (currChar == origin[i]) {
-                counter++;
-            }
-
-            if (currChar != origin[i] || i + 1 == origin.length) {
-                if (counter > maxCounter) {
-                    maxCounter = counter;
-                    maxChar = currChar;
-                }
-                currChar = origin[i];
-                counter = 1;
-            }
-        }
-
-        return new Pair<>(maxChar, maxCounter);
+        return new Pair<>('s', 4);
     }
 
     public static class Pair<T, V> {
