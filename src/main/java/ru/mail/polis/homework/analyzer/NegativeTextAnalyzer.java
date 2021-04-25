7acaa12b6d50588f31af9a91695bcae682e11265
Daniil Shilov <shilov.da2001@mail.ru>
2021-04-25 15:34:28 +0300

    Add inheritance

--- src/main/java/ru/mail/polis/homework/analyzer/NegativeTextAnalyzer.java
+++ src/main/java/ru/mail/polis/homework/analyzer/NegativeTextAnalyzer.java
@@ -1,25 +1,15 @@
 package ru.mail.polis.homework.analyzer;
 
-public class NegativeTextAnalyzer implements TextAnalyzer {
+public class NegativeTextAnalyzer extends SpamAnalyzer {
     private static final FilterType FILTER_TYPE = FilterType.NEGATIVE_TEXT;
     private static final String[] NEGATIVE_TEXT = {"=(", ":(", ":|"};
 
-    protected static boolean contains(String string, String[] words) {
-        for (String word : words) {
-            if (string.contains(word)) {
-                return true;
-            }
-        }
-        return false;
+    public NegativeTextAnalyzer() {
+        super(NEGATIVE_TEXT);
     }
 
     @Override
     public FilterType filterType() {
         return FILTER_TYPE;
-    }
-
-    @Override
-    public boolean analyze(String text) {
-        return text != null && !text.isEmpty() && contains(text, NEGATIVE_TEXT);
     }
 }
--- src/main/java/ru/mail/polis/homework/analyzer/SpamAnalyzer.java
+++ src/main/java/ru/mail/polis/homework/analyzer/SpamAnalyzer.java
@@ -3,6 +3,15 @@ package ru.mail.polis.homework.analyzer;
 public class SpamAnalyzer implements TextAnalyzer {
     private static final FilterType FILTER_TYPE = FilterType.SPAM;
 
+    private static boolean contains(String string, String[] words) {
+        for (String word : words) {
+            if (string.contains(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private final String[] spam;
 
     public SpamAnalyzer(String[] spam) {
@@ -16,6 +25,6 @@ public class SpamAnalyzer implements TextAnalyzer {
 
     @Override
     public boolean analyze(String text) {
-        return text != null && !text.isEmpty() && NegativeTextAnalyzer.contains(text, spam);
+        return text != null && !text.isEmpty() && contains(text, spam);
     }
 }
