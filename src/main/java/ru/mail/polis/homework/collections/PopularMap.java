6e06957668f9a01756371a156aecd011d50dd511
Данил Терешин <tereshin.da@edu.spbstu.ru>
2021-05-01 19:47:40 +0300

    v1

--- src/main/java/ru/mail/polis/homework/collections/PopularMap.java
+++ src/main/java/ru/mail/polis/homework/collections/PopularMap.java
@@ -1,11 +1,7 @@
 package ru.mail.polis.homework.collections;
 
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 /**
@@ -14,20 +10,21 @@ import java.util.Set;
  * Популярность - это количество раз, который этот ключ/значение учавствовал/ло в других методах мапы, такие как
  * containsKey, get, put, remove (в качестве параметра и возвращаемого значения).
  * Считаем, что null я вам не передаю ни в качестве ключа, ни в качестве значения
- *
+ * <p>
  * Так же надо сделать итератор (подробности ниже).
- *
+ * <p>
  * Важный момент, вам не надо реализовывать мапу, вы должны использовать композицию.
  * Вы можете использовать любые коллекции, которые есть в java.
- *
+ * <p>
  * Помните, что по мапе тоже можно итерироваться
- *
- *         for (Map.Entry<K, V> entry : map.entrySet()) {
- *             entry.getKey();
- *             entry.getValue();
- *         }
- *
+ * <p>
+ * for (Map.Entry<K, V> entry : map.entrySet()) {
+ * entry.getKey();
+ * entry.getValue();
+ * }
+ * <p>
  * Всего 9 баллов (2 за общие методы, 6 за специальные)
+ *
  * @param <K> - тип ключа
  * @param <V> - тип значения
  */
@@ -45,62 +42,107 @@ public class PopularMap<K, V> implements Map<K, V> {
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
     public boolean containsKey(Object key) {
+        for (Entry<K, V> entry : map.entrySet()) {
+            if (entry.getKey() == key) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean containsValue(Object value) {
+        for (Entry<K, V> entry : map.entrySet()) {
+            if (entry.getValue() == value) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public V get(Object key) {
+        for (Entry<K, V> entry : map.entrySet()) {
+            if (entry.getKey() == key) {
+                return entry.getValue();
+            }
+        }
         return null;
     }
 
     @Override
     public V put(K key, V value) {
+        for (Entry<K, V> entry : map.entrySet()) {
+            if (entry.getKey() == key) {
+                V previousValue = entry.getValue();
+                map.put(key, value);
+                return previousValue;
+            }
+        }
         return null;
     }
 
     @Override
     public V remove(Object key) {
+        for (Entry<K, V> entry : map.entrySet()) {
+            if (entry.getKey() == key) {
+                V previousValue = entry.getValue();
+                map.remove(key);
+                return previousValue;
+            }
+        }
         return null;
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-        throw new UnsupportedOperationException("putAll");
+        for (Entry<? extends K, ? extends V> entry : m.entrySet()) {
+            map.put(entry.getKey(), entry.getValue());
+        }
     }
 
     @Override
     public void clear() {
-
+        for (Entry<K, V> entry : map.entrySet()) {
+            map.remove(entry.getKey());
+        }
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        Set<K> keys = new HashSet<>();
+        for (Entry<K, V> entry : map.entrySet()) {
+            keys.add(entry.getKey());
+        }
+        return keys;
     }
 
     @Override
     public Collection<V> values() {
-        return null;
+        List<V> values = new ArrayList<>();
+        for (Entry<K, V> entry : map.entrySet()) {
+            values.add(entry.getValue());
+        }
+        return values;
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        Set<Entry<K, V>> entries = new HashSet<>();
+        for (Entry<K, V> entry : map.entrySet()) {
+            entries.add(entry);
+        }
+        return entries;
     }
 
     /**
