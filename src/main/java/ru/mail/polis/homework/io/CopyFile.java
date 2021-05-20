438e811a301e2d32acc19657e7a6aef418d6accf
Robert Andreev <andreev.robert.a@gmail.com>
2021-05-21 01:43:07 +0300

    Make copyFiles

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 public class CopyFile {
 
     /**
@@ -9,8 +17,29 @@ public class CopyFile {
      *
      * 4 баллов
      */
-    public static String copyFiles(String pathFrom, String pathTo) {
-        return null;
-    }
 
+    public static String copyFiles(String pathFrom, String pathTo) {
+        Path fromPath = Paths.get(pathFrom);
+        Path toPath = Paths.get(pathTo);
+        if (Files.notExists(fromPath)) {
+            return null;
+        }
+        try {
+            Files.createDirectories(toPath.getParent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (Stream<Path> stream = Files.walk(fromPath)) {
+            stream.forEachOrdered(path -> {
+                try {
+                    Files.copy(path, toPath.resolve(fromPath.relativize(path)), REPLACE_EXISTING);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return toPath.toString();
+    }
 }
