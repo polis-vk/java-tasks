56d6eb32cab4f5fba79bd2113fcc0c87d2557752
Данил Терешин <tereshin.da@edu.spbstu.ru>
2021-05-23 22:58:35 +0300

    Edits to hw5

--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,11 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Directories {
 
@@ -13,7 +18,24 @@ public class Directories {
      * 2 балла
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        if (!file.exists()) {
+            return 0;
+        }
+
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        }
+
+        int filesCount = 0;
+        if (file.isDirectory()) {
+            for (File currentFile : file.listFiles()) {
+                filesCount += removeWithFile(currentFile.getPath());
+            }
+        }
+        file.delete();
+        return filesCount + 1;
     }
 
     /**
@@ -21,6 +43,25 @@ public class Directories {
      * 2 балла
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path currentPath = Paths.get(path);
+        if (!Files.exists(currentPath)) {
+            return 0;
+        }
+
+        if (Files.isRegularFile(currentPath)) {
+            Files.delete(currentPath);
+            return 1;
+        }
+
+        int filesCount = 0;
+        if (Files.isDirectory(currentPath)) {
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(currentPath)) {
+                for (Path tempDir : directoryStream) {
+                    filesCount += removeWithPath(tempDir.toString());
+                }
+            }
+        }
+        Files.delete(currentPath);
+        return filesCount + 1;
     }
 }
