8e7262c0d11eed74cba98707c3443b4ae36bd0d0
Artem <artem.yasevich@mail.ru>
2021-05-26 16:15:26 +0300

    Homework 5 some fixes

--- src/test/java/ru/mail/polis/homework/objects/SerializerTest.java
+++ src/test/java/ru/mail/polis/homework/objects/SerializerTest.java
@@ -13,24 +13,24 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class SerializerTest {
-    List<Animal> animals = Animal.randomListOfAnimals(10);
-    final String testFileName = "testSerializer.txt";
-    Serializer serializer = new Serializer();
+    private final List<Animal> animals = Animal.randomListOfAnimals(10);
+    private final static String TEST_FILE_NAME = "testSerializer.txt";
+    private final Serializer serializer = new Serializer();
 
     @Before
     public void setUp() throws IOException {
-        if (Files.notExists(Paths.get(testFileName))) {
-            Files.createFile(Paths.get(testFileName));
+        if (Files.notExists(Paths.get(TEST_FILE_NAME))) {
+            Files.createFile(Paths.get(TEST_FILE_NAME));
         }
     }
 
     @Test
     public void testDefaultSerializer() throws IOException {
         long start = System.currentTimeMillis();
-        serializer.defaultSerialize(animals, testFileName);
-        List<Animal> deserialized = serializer.defaultDeserialize(testFileName);
+        serializer.defaultSerialize(animals, TEST_FILE_NAME);
+        List<Animal> deserialized = serializer.defaultDeserialize(TEST_FILE_NAME);
         assertEquals(animals, deserialized);
-        long size = Files.size(Paths.get(testFileName));
+        long size = Files.size(Paths.get(TEST_FILE_NAME));
         long time = System.currentTimeMillis() - start;
         System.out.printf("Default: Size: %d bytes, Execution time: %d ms%n", size, time);
     }
@@ -38,16 +38,16 @@ public class SerializerTest {
     @Test
     public void testCustomSerializer() throws IOException {
         long start = System.currentTimeMillis();
-        serializer.customSerialize(animals, testFileName);
-        List<Animal> deserialized = serializer.customDeserialize(testFileName);
+        serializer.customSerialize(animals, TEST_FILE_NAME);
+        List<Animal> deserialized = serializer.customDeserialize(TEST_FILE_NAME);
         assertEquals(animals, deserialized);
-        long size = Files.size(Paths.get(testFileName));
+        long size = Files.size(Paths.get(TEST_FILE_NAME));
         long time = System.currentTimeMillis() - start;
         System.out.printf("Custom: Size: %d bytes, Execution time: %d ms%n", size, time);
     }
 
     @After
     public void tearDown() throws IOException {
-        Files.deleteIfExists(Paths.get(testFileName));
+        Files.deleteIfExists(Paths.get(TEST_FILE_NAME));
     }
 }
