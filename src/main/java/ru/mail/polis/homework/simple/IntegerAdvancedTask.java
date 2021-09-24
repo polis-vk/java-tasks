12c3d3fa2007a45d90a91c9770565a9f124e36ca
gedar0082 <gedar0082@gmail.com>
2021-09-24 14:08:38 +0300

    Solve IntegerAdvancedTask

--- src/main/java/ru/mail/polis/homework/simple/IntegerAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerAdvancedTask.java
@@ -16,7 +16,7 @@ public class IntegerAdvancedTask {
      * Пример: (1, 2, 3) -> 7
      */
     public static long progression(int a, double q, int n) {
-        return 0;
+        return (long) (a * (1 - Math.pow(q, n)) / (1 - q)); //geometric progression sum formula
     }
 
     /**
@@ -28,7 +28,20 @@ public class IntegerAdvancedTask {
      * Пример: (10, 3, 5, 5, 20, 11) -> 2
      */
     public static int snake(int up, int right, int down, int left, int grassX, int grassY) {
-        return 0;
+        int count = 1;
+        int currentX = 0;
+        int currentY = 0;
+        if (currentX + up >= grassY || currentY + right >= grassX) return count;
+        if(up-down <=0 && right - left <=0)return Integer.MAX_VALUE;
+        while(true){
+            if (currentX + up >= grassY || currentY + right >= grassX) return count;
+            else {
+                currentX += (up-down);
+                currentY += (right-left);
+                count++;
+            }
+        }
+
     }
 
     /**
@@ -38,7 +51,8 @@ public class IntegerAdvancedTask {
      * Пример: (454355, 2) -> D
      */
     public static char kDecimal(int n, int order) {
-        return 0;
+        String hexValue = Integer.toHexString(n).toUpperCase();
+        return hexValue.charAt(hexValue.length() - order);
     }
 
     /**
@@ -49,7 +63,16 @@ public class IntegerAdvancedTask {
      * (6726455) -> 2
      */
     public static byte minNumber(long a) {
-        return 0;
+        String hexValue = Long.toHexString(a).toUpperCase();
+        byte min = Byte.MAX_VALUE;
+        byte resIndex = 0;
+        for (int i = hexValue.length() - 1; i >= 0; i--) {
+            if (hexValue.charAt(i) < min) {
+                resIndex = (byte) (hexValue.length() - i);
+                min = (byte) hexValue.charAt(i);
+            }
+        }
+        return resIndex;
     }
 
 }
