ff30a6c7db0b141ed55495b4aac03e0fb7b07e98
LutsenkoDm <lutsenkodm@mail.ru>
2021-09-25 01:56:05 +0300

    Add first task

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.simple;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
@@ -17,10 +20,42 @@ public class DoubleAdvancedTask {
      * Пример: (1, -4, -7, 10) -> "-2.0, 1.0, 5.0"
      */
     public static String equation(int a, int b, int c, int d) {
-        double x1 = 0;
-        double x2 = 0;
-        double x3 = 0;
-        return x1 + ", " + x2 + ", " + x3;
+        double x1;
+        double x2;
+        double x3;
+        if (b == 0) {
+            if (c == 0 && d == 0) {
+                return 0.0 + ", " + 0.0 + ", " + 0.0;
+            }
+            //Формула из раздела "Неалгебраическое решение в терминах вещественных величин" отсюда: https://ru.wikipedia.org/wiki/Casus_irreducibilis
+            double p = (double) c / a;
+            double q = d / (27.0 * a);
+            double sqrtTerm = Math.sqrt(-p / 3.0);
+            double acosTerm = 1.0 / 3.0 * Math.acos(((3.0 * q) / (2.0 * p) * sqrtTerm));
+            x1 = 2 * sqrtTerm * Math.cos(acosTerm - (0 * 2 * Math.PI) / 3.0); //0 оставлен для того, чтобы было одинаково
+            x2 = 2 * sqrtTerm * Math.cos(acosTerm - (1 * 2 * Math.PI) / 3.0);
+            x3 = 2 * sqrtTerm * Math.cos(acosTerm - (2 * 2 * Math.PI) / 3.0);
+        } else if (d == 0) {
+            //Делим на аргумент и решаем как квадратное уравнение + нулевой корень
+            double discriminantSqrt = Math.sqrt(b * b - 4 * a * c);
+            x1 = (-b + discriminantSqrt) / (2 * a);
+            x2 = (-b - discriminantSqrt) / (2 * a);
+            x3 = 0;
+        } else {
+            //Тригонометрическая формула Виета: https://ru.wikipedia.org/wiki/Тригонометрическая_формула_Виета
+            double reducedB = (double) b / a;
+            double reducedC = c != 0 ? (double) c / a : 0;
+            double reducedD = (double) d / a;
+            double q = (reducedB * reducedB - 3 * reducedC) / 9.0;
+            double r = (2 * reducedB * reducedB * reducedB - 9 * reducedB * reducedC + 27 * reducedD) / 54.0;
+            double fi = 1.0 / 3.0 * Math.acos(r / Math.sqrt(q * q * q));
+            x1 = -2.0 * Math.sqrt(q) * Math.cos(fi) - reducedB / 3.0;
+            x2 = -2.0 * Math.sqrt(q) * Math.cos(fi + 2.0 * Math.PI / 3.0) - reducedB / 3.0;
+            x3 = -2.0 * Math.sqrt(q) * Math.cos(fi - 2.0 * Math.PI / 3.0) - reducedB / 3.0;
+        }
+        Double[] rootsArray = new Double[]{x1, x2, x3};
+        Arrays.sort(rootsArray, Comparator.reverseOrder());
+        return rootsArray[0] + ", " + rootsArray[1] + ", " + rootsArray[2];
     }
 
     /**
@@ -29,6 +64,9 @@ public class DoubleAdvancedTask {
      * (0, 1, 0, 5) -> 4
      */
     public static float length(double a1, double b1, double a2, double b2) {
+        if (a1 == a2) {
+            return (float) (Math.abs(b2 - b1) * Math.cos(Math.atan(a1)));
+        }
         return 0;
     }
 
@@ -44,6 +82,14 @@ public class DoubleAdvancedTask {
                                          int x2, int y2, int z2,
                                          int x3, int y3, int z3,
                                          int x4, int y4) {
-        return 0;
+        /* |(x4 - x1) (y4 - y1) (z4 - z1)|
+           |(x2 - x1) (y2 - y1) (z2 - z1)| = 0
+           |(x3 - x1) (y3 - y1) (z3 - z1)| */
+        double knownDeterminantSum =
+                (x4 - x1) * (y2 - y1) * (z3 - z1)
+                        + (y4 - y1) * (z2 - z1) * (x3 - x1)
+                        - (x4 - x1) * (z2 - z1) * (y3 - y1)
+                        - (y4 - y1) * (x2 - x1) * (z3 - z1);
+        return -knownDeterminantSum / ((x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)) + z1;
     }
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerAdvancedTask.java
@@ -1,6 +1,8 @@
 package ru.mail.polis.homework.simple;
 
 
+import java.util.Locale;
+
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
@@ -10,13 +12,11 @@ package ru.mail.polis.homework.simple;
 public class IntegerAdvancedTask {
 
     /**
-     * Сумма первых n-членов геометрической прогрессии с первым элементом a и множителем r
-     * a + aq + aq^2 + ... + aq^(n-1)
-     *
+     * Сумма элементов геометрической прогрессии с 1 элементом a и множителем r (a + aq + aq^2 + ... + aq^n)
      * Пример: (1, 2, 3) -> 7
      */
     public static long progression(int a, double q, int n) {
-        return 0;
+        return (long) (a * (1 - Math.pow(q, n)) / (1 - q));
     }
 
     /**
@@ -24,11 +24,23 @@ public class IntegerAdvancedTask {
      * сначала наверх на up, потом направо на right. Ночью она двигается вниз на down и налево на left.
      * Сколько суток понадобится гусенице, чтобы доползти до правого верхнего поля с травой?
      * Если она этого никогда не сможет сделать, Верните число Integer.MAX_VALUE;
-     * Считаем, что на каждой клетке с координатами >= grassX или >= grassY находится трава
+     * Считаем, что на каждой клетке с координатами >= grassX и >= grassY находится трава
      * Пример: (10, 3, 5, 5, 11, 20) -> 2
      */
     public static int snake(int up, int right, int down, int left, int grassX, int grassY) {
-        return 0;
+        if (up >= grassY || right >= grassX) {
+            return 1;
+        }
+        if (down >= up && left >= right) {
+            return Integer.MAX_VALUE;
+        }
+        int step1 = up - down;
+        int step2 = right - left;
+        int heightBeforeLastClimb = grassY - up;
+        int widthBeforeLastClimb = grassX - right;
+        int daysUp = heightBeforeLastClimb % step1 == 0 ? heightBeforeLastClimb / step1 + 1 : heightBeforeLastClimb / step1 + 2;
+        int daysRight = widthBeforeLastClimb % step2 == 0 ? widthBeforeLastClimb / step2 + 1 : widthBeforeLastClimb / step2 + 2;
+        return Integer.max(daysUp, daysRight);
     }
 
     /**
@@ -37,7 +49,8 @@ public class IntegerAdvancedTask {
      * Пример: (454355, 2) -> D
      */
     public static char kDecimal(int n, int order) {
-        return 0;
+        String nStr = Integer.toHexString(n).toUpperCase(Locale.ROOT);
+        return nStr.charAt(nStr.length() - order);
     }
 
     /**
@@ -47,7 +60,9 @@ public class IntegerAdvancedTask {
      * (6726455) -> 2
      */
     public static byte minNumber(long a) {
-        return 0;
+        String aStr = Long.toHexString(a);
+        int minNumber = aStr.chars().min().getAsInt();
+        return (byte) (aStr.length() - aStr.lastIndexOf(Character.toString(minNumber)));
     }
 
 }
