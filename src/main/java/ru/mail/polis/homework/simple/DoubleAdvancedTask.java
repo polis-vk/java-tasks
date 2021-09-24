ec5aff4ad658ddc4006eece54235472f9b6b2b14
Евгений Морозов <windrag000@gmail.com>
2021-09-24 18:04:16 +0300

    DoubleTasks complete

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.simple;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
@@ -17,9 +20,44 @@ public class DoubleAdvancedTask {
      * Пример: (1, -4, -7, 10) -> "-2.0, 1.0, 5.0"
      */
     public static String equation(int a, int b, int c, int d) {
-        double x1 = 0;
-        double x2 = 0;
-        double x3 = 0;
+        // Тригонометрическая формула Виета
+        double x1, x2, x3;
+        double at = (double) b / a;
+        double bt = (double) c / a;
+        double ct = (double) d / a;
+        double q = (at * at - 3 * bt) / 9;
+        double r = (2 * at * at * at - 9 * at * bt + 27 * ct) / 54;
+        // Умножаем q на a^2, r на а^3 и изменяем формулу s на q^3*4 - r^2 чтобы s было целочисленным.
+        BigInteger ab = BigInteger.valueOf(a);
+        BigInteger bb = BigInteger.valueOf(b);
+        BigInteger cb = BigInteger.valueOf(c);
+        BigInteger db = BigInteger.valueOf(d);
+        BigInteger qd = bb.pow(2).subtract(ab.multiply(cb).multiply(BigInteger.valueOf(3)));
+        BigInteger rd = BigInteger.valueOf(2).multiply(bb.pow(3))
+                .subtract(BigInteger.valueOf(9).multiply(bb).multiply(cb).multiply(ab))
+                .add(BigInteger.valueOf(27).multiply(db).multiply(ab.pow(2)));
+        BigInteger s = qd.pow(3).multiply(BigInteger.valueOf(4)).subtract(rd.pow(2));
+        double[] x = new double[3];
+        if (s.signum() == 1) {
+            double fi = Math.acos(r / Math.pow(q, 1.5)) / 3;
+            x[0] = -2 * Math.sqrt(q) * Math.cos(fi) - at / 3;
+            x[1] = -2 * Math.sqrt(q) * Math.cos(fi + 2 * Math.PI / 3) - at / 3;
+            x[2] = -2 * Math.sqrt(q) * Math.cos(fi - 2 * Math.PI / 3) - at / 3;
+        } else if (s.signum() == -1) {
+            double archArg = Math.abs(r) / Math.pow(Math.abs(q), 1.5);
+            double fi = Math.log(archArg + Math.sqrt(archArg * archArg - 1));
+            x[0] = -2 * Math.signum(r) * Math.sqrt(Math.abs(q)) * Math.cosh(fi) - at / 3;
+            x[1] = x[0];
+            x[2] = x[0];
+        } else {
+            x[0] = -2 * Math.cbrt(r) - at / 3;
+            x[1] = Math.cbrt(r) - at / 3;
+            x[2] = x[1];
+        }
+        Arrays.sort(x);
+        x1 = x[2];
+        x2 = x[1];
+        x3 = x[0];
         return x1 + ", " + x2 + ", " + x3;
     }
 
@@ -29,7 +67,11 @@ public class DoubleAdvancedTask {
      * (0, 1, 0, 5) -> 4
      */
     public static float length(double a1, double b1, double a2, double b2) {
-        return 0;
+        if (a1 == a2) {
+            return (float) (Math.abs(b2 - b1) / Math.sqrt(a1 * a1 + 1));
+        } else {
+            return 0f;
+        }
     }
 
     /**
@@ -44,6 +86,11 @@ public class DoubleAdvancedTask {
                                          int x2, int y2, int z2,
                                          int x3, int y3, int z3,
                                          int x4, int y4) {
-        return 0;
+        int a, b, c, d;
+        a = (y2 - y1) * (z3 - z1) - (y3 - y1) * (z2 - z1);
+        b = (x3 - x1) * (z2 - z1) - (x2 - x1) * (z3 - z1);
+        c = (x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1);
+        d = -a * x1 - b * y1 - c * z1;
+        return (double) (a * x4 + b * y4 + d) / -c;
     }
 }
