4d710ce1e628ef3d0162cc198c10dbdd92dd45a2
Orlova Polina <mau7777777@ya.ru>
2021-09-30 18:25:45 +0300

    only one missing

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.simple;
 
+import java.util.Arrays;
+
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
@@ -16,12 +18,40 @@ public class DoubleAdvancedTask {
      * Если используете какой-то конкретный способ, напишите какой.
      * Пример: (1, -4, -7, 10) -> "-2.0, 1.0, 5.0"
      */
+    //use general cubic formula
     public static String equation(int a, int b, int c, int d) {
         double x1 = 0;
         double x2 = 0;
         double x3 = 0;
 
-        return x1 + ", " + x2 + ", " + x3;
+        double delta0 = Math.pow(b, 2) - 3 * a * c;
+        double delta1 = 2 * Math.pow(b, 3) - 9 * a * b * c + 27 * Math.pow(a, 2) * d;
+        double c1 = Math.pow((delta0 + Math.abs(Math.pow(delta0, 2) - 4 * Math.pow(delta1, 3))) / 2.0, 1 / 3.0);
+        double c2 = Math.pow((delta0 - Math.abs(Math.pow(delta0, 2) - 4 * Math.pow(delta1, 3))) / 2.0, 1 / 3.0);
+
+        double k1 = (-1 + Math.sqrt(-3.0)) / 2.0;
+        double k2 = (-1 - Math.sqrt(-3.0)) / 2.0;
+
+        if (c1 == 0 && c2 == 0){
+            x1 = (- 1.0 / 3 * a) * b;
+
+        } else if (c1 == 0){
+            x1 = (- 1.0 / 3 * a) * (b + k1 * c2 + delta0 / (k1 * c2));
+            x2 = (- 1.0 / 3 * a) * (b + k2 * c2 + delta0 / (k2 * c2));
+        } else {
+
+        }
+
+
+
+
+
+
+
+        double[] xes = {x1, x2, x3};
+        Arrays.sort(xes);
+
+        return xes[2] + ", " + xes[1] + ", " + xes[0];
     }
 
     /**
@@ -49,6 +79,22 @@ public class DoubleAdvancedTask {
                                          int x2, int y2, int z2,
                                          int x3, int y3, int z3,
                                          int x4, int y4) {
-        return 0;
+        //vector PQ
+        double a1 = x2 - x1;
+        double b1 = y2 - y1;
+        double c1 = z2 - z1;
+        //vector PR
+        double a2 = x3 - x1;
+        double b2 = y3 - y1;
+        double c2 = z3 - z1;
+
+        //умножение векторов
+        double a = b1 * c2 - b2 * c1;
+        double b = a2 * c1 - a1 * c2;
+        double c = a1 * b2 - b1 * a2;
+        double d = (-a * x1 - b * y1 - c * z1);
+
+        // a*x + b*y + c*z + d = 0
+        return (-a * x4 - b * y4 - d) / c;
     }
 }
