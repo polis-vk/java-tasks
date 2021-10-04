cdc7700c55acf41cccf3ca4f8e0cebf33f15fac8
LutsenkoDm <55806143+LutsenkoDm@users.noreply.github.com>
2021-10-04 23:01:22 +0300

    Add CustomLinkedList implementation

--- src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
@@ -14,7 +14,16 @@ public class CustomLinkedList {
      * @param value - data for create Node.
      */
     public void add(int value) {
-
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node tempNode = head;
+            while (tempNode.next != null) {
+                tempNode = tempNode.next;
+            }
+            tempNode.setNext(newNode);
+        }
     }
 
     /**
@@ -24,33 +33,74 @@ public class CustomLinkedList {
      *
      * @param index - position what element need remove.
      */
-    public void removeElement(int index) {
 
+    public void removeElement(int index) {
+        if (head == null || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < index - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            if (tempNode.next == null) {
+                throw new IndexOutOfBoundsException();
+            }
+            tempNode.setNext(tempNode.next.next);
+        }
     }
 
     /**
      * Реализовать метод:
      * Переварачивает все элементы списка.
      * Пример:
-     *  Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
-     *  После исполнения метода последовательность должа быть такой "4 -> 3 -> 2 -> 1 -> null"
+     * Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
+     * После исполнения метода последовательность должа быть такой "4 -> 3 -> 2 -> 1 -> null"
      */
     public void revertList() {
+        if (head != null && head.next != null) {
+            Node tempNode = head;
+            while (tempNode.next != null) {
+                tempNode = tempNode.next;
+            }
+            revertTo(null);
+            head = tempNode;
+        }
+    }
 
+    private void revertTo(Node endNode) {
+        if (endNode == head) {
+            endNode.setNext(null);
+            return;
+        }
+        Node tempNode = head;
+        while (tempNode.next.next != endNode) {
+            tempNode = tempNode.next;
+        }
+        tempNode.next.setNext(tempNode);
+        revertTo(tempNode.next);
     }
 
     /**
      * Метод выводит всю последовательность хранящуюся в списке начиная с head.
      * Формат вывода:
-     *  - значение каждой Node должно разделяться " -> "
-     *  - последовательность всегда заканчивается на null
-     *  - если в списке нет элементов - верните строку "null"
+     * - значение каждой Node должно разделяться " -> "
+     * - последовательность всегда заканчивается на null
+     * - если в списке нет элементов - верните строку "null"
      *
      * @return - String with description all list
      */
     @Override
     public String toString() {
-        return "1 -> 2 -> 3 -> null";
+        StringBuilder stringBuilder = new StringBuilder();
+        Node tempNode = head;
+        while (tempNode != null) {
+            stringBuilder.append(tempNode.value).append(" -> ");
+            tempNode = tempNode.next;
+        }
+        return stringBuilder.append("null").toString();
     }
 
     private static class Node {
