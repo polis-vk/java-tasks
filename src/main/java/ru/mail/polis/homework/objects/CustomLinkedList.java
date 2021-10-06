d28fdfab0a93a7925dd582469b3e6f7ab8a7fe03
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2021-10-06 17:47:19 +0300

    Homework 2

--- src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
@@ -6,6 +6,7 @@ package ru.mail.polis.homework.objects;
 public class CustomLinkedList {
 
     private Node head;
+    private int elCount = 0;
 
     /**
      * Реализовать метод:
@@ -14,7 +15,16 @@ public class CustomLinkedList {
      * @param value - data for create Node.
      */
     public void add(int value) {
-
+        if (head == null) {
+            head = new Node(value);
+        } else {
+            Node last = head;
+            while (last.next != null) {
+                last = last.next;
+            }
+            last.next = new Node(value);
+        }
+        elCount++;
     }
 
     /**
@@ -25,32 +35,67 @@ public class CustomLinkedList {
      * @param index - position what element need remove.
      */
     public void removeElement(int index) {
-
+        if (index < 0 || index > elCount - 1) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node cur = head;
+        if (index == 0) {
+            if (cur.next != null) {
+                head = cur.next;
+            } else {
+                head = null;
+            }
+        } else {
+            for (int i = 0; i < index - 1; i++) {
+                cur = cur.next;
+            }
+            if (index == elCount - 1) {
+                cur.next = null;
+            } else {
+                cur.next = cur.next.next;
+            }
+        }
+        elCount--;
     }
 
     /**
      * Реализовать метод:
      * Переварачивает все элементы списка.
      * Пример:
-     *  Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
-     *  После исполнения метода последовательность должа быть такой "4 -> 3 -> 2 -> 1 -> null"
+     * исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
+     * После исполнения метода последовательность должа быть такой "4 -> 3 -> 2 -> 1 -> null"
      */
     public void revertList() {
-
+        Node cur = head;
+        Node prev = null;
+        Node next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head = prev;
     }
 
     /**
      * Метод выводит всю последовательность хранящуюся в списке начиная с head.
      * Формат вывода:
-     *  - значение каждой Node должно разделяться " -> "
-     *  - последовательность всегда заканчивается на null
-     *  - если в списке нет элементов - верните строку "null"
+     * - значение каждой Node должно разделяться " -> "
+     * - последовательность всегда заканчивается на null
+     * - если в списке нет элементов - верните строку "null"
      *
      * @return - String with description all list
      */
     @Override
     public String toString() {
-        return "1 -> 2 -> 3 -> null";
+        StringBuilder buf = new StringBuilder();
+        Node cur = head;
+        for (int i = 0; i < elCount; i++) {
+            buf.append(cur.value).append(" -> ");
+            cur = cur.next;
+        }
+        return buf + "null";
     }
 
     private static class Node {
