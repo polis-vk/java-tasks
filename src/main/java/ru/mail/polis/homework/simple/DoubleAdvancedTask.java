8fc4507b23bb37098dcbf31df909b6dd59e77da6
DenZhid <denis.zhidkov01@gmail.com>
2021-10-06 15:05:41 +0300

    Removed unnecessary import

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -1,7 +1,5 @@
 package ru.mail.polis.homework.simple;
 
-import java.util.Arrays;
-
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
