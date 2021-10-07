d83c64a94690b02269e749191318f2fe1d996f2c
Alexandra-Tkachenko <91024312+Alexandra-Tkachenko@users.noreply.github.com>
2021-10-08 02:26:20 +0300

    Tasks

--- src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
@@ -1,20 +1,23 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
- * Вам придется реализовать Iterable класс CustomArrayWrapper вместе с методами которые
- * могут возващать итераторы только по четным/нечетным позициям в массиве. Пример с классического
- * итератора можете взять из лекции. Обратите внимание что подсчет четного или нечетного элемента
+ * Вам придется реализовать Iterable класс CustomArrayWrapper вместе с методами, которые
+ * могут возвращать итераторы только по четным/нечетным позициям в массиве. Пример классического
+ * итератора можете взять из лекции. Обратите внимание, что подсчет четного или нечетного элемента
  * идет с человеческой точки зрения.
  * Пример:
- * дан массив [100, 0 ,100, 0, 100]
+ * Дан массив [100, 0, 100, 0, 100],
  * тогда все элементы со значением 100 имеют нечетную позицию, а элементы = 0 - четную.
  */
 public class CustomArrayWrapper implements Iterable<Integer> {
 
     private final int[] array;          // массив
-    private int position;               // следующая позиция куда будет вставлен элемент
+    private int position;               // следующая позиция, куда будет вставлен элемент
+    private int modCount;               // количество раз изменений массива, т.е. вызовов add и edit
 
     public CustomArrayWrapper(int size) {
         this.array = new int[size];
@@ -24,11 +27,13 @@ public class CustomArrayWrapper implements Iterable<Integer> {
         checkIndex(position);
         array[position] = value;
         position++;
+        modCount++;
     }
 
     public void edit(int index, int value) {
         checkIndex(index);
         array[index] = value;
+        modCount++;
     }
 
     public int get(int index) {
@@ -42,33 +47,33 @@ public class CustomArrayWrapper implements Iterable<Integer> {
 
     /**
      * Реализовать метод:
-     * Возврящает обычный итератор.
+     * Возвращает обычный итератор.
      *
      * @return default Iterator
      */
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iter();
     }
 
     /**
      * Реализовать метод:
-     * Возвращает итератор который проходит только четные элементы.
+     * Возвращает итератор, который проходит только четные элементы.
      *
      * @return Iterator for EVEN elements
      */
     public Iterator<Integer> evenIterator() {
-        return null;
+        return new Iter(1);
     }
 
     /**
      * Реализовать метод:
-     * Возвращает итератор который проходит нечетные элементы
+     * Возвращает итератор, который проходит нечетные элементы
      *
      * @return Iterator for ODD elements
      */
     public Iterator<Integer> oddIterator() {
-        return null;
+        return new Iter(0);
     }
 
     private void checkIndex(int index) {
@@ -77,4 +82,35 @@ public class CustomArrayWrapper implements Iterable<Integer> {
         }
     }
 
+    private class Iter implements Iterator<Integer> {
+        private int position;
+        private int fixedModCount = modCount;
+        private boolean isDefault;
+
+        public Iter() {
+            this.isDefault = true;
+        }
+
+        public Iter(int position) {
+            this.position = position;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return position < array.length;
+        }
+
+        @Override
+        public Integer next() {
+            if (fixedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (position >= array.length) {
+                throw new NoSuchElementException();
+            }
+            int step = isDefault ? 1 : 2;
+            position += step;
+            return array[position - step];
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
@@ -1,7 +1,9 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.LinkedList;
+
 /**
- * Реализовать все методы односвязанного списка.
+ * Реализовать все методы односвязного списка.
  */
 public class CustomLinkedList {
 
@@ -9,52 +11,86 @@ public class CustomLinkedList {
 
     /**
      * Реализовать метод:
-     * Добавляет элемент в односвязны список.
+     * Добавляет элемент в односвязный список.
      *
      * @param value - data for create Node.
      */
     public void add(int value) {
-
+        if (head == null) {
+            head = new Node(value);
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node(value);
+        }
     }
 
     /**
      * Реализовать метод:
-     * Удаляет элемент в указанной позиции, при это связывая его соседние элементы друг с другом.
+     * Удаляет элемент в указанной позиции, при этом связывая его соседние элементы друг с другом.
      * Если был передан невалидный index - надо выкинуть исключение IndexOutOfBoundsException.
      *
      * @param index - position what element need remove.
      */
     public void removeElement(int index) {
-
+        if (index < 0 || head == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node current = head;
+        for (int currentIndex = 0; current.next != null; currentIndex++, current = current.next) {
+            if (currentIndex + 1 == index) {
+                current.next = current.next.next;
+                return;
+            }
+        }
+        throw new IndexOutOfBoundsException();
     }
 
     /**
      * Реализовать метод:
-     * Переварачивает все элементы списка.
+     * Переворачивает все элемента списка.
      * Пример:
-     *  Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
-     *  После исполнения метода последовательность должа быть такой "4 -> 3 -> 2 -> 1 -> null"
+     * исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
+     * После исполнения метода последовательность должна быть такой "4 -> 3 -> 2 -> 1 -> null"
      */
     public void revertList() {
-
+        if (head != null && head.next != null) {
+            Node reversedSlice = null;
+            for (Node current = head; current != null; ) {
+                Node next = current.next;
+                current.next = reversedSlice;
+                reversedSlice = current;
+                current = next;
+            }
+            head = reversedSlice;
+        }
     }
 
     /**
-     * Метод выводит всю последовательность хранящуюся в списке начиная с head.
+     * Метод выводит всю последовательность, хранящуюся в списке начиная с head.
      * Формат вывода:
-     *  - значение каждой Node должно разделяться " -> "
-     *  - последовательность всегда заканчивается на null
-     *  - если в списке нет элементов - верните строку "null"
+     * - значение каждой Node должно разделяться " -> "
+     * - последовательность всегда заканчивается на null
+     * - если в списке нет элементов - верните строку "null"
      *
      * @return - String with description all list
      */
     @Override
     public String toString() {
-        return "1 -> 2 -> 3 -> null";
+        if (head == null) {
+            return "null";
+        }
+        String res = "";
+        for (Node current = head; current != null; current = current.next) {
+            res += "" + current.value + (current.next != null ? " -> " : "");
+        }
+        return res + " -> null";
     }
 
     private static class Node {
-        private int value;
+        private final int value;
         private Node next;
 
         public Node(int value) {
