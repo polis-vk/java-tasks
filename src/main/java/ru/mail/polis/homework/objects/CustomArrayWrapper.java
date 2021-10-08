50b942de5094426c8632e3046adba8499447dfbc
radkoff26 <radko.slava@mail.ru>
2021-10-08 23:09:37 +0300

    homework week 2

--- src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
@@ -1,6 +1,9 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Вам придется реализовать Iterable класс CustomArrayWrapper вместе с методами которые
@@ -48,7 +51,34 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      */
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        // Суть initialArray в том, что он является копией изначального массива
+        // Каждая проверка на изменение основного массива во время итерации будет происходить с помощью него
+        return new Iterator<Integer>() {
+            int index = 0;
+            final int[] initialArray = Arrays.copyOf(array, array.length);
+
+            @Override
+            public boolean hasNext() {
+                if (!Arrays.equals(array, initialArray)) {
+                    throw new ConcurrentModificationException();
+                }
+
+                return index < initialArray.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (!Arrays.equals(array, initialArray)) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (index >= array.length) {
+                    throw new NoSuchElementException();
+                }
+
+                return array[index++];
+            }
+        };
     }
 
     /**
@@ -58,7 +88,35 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      * @return Iterator for EVEN elements
      */
     public Iterator<Integer> evenIterator() {
-        return null;
+        return new Iterator<Integer>() {
+            int index = 1;
+            final int[] initialArray = Arrays.copyOf(array, array.length);
+
+            @Override
+            public boolean hasNext() {
+                if (!Arrays.equals(array, initialArray)) {
+                    throw new ConcurrentModificationException();
+                }
+
+                return index < initialArray.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (!Arrays.equals(array, initialArray)) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (index >= array.length) {
+                    throw new NoSuchElementException();
+                }
+
+                int result = array[index];
+                index += 2;
+
+                return result;
+            }
+        };
     }
 
     /**
@@ -68,7 +126,35 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      * @return Iterator for ODD elements
      */
     public Iterator<Integer> oddIterator() {
-        return null;
+        return new Iterator<Integer>() {
+            int index = 0;
+            final int[] initialArray = Arrays.copyOf(array, array.length);
+
+            @Override
+            public boolean hasNext() {
+                if (!Arrays.equals(array, initialArray)) {
+                    throw new ConcurrentModificationException();
+                }
+
+                return index < initialArray.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (!Arrays.equals(array, initialArray)) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (index >= array.length) {
+                    throw new NoSuchElementException();
+                }
+
+                int result = array[index];
+                index += 2;
+
+                return result;
+            }
+        };
     }
 
     private void checkIndex(int index) {
--- src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
@@ -5,7 +5,9 @@ package ru.mail.polis.homework.objects;
  */
 public class CustomLinkedList {
 
-    private Node head;
+    private Node first = null;
+    private Node last;
+    private int size;
 
     /**
      * Реализовать метод:
@@ -14,7 +16,16 @@ public class CustomLinkedList {
      * @param value - data for create Node.
      */
     public void add(int value) {
-
+        if (first == null) {
+            first = new Node(value);
+            first.next = null;
+            last = first;
+        } else {
+            Node curr = last;
+            last = new Node(value);
+            curr.next = last;
+        }
+        size++;
     }
 
     /**
@@ -25,44 +36,116 @@ public class CustomLinkedList {
      * @param index - position what element need remove.
      */
     public void removeElement(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
 
+        if (size == 1) {
+            first = null;
+            last = null;
+            size--;
+            return;
+        }
+
+        Node prev = null;
+        Node current = first;
+
+        int i = 0;
+
+        // Ищем элемент по заданному индексу
+        while (i != index) {
+            prev = current;
+            current = current.next;
+            i++;
+        }
+
+        // Рассматриваем несколько случаев, удаляя таким образом элемент
+        if (last == current) {
+            last = prev;
+            last.next = null;
+        } else if (first == current) {
+            first = first.next;
+        } else {
+            prev.next = current.next;
+        }
+
+        size--;
     }
 
     /**
      * Реализовать метод:
      * Переварачивает все элементы списка.
      * Пример:
-     *  Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
-     *  После исполнения метода последовательность должа быть такой "4 -> 3 -> 2 -> 1 -> null"
+     * Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
+     * После исполнения метода последовательность должа быть такой "4 -> 3 -> 2 -> 1 -> null"
      */
     public void revertList() {
+        if (size < 2) {
+            return;
+        }
 
+        if (size == 2) {
+            Node temp = last;
+            first.next = null;
+            last = first;
+            first = temp;
+            first.next = last;
+            return;
+        }
+
+        Node prev = first;
+        Node curr = prev.next;
+        Node temp;
+        prev.next = null;
+
+        // Меняем ссылки у элементов со следующего на предыдущий от начального до конечного
+        do {
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        } while (curr != null);
+
+        // Меняем местами первый и последний элементы и их ссылки
+        temp = last;
+        last = first;
+        last.next = null;
+        first = temp;
     }
 
     /**
      * Метод выводит всю последовательность хранящуюся в списке начиная с head.
      * Формат вывода:
-     *  - значение каждой Node должно разделяться " -> "
-     *  - последовательность всегда заканчивается на null
-     *  - если в списке нет элементов - верните строку "null"
+     * - значение каждой Node должно разделяться " -> "
+     * - последовательность всегда заканчивается на null
+     * - если в списке нет элементов - верните строку "null"
      *
      * @return - String with description all list
      */
     @Override
     public String toString() {
-        return "1 -> 2 -> 3 -> null";
+        StringBuilder builder = new StringBuilder();
+
+        Node current = first;
+        while (current != null) {
+            builder.append(current.value);
+            builder.append(" -> ");
+            current = current.next;
+        }
+
+        if (builder.toString().isEmpty()) {
+            return "null";
+        }
+
+        return builder.append("null").toString();
     }
 
     private static class Node {
-        private int value;
+        private final int value;
         private Node next;
 
         public Node(int value) {
             this.value = value;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
         }
     }
 }
