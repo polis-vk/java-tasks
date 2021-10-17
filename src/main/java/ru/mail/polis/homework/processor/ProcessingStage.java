1ea80e8c0400e221a862a24b576221b882da4155
PrimeFall <artem891891@gmail.com>
2021-10-18 00:09:09 +0300

    Done ProcessingStage

--- src/main/java/ru/mail/polis/homework/processor/ProcessingStage.java
+++ src/main/java/ru/mail/polis/homework/processor/ProcessingStage.java
@@ -4,4 +4,17 @@ package ru.mail.polis.homework.processor;
  * Стадия обработки текста (2 балла)
  */
 public enum ProcessingStage {
+    PREPROCESS(2),
+    PROCESS(1),
+    POSTPROCESS(0);
+
+    private int priority;
+
+    ProcessingStage(int priority) {
+        this.priority = priority;
+    }
+
+    public int priority() {
+        return priority;
+    }
 }
