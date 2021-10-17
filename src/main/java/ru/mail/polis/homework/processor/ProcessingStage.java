feea085041e89582b8350810fd265acc6c945176
mariohuq <mariohuq@github.com>
2021-10-17 14:56:22 +0300

    ProcessingStage: add explicit order

--- src/main/java/ru/mail/polis/homework/processor/ProcessingStage.java
+++ src/main/java/ru/mail/polis/homework/processor/ProcessingStage.java
@@ -4,7 +4,17 @@ package ru.mail.polis.homework.processor;
  * Стадия обработки текста (2 балла)
  */
 public enum ProcessingStage {
-    PRE_PROCESSING,
-    PROCESSING,
-    POST_PROCESSING,
+    PRE_PROCESSING(0),
+    PROCESSING(1),
+    POST_PROCESSING(2);
+
+    private final int order;
+
+    ProcessingStage(int order) {
+        this.order = order;
+    }
+
+    public boolean precedes(ProcessingStage other) {
+        return order < other.order;
+    }
 }
