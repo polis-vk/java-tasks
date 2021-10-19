7369eee5119b70621074b327ce6c192711e8f2e1
nikita.derevyanko <nikita.derevyanko@corp.mail.ru>
2021-10-19 04:08:40 +0300

    Homework 4: add tasks

---
+++ src/main/java/ru/mail/polis/homework/collections/structure/SearchInTheShredderList.java
@@ -0,0 +1,32 @@
+package ru.mail.polis.homework.collections.structure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Задание оценивается в 2 балла.
+ * Одна из самых популярных задач.
+ * Наша структура хранит обрывки слов. Надо реализовать метод positionPartString
+ * который вернет: позиции где находятся подстроки, из которых можно составить
+ * переданное слово. Отрабатывать метод должен за О(n).
+ */
+public class SearchInTheShredderList {
+    private final List<String> partStrings = new ArrayList<>();
+
+    public void add(String value) {
+        partStrings.add(value);
+    }
+
+    public String get(int index) {
+        return partStrings.get(index);
+    }
+
+    /**
+     * Ищем позиции подстрок из которых можно составить передаваемое слово
+     * @param value - передаваемоей слово
+     * @return - либо массив с реальными позициями подстрок если нашли, либо - null
+     */
+    public int[] positionPartString(String value) {
+        return null;
+    }
+}
