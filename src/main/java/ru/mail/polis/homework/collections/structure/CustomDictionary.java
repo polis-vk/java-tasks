59d780e732901ddd60d111efe5f7e6c1b81a21ef
vspochernin <vspochernin@gmail.com>
2021-10-23 23:30:50 +0300

    part4-CustomDictionary: implement class

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,7 +1,13 @@
 package ru.mail.polis.homework.collections.structure;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Задание оценивается в 4 балла.
@@ -11,15 +17,17 @@ import java.util.List;
  */
 public class CustomDictionary {
 
+    Set<String> data = new LinkedHashSet<>();
+
     /**
      * Сохранить строку в структуру данных
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
      *
-     * Сложность - []
+     * Сложность - [O(1) в среднем]
      */
     public boolean add(String value) {
-        return false;
+        return data.add(value);
     }
 
     /**
@@ -27,10 +35,10 @@ public class CustomDictionary {
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
      *
-     * Сложность - []
+     * Сложность - [O(1) в среднем]
      */
     public boolean contains(String value) {
-        return false;
+        return data.contains(value);
     }
 
     /**
@@ -38,10 +46,10 @@ public class CustomDictionary {
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
      *
-     * Сложность - []
+     * Сложность - [O(1) в среднем]
      */
     public boolean remove(String value) {
-        return false;
+        return data.remove(value);
     }
 
     /**
@@ -57,20 +65,36 @@ public class CustomDictionary {
      * @return - список слов которые состоят из тех же букв, что и передаваемая
      * строка.
      *
-     * Сложность - []
+     * Сложность - [O(n) в среднем]
      */
     public List<String> getSimilarWords(String value) {
-        return Collections.emptyList();
+        List<String> result = new LinkedList<>();
+
+        Set<Character> valueSet = new HashSet<>();
+        for (Character ch : value.toCharArray()) {
+            valueSet.add(Character.toLowerCase(ch));
+        }
+
+        for (String s : data) {
+            Set<Character> strSet = new HashSet<>();
+            for (Character ch : s.toCharArray()) {
+                strSet.add(Character.toLowerCase(ch));
+            }
+            if (strSet.equals(valueSet)) {
+                result.add(s);
+            }
+        }
+        return result;
     }
 
     /**
      * Колл-во хранимых строк.
      * @return - Колл-во хранимых строк.
      *
-     * Сложность - []
+     * Сложность - [O(1)]
      */
     public int size() {
-        return 0;
+        return data.size();
     }
 
 
