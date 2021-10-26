93d41f3b5f96e10cc26cdb74317f181f5888031c
VladislavFetisov <fetisov2.vv@edu.spbstu.ru>
2021-10-26 22:04:43 +0300

    Homework 4

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,7 +1,13 @@
 package ru.mail.polis.homework.collections.structure;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 
 /**
  * Задание оценивается в 4 балла.
@@ -10,68 +16,178 @@ import java.util.List;
  * Напишите какая сложность операций у вас получилась для каждого метода.
  */
 public class CustomDictionary {
+    private static class Node {
+        private final Map<Character, Node> children = new LinkedHashMap<>();
+        private final Node parent;
+
+        private Node(Node parent) {
+            this.parent = parent;
+        }
+
+        public Map<Character, Node> getChildren() {
+            return children;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+    }
+
+    private final Node root = new Node(null);
+
+    private int size = 0;
+
+    private String withZero(String value) {
+        return value + '0';
+    }
+
+
+    public Node findNode(String element) {
+        Node current = root;
+        for (char character : element.toCharArray()) {
+            if (current == null) return null;
+            current = current.children.get(character);
+        }
+        return current;
+    }
 
     /**
      * Сохранить строку в структуру данных
+     *
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(log(n))]
      */
     public boolean add(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+        Node current = root;
+        boolean modified = false;
+        for (char letter : withZero(value).toCharArray()) {
+            Node existing = current.getChildren().get(letter);
+            if (existing != null) {
+                current = existing;
+            } else {
+                Node newNode = new Node(current);
+                current.getChildren().put(letter, newNode);
+                current = newNode;
+                modified = true;
+            }
+        }
+        if (modified) {
+            size++;
+        }
+        return modified;
     }
 
     /**
      * Проверяем, хранится ли такая строка уже у нас
+     *
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(log(n))]
      */
     public boolean contains(String value) {
-        return false;
+        return findNode(withZero(value)) != null;
     }
 
     /**
      * Удаляем сохраненную строку если она есть
+     *
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(log(n))]
      */
     public boolean remove(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+        String sWithZero = withZero(value);
+        Node current = findNode(sWithZero);
+        if (current == null) {
+            return false;
+        }
+        int i = sWithZero.length() - 1;
+        while (current.getParent() != null) {
+            if (!current.getChildren().isEmpty()) {
+                break;
+            }
+            current.getParent().getChildren().remove(sWithZero.charAt(i));
+            i--;
+            current = current.getParent();
+        }
+        size--;
+        return true;
     }
 
     /**
      * Возвращает список из сохраненных ранее строк, которые состоят
      * из тех же букв что нам передали строку.
      * Примеры: сохраняем строки ["aaa", "aBa", "baa", "aaB"]
-     *          При поиске по строке "AAb" нам должен вернуться следующий
-     *          список: ["aBa","baa","aaB"]
-     *
+     * При поиске по строке "AAb" нам должен вернуться следующий
+     * список: ["aBa","baa","aaB"]
+     * <p>
      * Как можно заметить - регистр строки не должен влиять на поиск, при этом
      * возвращаемые строки хранятся в том виде что нам передали изначально.
      *
      * @return - список слов которые состоят из тех же букв, что и передаваемая
      * строка.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(nlog(n))]
      */
+
     public List<String> getSimilarWords(String value) {
-        return Collections.emptyList();
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (char letter : value.toLowerCase(Locale.ROOT).toCharArray()) {
+            hm.computeIfPresent(letter, (k, v) -> v + 1);
+            hm.putIfAbsent(letter, 1);
+        }
+        return depthSearch(root, hm, new StringBuilder(), new ArrayList<>());
     }
 
+    private List<String> depthSearch(Node current, HashMap<Character, Integer> localHm, StringBuilder word, List<String> result) {
+        for (Map.Entry<Character, Node> entry : current.getChildren().entrySet()) {
+            if (entry.getKey() == '0') {
+                boolean trigger = localHm.values().stream().mapToInt(value -> value).anyMatch(value -> value != 0);
+                if (!trigger) {
+                    result.add(word.toString());
+                    return result;
+                }
+            }
+            char lowerLetter = Character.toLowerCase(entry.getKey());
+            if (localHm.containsKey(lowerLetter) && localHm.get(lowerLetter) != 0) {
+                word.append(entry.getKey());
+
+                localHm.compute(lowerLetter, (k, v) -> v - 1);
+                depthSearch(entry.getValue(), localHm, word, result);
+
+                localHm.compute(lowerLetter, (k, v) -> v + 1);
+
+                word.deleteCharAt(word.length() - 1);
+            }
+        }
+        return result;
+    }
     /**
      * Колл-во хранимых строк.
-     * @return - Колл-во хранимых строк.
      *
-     * Сложность - []
+     * @return - Колл-во хранимых строк.
+     * <p>
+     * Сложность - [O(1)]
      */
     public int size() {
-        return 0;
+        return size;
     }
 
 
+    public static void main(String[] args) {
+        CustomDictionary cd = new CustomDictionary();
+        String[] arr = new String[]{"aaa", "aBa", "baa", "aaB"};
+        Arrays.stream(arr).forEach(cd::add);
+        System.out.println(cd.getSimilarWords("AAb"));
+    }
 }
