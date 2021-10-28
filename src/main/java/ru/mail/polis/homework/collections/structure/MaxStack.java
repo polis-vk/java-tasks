edd5264d9f832d844a6067c4742bc65a70beef25
mariohuq <mariohuq@github.com>
2021-10-28 18:43:49 +0300

    MaxStack: try solve

--- src/main/java/ru/mail/polis/homework/collections/structure/MaxStack.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/MaxStack.java
@@ -1,6 +1,8 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Collection;
 import java.util.Stack;
+import java.util.function.Predicate;
 
 /**
  * Задание оценивается в 2 балла.
@@ -9,9 +11,77 @@ import java.util.Stack;
  * храниться в Stack. Отрабатывать метод должен за О(1).
  */
 public class MaxStack extends Stack<Integer> {
+    private final Stack<Integer> maxes = new Stack<>();
 
     public Integer getMaxValue() {
-        return 0;
+        return maxes.peek();
     }
 
+    @Override
+    public Integer push(Integer item) {
+        maxes.push(Integer.max(getMaxValue(), item));
+        return super.push(item);
+    }
+
+    @Override
+    public synchronized Integer pop() {
+        maxes.pop();
+        return super.pop();
+    }
+
+    @Override
+    public void clear() {
+        maxes.clear();
+        super.clear();
+    }
+
+    @Override
+    public synchronized Integer remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public synchronized void removeAllElements() {
+        maxes.removeAllElements();
+        super.removeAllElements();
+    }
+
+    @Override
+    public synchronized boolean removeElement(Object obj) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected synchronized void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super Integer> filter) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public synchronized void removeElementAt(int index) {
+        if (index != size() - 1) {
+            throw new IllegalArgumentException("Can only remove last element");
+        }
+        super.removeElementAt(index);
+    }
+
+    @Override
+    public synchronized void setSize(int newSize) {
+        maxes.setSize(newSize);
+        super.setSize(newSize);
+    }
 }
