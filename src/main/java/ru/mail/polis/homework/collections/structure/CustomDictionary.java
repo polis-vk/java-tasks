37b700237f31ea12447abc9f1195866ffb5eeb3e
mariohuq <mariohuq@github.com>
2021-10-28 18:43:15 +0300

    CustomDictionary: try solve

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,58 +1,87 @@
 package ru.mail.polis.homework.collections.structure;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.ListIterator;
+import java.util.LinkedList;
+import java.util.BitSet;
+import java.util.Objects;
 
 /**
  * Задание оценивается в 4 балла.
- * Необходимо реализовать класс которы умеет хранить строки и возвращать
- * список строк состоящий из того же набора буков, что ему передали строку.
- * Напишите какая сложность операций у вас получилась для каждого метода.
+ * Необходимо реализовать класс, который умеет хранить строки и возвращать
+ * список строк, состоящий из того же набора букв, что и переданная ему строка.
+ * Напишите, какая сложность операций у вас получилась для каждого метода.
  */
 public class CustomDictionary {
 
+    public static final int ALPHABET_SIZE = 26;
+    private final Map<LetterSet, List<String>> storage = new HashMap<>();
+    private final Map<String, ListIterator<String>> cache = new HashMap<>();
+
     /**
      * Сохранить строку в структуру данных
+     *
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - O(log(storage.size()) + log(cache.size()))
      */
     public boolean add(String value) {
-        return false;
+        if (value == null) {
+            return false;
+        }
+        if (contains(value)) {
+            return false;
+        }
+        final LetterSet letters = new LetterSet(value);
+        List<String> list = storage.computeIfAbsent(letters, k -> new LinkedList<>());
+        final ListIterator<String> it = list.listIterator(list.size());
+        cache.put(value, it);
+        it.add(value);
+        return true;
     }
 
     /**
      * Проверяем, хранится ли такая строка уже у нас
+     *
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - O(log(cache.size()))
      */
     public boolean contains(String value) {
-        return false;
+        return cache.containsKey(value);
     }
 
     /**
      * Удаляем сохраненную строку если она есть
+     *
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - O(log(cache.size()))
      */
     public boolean remove(String value) {
-        return false;
+        final ListIterator<String> iterator = cache.get(value);
+        if (iterator == null) {
+            return false;
+        }
+        iterator.remove();
+        cache.remove(value);
+        return true;
     }
 
     /**
      * Возвращает список из сохраненных ранее строк, которые состоят
-     * из того же набора букв что нам передали строку.
+     * из того же набора букв что и переданная ему строка.
      * Примеры:
      * сохраняем строки ["aaa", "aBa", "baa", "aaB"]
      * При поиске по строке "AAb" нам должен вернуться следующий
      * список: ["aBa","baa","aaB"]
-     *
-     * сохраняем строки ["aaa", "aAa", "a"]
+     * <p>
+     * Сохраняем строки ["aaa", "aAa", "a"]
      * поиск "aaaa"
      * результат: []
      * Как можно заметить - регистр строки не должен влиять на поиск, при этом
@@ -60,22 +89,60 @@ public class CustomDictionary {
      *
      * @return - список слов которые состоят из тех же букв, что и передаваемая
      * строка.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - O(log(storage.size()))
      */
     public List<String> getSimilarWords(String value) {
-        return Collections.emptyList();
+        return storage.get(new LetterSet(value));
     }
 
     /**
-     * Колл-во хранимых строк.
-     * @return - Колл-во хранимых строк.
+     * Кол-во хранимых строк.
      *
-     * Сложность - []
+     * @return - Колл-во хранимых строк.
+     * <p>
+     * Сложность - O(1)
      */
     public int size() {
-        return 0;
+        return cache.size();
     }
 
+    private static class LetterSet {
+        final BitSet internal = new BitSet(ALPHABET_SIZE);
 
+        public LetterSet(String word) {
+            for (char c : word.toCharArray()) {
+                if (!isLetter(c)) {
+                    continue;
+                }
+                internal.set(index(lowerCase(c)), true);
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            if (!super.equals(o)) return false;
+            LetterSet that = (LetterSet) o;
+            return internal.equals(that.internal);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(internal);
+        }
+
+        private static int index(char c) {
+            return c - 'a';
+        }
+
+        private static char lowerCase(char c) {
+            return Character.toLowerCase(c);
+        }
+
+        private static boolean isLetter(char c) {
+            return 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z';
+        }
+    }
 }
