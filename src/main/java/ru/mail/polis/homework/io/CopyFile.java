05e8ceb01389f04e117bb546617e9bd4b80eb57e
Vladislav Pochernin <vspochernin@gmail.com>
2021-10-31 16:02:58 +0300

    part6-CopyFile: implement copy only file

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,10 +1,28 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class CopyFile {
+
+    private static void copyFile(Path from, Path to) {
+        try (InputStream input = Files.newInputStream(from); OutputStream output = Files.newOutputStream(to)) {
+            byte[] buffer = new byte[1024];
+            int recorded = input.read(buffer);
+            while (recorded > 0) {
+                output.write(buffer, 0, recorded);
+                recorded = input.read(buffer);
+            }
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
@@ -19,6 +37,16 @@ public class CopyFile {
         Path from = Paths.get(pathFrom);
         if (Files.notExists(from)) {
             return null;
+        }
+
+        Path to = Paths.get(pathTo);
+        if (Files.isRegularFile(from)) {
+            try {
+                Files.createDirectories(to.getParent());
+                copyFile(from, to);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return null;
