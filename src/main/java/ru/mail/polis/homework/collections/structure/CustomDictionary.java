9e9d508901b8f4cc8e266a2393fe3b6ef7b802e5
Alexandra-Tkachenko <91024312+Alexandra-Tkachenko@users.noreply.github.com>
2021-11-01 15:03:58 +0300

    CustomDictionary

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,81 +1,104 @@
 package ru.mail.polis.homework.collections.structure;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Задание оценивается в 4 балла.
- * Необходимо реализовать класс которы умеет хранить строки и возвращать
- * список строк состоящий из того же набора буков, что ему передали строку.
- * Напишите какая сложность операций у вас получилась для каждого метода.
+ * Необходимо реализовать класс, умеющий хранить строки и возвращать список строк,
+ * состоящий из того же набора буков, что и переданная строка.
+ * Collection            |  Add   |  Remove  | Contains  |   Next   | Size |  Get  |  Put  |
+ * ----------------------|--------|----------|-----------|----------|------|-------|-------|
+ * ArrayList             |  O(1)  |   O(n)   |   O(n)    |   O(1)   | O(1) | O(1)  |       |
+ * HashMap               |        |          |  for key  |  O(h/n)  | O(1) | O(1)  |  O(1) |
+ *                       |        |          |   O(1)    |          |      |       |       |
+ * для HashMap операции put и get выполняются за О(1) с учтом, что хеш-функция правильно
+ * распределяет элементы по buckets, в противоположном случае требуется добавить m к
+ * сложности посчитанных функций - amount of collision
  */
-public class CustomDictionary {
+public final class CustomDictionary {
 
+    private final Map<String, List<String>> relation = new HashMap<>();
+    private int size;
     /**
-     * Сохранить строку в структуру данных
+     * Сохранить строку в структуру данных.
      * @param value - передаваемая строка
-     * @return - успешно сохранили строку или нет.
-     *
-     * Сложность - []
+     * @return - успешно сохранили строку или нет
+     * Сложность - О(nlogn), n = value.length,
+     * т.к. сортировка за O(nlogn), get за О(1), contains за O(n),
      */
     public boolean add(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        String sortedString = sortedString(value);
+        List<String> originalStringList = relation.getOrDefault(sortedString(value), new ArrayList<>());
+        if (originalStringList.contains(value)) {
+            return false;
+        }
+        originalStringList.add(value);
+        relation.put(sortedString, originalStringList);
+        size++;
+        return true;
     }
 
     /**
-     * Проверяем, хранится ли такая строка уже у нас
+     * Проверяем, хранится ли такая строка уже.
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
-     *
-     * Сложность - []
+     * Сложность - О(nlogn), n = value.length,
+     * т.к. get - за О(1) и сортировка за O(nlogn)
      */
     public boolean contains(String value) {
-        return false;
+        List<String> originalStringList = relation.get(sortedString(value));
+        return originalStringList != null && originalStringList.contains(value);
     }
 
     /**
-     * Удаляем сохраненную строку если она есть
-     * @param value - какую строку мы хотим удалить
-     * @return - true если удалили, false - если такой строки нет
-     *
-     * Сложность - []
+     * Удаляем сохраненную строку, если она есть.
+     * @param value - какую строку удалить
+     * @return - true, если удалили, или false, если такой строки нет
+     * Сложность - О(nlogn), n = value.length,
+     * т.к. get - за О(1), contains за O(nlogn), remove за O(n)
      */
     public boolean remove(String value) {
-        return false;
+        if (!contains(value)) {
+            return false;
+        }
+        size--;
+        return relation.get(sortedString(value)).remove(value);
     }
 
     /**
      * Возвращает список из сохраненных ранее строк, которые состоят
-     * из того же набора букв что нам передали строку.
-     * Примеры:
-     * сохраняем строки ["aaa", "aBa", "baa", "aaB"]
-     * При поиске по строке "AAb" нам должен вернуться следующий
-     * список: ["aBa","baa","aaB"]
-     *
-     * сохраняем строки ["aaa", "aAa", "a"]
-     * поиск "aaaa"
-     * результат: []
-     * Как можно заметить - регистр строки не должен влиять на поиск, при этом
-     * возвращаемые строки хранятся в том виде что нам передали изначально.
-     *
-     * @return - список слов которые состоят из тех же букв, что и передаваемая
-     * строка.
-     *
-     * Сложность - []
+     * из того же набора букв, что и переданая строка.
+     * Регистр строки не влияет на поиск, а возвращаемые строки хранятся
+     * в том же виде, что и изначально.
+     * @return - список слов, состоящих из тех же букв передаваемой строки
+     * Сложность - О(nlogn), n = value.length,
+     * т.к. get - за О(1) и сортировка за O(nlogn)
      */
     public List<String> getSimilarWords(String value) {
-        return Collections.emptyList();
+        List<String> valueList = relation.getOrDefault(sortedString(value), null);
+        return valueList != null ? valueList : Collections.emptyList();
     }
 
     /**
-     * Колл-во хранимых строк.
-     * @return - Колл-во хранимых строк.
-     *
-     * Сложность - []
+     * Кол-во хранимых строк.
+     * @return - Кол-во хранимых строк
+     * Сложность - O(1)
      */
     public int size() {
-        return 0;
+        return size;
     }
 
-
+    /**
+     * Cортировка строки в алфавитном порядке посимвольно.
+     * Сложность - О(nlogn), n = value.length,
+     * т.к. JDK использует quicksort для примитивов
+     */
+    private String sortedString(String value) {
+        return value.toLowerCase(Locale.ROOT).trim().chars().sorted().collect(StringBuilder::new,
+                StringBuilder::appendCodePoint, StringBuilder::append).toString();
+    }
 }
+
