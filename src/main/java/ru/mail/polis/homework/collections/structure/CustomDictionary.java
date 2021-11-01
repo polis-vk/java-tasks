54f28aae9e8dea6ef873f70a6be54e33d1a2d0ed
Kirill Pobedonostsev <pobedonoscevkirill@gmail.com>
2021-11-01 05:51:47 +0300

    Remove unused imports

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,17 +1,12 @@
 package ru.mail.polis.homework.collections.structure;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 /**
  * Задание оценивается в 4 балла.
