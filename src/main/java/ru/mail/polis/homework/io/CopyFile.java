7a12c01338d07222f027b6e9009fbf58e9d464d0
VladislavFetisov <fetisov2.vv@edu.spbstu.ru>
2021-11-01 23:05:57 +0300

    hometask 6

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,15 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -9,6 +19,45 @@ public class CopyFile {
      * 3 балла
      */
     public static String copyFiles(String pathFrom, String pathTo) {
+        Path start = Path.of(pathFrom);
+        Path other = Path.of(pathTo);
+        try {
+            Files.walkFileTree(start, new FileVisitor<>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    Path pathTo = Paths.get(other.toString(), dir.toString());
+                    if (!Files.exists(pathTo)) {
+                        Files.createDirectory(pathTo);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Path createdFile=Paths.get(other.toString(), file.toString());
+                    if (!Files.exists(createdFile)) {
+                        Files.createFile(createdFile);
+                        try (InputStream in = Files.newInputStream(file);
+                             OutputStream out = Files.newOutputStream(createdFile)) {
+                            out.write(in.readAllBytes());
+                        }
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    return FileVisitResult.TERMINATE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
