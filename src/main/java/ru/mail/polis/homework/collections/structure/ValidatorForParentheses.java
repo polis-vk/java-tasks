73f589dce6bd90a3929688fcd527a018e94451a7
Alexandra-Tkachenko <91024312+Alexandra-Tkachenko@users.noreply.github.com>
2021-11-01 05:46:38 +0300

    ValidatorForParentheses

--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,54 +1,43 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Задание оценивается в 2 балла.
- * Одна из самых популярных задач.
  * Реализовать метод, который проверяет правильность написания скобок в строке.
  * В строке помимо скобок могут содержаться и другие символы.
  * Скобки могут быть: [],{},<>,()
- * Примеры:
- * "(-b + (x)^2)/(2+4)" - true
- * "Понедельники меня угнетают ((" - false
- * <p>
  * Отрабатывать метод должен за О(n)
  */
 public class ValidatorForParentheses {
+
+    private static final Map<Character, Character> brackets = Map.of('(', ')', '[', ']', '{', '}');
 
     public static boolean validate(String value) {
         if (value == null || value.isEmpty()) {
             return false;
         }
-        return realizeValidate(value);
+        return Pattern.compile("[({\\[<)}\\]>]+[\\s\\w\\W]*").matcher(value).matches() && realizeValidate(value);
     }
 
     private static boolean realizeValidate(String value) {
-        LinkedList<Character> needToClose = new LinkedList<>();//last open bracket stack
+        Deque<Character> deque = new LinkedList<>();
         for (Character symbol : value.toCharArray()) {
-            if (symbol.equals('(') || symbol.equals('[') || symbol.equals('{') || symbol.equals('<')) {
-                needToClose.add(symbol);
+            if (brackets.containsKey(symbol)) {
+                deque.addFirst(symbol);
             } else {
-                if (symbol.equals(')') || symbol.equals(']') || symbol.equals('}') || symbol.equals('>')) {
-                    if (needToClose.size() == 0 || !needToClose.pollLast().equals(getOpenBracket(symbol))) {
+                if (brackets.containsValue(symbol)) {
+                    if (!deque.isEmpty() && brackets.get(deque.getFirst()).equals(symbol)) {
+                        deque.removeFirst();
+                    } else {
                         return false;
                     }
                 }
             }
         }
-        return needToClose.isEmpty();
-    }
-
-    private static Character getOpenBracket(Character bracket) {
-        if (bracket.equals(')')) {
-            return '(';
-        }
-        if (bracket.equals(']')) {
-            return '[';
-        }
-        if (bracket.equals('>')) {
-            return '<';
-        }
-        return '{';
+        return deque.isEmpty();
     }
 }
