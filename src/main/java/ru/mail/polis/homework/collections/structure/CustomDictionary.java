8cf96adfc119fc1612d757d6cc80b53d76084985
Денис Жидков <denis.zhidkov01@gmail.com>
2021-11-03 17:20:51 +0300

    Fixed CustomDictionary

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,10 +1,10 @@
 package ru.mail.polis.homework.collections.structure;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Задание оценивается в 4 балла.
@@ -14,115 +14,128 @@ import java.util.Objects;
  */
 public class CustomDictionary {
 
-    private final Map<String, Words> dict = new HashMap<>();
+    private final Map<List<Character>, List<String>> dict = new HashMap<>();
+    private int size = 0;
 
     /**
      * Сохранить строку в структуру данных
+     *
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
-     *
-     * Сложность - O(n), где n - количество букв строке
+     * <p>
+     * Сложность - O(m * log(m)), где m - количество символов в строке
      */
     public boolean add(String value) {
         if (value == null || value.equals("")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException ();
         }
-        return dict.putIfAbsent(value, new Words(value)) == null;
+        List<Character> key = toOrderedListOfLowerCaseChars(value);
+        if (dict.containsKey(key)) {
+            if (dict.get(key).contains(value)) {
+                return false;
+            } else {
+                dict.get(key).add(value);
+                size++;
+            }
+        } else {
+            List<String> newValue = new ArrayList<>();
+            newValue.add(value);
+            dict.put(key, newValue);
+            size++;
+        }
+        return true;
     }
 
     /**
      * Проверяем, хранится ли такая строка уже у нас
+     *
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
-     *
-     * Сложность - O(1)
+     * <p>
+     * Сложность - O(m * log(m)), где m - количество символов в строке
      */
     public boolean contains(String value) {
-        return dict.containsKey(value);
+        if (value == null || value.equals("")) {
+            throw new IllegalArgumentException();
+        }
+        List<Character> key = toOrderedListOfLowerCaseChars(value);
+        return size() != 0 && dict.containsKey(key) && dict.get(key).contains(value);
     }
 
     /**
      * Удаляем сохраненную строку, если она есть
+     *
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
-     *
-     * Сложность - O(1)
+     * <p>
+     * Сложность - O(m * log(m)), где m - количество символов в строке
      */
     public boolean remove(String value) {
-        if (!dict.containsKey(value)) {
-            return false;
+        if (value == null || value.equals("")) {
+            throw new IllegalArgumentException();
         }
-        dict.remove(value);
-        return true;
+        List<Character> key = toOrderedListOfLowerCaseChars(value);
+        if (contains(value)) {
+            List<String> dictValue = dict.get(key);
+            dictValue.remove(value);
+            if (dictValue.isEmpty()) {
+                dict.remove(key, dictValue);
+            }
+            size--;
+            return true;
+        }
+        return false;
     }
 
     /**
      * Возвращает список из сохраненных ранее строк, которые состоят
      * из тех же букв, что и переданная строка.
      * Примеры: сохраняем строки ["aaa", "aBa", "baa", "aaB"]
-     *          При поиске по строке "AAb" нам должен вернуться следующий
-     *          список: ["aBa","baa","aaB"]
-     *
+     * При поиске по строке "AAb" нам должен вернуться следующий
+     * список: ["aBa","baa","aaB"]
+     * <p>
      * Как можно заметить - регистр строки не должен влиять на поиск, при этом
      * возвращаемые строки хранятся в том виде, что нам передали изначально.
      *
      * @return - список слов, которые состоят из тех же букв, что и передаваемая
      * строка.
-     *
-     * Сложность - O(n * m), где n - количество записей в словаре, где m - количество букв в строке, по которой ищем
+     * <p>
+     * Сложность - O(m * log(m)), где m - количество символов в строке
      */
     public List<String> getSimilarWords(String value) {
-        List<String> res = new ArrayList<>();
-        Words wordsOfValue = new Words(value);
-        for (Map.Entry<String, Words> entry: dict.entrySet()) {
-            if (wordsOfValue.equals(entry.getValue())) {
-                res.add(entry.getKey());
-            }
+        if (value == null || value.equals("")) {
+            throw new IllegalArgumentException();
         }
-        return res;
+        List<Character> key = toOrderedListOfLowerCaseChars(value);
+        return (size != 0 && dict.containsKey(key))? dict.get(key) : new ArrayList<>();
     }
 
     /**
      * Колл-во хранимых строк.
-     * @return - Колл-во хранимых строк.
      *
+     * @return - Колл-во хранимых строк.
+     * <p>
      * Сложность - O(1)
      */
     public int size() {
-        return dict.size();
+        return size;
     }
 
-    private static class Words {
-
-        private final Map<Character, Integer> words = new HashMap<>();
-
-        public Words(String str) {
-            String lowCaseString = str.toLowerCase();
-            for (int i = 0; i < lowCaseString.length(); i++) {
-                char c = lowCaseString.charAt(i);
-                if (words.containsKey(c)) {
-                    words.put(c, words.get(c) + 1);
-                } else {
-                    words.put(c, 1);
-                }
-            }
+    /**
+     * Функция, преобразующая строку в отсортированный лист символов
+     *
+     * @param value - строка, которую необходимо преобразовать
+     * @return - отсортированный список символов
+     * <p>
+     * Сложность - O(m * log(m)), где m - количество символов в списке
+     */
+    private List<Character> toOrderedListOfLowerCaseChars(String value) {
+        String lowerCaseStr = value.toLowerCase();
+        List<Character> res = new ArrayList<>();
+        for (int i = 0; i < lowerCaseStr.length(); i++) {
+            res.add(lowerCaseStr.charAt(i));
         }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Words other = (Words) o;
-            return Objects.equals(words, other.words);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(words);
-        }
+        Collections.sort(res);
+        return res;
     }
 }
