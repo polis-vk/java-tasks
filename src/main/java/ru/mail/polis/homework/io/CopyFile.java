55a7b57cbdc84a8ce9035b4348dba0e1d37e9f55
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2021-11-06 12:27:40 +0300

    Added CopyFile

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,15 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -9,7 +19,44 @@ public class CopyFile {
      * 3 балла
      */
     public static String copyFiles(String pathFrom, String pathTo) {
+        if (pathFrom == null || pathTo == null || Files.notExists(Paths.get(pathFrom))) {
+            return null;
+        }
+        try {
+            Files.walkFileTree(Path.of(pathFrom), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    Path tmp = Paths.get(pathTo, String.valueOf((Paths.get(pathFrom).relativize(dir))));
+                    Files.createDirectories(tmp);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Path tmp = Paths.get(pathTo, String.valueOf((Paths.get(pathFrom).relativize(file))));
+                    try {
+                        Files.createFile(tmp);
+                    } catch (IOException exception) {
+                        Files.createDirectories(tmp.getParent());
+                        Files.createFile(tmp);
+                    }
+                    copyFile(file, tmp);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
+    private static void copyFile(Path pathFrom, Path pathTo) throws IOException {
+        try (InputStream input = Files.newInputStream(pathFrom);
+             OutputStream output = Files.newOutputStream(pathTo)) {
+            byte[] b = new byte[(int) Files.size(pathFrom)];
+            input.read(b);
+            output.write(b);
+            output.flush();
+        }
+    }
 }
