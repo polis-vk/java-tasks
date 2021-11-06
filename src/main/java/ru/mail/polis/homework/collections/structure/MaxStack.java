beebd4a09b232585ca383a769113b754cd99539b
PrimeFall <artem891891@gmail.com>
2021-11-06 16:13:47 +0300

    Done MaxStack

--- src/main/java/ru/mail/polis/homework/collections/structure/MaxStack.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/MaxStack.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 /**
@@ -9,9 +10,38 @@ import java.util.Stack;
  * храниться в Stack. Отрабатывать метод должен за О(1).
  */
 public class MaxStack extends Stack<Integer> {
+    private Stack<Integer> maxStack = new Stack<>();
 
     public Integer getMaxValue() {
-        return 0;
+        if (maxStack.isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return maxStack.peek();
     }
 
+    @Override
+    public Integer push(Integer item) {
+        maxStack.push(maxStack.isEmpty() ? item : (getMaxValue() > item ? getMaxValue() : item));
+        return super.push(item);
+    }
+
+    @Override
+    public Integer pop() {
+        if (super.isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        maxStack.pop();
+        return super.pop();
+    }
+
+    @Override
+    public Integer peek() {
+        if (super.isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return super.peek();
+    }
 }
