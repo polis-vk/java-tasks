fc1fc3f23ffc9e7169cebcaf813bf100abb842b0
CORADEAD <Nahnupa2002>
2021-11-07 14:51:39 +0300

    Realized all necessary IO func but without Structure IO

--- src/main/java/ru/mail/polis/homework/io/blocking/StructureInputStream.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureInputStream.java
@@ -1,9 +1,7 @@
 package ru.mail.polis.homework.io.blocking;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.Objects;
 
 /**
  * Вам нужно реализовать StructureInputStream, который умеет читать данные из файла.
@@ -33,5 +31,156 @@ public class StructureInputStream extends FileInputStream {
      */
     public Structure[] readStructures() throws IOException {
         return new Structure[0];
+    }
+
+    public SubStructure readSubStructure() throws IOException {
+        final int id = this.readInt();
+        final String name = this.readUTF();
+        final boolean flag = this.readBoolean();
+        final double score = this.readDouble();
+        return new SubStructure(id, name, flag, score);
+    }
+
+    public SubStructure[] readSubStructures() throws IOException {
+        int len = this.readInt();
+        if (len < 0) {
+            return null;
+        }
+        SubStructure[] subStructures = new SubStructure[len];
+
+        for (int i = 0; i < len; i++) {
+            subStructures[i] = readSubStructure();
+        }
+        return subStructures;
+    }
+
+    public final void readFully(byte b[], int off, int len) throws IOException {
+        Objects.checkFromIndexSize(off, len, b.length);
+        int n = 0;
+        while (n < len) {
+            int count = super.read(b, off + n, len - n);
+            if (count < 0)
+                throw new EOFException();
+            n += count;
+        }
+    }
+
+    public final boolean readBoolean() throws IOException {
+        int ch = super.read();
+        if (ch < 0)
+            throw new EOFException();
+        return (ch != 0);
+    }
+
+    public final int readByte() throws IOException {
+        int ch = super.read();
+        if (ch < 0)
+            throw new EOFException();
+        return ch;
+    }
+
+    private byte readBuffer[] = new byte[8];
+
+    public final long readLong() throws IOException {
+        readFully(readBuffer, 0, 8);
+        return (((long) readBuffer[0] << 56) +
+                ((long) (readBuffer[1] & 255) << 48) +
+                ((long) (readBuffer[2] & 255) << 40) +
+                ((long) (readBuffer[3] & 255) << 32) +
+                ((long) (readBuffer[4] & 255) << 24) +
+                ((readBuffer[5] & 255) << 16) +
+                ((readBuffer[6] & 255) << 8) +
+                ((readBuffer[7] & 255) << 0));
+    }
+
+    public final int readInt() throws IOException {
+//        int ch1 = super.read();
+//        int ch2 = super.read();
+//        int ch3 = super.read();
+//        int ch4 = super.read();
+//        if ((ch1 | ch2 | ch3 | ch4) < 0)
+//            throw new EOFException();
+//        return ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
+        readFully(readBuffer, 0, 4);
+        return (((readBuffer[4] & 255) << 24) +
+                ((readBuffer[5] & 255) << 16) +
+                ((readBuffer[6] & 255) << 8) +
+                ((readBuffer[7] & 255) << 0));
+    }
+
+    public final double readDouble() throws IOException {
+        return Double.longBitsToDouble(readLong());
+    }
+
+    public final String readUTF() throws IOException {
+        int utflen = this.readInt();
+        byte[] bytearr = new byte[utflen];
+        char[] chararr = new char[utflen];
+
+        int c, char2, char3;
+        int count = 0;
+        int chararr_count = 0;
+
+        this.readFully(bytearr, 0, utflen);
+
+        while (count < utflen) {
+            c = (int) bytearr[count] & 0xff;
+            if (c > 127) break;
+            count++;
+            chararr[chararr_count++] = (char) c;
+        }
+
+        while (count < utflen) {
+            c = (int) bytearr[count] & 0xff;
+            switch (c >> 4) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    /* 0xxxxxxx*/
+                    count++;
+                    chararr[chararr_count++] = (char) c;
+                    break;
+                case 12:
+                case 13:
+                    /* 110x xxxx   10xx xxxx*/
+                    count += 2;
+                    if (count > utflen)
+                        throw new UTFDataFormatException(
+                                "malformed input: partial character at end");
+                    char2 = (int) bytearr[count - 1];
+                    if ((char2 & 0xC0) != 0x80)
+                        throw new UTFDataFormatException(
+                                "malformed input around byte " + count);
+                    chararr[chararr_count++] = (char) (((c & 0x1F) << 6) |
+                            (char2 & 0x3F));
+                    break;
+                case 14:
+                    /* 1110 xxxx  10xx xxxx  10xx xxxx */
+                    count += 3;
+                    if (count > utflen)
+                        throw new UTFDataFormatException(
+                                "malformed input: partial character at end");
+                    char2 = (int) bytearr[count - 2];
+                    char3 = (int) bytearr[count - 1];
+                    if (((char2 & 0xC0) != 0x80) || ((char3 & 0xC0) != 0x80))
+                        throw new UTFDataFormatException(
+                                "malformed input around byte " + (count - 1));
+                    chararr[chararr_count++] = (char) (((c & 0x0F) << 12) |
+                            ((char2 & 0x3F) << 6) |
+                            ((char3 & 0x3F) << 0));
+                    break;
+                default:
+                    /* 10xx xxxx,  1111 xxxx */
+                    throw new UTFDataFormatException(
+                            "malformed input around byte " + count);
+            }
+        }
+        // The number of chars produced may be less than utflen
+        return new String(chararr, 0, chararr_count);
     }
 }
--- src/main/java/ru/mail/polis/homework/io/blocking/StructureOutputStream.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureOutputStream.java
@@ -1,9 +1,6 @@
 package ru.mail.polis.homework.io.blocking;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Вам нужно реализовать StructureOutputStream, который умеет писать данные в файл.
@@ -11,6 +8,8 @@ import java.io.IOException;
  * 3 балла
  */
 public class StructureOutputStream extends FileOutputStream {
+
+    protected int written;
 
     public StructureOutputStream(File name) throws FileNotFoundException {
         super(name);
@@ -27,5 +26,110 @@ public class StructureOutputStream extends FileOutputStream {
      */
     public void write(Structure[] structures) throws IOException {
 
+    }
+
+    private void incCount(int value) {
+        int temp = written + value;
+        if (temp < 0) {
+            temp = Integer.MAX_VALUE;
+        }
+        written = temp;
+    }
+
+    public final void writeBoolean(boolean v) throws IOException {
+        super.write(v ? 1 : 0);
+        incCount(1);
+    }
+
+    public final void writeByte(int v) throws IOException {
+        super.write(v);
+        incCount(1);
+    }
+
+    private final byte[] writeBuffer = new byte[8];
+
+    public final void writeInt(int v) throws IOException {
+        writeBuffer[0] = (byte) (v >>> 24);
+        writeBuffer[1] = (byte) (v >>> 16);
+        writeBuffer[2] = (byte) (v >>> 8);
+        writeBuffer[3] = (byte) (v >>> 0);
+        super.write(writeBuffer, 0, 4);
+        incCount(4);
+    }
+
+    public final void writeLong(long v) throws IOException {
+        writeBuffer[0] = (byte) (v >>> 56);
+        writeBuffer[1] = (byte) (v >>> 48);
+        writeBuffer[2] = (byte) (v >>> 40);
+        writeBuffer[3] = (byte) (v >>> 32);
+        writeBuffer[4] = (byte) (v >>> 24);
+        writeBuffer[5] = (byte) (v >>> 16);
+        writeBuffer[6] = (byte) (v >>> 8);
+        writeBuffer[7] = (byte) (v >>> 0);
+        super.write(writeBuffer, 0, 8);
+        incCount(8);
+    }
+
+    public final void writeDouble(double v) throws IOException {
+        writeLong(Double.doubleToLongBits(v));
+    }
+
+    int writeUTF(String str) throws IOException {
+        final int strlen = str.length();
+        long utflen = strlen; // optimized for ASCII
+
+        for (int i = 0; i < strlen; i++) {
+            int c = str.charAt(i);
+            if (c >= 0x80 || c == 0)
+                utflen += (c >= 0x800) ? 2 : 1;
+        }
+
+        if (utflen > Integer.MAX_VALUE || /* overflow */ utflen < strlen)
+            throw new UTFDataFormatException(tooLongMsg(str, (int) utflen));
+
+        final byte[] bytearr = new byte[(int) utflen + 4];
+
+        int count = 0;
+        bytearr[count++] = (byte) ((utflen >>> 32) & 0xFF);
+        bytearr[count++] = (byte) ((utflen >>> 16) & 0xFF);
+        bytearr[count++] = (byte) ((utflen >>> 8) & 0xFF);
+        bytearr[count++] = (byte) ((utflen >>> 0) & 0xFF);
+
+        int i = 0;
+        for (i = 0; i < strlen; i++) { // optimized for initial run of ASCII
+            int c = str.charAt(i);
+            if (c >= 0x80 || c == 0) break;
+            bytearr[count++] = (byte) c;
+        }
+
+        for (; i < strlen; i++) {
+            int c = str.charAt(i);
+            if (c < 0x80 && c != 0) {
+                bytearr[count++] = (byte) c;
+            } else if (c >= 0x800) {
+                bytearr[count++] = (byte) (0xE0 | ((c >> 12) & 0x0F));
+                bytearr[count++] = (byte) (0x80 | ((c >> 6) & 0x3F));
+                bytearr[count++] = (byte) (0x80 | ((c >> 0) & 0x3F));
+            } else {
+                bytearr[count++] = (byte) (0xC0 | ((c >> 6) & 0x1F));
+                bytearr[count++] = (byte) (0x80 | ((c >> 0) & 0x3F));
+            }
+        }
+        super.write(bytearr, 0, (int) utflen + 4);
+        return (int) utflen + 4;
+    }
+
+    private static String tooLongMsg(String s, int bits32) {
+        int slen = s.length();
+        String head = s.substring(0, 8);
+        String tail = s.substring(slen - 8, slen);
+        // handle int overflow with max 3x expansion
+        long actualLength = (long) slen + Integer.toUnsignedLong(bits32 - slen);
+        return "encoded string (" + head + "..." + tail + ") too long: "
+                + actualLength + " bytes";
+    }
+
+    public final int size() {
+        return written;
     }
 }
