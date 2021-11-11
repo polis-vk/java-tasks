cae6100a523b54c07cd6c2806adaf45762911ec7
PaaavelZ <paaavel.2024@gmail.com>
2021-11-11 14:32:12 +0300

    Rollback description of CopyFile-task

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -5,6 +5,12 @@ import java.nio.file.*;
 
 public class CopyFile {
 
+    /**
+     * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
+     * Файлы копировать ручками через стримы. Исползуем новый API
+     * В тесте для создания нужных файлов для первого запуска надо расскоментировать код в setUp()
+     * 3 балла
+     */
     public static String copyFiles(String pathFrom, String pathTo) {
         Path from = Paths.get(pathFrom);
         Path to = Paths.get(pathTo);
