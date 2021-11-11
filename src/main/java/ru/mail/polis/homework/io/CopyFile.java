132f2781b90c1ba48ca8fa451b2badfbaf446b40
Alexandra-Tkachenko <91024312+Alexandra-Tkachenko@users.noreply.github.com>
2021-11-12 02:22:26 +0300

    Update CopyFile.java

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,12 @@
 package ru.mail.polis.homework.io;
 
+import java.io.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 public class CopyFile {
 
     /**
@@ -8,8 +15,53 @@ public class CopyFile {
      * В тесте для создания нужных файлов для первого запуска надо расскоментировать код в setUp()
      * 3 балла
      */
+
+    public static final int BUFFER_SIZE = 1024;
+
     public static String copyFiles(String pathFrom, String pathTo) {
-        return null;
+        if (Objects.requireNonNull(pathFrom).isEmpty() || Objects.requireNonNull(pathTo).isEmpty()
+                || pathFrom.equals(pathTo)) {
+            return null;
+        }
+        Path from = Paths.get(pathFrom);
+        if (!Files.exists(from)) {
+            return null;
+        }
+        Path to = Paths.get(pathTo);
+        try {
+            if (!Files.exists(to)) {
+                Files.createDirectories(Files.isRegularFile(from) ? to.getParent() : to);
+            }
+            copyFileInside(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return pathTo;
     }
 
+    private static void copyFileInside(Path from, Path to) throws IOException {
+        if (!Files.isDirectory(from)) {
+            copyFile(from, to);
+            return;
+        }
+        if (!Files.exists(to)) {
+            Files.createDirectory(to);
+        }
+        try (DirectoryStream<Path> paths = Files.newDirectoryStream(from)) {
+            for (Path path : paths) {
+                copyFileInside(path, to.resolve(from.relativize(path)));
+            }
+        }
+    }
+
+    private static void copyFile(Path from, Path to) throws IOException {
+        try (InputStream is = Files.newInputStream(from);
+             OutputStream os = Files.newOutputStream(to)) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        }
+    }
 }
