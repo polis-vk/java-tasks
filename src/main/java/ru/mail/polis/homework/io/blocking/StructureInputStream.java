6f4eea4168006cde842125992beefe031ee26e59
mariohuq <mariohuq@github.com>
2021-11-13 01:51:27 +0300

    Structure: solve, tested

--- src/main/java/ru/mail/polis/homework/io/blocking/StructureInputStream.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureInputStream.java
@@ -1,9 +1,9 @@
 package ru.mail.polis.homework.io.blocking;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Вам нужно реализовать StructureInputStream, который умеет читать данные из файла.
@@ -12,19 +12,37 @@ import java.io.IOException;
  */
 public class StructureInputStream extends FileInputStream {
 
-    private Structure[] structures;
+    private final List<Structure> structures = new ArrayList<>();
 
     public StructureInputStream(File fileName) throws FileNotFoundException {
         super(fileName);
     }
-
 
     /**
      * Метод должен вернуть следующую прочитанную структуру.
      * Если структур в файле больше нет, то вернуть null
      */
     public Structure readStructure() throws IOException {
-        return null;
+        Structure result = new Structure();
+        try {
+            result.setId(readLong());
+            result.setName(readNullableString());
+            result.setSubStructures(readNullableSubstructures());
+            result.setCoeff(readFloat());
+            int bits = readUnsignedByte();
+            result.setFlag1(cast(bits));
+            bits >>= 1;
+            result.setFlag2(cast(bits));
+            bits >>= 1;
+            result.setFlag3(cast(bits));
+            bits >>= 1;
+            result.setFlag4(cast(bits));
+            result.setParam(readByte());
+            structures.add(result);
+            return result;
+        } catch (EOFException e) {
+            return null;
+        }
     }
 
     /**
@@ -32,6 +50,99 @@ public class StructureInputStream extends FileInputStream {
      * Если файл уже прочитан, но возвращается полный массив.
      */
     public Structure[] readStructures() throws IOException {
-        return new Structure[0];
+        while (readStructure() != null) {
+            // should be empty
+        }
+        return structures.toArray(new Structure[0]);
+    }
+
+    private long readLong() throws IOException {
+        long result = 0;
+        for (int i = 0; i < Long.BYTES; i++) {
+            result |= (long) readUnsignedByte() << (i * 8);
+        }
+        return result;
+    }
+
+    private String readNullableString() throws IOException {
+        if (!readBoolean()) {
+            return null;
+        }
+        return readString();
+    }
+
+    private SubStructure[] readNullableSubstructures() throws IOException {
+        if (!readBoolean()) {
+            return null;
+        }
+        return readSubstructures();
+    }
+
+    private SubStructure[] readSubstructures() throws IOException {
+        final SubStructure[] result = new SubStructure[readInt()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = readNullableSubstructure();
+        }
+        return result;
+    }
+
+    private SubStructure readNullableSubstructure() throws IOException {
+        if (!readBoolean()) {
+            return null;
+        }
+        return readSubstructure();
+    }
+
+    private SubStructure readSubstructure() throws IOException {
+        return new SubStructure(readInt(), readString(), readBoolean(), readDouble());
+    }
+
+    private int readInt() throws IOException {
+        int result = 0;
+        for (int i = 0; i < Integer.BYTES; i++) {
+            result |= readUnsignedByte() << (i * 8);
+        }
+        return result;
+    }
+
+    private String readString() throws IOException {
+        byte[] bytes = new byte[readInt()];
+        if (read(bytes) != bytes.length) {
+            throw new EOFException();
+        }
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+
+    private double readDouble() throws IOException {
+        return Double.longBitsToDouble(readLong());
+    }
+
+    private float readFloat() throws IOException {
+        return Float.intBitsToFloat(readInt());
+    }
+
+    private boolean readBoolean() throws IOException {
+        int bits = read();
+        if (bits < 0) {
+            throw new EOFException();
+        }
+        return cast(bits);
+    }
+
+    private byte readByte() throws IOException {
+        return (byte) (readUnsignedByte() - 128);
+    }
+
+    private int readUnsignedByte() throws IOException {
+        int bits = read();
+        if (bits < 0) {
+            throw new EOFException();
+        }
+        return bits;
+    }
+
+    private boolean cast(int value) {
+        return (value & 1) != 0;
     }
 }
--- src/main/java/ru/mail/polis/homework/io/blocking/StructureOutputStream.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureOutputStream.java
@@ -1,9 +1,7 @@
 package ru.mail.polis.homework.io.blocking;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Вам нужно реализовать StructureOutputStream, который умеет писать данные в файл.
@@ -17,15 +15,108 @@ public class StructureOutputStream extends FileOutputStream {
     }
 
     /**
-     * Метод должен вернуть записать прочитанную структуру.
+     * Метод должен записать прочитанную структуру.
      */
     public void write(Structure structure) throws IOException {
+        writeLong(structure.getId());
+        writeNullableString(structure.getName());
+        writeNullableSubstructures(structure.getSubStructures());
+        writeFloat(structure.getCoeff());
+        int bits = 0;
+        bits |= cast(structure.isFlag4());
+        bits <<= 1;
+        bits |= cast(structure.isFlag3());
+        bits <<= 1;
+        bits |= cast(structure.isFlag2());
+        bits <<= 1;
+        bits |= cast(structure.isFlag1());
+        write(bits);
+        writeByte(structure.getParam());
     }
 
     /**
      * Метод должен вернуть записать массив прочитанных структур.
      */
     public void write(Structure[] structures) throws IOException {
+        for (Structure structure : structures) {
+            write(structure);
+        }
+    }
 
+    private void writeLong(long value) throws IOException {
+        long copied = value;
+        for (int i = 0; i < Long.BYTES; i++) {
+            write((int) (copied & 255));
+            copied >>= 8;
+        }
+    }
+
+    private void writeNullableString(String value) throws IOException {
+        writeBoolean(value != null);
+        if (value != null) {
+            writeString(value);
+        }
+    }
+
+    private void writeNullableSubstructures(SubStructure[] values) throws IOException {
+        writeBoolean(values != null);
+        if (values != null) {
+            writeSubstructures(values);
+        }
+    }
+
+    private void writeSubstructures(SubStructure[] values) throws IOException {
+        writeInt(values.length);
+        for (SubStructure value : values) {
+            writeNullableSubstructure(value);
+        }
+    }
+
+    private void writeNullableSubstructure(SubStructure value) throws IOException {
+        writeBoolean(value != null);
+        if (value != null) {
+            writeSubstructure(value);
+        }
+    }
+
+    private void writeSubstructure(SubStructure value) throws IOException {
+        writeInt(value.getId());
+        writeString(value.getName());
+        writeBoolean(value.isFlag());
+        writeDouble(value.getScore());
+    }
+
+    private void writeInt(int value) throws IOException {
+        int copied = value;
+        for (int i = 0; i < Integer.BYTES; i++) {
+            write(copied & 255);
+            copied >>= 8;
+        }
+    }
+
+    private void writeString(String value) throws IOException {
+        byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
+        writeInt(bytes.length);
+        write(bytes);
+    }
+
+    private void writeDouble(double value) throws IOException {
+        writeLong(Double.doubleToLongBits(value));
+    }
+
+    private void writeFloat(float value) throws IOException {
+        writeInt(Float.floatToIntBits(value));
+    }
+
+    private void writeBoolean(boolean value) throws IOException {
+        write(cast(value));
+    }
+
+    private void writeByte(byte value) throws IOException {
+        write(value + 128);
+    }
+
+    private int cast(boolean value) {
+        return value ? 1 : 0;
     }
 }
