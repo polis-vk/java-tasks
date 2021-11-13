50e316ef8a570302be936dec687569dd08e8f482
Sergey Esipenko <sergey.esipenko@corp.mail.ru>
2021-11-13 19:38:25 +0300

    add test on primitive arrays

--- src/test/java/ru/mail/polis/homework/reflection/HardTest.java
+++ src/test/java/ru/mail/polis/homework/reflection/HardTest.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.reflection;
 
+import java.util.Arrays;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import ru.mail.polis.homework.reflection.objects.hard.Gender;
@@ -32,5 +35,37 @@ public class HardTest extends TestBase {
         Medium abaca = new Medium(21, "Abaca Dacaba", 57.0);
         Hard abra = new Hard(24, "Abra Cadabra", new String[] { "abracadabra", null, "", "a" }, 60.0, abaca, Gender.FEMALE, 100);
         testToString(abra, "{bestFriend: {age: 21, name: Abaca Dacaba}, gender: FEMALE, name: Abra Cadabra, nicknames: [abracadabra, null, , a], age: 24}");
+    }
+
+    @Test
+    public void testIntArrayNull() {
+        IntArrayHolder x = new IntArrayHolder(null);
+        String expected = "{array: null}";
+        String actual = ReflectionToStringHelper.reflectiveToString(x);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIntArrayEmpty() {
+        IntArrayHolder x = new IntArrayHolder(new int[0]);
+        String expected = "{array: []}";
+        String actual = ReflectionToStringHelper.reflectiveToString(x);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIntArray() {
+        IntArrayHolder x = new IntArrayHolder(new int[] {1, 2, 3});
+        String expected = "{array: [1, 2, 3]}";
+        String actual = ReflectionToStringHelper.reflectiveToString(x);
+        Assert.assertEquals(expected, actual);
+    }
+
+    private static class IntArrayHolder {
+        private final int[] array;
+
+        private IntArrayHolder(int[] array) {
+            this.array = array;
+        }
     }
 }
