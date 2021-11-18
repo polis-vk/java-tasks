4d1cfeaf0aaf958d26c50f156f52f1f35548bf5b
Sanerins <askosnitskiy@gmail.com>
2021-11-18 15:18:34 +0300

    Initial implementation

--- src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
+++ src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
@@ -1,49 +1,118 @@
 package ru.mail.polis.homework.reflection;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Необходимо реализовать метод reflectiveToString, который для произвольного объекта
  * возвращает его строковое описание в формате:
- *
+ * <p>
  * {field_1: value_1, field_2: value_2, ..., field_n: value_n}
- *
+ * <p>
  * где field_i - имя поля
- *     value_i - его строковое представление (String.valueOf),
- *               за исключением массивов, для которых value формируется как:
- *               [element_1, element_2, ..., element_m]
- *                 где element_i - строковое представление элемента (String.valueOf)
- *                 элементы должны идти в том же порядке, что и в массиве.
- *
+ * value_i - его строковое представление (String.valueOf),
+ * за исключением массивов, для которых value формируется как:
+ * [element_1, element_2, ..., element_m]
+ * где element_i - строковое представление элемента (String.valueOf)
+ * элементы должны идти в том же порядке, что и в массиве.
+ * <p>
  * Все null'ы следует представлять строкой "null".
- *
+ * <p>
  * Порядок полей
  * Сначала следует перечислить в алфавитном порядке поля, объявленные непосредственно в классе объекта,
  * потом в алфавитном порядке поля объявленные в родительском классе и так далее по иерархии наследования.
  * Примеры можно посмотреть в тестах.
- *
+ * <p>
  * Какие поля выводить
  * Необходимо включать только нестатические поля. Также нужно пропускать поля, помеченные аннотацией @SkipField
- *
+ * <p>
  * Упрощения
  * Чтобы не усложнять задание, предполагаем, что нет циклических ссылок, inner классов, и transient полей
- *
+ * <p>
  * Реализация
  * В пакете ru.mail.polis.homework.reflection можно редактировать только этот файл
  * или добавлять новые (не рекомендуется, т.к. решение вполне умещается тут в несколько методов).
  * Редактировать остальные файлы нельзя.
- *
+ * <p>
  * Баллы
  * В задании 3 уровня сложности, для каждого свой набор тестов:
- *   Easy - простой класс, нет наследования, массивов, статических полей, аннотации SkipField (4 балла)
- *   Medium - нет наследования, массивов, но есть статические поля и поля с аннотацией SkipField (6 баллов)
- *   Hard - нужно реализовать все требования задания (10 баллов)
- *
+ * Easy - простой класс, нет наследования, массивов, статических полей, аннотации SkipField (4 балла)
+ * Medium - нет наследования, массивов, но есть статические поля и поля с аннотацией SkipField (6 баллов)
+ * Hard - нужно реализовать все требования задания (10 баллов)
+ * <p>
  * Итого, по заданию можно набрать 20 баллов
  * Баллы могут снижаться за неэффективный или неаккуратный код
  */
 public class ReflectionToStringHelper {
 
     public static String reflectiveToString(Object object) {
-        // TODO: implement
-        return null;
+        if (object == null) {
+            return "null";
+        }
+        StringBuilder result = new StringBuilder();
+        Class<?> objClass = object.getClass();
+        result.append('{');
+
+        do {
+            result.append(getFields(objClass, object));
+            objClass = objClass.getSuperclass();
+        } while (!objClass.equals(Object.class));
+        if (result.length() > 1) {
+            result.setLength(result.length() - 2);
+        }
+        result.append('}');
+        return result.toString();
+    }
+
+    private static String getFields(Class<?> objClass, Object object) {
+        StringBuilder result = new StringBuilder();
+        List<Field> fields = Arrays.asList(objClass.getDeclaredFields());
+        fields.sort(Comparator.comparing(Field::getName));
+        for (Field field : fields) {
+            try {
+                if (!field.isAnnotationPresent(SkipField.class) && !Modifier.isStatic(field.getModifiers())) {
+                    result.append(field.getName());
+                    result.append(": ");
+                    field.setAccessible(true);
+                    if (field.get(object) == null) {
+                        result.append("null");
+                    } else if (field.getType().isArray()) {
+                        result.append(printArray(field.get(object)));
+                    } else {
+                        result.append(field.get(object));
+                    }
+                    result.append(", ");
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return result.toString();
+    }
+
+    private static String printArray(Object arr) {
+        StringBuilder result = new StringBuilder();
+        result.append("[");
+
+        Object element;
+        for (int i = 0; i < Array.getLength(arr); i++) {
+            element = Array.get(arr, i);
+            if (element == null) {
+                result.append("null");
+            } else {
+                result.append(Array.get(arr, i));
+            }
+            result.append(", ");
+        }
+        if (result.length() > 1) {
+            result.setLength(result.length() - 2);
+        }
+
+        result.append("]");
+        return result.toString();
     }
 }
