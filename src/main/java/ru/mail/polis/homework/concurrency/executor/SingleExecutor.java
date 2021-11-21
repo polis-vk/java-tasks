5a735b6112ecce49bfa73d92aafc22bda4d4c8ea
Komissarov Gleb <gl.k@aol.com>
2021-11-21 23:35:46 +0300

    Single executor

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,25 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 баллов
  */
 public class SingleExecutor implements Executor {
 
+    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+    private final CustomThread thread = new CustomThread();
+
+    public SingleExecutor() {
+        thread.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +27,11 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (thread.getRunningStatus()) {
+            queue.add(command);
+        } else {
+            throw new RejectedExecutionException();
+        }
     }
 
     /**
@@ -25,6 +39,8 @@ public class SingleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        thread.setFlushTrue();
+        thread.setRunningFalse();
     }
 
     /**
@@ -32,6 +48,40 @@ public class SingleExecutor implements Executor {
      * 2 балла за метод
      */
     public void shutdownNow() {
+        thread.setRunningFalse();
+        thread.interrupt();
+    }
 
+    private class CustomThread extends Thread {
+
+        private boolean running = true;
+        private boolean flush = false;
+
+        @Override
+        public void run() {
+            while (running) {
+                if (!queue.isEmpty()) {
+                    queue.poll().run();
+                }
+            }
+
+            if (flush) {
+                for (Runnable runnable : queue) {
+                    runnable.run();
+                }
+            }
+        }
+
+        public void setRunningFalse() {
+            running = false;
+        }
+
+        public boolean getRunningStatus() {
+            return running;
+        }
+
+        public void setFlushTrue() {
+            flush = true;
+        }
     }
 }
