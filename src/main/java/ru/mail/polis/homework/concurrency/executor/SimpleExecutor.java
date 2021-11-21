c2ef4742d7d3394d7754753b22164c7580082ebb
Dmitree-Max <d.s.maximenko@gmail.com>
2021-11-21 18:20:39 +0300

    Did homework 9. Tests are NOT PASS stably maybe due to bugs in code or in tests . Dmitree Maximenko.

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,22 +1,34 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
  * Ленивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 баллов
  */
 public class SimpleExecutor implements Executor {
 
-    public SimpleExecutor(int maxThreadCount) {
+    private final BlockingQueue<Runnable> tasks;
+    private final List<Thread> threads;
+    private final int maxThreads;
+    private volatile boolean finish = false;
 
+    public SimpleExecutor(int maxThreadCount) {
+        maxThreads = maxThreadCount;
+        tasks = new LinkedBlockingQueue<>();
+        threads = new ArrayList<>();
     }
 
     /**
@@ -25,7 +37,16 @@ public class SimpleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
-
+        if (finish) {
+            throw new RejectedExecutionException();
+        }
+        if (!doWeHaveWaitingThreads() && canWeCreateThread()) {
+            createThread();
+        }
+        synchronized (tasks) {
+            tasks.add(command);
+            tasks.notify();
+        }
     }
 
     /**
@@ -33,6 +54,7 @@ public class SimpleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        finish = true;
     }
 
     /**
@@ -40,13 +62,64 @@ public class SimpleExecutor implements Executor {
      * 1 балла за метод
      */
     public void shutdownNow() {
-
+        finish = true;
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        synchronized (threads) {
+            return threads.size();
+        }
+    }
+
+    private boolean doWeHaveWaitingThreads() {
+        synchronized (threads) {
+            for (Thread thread : threads) {
+                if (thread.getState() == Thread.State.WAITING) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean canWeCreateThread() {
+        synchronized (threads) {
+            return threads.size() < maxThreads;
+        }
+    }
+
+    private void createThread() {
+        Thread newThread = new Thread(() -> {
+            while (true) {
+                Runnable task;
+
+                synchronized (tasks) {
+                    while (tasks.isEmpty()) {
+                        try {
+                            tasks.wait();
+                        } catch (InterruptedException e) {
+                            return;
+                        }
+                    }
+
+                    task = tasks.poll();
+                }
+
+                task.run();
+            }
+        });
+
+        synchronized (threads) {
+            threads.add(newThread);
+        }
+
+        newThread.start();
     }
 }
--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,35 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 баллов
  */
 public class SingleExecutor implements Executor {
+    private final Thread worker;
+    private final BlockingQueue<Runnable> tasks;
+    private volatile boolean finish = false;
 
+    public SingleExecutor() {
+        tasks = new LinkedBlockingQueue<>();
+        worker = new Thread(() -> {
+            while (true) {
+                try {
+                    tasks.take().run();
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
+        });
+        worker.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +37,10 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (finish) {
+            throw new RejectedExecutionException();
+        }
+        tasks.add(command);
     }
 
     /**
@@ -25,6 +48,7 @@ public class SingleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        finish = true;
     }
 
     /**
@@ -32,6 +56,7 @@ public class SingleExecutor implements Executor {
      * 2 балла за метод
      */
     public void shutdownNow() {
-
+        finish = true;
+        worker.interrupt();
     }
 }
