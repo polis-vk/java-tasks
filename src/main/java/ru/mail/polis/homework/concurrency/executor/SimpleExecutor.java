0688e1412fccdfec87c2c12cddf569df7c211fe4
Komissarov Gleb <gl.k@aol.com>
2021-11-23 17:53:35 +0300

    SimpleExecutor

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,22 +1,34 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
  * Ленивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 баллов
  */
 public class SimpleExecutor implements Executor {
 
-    public SimpleExecutor(int maxThreadCount) {
+    private final List<CustomThread> threads = new CopyOnWriteArrayList<>();
+    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+    private final AtomicInteger available = new AtomicInteger(0);
+    private final int MAX_COUNT;
+    private boolean ending = true;
 
+    public SimpleExecutor(int maxThreadCount) {
+        MAX_COUNT = maxThreadCount;
     }
 
     /**
@@ -24,8 +36,19 @@ public class SimpleExecutor implements Executor {
      * 8 баллов
      */
     @Override
-    public void execute(Runnable command) {
+    public synchronized void execute(Runnable command) {
+        if (command == null) {
+            throw new NullPointerException();
+        }
 
+        if (ending) {
+            queue.add(command);
+            if (available.get() == threads.size() && threads.size() < MAX_COUNT) {
+                threads.add(new CustomThread());
+            }
+        } else {
+            throw new RejectedExecutionException();
+        }
     }
 
     /**
@@ -33,6 +56,10 @@ public class SimpleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        ending = false;
+        for (CustomThread thread : threads) {
+            thread.kill();
+        }
     }
 
     /**
@@ -40,6 +67,11 @@ public class SimpleExecutor implements Executor {
      * 1 балла за метод
      */
     public void shutdownNow() {
+        ending = false;
+        for (CustomThread thread : threads) {
+            thread.kill();
+            thread.interrupt();
+        }
 
     }
 
@@ -47,6 +79,44 @@ public class SimpleExecutor implements Executor {
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        return threads.size();
+    }
+
+    private class CustomThread extends Thread {
+
+        private boolean alive = true;
+        private boolean start = false;
+
+        public CustomThread() {
+            available.incrementAndGet();
+            start();
+        }
+
+        @Override
+        public void run() {
+            Runnable check;
+
+            while (alive) {
+                if (!queue.isEmpty()) {
+                    try {
+                        check = queue.take();
+                        if (start) {
+                            available.incrementAndGet();
+                        }
+
+                        check.run();
+                        available.decrementAndGet();
+                        start = true;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        public void kill() {
+            alive = false;
+        }
+
     }
 }
