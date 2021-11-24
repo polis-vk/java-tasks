cc89cb67eeeef5ed0e2486ba16eb093ab3e7c7b6
PaaavelZ <paaavel.2024@gmail.com>
2021-11-24 21:11:26 +0300

    Initialize hw5

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,7 +1,12 @@
 package ru.mail.polis.homework.streams.lib;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Класс для работы со статистикой по библиотеке.
@@ -18,7 +23,12 @@ public class LibraryStatistic {
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return null;
+        List<ArchivedData> filteredByGenreList = library.getArchive().stream()
+                .filter(user -> user.getBook().getGenre().equals(genre))
+                .collect(Collectors.toList());
+        return filteredByGenreList.stream()
+                .filter(user -> countBooksWhichWereReadFor14OrMoreDays(user.getUser(), filteredByGenreList) >= 5)
+                .collect(Collectors.toMap(ArchivedData::getUser, user -> user.getBook().getPage()));
     }
 
     /**
@@ -29,7 +39,20 @@ public class LibraryStatistic {
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        return null;
+        Stream<ArchivedData> ads1 = library.getArchive().stream()
+                .filter(data -> data.getReturned() != null);
+        Stream<Map.Entry<Genre, Integer>> ads2 = ads1.collect(Collectors.toMap(data -> data.getBook().getGenre(),
+                data -> countBooksInDefinedGenre(ads1, data.getBook().getGenre())))
+                .entrySet().stream()
+                    .sorted((v1, v2) -> v2.getValue().compareTo(v1.getValue()));
+        Stream<Map.Entry<Genre, Integer>> finalStream = ads2
+                .takeWhile(pair -> pair.getValue().equals(ads2.findAny().get().getValue()));
+
+        if (hasGenre(finalStream, user.getBook().getGenre())) {
+            return user.getBook().getGenre();
+        } else {
+            return finalStream.findAny().get().getKey();
+        }
     }
 
     /**
@@ -39,7 +62,11 @@ public class LibraryStatistic {
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        return null;
+        return library.getArchive().stream()
+                .collect(Collectors.groupingBy(ArchivedData::getUser)).entrySet().stream()
+                .filter(user -> isUnreliableUser(user.getValue()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -49,7 +76,9 @@ public class LibraryStatistic {
      * @return - список книг
      */
     public List<Book> booksWithMoreCountPages(Library library, int countPage) {
-        return null;
+        return library.getBooks().stream()
+                .filter(book -> book.getPage() >= countPage)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -58,6 +87,64 @@ public class LibraryStatistic {
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-        return null;
+        return library.getBooks().stream()
+                .collect(Collectors.groupingBy(Book::getGenre)).entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, pair -> findMostPopularAuthor(pair.getValue())));
     }
+
+    private long countBooksWhichWereReadFor14OrMoreDays(User u, List<ArchivedData> l) {
+        return l.stream()
+                .filter(record -> record.getUser().equals(u)
+                        && readAllBook(record)
+                        && (elapsedNDays(record.getTake(), record.getReturned(), 14) >= 0))
+                .count();
+    }
+
+    private boolean readAllBook(ArchivedData ad) {
+        return ad.getUser().getReadedPages() == ad.getBook().getPage();
+    }
+
+    // Returns timestamps' comparing like typical 'comparing' operation
+    private int elapsedNDays(Timestamp start, Timestamp end, int N) {
+        Timestamp _end = end;
+        if (_end == null) {
+            _end = Timestamp.valueOf(LocalDateTime.now());
+        }
+        int day1 = Integer.parseInt(start.toString().substring(8, 9));
+        int day2 = Integer.parseInt(_end.toString().substring(8, 9));
+        int result = Math.abs(day1 - day2);
+        return Integer.compare(result, N);
+    }
+
+    private int countBooksInDefinedGenre(Stream<ArchivedData> ads, Genre genre) {
+        return ads.mapToInt(ad -> (genre.equals(ad.getBook().getGenre())) ? 1 : 0).sum();
+    }
+
+    private boolean hasGenre(Stream<Map.Entry<Genre, Integer>> ads, Genre genre) {
+        return ads.filter(ad -> ad.getKey().equals(genre)).count() == 1;
+    }
+
+    private String findMostPopularAuthor(List<Book> books) {
+        return books.stream()
+                .collect(Collectors.toMap(Book::getAuthor, book -> Collections.frequency(books, book.getAuthor())))
+                .entrySet().stream()
+                .sorted(this::compareAuthors)
+                .findAny().get().getKey();
+    }
+
+    private int compareAuthors(Map.Entry<String, Integer> author1, Map.Entry<String, Integer> author2) {
+        int comparing1 = author1.getValue().compareTo(author2.getValue());
+        if (comparing1 != 0) {
+            return comparing1;
+        }
+        return author1.getKey().compareTo(author2.getKey());
+    }
+
+    private boolean isUnreliableUser(List<ArchivedData> data) {
+        long nOverdues = data.stream()
+                .filter(d -> elapsedNDays(d.getTake(), d.getReturned(), 14) > 0)
+                .count();
+        return (double)nOverdues/data.size() > 0.5;
+    }
+
 }
--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -3,6 +3,7 @@ package ru.mail.polis.homework.streams.store;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
@@ -20,7 +21,11 @@ public class StoreStatistic {
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return 0L;
+        return orders.stream()
+                .filter(order -> isTimestampBetween(order.getTime(), from, to))
+                .flatMap(order -> order.getItemCount().keySet().stream())
+                .filter(item -> item.equals(typeItem))
+                .count();
     }
 
     /**
@@ -30,7 +35,9 @@ public class StoreStatistic {
      * значение - map товар/кол-во
      */
     public Map<Timestamp, Map<Item, Integer>> statisticItemsByDay(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .map(order -> new Order(normalizeTimestamp(order.getTime()), order.getItemCount()))
+                .collect(Collectors.toMap(Order::getTime, Order::getItemCount));
     }
 
     /**
@@ -39,7 +46,10 @@ public class StoreStatistic {
      * @return - товар
      */
     public Item mostPopularItem(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .flatMap(order -> order.getItemCount().entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
     }
 
     /**
@@ -48,6 +58,28 @@ public class StoreStatistic {
      * @return map - заказ / общая сумма заказа
      */
     public Map<Order, Long> sum5biggerOrders(List<Order> orders) {
-        return null;
+        return orders.stream().collect(Collectors.toMap(order -> order, this::countTotalPrice))
+                .entrySet().stream()
+                .sorted((pair1, pair2) -> pair2.getValue().compareTo(pair1.getValue()))
+                .limit(5)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
+
+    private boolean isTimestampBetween(Timestamp src, Timestamp from, Timestamp to) {
+        return src.compareTo(from) >= 0 && src.compareTo(to) <= 0;
+    }
+
+    private Timestamp normalizeTimestamp(Timestamp src) {
+        String normalized = src.toString().substring(0, 10);
+        return Timestamp.valueOf(normalized + " 00:00:00 000 UTC");
+    }
+
+    private long countTotalPrice(Order o) {
+        long totalPrice = 0;
+        for (Map.Entry<Item, Integer> item: o.getItemCount().entrySet()) {
+            totalPrice += item.getValue();
+        }
+        return totalPrice;
+    }
+
 }
