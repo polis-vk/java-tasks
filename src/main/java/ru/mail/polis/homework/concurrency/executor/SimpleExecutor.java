f63ea32b5752d24f0301d4da1decc79f53b8f160
VladislavFetisov <fetisov2.vv@edu.spbstu.ru>
2021-11-24 20:11:33 +0300

    Homework_9_Vladislav_Fetisov

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,22 +1,33 @@
 package ru.mail.polis.homework.concurrency.executor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
  * Ленивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 баллов
  */
 public class SimpleExecutor implements Executor {
+    private final LinkedBlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
+    private final AtomicInteger threadsCount = new AtomicInteger(0);
+    private final int maxThreadsCount;
+    private final AtomicBoolean shutdownCalled = new AtomicBoolean(false);
+    private final AtomicInteger freeThreadsCount = new AtomicInteger(0);
+    private final SingleThread[] threads;
 
     public SimpleExecutor(int maxThreadCount) {
-
+        threads = new SingleThread[maxThreadCount];
+        this.maxThreadsCount = maxThreadCount;
     }
 
     /**
@@ -25,7 +36,18 @@ public class SimpleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (shutdownCalled.get()) {
+            throw new RejectedExecutionException();
+        }
 
+        tasks.add(command);
+        synchronized (threadsCount) {
+            if (threadsCount.get() < maxThreadsCount && !tasks.isEmpty() && freeThreadsCount.get() == 0) {
+                SingleThread singleThread = new SingleThread();
+                singleThread.start();
+                threads[threadsCount.getAndIncrement()] = singleThread;
+            }
+        }
     }
 
     /**
@@ -33,6 +55,7 @@ public class SimpleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        shutdownCalled.set(true);
     }
 
     /**
@@ -40,13 +63,35 @@ public class SimpleExecutor implements Executor {
      * 1 балла за метод
      */
     public void shutdownNow() {
-
+        shutdownCalled.set(true);
+        for (int i = 0; i < threadsCount.get(); i++) {
+            threads[i].interrupt();
+        }
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        return threadsCount.get();
+    }
+
+    private class SingleThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (!shutdownCalled.get()) {
+                    freeThreadsCount.incrementAndGet();
+                    Runnable task = tasks.take();
+                    freeThreadsCount.decrementAndGet();
+                    task.run();
+                }
+            } catch (InterruptedException e) {
+                System.err.println(Thread.currentThread().getName() + " was interrupted");
+            }
+        }
     }
 }
+
+
+
--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,25 @@
 package ru.mail.polis.homework.concurrency.executor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 баллов
  */
 public class SingleExecutor implements Executor {
+    private final LinkedBlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
+    private final AtomicBoolean shutdownCalled = new AtomicBoolean(false);
+    private final SingleThread singleThread = new SingleThread();
 
+    {
+        singleThread.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +27,10 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (shutdownCalled.get()) {
+            throw new RejectedExecutionException();
+        }
+        tasks.add(command);
     }
 
     /**
@@ -25,6 +38,7 @@ public class SingleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        shutdownCalled.set(true);
     }
 
     /**
@@ -32,6 +46,23 @@ public class SingleExecutor implements Executor {
      * 2 балла за метод
      */
     public void shutdownNow() {
-
+        shutdownCalled.set(true);
+        singleThread.interrupt();
     }
+
+    private class SingleThread extends Thread {
+        @Override
+        public void run() {
+            while (!shutdownCalled.get() || !tasks.isEmpty()) {
+                try {
+                    Runnable task = tasks.take();
+                    task.run();
+                } catch (InterruptedException e) {
+                    System.out.println("I was interrupted");
+                    return;
+                }
+            }
+        }
+    }
+
 }
