1c0b3632d3b23e8395061c54cc768e8d987a3323
Alexandra-Tkachenko <91024312+Alexandra-Tkachenko@users.noreply.github.com>
2021-11-25 19:49:30 +0300

    Update ReflectionToStringHelper.java

--- src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
+++ src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
@@ -1,49 +1,64 @@
 package ru.mail.polis.homework.reflection;
 
-/**
- * Необходимо реализовать метод reflectiveToString, который для произвольного объекта
- * возвращает его строковое описание в формате:
- *
- * {field_1: value_1, field_2: value_2, ..., field_n: value_n}
- *
- * где field_i - имя поля
- *     value_i - его строковое представление (String.valueOf),
- *               за исключением массивов, для которых value формируется как:
- *               [element_1, element_2, ..., element_m]
- *                 где element_i - строковое представление элемента (String.valueOf)
- *                 элементы должны идти в том же порядке, что и в массиве.
- *
- * Все null'ы следует представлять строкой "null".
- *
- * Порядок полей
- * Сначала следует перечислить в алфавитном порядке поля, объявленные непосредственно в классе объекта,
- * потом в алфавитном порядке поля объявленные в родительском классе и так далее по иерархии наследования.
- * Примеры можно посмотреть в тестах.
- *
- * Какие поля выводить
- * Необходимо включать только нестатические поля. Также нужно пропускать поля, помеченные аннотацией @SkipField
- *
- * Упрощения
- * Чтобы не усложнять задание, предполагаем, что нет циклических ссылок, inner классов, и transient полей
- *
- * Реализация
- * В пакете ru.mail.polis.homework.reflection можно редактировать только этот файл
- * или добавлять новые (не рекомендуется, т.к. решение вполне умещается тут в несколько методов).
- * Редактировать остальные файлы нельзя.
- *
- * Баллы
- * В задании 3 уровня сложности, для каждого свой набор тестов:
- *   Easy - простой класс, нет наследования, массивов, статических полей, аннотации SkipField (4 балла)
- *   Medium - нет наследования, массивов, но есть статические поля и поля с аннотацией SkipField (6 баллов)
- *   Hard - нужно реализовать все требования задания (10 баллов)
- *
- * Итого, по заданию можно набрать 20 баллов
- * Баллы могут снижаться за неэффективный или неаккуратный код
- */
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.*;
+
 public class ReflectionToStringHelper {
+    public static final String NULL = "null";
 
     public static String reflectiveToString(Object object) {
-        // TODO: implement
-        return null;
+        if (object == null) {
+            return NULL;
+        }
+        StringBuilder builder = new StringBuilder("{");
+        Class<?> objectClass = object.getClass();
+        while (objectClass != null) {
+            try {
+                builder.append(filter(objectClass.getDeclaredFields(), object));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            objectClass = objectClass.getSuperclass();
+        }
+        if (builder.length() != 1) {
+            builder.setLength(builder.length() - 2);
+        }
+        return builder.append("}").toString();
+    }
+
+    private static StringBuilder filter(Field[] fields, Object object) throws IllegalAccessException {
+        Arrays.sort(fields, Comparator.comparing(Field::getName));
+        StringBuilder builder = new StringBuilder();
+        for (Field field : fields) {
+            if (Modifier.isStatic(field.getModifiers()) || field.isAnnotationPresent(SkipField.class)) {
+                continue;
+            }
+            field.setAccessible(true);
+            builder.append(field.getName()).append(": ");
+            addField(field.get(object), builder);
+            builder.append(", ");
+        }
+        return builder;
+    }
+
+    private static void addField(Object object, StringBuilder builder) {
+        if (object == null) {
+            builder.append(NULL);
+            return;
+        }
+        if (!object.getClass().isArray()) {
+            builder.append(object);
+            return;
+        }
+        builder.append("[");
+        for (int i = 0; i < Array.getLength(object); ++i) {
+            builder.append(Array.get(object, i));
+            if (i != Array.getLength(object) - 1) {
+                builder.append(", ");
+            }
+        }
+        builder.append("]");
     }
 }
