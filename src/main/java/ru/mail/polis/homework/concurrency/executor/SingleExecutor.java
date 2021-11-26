08cf2d33028862a430c0925c637d67ba4b9e22f3
sorgeligt <ubutno@gmail.com>
2021-11-26 19:03:03 +0300

    SingleExecutor done

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,26 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 баллов
  */
 public class SingleExecutor implements Executor {
+    private AtomicBoolean stop = new AtomicBoolean(false);
+    private final BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
+    private final SingleThread singleThread = new SingleThread();
 
+    public SingleExecutor() {
+        singleThread.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,13 +28,19 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (stop.get()) {
+            throw new RejectedExecutionException();
+        }
+        tasks.add(command);
     }
+
 
     /**
      * Дает текущим задачам выполниться. Добавление новых - бросает RejectedExecutionException
      * 1 балл за метод
      */
     public void shutdown() {
+        stop = new AtomicBoolean(true);
     }
 
     /**
@@ -32,6 +48,21 @@ public class SingleExecutor implements Executor {
      * 2 балла за метод
      */
     public void shutdownNow() {
+        singleThread.interrupt();
+        stop = new AtomicBoolean(true);
+    }
 
+    private class SingleThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                while (!stop.get() || !tasks.isEmpty()) {
+                    Runnable task = tasks.take();
+                    task.run();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
