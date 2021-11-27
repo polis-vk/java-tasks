188659a899aebe5a7ddced8e551798bf353046f3
zadorotskas <zadorotskas@yandex.ru>
2021-11-28 00:37:16 +0300

    Homework 5: StoreStatistic

--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -1,8 +1,8 @@
 package ru.mail.polis.homework.streams.store;
 
 import java.sql.Timestamp;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
@@ -13,41 +13,85 @@ public class StoreStatistic {
     /**
      * Вернуть сколько было продано определенного товара за переданный промежуток времени
      *
-     * @param orders - список заказов
+     * @param orders   - список заказов
      * @param typeItem - вид товара
-     * @param from - с какого момента вести подсчет
-     * @param to - по какой момент вести подсчет
+     * @param from     - с какого момента вести подсчет
+     * @param to       - по какой момент вести подсчет
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return 0L;
+        return orders.stream()
+                .filter(order -> from.after(order.getTime()) && to.before(order.getTime()))
+                .mapToLong(order -> order.getItemCount().getOrDefault(typeItem, 0))
+                .sum();
     }
 
     /**
      * Вернуть статистику по каждому дню сколько какого товара было проданно
+     *
      * @param orders - список заказов
      * @return - map, где ключ - начало дня (00:00:00 000 UTC) для которого собрана статистика
      * значение - map товар/кол-во
      */
     public Map<Timestamp, Map<Item, Integer>> statisticItemsByDay(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .collect(Collectors.groupingBy(
+                        order -> startOfDay(order.getTime())
+                )).entrySet().stream()
+                .collect(Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry -> entry.getValue().stream()
+                                        .flatMap(order -> order.getItemCount().entrySet().stream())
+                                        .collect(Collectors.groupingBy(
+                                                Map.Entry::getKey,
+                                                Collectors.summingInt(Map.Entry::getValue)
+                                        ))
+                        )
+                );
     }
 
     /**
      * Вернуть самый популярный товар
+     *
      * @param orders - список заказов
      * @return - товар
      */
     public Item mostPopularItem(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .flatMap(order -> order.getItemCount().entrySet().stream())
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getKey,
+                        Collectors.summingInt(Map.Entry::getValue)
+                ))
+                .entrySet().stream()
+                .max(Comparator.comparingInt(Map.Entry::getValue))
+                .get().getKey();
     }
 
     /**
      * Вернуть сумму для 5 самых больших по кол-ву товаров заказу.
+     *
      * @param orders - список заказов
      * @return map - заказ / общая сумма заказа
      */
     public Map<Order, Long> sum5biggerOrders(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .sorted(Comparator.comparingInt(order -> order.getItemCount().size()))
+                .limit(5)
+                .collect(Collectors.toMap(
+                        (order) -> order,
+                        (order) -> order.getItemCount().values().stream().mapToLong(it -> it).sum()
+                ));
+    }
+
+    private static Timestamp startOfDay(Timestamp ts) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(ts);
+        calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
+        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return new Timestamp(calendar.getTimeInMillis());
     }
 }
