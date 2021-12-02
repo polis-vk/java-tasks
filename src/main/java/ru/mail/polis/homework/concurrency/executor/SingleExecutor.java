b0944e3e1081cb432570ac8b766319b5c574d7ca
Alexandra-Tkachenko <91024312+Alexandra-Tkachenko@users.noreply.github.com>
2021-12-02 17:47:27 +0300

    SingleExecutor

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,23 +1,36 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 баллов
  */
 public class SingleExecutor implements Executor {
+    private volatile boolean isStopped;
+    private final Thread thread = new Thread(this::run);
+    private final BlockingQueue<Runnable> timetable = new LinkedBlockingQueue<>();
 
+    public SingleExecutor() {
+        thread.start();
+    }
 
-    /**
-     * Метод ставит задачу в очередь на исполнение.
-     * 3 балла за метод
-     */
     @Override
     public void execute(Runnable command) {
+        if (isStopped) {
+            throw new RejectedExecutionException();
+        }
+        try {
+            timetable.put(command);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -25,6 +38,7 @@ public class SingleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        isStopped = true;
     }
 
     /**
@@ -32,6 +46,18 @@ public class SingleExecutor implements Executor {
      * 2 балла за метод
      */
     public void shutdownNow() {
+        shutdown();
+        timetable.clear();
+        thread.interrupt();
+    }
 
+    private void run() {
+        while (!timetable.isEmpty() || !isStopped) {
+            try {
+                timetable.take().run();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
