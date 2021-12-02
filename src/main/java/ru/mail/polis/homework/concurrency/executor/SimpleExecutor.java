779e724287a03704b7cdadf80bf5e74b0277ebcb
done_bd <dinil.bd@mail.ru>
2021-12-02 17:10:00 +0300

    SimpleExecutor could be done ¯\_(ツ)_/¯

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,24 +1,35 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
  * Ленивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 баллов
  */
 public class SimpleExecutor implements Executor {
 
-    private volatile boolean isShutdown = false;
+    private final List<InnerWorker> poolOfThreads;
+    private final int maxThreadCount;
+    private final AtomicInteger countOfFreeThreads = new AtomicInteger(0);
+    private final BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
+    private volatile boolean isShutDown;
 
     public SimpleExecutor(int maxThreadCount) {
-
+        this.maxThreadCount = maxThreadCount;
+        poolOfThreads = new ArrayList<>(maxThreadCount);
     }
 
     /**
@@ -27,7 +38,17 @@ public class SimpleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
-
+        if (isShutDown) {
+            throw new RejectedExecutionException();
+        }
+        synchronized (this) {
+            if (poolOfThreads.size() < maxThreadCount && countOfFreeThreads.get() == 0) {
+                InnerWorker worker = new InnerWorker();
+                poolOfThreads.add(worker);
+                worker.start();
+            }
+        }
+        tasks.add(command);
     }
 
     /**
@@ -35,7 +56,7 @@ public class SimpleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
-        isShutdown = true; // =)
+        isShutDown = true;
     }
 
     /**
@@ -43,13 +64,36 @@ public class SimpleExecutor implements Executor {
      * 1 балла за метод
      */
     public void shutdownNow() {
-
+        isShutDown = true;
+        tasks.clear();
+        for (Thread thread : poolOfThreads) {
+            thread.interrupt();
+        }
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        synchronized (this) {
+            return poolOfThreads.size();
+        }
+    }
+
+    private class InnerWorker extends Thread {
+        @Override
+        public void run() {
+            Runnable task;
+            while (!isShutDown && !Thread.currentThread().isInterrupted()) {
+                countOfFreeThreads.incrementAndGet();
+                try {
+                    task = tasks.take();
+                    countOfFreeThreads.decrementAndGet();
+                    task.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
