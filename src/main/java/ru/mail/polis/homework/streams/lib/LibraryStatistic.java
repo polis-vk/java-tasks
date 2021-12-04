6eac409010e209b09be32ef7a5c16937ec5d3a0a
Artyom Trofimov <artem24630@gmail.com>
2021-12-04 21:21:20 +0300

    implement first task

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -2,6 +2,7 @@ package ru.mail.polis.homework.streams.lib;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -10,24 +11,48 @@ import java.util.stream.Collectors;
  */
 public class LibraryStatistic {
 
+    private static final int DAYS_TO_SPECIALIST = 14;
+    private static final int BOOKS_COUNT_FOR_SPECIALIST = 5;
+    private static final int AVAILABLE_DAYS = 30;
+
     /**
      * Вернуть "специалистов" в литературном жанре с кол-вом прочитанных страниц.
      * Специалист жанра считается пользователь который прочел как минимум 5 книг в этом жанре,
      * при этом читал каждую из них не менее 14 дней.
+     *
      * @param library - данные библиотеки
-     * @param genre - жанр
+     * @param genre   - жанр
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
+        return library.getArchive()
+                .stream()
+                .filter(archiveCell -> archiveCell.getBook().getGenre().equals(genre))
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(archiveList -> archiveList.getValue().size() >= BOOKS_COUNT_FOR_SPECIALIST)
+                .filter(archiveList -> archiveList.getValue().stream()
+                        .allMatch(a -> isSpecialist(a)))
+                .collect(Collectors.toMap(Map.Entry::getKey, a ->
+                        a.getValue().stream()
+                                .mapToInt(b -> b.getBook().getPage())
+                                .sum()));
 
-        return null;
+    }
+
+    private boolean isSpecialist(ArchivedData archiveCell) {
+        if (archiveCell.getReturned() == null) {
+            return TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis() - archiveCell.getTake().getTime()) >= LibraryStatistic.DAYS_TO_SPECIALIST;
+        }
+        return TimeUnit.MILLISECONDS.toDays(archiveCell.getReturned().getTime() - archiveCell.getTake().getTime()) >= LibraryStatistic.DAYS_TO_SPECIALIST;
     }
 
     /**
      * Вернуть любимый жанр пользователя. Тот что чаще всего встречается. Не учитывать тот что пользователь читает в данный момент.
      * Если есть несколько одинаковых по весам жанров - брать в расчет то, что пользователь читает в данный момент.
+     *
      * @param library - данные библиотеки
-     * @param user - пользователь
+     * @param user    - пользователь
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
@@ -41,8 +66,8 @@ public class LibraryStatistic {
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        library.getArchive().stream()
-                .collect(Collectors.groupingBy(ArchivedData::getUser));
+        List<ArchivedData> archive = library.getArchive();
+
     }
 
     /**
