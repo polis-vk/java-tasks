d5b50aa0486bc75f50d8034f7c244771c43bdc7a
Sanerins <askosnitskiy@gmail.com>
2021-12-08 18:15:02 +0300

    Initial implementation

--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -3,6 +3,7 @@ package ru.mail.polis.homework.streams.store;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
@@ -20,7 +21,9 @@ public class StoreStatistic {
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return 0L;
+        return orders.stream().
+                filter(a -> a.getTime().compareTo(from) >= 0 && a.getTime().compareTo(to) <= 0).
+                count();
     }
 
     /**
@@ -30,7 +33,7 @@ public class StoreStatistic {
      * значение - map товар/кол-во
      */
     public Map<Timestamp, Map<Item, Integer>> statisticItemsByDay(List<Order> orders) {
-        return null;
+        return orders.stream().collect(Collectors.toMap());
     }
 
     /**
@@ -39,7 +42,7 @@ public class StoreStatistic {
      * @return - товар
      */
     public Item mostPopularItem(List<Order> orders) {
-        return null;
+        return orders.stream().reduce();
     }
 
     /**
