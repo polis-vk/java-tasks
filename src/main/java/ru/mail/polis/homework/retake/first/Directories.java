e2f8079f5ee9173851eddcc4b8639339579f7904
Евгений Морозов <windrag000@gmail.com>
2022-02-03 17:14:37 +0300

    Directories completed

--- src/main/java/ru/mail/polis/homework/retake/first/Directories.java
+++ src/main/java/ru/mail/polis/homework/retake/first/Directories.java
@@ -1,6 +1,10 @@
 package ru.mail.polis.homework.retake.first;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * 6 баллов
@@ -16,7 +20,27 @@ public class Directories {
      * 3 балла
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        if (!file.exists()) {
+            return 0;
+        }
+        int count = 0;
+        if (file.isDirectory()) {
+            File[] filesInDir = file.listFiles();
+            if (filesInDir != null) {
+                for (File fileInDir : filesInDir) {
+                    count += removeWithFile(fileInDir.getPath());
+                }
+            }
+            if (file.delete()) {
+                count += 1;
+            }
+        } else {
+            if (file.delete()) {
+                count = 1;
+            }
+        }
+        return count;
     }
 
     /**
@@ -24,6 +48,24 @@ public class Directories {
      * 3 балла
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path dir = Paths.get(path);
+        if (Files.notExists(dir)) {
+            return 0;
+        }
+        int count;
+        if (Files.isDirectory(dir)) {
+            count = Files.list(dir).mapToInt(file -> {
+                try {
+                    return removeWithPath(file.toString());
+                } catch (IOException e) {
+                    return 0;
+                }
+            }).sum();
+        } else {
+            Files.delete(dir);
+            return 1;
+        }
+        Files.delete(dir);
+        return ++count;
     }
 }
