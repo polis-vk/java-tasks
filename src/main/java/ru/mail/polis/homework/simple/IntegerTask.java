de6fb342d538c1529d6a923a5d28423b75f04cf4
mandarine <l9214453519@gmail.com>
2022-02-26 00:47:09 +0300

    Done IntergerTask

--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -14,7 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        return sum;
     }
 
     /**
@@ -24,7 +28,23 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        if (top <= bottom && top < height) {
+            return Integer.MAX_VALUE;
+        }
+        else {
+            int n = 0;
+            while (height > 0) {
+                n++;
+                height = height - top;
+                if (height <= 0) {
+                    break;
+                }
+                else {
+                    height = height + bottom;
+                }
+            }
+            return n;
+        }
     }
 
     /**
@@ -32,7 +52,13 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        int i = 1;
+        n = Math.abs(n);
+        while (i < order) {
+            n = n / 10;
+            i++;
+        }
+        return n % 10;
     }
 
 
@@ -41,6 +67,12 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        long fac = 1;
+        int i = 1;
+        while (i <= n) {
+            fac = fac * i;
+            i++;
+        }
+        return fac;
     }
 }
