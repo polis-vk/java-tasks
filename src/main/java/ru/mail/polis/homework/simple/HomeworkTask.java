335c18d5003af454f1de2ddd35be9d14f2af3213
mandarine <l9214453519@gmail.com>
2022-02-26 17:41:09 +0300

    Done Main, DoubleTask and HomeworkTask.

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -10,7 +10,11 @@ public class HomeworkTask {
      * Считаем, что функция определена на всем пространстве от a до b
      */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        return 0;
+        double integral = 0;
+        for (double i = a; i <= b; i = i + delta){
+            integral = integral + function.applyAsDouble(i) * delta;
+        }
+        return integral;
     }
 
     /**
@@ -18,7 +22,17 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
+        a = Math.abs(a);
+        int max = 0, n = 0, i = 0;
+        while (a > 0) {
+            n++;
+            if (a % 10 >= max) {
+                max = (int) (a % 10);
+                i = n;
+            }
+            a = a / 10;
+        }
+        return (byte) (n - i + 1);
     }
 
 
@@ -27,7 +41,7 @@ public class HomeworkTask {
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        return (double) (x3 - x1) * (y2 - y1) / (x2 - x1) + y1;
     }
 
     /**
@@ -36,7 +50,9 @@ public class HomeworkTask {
      * Это дополнительное задание, необязательное для выполнения
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+        double S;
+        S = 1.0 / 2 * Math.abs(x1 * y2 + x2 * y3 + x3 * y4 + x4 * y1 - x2 * y1 - x3 * y2 - x4 * y3 - x1 * y4);
+        return S;
     }
 
 }
