2ad197e642ac6270f97358b9ede087a94da7c363
Vladimir <i.am.polshin@gmail.com>
2022-03-05 22:21:22 +0300

    Done MaxTask, RepeatingCharacters

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MaxTask {
 
     /**
@@ -9,10 +12,51 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        if (count == 0) {
+            return new int[0];
+        }
+        if (count > array.length) {
+            return null;
+        }
+        int[] ans = Arrays.copyOf(array, count);
+        int index = -1;
+        boolean flag = false;
+        for (int i = count; i < array.length; i++) {
+            if (!flag) {
+                index = minValueIndex(ans);
+                flag = true;
+            }
+            if (array[i] > ans[index]) {
+                ans[index] = array[i];
+                flag = false;
+            }
+        }
+        return mySort(ans);
     }
 
+    static int minValueIndex(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) index = i;
+        }
+        return index;
+    }
+
+    // Добавить оптимизацию в сортировку (flag)
+    static int[] mySort(int[] array) {
+        int temp;
+        int[] current = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (current[j] < current[j + 1]) {
+                    temp = current[j];
+                    current[j] = current[j + 1];
+                    current[j + 1] = temp;
+                }
+            }
+        }
+        return current;
+    }
 }
--- src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
+++ src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
@@ -1,7 +1,7 @@
 package ru.mail.polis.homework.objects;
 
 
-import java.util.Objects;
+import java.util.*;
 
 /**
  * Нужно найти символ, который встречается подряд в строке чаще всего, и указать количество повторений.
@@ -13,7 +13,27 @@ import java.util.Objects;
 public class RepeatingCharacters {
 
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
-        return new Pair<>('s', 4);
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        LinkedHashMap<Character, Integer> temp = new LinkedHashMap<>();
+        char c;
+        temp.put(str.charAt(0), 1);
+        for (int i = 1; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (temp.containsKey(c) && str.charAt(i - 1) == c) {
+                temp.put(c, temp.get(c) + 1);
+            } else {
+                temp.put(c, 1);
+            }
+        }
+        Pair<Character, Integer> ans = new Pair<>(' ', 0);
+        for (Map.Entry<Character, Integer> pair : temp.entrySet()) {
+            if (pair.getValue() > ans.getSecond()) {
+                ans = new Pair<>(pair.getKey(), pair.getValue());
+            }
+        }
+        return ans;
     }
 
     public static class Pair<T, V> {
