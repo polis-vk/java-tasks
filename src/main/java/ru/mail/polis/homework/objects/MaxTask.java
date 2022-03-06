10e9445cfe5da81bb3127bfca90231730f0a8500
katemewow <katrinzel0@mail.ru>
2022-03-06 13:50:38 +0300

    MaxTask done

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -9,10 +11,25 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        int[] result = new int[count];
+        if (array.length < count) {
+            return null;
+        }
+        Arrays.fill(result, Integer.MIN_VALUE);
+        for (int i : array) {
+            for (int j = 0; j < result.length; j++)
+                if (i > result[j]) {
+                    int n = count - 1;
+                    while (n != j) {
+                        result[n] = result[n - 1];
+                        n--;
+                    }
+                    result[j] = i;
+                    break;
+                }
+        }
+        return result;
     }
-
 }
