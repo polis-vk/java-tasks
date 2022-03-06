8147ac55e72db88b1d8e092ff1c63f7dfa1e2902
Vladimir <i.am.polshin@gmail.com>
2022-03-06 22:13:44 +0300

    Done StringTasks

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.homework.objects;
 
+
 public class StringTasks {
 
     /**
@@ -14,6 +15,126 @@ public class StringTasks {
      * У класса Character есть полезные методы, например Character.isDigit()
      */
     public static Number valueOf(String str) {
-        return null;
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        StringBuilder strBuilder = new StringBuilder();
+        boolean isMinus = false;
+        boolean isE = false;
+        boolean isPoint = false;
+        boolean isDigit = false;
+        char c;
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i);
+            if (Character.isDigit(c)) {
+                strBuilder.append(c);
+                isDigit = true;
+            } else {
+                if (c == '-') {
+                    if (isMinus || isDigit || isPoint) {
+                        return null;
+                    }
+                    strBuilder.append(c);
+                    isMinus = true;
+                } else if (c == 'e') {
+                    if (isE) {
+                        return null;
+                    }
+                    strBuilder.append(c);
+                    isE = true;
+                    isDigit = false;
+                    isMinus = false;
+                    isPoint = false;
+                } else if (c == '.') {
+                    if (isPoint) {
+                        return null;
+                    }
+                    strBuilder.append(c);
+                    isPoint = true;
+                }
+            }
+        }
+        if (isE && !isDigit) {
+            return null;
+        }
+        return returnAnswer(strBuilder.toString(), isMinus, isE, isPoint);
+    }
+
+    public static Number returnAnswer(String str, boolean isMinus, boolean isE, boolean isPoint) {
+        if (isE || isPoint) {
+            return castDouble(str, isE);
+        }
+        long number = (Long) castIntOrLong(str, isMinus, false);
+        if (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE) {
+            return castIntOrLong(str, isMinus, true);
+        }
+        return number;
+    }
+
+    public static Number castIntOrLong(String str, boolean isMinus, boolean isInteger) {
+        long number = 0L;
+        for (int i = isMinus ? 1 : 0; i < str.length(); i++) {
+            number += getDigit(str.charAt(i));
+            number *= 10;
+        }
+        number = (isMinus ? -1 : 1) * number / 10;
+        if (isInteger) {
+            return (int) number;
+        }
+        return number;
+    }
+
+    public static double castDouble(String str, boolean isE) {
+        String beforeE;
+        String afterE = "0";
+        String beforePoint;
+        String afterPoint = "0";
+        if (isE) {
+            beforeE = str.split("e")[0];
+            afterE = str.split("e")[1];
+        } else {
+            beforeE = str;
+        }
+        if (beforeE.contains(".")) {
+            beforePoint = beforeE.split("\\.")[0];
+            afterPoint = beforeE.split("\\.")[1];
+        } else {
+            beforePoint = beforeE;
+        }
+        double number = 1.0 * (Long) castIntOrLong(beforePoint, (beforePoint.charAt(0) == '-'), false);
+        for (int i = 0; i < afterPoint.length(); i++) {
+            number *= 10;
+            number += getDigit(afterPoint.charAt(i));
+        }
+        number /= Math.pow(10, afterPoint.length());
+        number *= Math.pow(10, 1.0 * (Long) castIntOrLong(afterE, (afterE.charAt(0) == '-'), false));
+        return number;
+    }
+
+    public static int getDigit(char c) {
+        switch (c) {
+            case '0':
+                return 0;
+            case '1':
+                return '1' - '0';
+            case '2':
+                return '2' - '0';
+            case '3':
+                return '3' - '0';
+            case '4':
+                return '4' - '0';
+            case '5':
+                return '5' - '0';
+            case '6':
+                return '6' - '0';
+            case '7':
+                return '7' - '0';
+            case '8':
+                return '8' - '0';
+            case '9':
+                return '9' - '0';
+            default:
+                return -1;
+        }
     }
 }
