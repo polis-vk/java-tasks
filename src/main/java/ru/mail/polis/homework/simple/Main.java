5ca90efbe97b2f388d448e56f4a836cfc1ef6d27
Egorka <trololo221@bk.ru>
2022-03-09 23:49:04 +0300

    Исправил Main

--- src/main/java/ru/mail/polis/homework/simple/Main.java
+++ src/main/java/ru/mail/polis/homework/simple/Main.java
@@ -1,14 +1,9 @@
 package ru.mail.polis.homework.simple;
 
-import ru.mail.polis.homework.objects.MaxTask;
-
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {5, 7, 1, 8, 9, 1, 4, 5, 10, 2, 8, 3, 1};
-        System.out.println(Arrays.toString(MaxTask.getMaxArray(arr, 4)));
+        System.out.println(hello());
     }
 
     static String hello() {
