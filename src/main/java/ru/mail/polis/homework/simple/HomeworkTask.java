6e0875cb8d56ddc2a4d9019275a43102c28c0166
Maria <mari.ponosova.1997@mail.ru>
2022-03-10 13:31:11 +0300

    Solved homework 1

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -10,7 +10,12 @@ public class HomeworkTask {
      * Считаем, что функция определена на всем пространстве от a до b
      */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        return 0;
+        double integral = 0;
+        while (b >= a) {
+            integral += function.applyAsDouble(a) * delta;
+            a += delta;
+        }
+        return integral;
     }
 
     /**
@@ -18,16 +23,27 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
+        byte maxNumber = 0;
+        byte number = 0;
+        for (byte count = 1; ; count++) {
+            if (a > 0) {
+                if (a % 10 >= maxNumber) {
+                    maxNumber = (byte) (a % 10);
+                    number = count;
+                }
+                a /= 10;
+                continue;
+            }
+            return (byte) (count - number);
+        }
     }
-
 
     /**
      * Даны две точки в пространстве (x1, y1) и (x2, y2). Вам нужно найти Y координату третьей точки (x3, y3),
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        return (double) (x3 - x1) * (y2 - y1) / (x2 - x1) + y1;
     }
 
     /**
@@ -36,7 +52,8 @@ public class HomeworkTask {
      * Это дополнительное задание, необязательное для выполнения
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+        return (0.5 * Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) +
+                (0.5 * Math.abs((x3 - x1) * (y4 - y1) - (x4 - x1) * (y3 - y1))));
     }
 
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -1,6 +1,5 @@
 package ru.mail.polis.homework.simple;
 
-
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
@@ -14,7 +13,17 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (n == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -24,7 +33,13 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        if (top >= height) {
+            return 1;
+        }
+        if (top - bottom <= 0) {
+            return Integer.MAX_VALUE;
+        }
+        return (int) Math.ceil((height - top) / (double) (top - bottom)) + 1;
     }
 
     /**
@@ -32,15 +47,27 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        if (order <= 0) {
+            throw new IllegalArgumentException();
+        }
+        return Math.abs(n / (int) Math.pow(10, order - 1) % 10);
     }
-
 
     /**
      * Выведите факториал от числа n
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (n == 0) {
+            return 1;
+        }
+        long factorial = 1;
+        for (byte i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
     }
 }
