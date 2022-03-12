1beaa3fce3d8ec3ff643ce26b0849fd479150291
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2022-03-12 12:35:07 +0300

    Homework-2. MaxTask, RepeatingCharacters done.

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -9,10 +11,28 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        if (array == null || array.length < count) {
+            return null;
+        } else if (array.length == 0 || count == 0) {
+            return new int[0];
+        }
+        int[] maxArray = new int[count];
+        Arrays.fill(maxArray, Integer.MIN_VALUE);
+        for (int digit : array) {
+            if (digit >= maxArray[count - 1]) {
+                maxArray[count - 1] = digit;
+                int j = 1;
+                int temp;
+                while (count - j - 1 >= 0 && maxArray[count - j] > maxArray[count - j - 1]) {
+                    temp = maxArray[count - j - 1];
+                    maxArray[count - j - 1] = maxArray[count - j];
+                    maxArray[count - j] = temp;
+                    j++;
+                }
+            }
+        }
+        return maxArray;
     }
-
 }
