20906cca0c67b94eeb55f037ad0f87e536824aba
ballo <ballo.av@edu.spbstu.ru>
2022-03-13 19:07:23 +0300

    homework-2 solved

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -9,10 +11,27 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        if (array == null || count > array.length) {
+            return null;
+        }
+        int[] result = new int[count];
+        if (count != 0) {
+            Arrays.fill(result, Integer.MIN_VALUE);
+            for (int element : array) {
+                if (element > result[count - 1]) {
+                    result[count - 1] = element;
+                    for (int i = count - 2; i >= 0; i--) {
+                        if (element > result[i]) {
+                            result[i + 1] = result[i];
+                            result[i] = element;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
     }
 
 }
--- src/test/java/ru/mail/polis/homework/objects/StringTasksTest.java
+++ src/test/java/ru/mail/polis/homework/objects/StringTasksTest.java
@@ -9,6 +9,10 @@ public class StringTasksTest {
 
     @Test
     public void valueOf() {
+        assertEquals(null, StringTasks.valueOf("15e-5-5959"));
+        assertEquals(null, StringTasks.valueOf("154-54e-5"));
+        assertEquals(null, StringTasks.valueOf("15e-"));
+        assertEquals(null, StringTasks.valueOf("-e1"));
         assertEquals(null, StringTasks.valueOf(null));
         assertEquals(null, StringTasks.valueOf(""));
         assertEquals(0, StringTasks.valueOf("0"));
