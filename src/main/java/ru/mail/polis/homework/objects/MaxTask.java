d66a42dce9b8c6f77a13992e077fd4ffd9263ec4
nikitabandurin2002@gmail.com <)M8YM%XO]UU#)Vk>
2022-03-15 00:28:59 +0300

    All tests are finished for MaxTask

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -11,35 +11,39 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
         if (array == null || array.length < count) { return null; }
 
-        int[] maxFromRemaining = new int[count];
-        int[] indexesOfMaxNum = new int[count];
-        Arrays.fill(indexesOfMaxNum,-1);
-        int currMax = Integer.MIN_VALUE;
-        boolean goOutFromLoop;
-
-        for (int i = 0; i < count; i++) {
-            for (int j = 0; j < array.length; j++) {
-                goOutFromLoop = false;
-                for (int k : indexesOfMaxNum) {
-                    if (k == j) {
-                        goOutFromLoop = true;
-                        break;
-                    }
-                }
-                if (goOutFromLoop) { continue; }
-                if (array[j] > currMax) {
-                    currMax = array[j];
-                    indexesOfMaxNum[i] = j;
+        int[] arrMaxNum = Arrays.copyOf(array, count);
+        int minValOfArrMaxNum = Integer.MAX_VALUE;
+        int indexMinValOfArrMaxNum = 0;
+        for (int i = 0; i < arrMaxNum.length; i++) {
+            if (arrMaxNum[i] < minValOfArrMaxNum) {
+                minValOfArrMaxNum = arrMaxNum[i];
+                indexMinValOfArrMaxNum = i;
+            }
+        }
+        for (int j = count; j < array.length; j++) {
+            if (array[j] > minValOfArrMaxNum) {
+                arrMaxNum[indexMinValOfArrMaxNum] = array[j];
+            }
+            minValOfArrMaxNum = Integer.MAX_VALUE;
+            for (int i = 0; i < arrMaxNum.length; i++) {
+                if (arrMaxNum[i] < minValOfArrMaxNum) {
+                    minValOfArrMaxNum = arrMaxNum[i];
+                    indexMinValOfArrMaxNum = i;
                 }
             }
-            maxFromRemaining[i] = currMax;
-            currMax = Integer.MIN_VALUE;
         }
-        return maxFromRemaining;
+        //сортируем в обратном порядке
+        for (int i = 0; i < arrMaxNum.length; i++) {
+            arrMaxNum[i] = -arrMaxNum[i];
+        }
+        Arrays.sort(arrMaxNum);
+        for (int i = 0; i < arrMaxNum.length; i++) {
+            arrMaxNum[i] = -arrMaxNum[i];
+        }
+        return arrMaxNum;
     }
 }
