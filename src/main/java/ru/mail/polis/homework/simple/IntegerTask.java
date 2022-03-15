d0d3483bb7ccbe967349f68a8d9c71e82174649f
lidia <lidiapshenichnay@yandex.ru>
2022-03-16 01:01:00 +0300

    Task 2

--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -14,11 +14,7 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            count += i;
-        }
-        return count;
+        return 0;
     }
 
     /**
@@ -28,21 +24,7 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        if (bottom >= top && top < height) {
-            return Integer.MAX_VALUE;
-        }
-        int result = 0;
-        int step = Math.abs(top - bottom);
-        int len = 0;
-        for (int i = 0; i < height; i += step) {
-            if (len + top >= height) {
-                result++;
-                break;
-            }
-            len += step;
-            result++;
-        }
-        return result;
+        return 0;
     }
 
     /**
@@ -50,11 +32,7 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        int currentNum = n;
-        for (int i = 0; i < order - 1; i++) {
-            currentNum = currentNum / 10;
-        }
-        return Math.abs(currentNum % 10);
+        return 0;
     }
 
 
@@ -63,10 +41,6 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        long result = 1;
-        for (byte i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        return 0;
     }
 }
