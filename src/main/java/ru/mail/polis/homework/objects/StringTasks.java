3368b75c57549784edc4a40dd05b559294cdccc1
Egorka <trololo221@bk.ru>
2022-03-16 21:17:09 +0300

    Сделал StringTasks

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -25,60 +25,115 @@ public class StringTasks {
         boolean wasMinus = false;
         boolean wasE = false;
         boolean wasDot = false;
+        boolean wasDigit = false;
 
         for (int i = 0; i < lenghtOfStr; i++) {
             currentChar = str.charAt(i);
 
-            if (currentChar == '-' && !wasMinus) {
+            if ((currentChar == 'e' && wasE) || (currentChar == '.' && wasDot)){
+                return null;
+            }
+            if (currentChar == 'e' && !wasDigit) {
+                return null;
+            }
+            if (currentChar == '-' && (wasMinus || wasDigit)) {
+                return null;
+            }
+            if (currentChar == '.' && (wasE || !wasDigit)) {
+                return null;
+            }
+
+
+            if (currentChar == '-') {
                 digitString = digitString + "-";
                 wasMinus = true;
             }
-            if (wasMinus && wasE) {
-                return null;
-            }
-            if (currentChar == '.' && !wasDot) {
+            if (currentChar == '.') {
                 digitString = digitString + ".";
                 wasDot = true;
             }
-            if (currentChar == 'e' && wasE) {
-                return null;
-            }
-            if (currentChar == 'e' && !wasE) {
+            if (currentChar == 'e') {
                 digitString = digitString + "e";
                 wasE = true;
                 wasMinus = false; // после числа Эйлера может быть еще один минус
+                wasDigit = false;
             }
-
             if (Character.isDigit(currentChar)) {
                 digitString = digitString + currentChar;
+                wasDigit = true;
             }
-
-//            if (Character.isDigit(currentChar)) {
-//                if (wasDot && !wasE) {
-//                    numberAfterDot += 1;
-//                    if (digit < 0) {
-//                        digitD = digitD - Character.getNumericValue(currentChar) * Math.pow(0.1, numberAfterDot);
-//                    } else {
-//                        digitD = digitD + Character.getNumericValue(currentChar) * Math.pow(0.1, numberAfterDot);
-//                    }
-//                    result = digitD;
-//                } else {
-//                    if (digit < 0) {
-//                        digit = digit * 10 - Character.getNumericValue(currentChar);
-//                    } else {
-//                        digit = digit * 10 + Character.getNumericValue(currentChar);
-//                    }
-//                    result = digit;
-//                }
-//
-//                if (wasMinus) {
-//                    digit *= -1;
-//                    wasMinus = false;
-//                }
-//            }
-
         }
 
-        return null;
+        char lastSymbol = digitString.charAt(digitString.length() - 1);
+        if (lastSymbol == '-' || lastSymbol == 'e' || lastSymbol == '.') {
+            return null;
+        }
+
+        int lengthOfDigitStr = digitString.length();
+        char currentSymbol;
+        int zeroChar = (int) '0';
+
+        if (wasDot || wasE) {
+            double digit = 0.0;
+            wasE = false;
+            wasMinus = false;
+            wasDot = false;
+            int powerOfE = 0;
+            int positionDot = 0;
+
+            for(int i = 0; i < lengthOfDigitStr; i++) {
+                currentSymbol = digitString.charAt(i);
+
+                if (currentSymbol == 'e') {
+                    wasE = true;
+                }
+                if (wasE && currentSymbol == '-') {
+                    wasMinus = true;
+                }
+                if (currentSymbol == '.') {
+                    wasDot = true;
+                    positionDot = i;
+                }
+
+                if (Character.isDigit(currentSymbol) && wasE) {
+                    powerOfE = powerOfE * 10 + ((int) currentSymbol - zeroChar);
+                } else if (Character.isDigit(currentSymbol) && wasDot && !wasE) {
+                    digit += ((double) currentSymbol - zeroChar) * Math.pow(10, -(i - positionDot));
+                } else if (Character.isDigit(currentSymbol)) {
+                    digit = digit * 10 + ((int) currentSymbol - zeroChar);
+                }
+
+                if (wasE && wasMinus) {
+                    powerOfE *= -1;
+                }
+
+            }
+
+            if (digitString.charAt(0) == '-') {
+                digit *= -1;
+            }
+
+            return digit * Math.pow(10, powerOfE);
+        } else {
+            long digit = 0;
+
+            for (int i = 0; i < lengthOfDigitStr; i++) {
+                currentSymbol = digitString.charAt(i);
+
+                if (Character.isDigit(currentSymbol)){
+                    digit = digit * 10 + ((int) currentSymbol - zeroChar);
+                }
+            }
+
+            if (digitString.charAt(0) == '-') {
+                digit *= -1;
+            }
+
+            if (digit >= Integer.MIN_VALUE && digit <= Integer.MAX_VALUE) {
+                return (int) digit;
+            }
+
+            return digit;
+        }
     }
 }
