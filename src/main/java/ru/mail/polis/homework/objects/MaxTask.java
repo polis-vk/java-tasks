b3a7d778680d86c85fba6c80abe21ae6e281f7bf
Porublev Egor <Ameiza_228@list.ru>
2022-03-17 18:11:04 +0300

    MaxTask and RepeatingCharacters + build.gradle for idea done

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -9,10 +11,26 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        if (array == null || count > array.length) {
+            return null;
+        }
+        int[] result = new int[count];
+        int[] tempArr = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < count; i++) {
+            int maxValue = Integer.MIN_VALUE;
+            int index = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (tempArr[j] > maxValue) {
+                    maxValue = array[j];
+                    index = j;
+                }
+            }
+            tempArr[index] = Integer.MIN_VALUE;
+            result[i] = maxValue;
+        }
+        return result;
     }
-
 }
+
