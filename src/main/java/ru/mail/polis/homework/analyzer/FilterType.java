5632195b8b033b637984f36a0e99656ee5224a63
topskier <andreiklimentov361@gmail.com>
2022-03-22 22:27:18 +0300

    Homework3 22.03

--- src/main/java/ru/mail/polis/homework/analyzer/FilterType.java
+++ src/main/java/ru/mail/polis/homework/analyzer/FilterType.java
@@ -4,4 +4,15 @@ package ru.mail.polis.homework.analyzer;
  * Типы фильтров (2 тугрика)
  */
 public enum FilterType {
+    SPAM(1),
+    TOO_LONG(2),
+    NEGATIVE_TEXT(3),
+    GOOD(5),
+    ORTHOGRAPHIC_ERR(4);
+    int priority;
+
+    FilterType(int priority) {
+        this.priority = priority;
+    }
+
 }
