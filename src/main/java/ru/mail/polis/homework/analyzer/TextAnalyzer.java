88a1a56d3d0abee692b27c58797cc2e31b45b841
topskier <andreiklimentov361@gmail.com>
2022-03-22 22:35:42 +0300

    Homework3 22.03

--- src/main/java/ru/mail/polis/homework/analyzer/TextAnalyzer.java
+++ src/main/java/ru/mail/polis/homework/analyzer/TextAnalyzer.java
@@ -2,36 +2,43 @@ package ru.mail.polis.homework.analyzer;
 
 
 /**
- * Базовый интерефейс фильтра, наследники этого интерефейса должны инкапсулировать в себе всю логику
+ * Базовый интерфейс фильтра, наследники этого интерфейса должны инкапсулировать в себе всю логику
  * анализа текста.
  * Ниже надо реализовать методы, которые создают фильтры заданного типа (то что они возвращают интерфейс, это как раз
  * прием ООП, где нам не важна конкретная реализация, а важен только контракт, что результат статических методов
  * умеет как-то анализировать текст). Сами статические методы мне нужны для создания тестов,
  * что бы без реальных классов (которые вы напишите) я смог "сэмулировать" их создание.
- *
+ * <p>
  * Так же необходимо создать все необходимы методы, которые будут вам нужны для прогона текста
  * по всем фильтрам в классе TextFilterManager
- *
- * 2 балла + (2 балла за каждый фильтр + 1 балл за тест на свой фильтр) ИТОГО 11
+ * <p>
+ * 2 тугрика + (2 тугрика за каждый фильтр + 1 тугрик за тест на свой фильтр) ИТОГО 11
  */
 public interface TextAnalyzer {
 
     static TextAnalyzer createTooLongAnalyzer(long maxLength) {
-        return null;
+        return new TooLongAnalyzer(maxLength);
     }
 
     static TextAnalyzer createSpamAnalyzer(String[] spam) {
-        return null;
+        return new SpamAnalyzer(spam);
     }
 
     static TextAnalyzer createNegativeTextAnalyzer() {
-        return null;
+        return new NegativeTextAnalyzer();
     }
 
     /**
      * придумать свой фильтр
      */
-    static <T> TextAnalyzer createCustomAnalyzer(T something) {
-        return null;
+
+    /**
+     * Фильтр по проверке орфографической ошибки жы и шы
+     */
+    static TextAnalyzer createOrthographicTextAnalyzer() {
+        return new OrthographicTextAnalyzer();
     }
+
+
+    FilterType FilterValue(String text);
 }
