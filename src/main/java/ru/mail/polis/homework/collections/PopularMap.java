b7ae061bbc0211259051747baf73e69edb1e9170
mandarine <l9214453519@gmail.com>
2022-04-01 23:16:15 +0300

    Done PopularMap

--- src/main/java/ru/mail/polis/homework/collections/PopularMap.java
+++ src/main/java/ru/mail/polis/homework/collections/PopularMap.java
@@ -4,8 +4,11 @@ package ru.mail.polis.homework.collections;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 /**
@@ -14,26 +17,36 @@ import java.util.Set;
  * Популярность - это количество раз, который этот ключ/значение учавствовал/ло в других методах мапы, такие как
  * containsKey, get, put, remove (в качестве параметра и возвращаемого значения).
  * Считаем, что null я вам не передаю ни в качестве ключа, ни в качестве значения
- *
+ * <p>
  * Так же надо сделать итератор (подробности ниже).
- *
+ * <p>
  * Важный момент, вам не надо реализовывать мапу, вы должны использовать композицию.
  * Вы можете использовать любые коллекции, которые есть в java.
- *
+ * <p>
  * Помните, что по мапе тоже можно итерироваться
- *
- *         for (Map.Entry<K, V> entry : map.entrySet()) {
- *             entry.getKey();
- *             entry.getValue();
- *         }
- *
+ * <p>
+ * for (Map.Entry<K, V> entry : map.entrySet()) {
+ * entry.getKey();
+ * entry.getValue();
+ * }
+ * <p>
  * Всего 10 тугриков (3 тугрика за общие методы, 2 тугрика за итератор, 5 тугриков за логику популярности)
+ *
  * @param <K> - тип ключа
  * @param <V> - тип значения
  */
 public class PopularMap<K, V> implements Map<K, V> {
 
     private final Map<K, V> map;
+    private final Map<Object, Integer> keyPopularity = new HashMap<>();
+    private final Map<Object, Integer> valuePopularity = new HashMap<>();
+
+    private void addValuePopularity(Object key) {
+        V value = map.get(key);
+        if (value != null) {
+            valuePopularity.compute(value, (k, v) -> (v == null) ? 1 : v + 1);
+        }
+    }
 
     public PopularMap() {
         this.map = new HashMap<>();
@@ -45,84 +58,101 @@ public class PopularMap<K, V> implements Map<K, V> {
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return map.isEmpty();
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return false;
+        keyPopularity.compute(key, (k, v) -> (v == null) ? v = 1 : v + 1);
+        return map.containsKey(key);
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        valuePopularity.compute(value, (k, v) -> (v == null) ? 1 : v + 1);
+        return map.containsValue(value);
     }
 
     @Override
     public V get(Object key) {
-        return null;
+        keyPopularity.compute(key, (k, v) -> (v == null) ? v = 1 : v + 1);
+        V value = map.get(key);
+        if (value != null) {
+            valuePopularity.compute(value, (k, v) -> (v == null) ? 1 : v + 1);
+        }
+        return value;
     }
 
     @Override
     public V put(K key, V value) {
-        return null;
+        keyPopularity.compute(key, (k, v) -> (v == null) ? 1 : v + 1);
+        valuePopularity.compute(value, (k, v) -> (v == null) ? 1 : v + 1);
+        addValuePopularity(key);
+        return map.put(key, value);
     }
 
     @Override
     public V remove(Object key) {
-        return null;
+        keyPopularity.compute(key, (k, v) -> (v == null) ? 1 : v + 1);
+        addValuePopularity(key);
+        return map.remove(key);
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends V> m) {
-        throw new UnsupportedOperationException("putAll");
+        map.putAll(m);
     }
 
     @Override
     public void clear() {
-
+        map.clear();
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        return map.keySet();
     }
 
     @Override
     public Collection<V> values() {
-        return null;
+        return map.values();
     }
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        return null;
+        return map.entrySet();
     }
 
     /**
      * Возвращает самый популярный, на данный момент, ключ
      */
     public K getPopularKey() {
-        return null;
+        return (K) keyPopularity
+                .entrySet().stream()
+                .max(Entry.comparingByValue())
+                .get().getKey();
     }
-
 
     /**
      * Возвращает количество использование ключа
      */
     public int getKeyPopularity(K key) {
-        return 0;
+        return keyPopularity.getOrDefault(key, 0);
     }
 
     /**
      * Возвращает самое популярное, на данный момент, значение. Надо учесть что значени может быть более одного
      */
     public V getPopularValue() {
-        return null;
+        return (V) valuePopularity
+                .entrySet().stream()
+                .max(Entry.comparingByValue())
+                .get().getKey();
     }
 
     /**
@@ -130,7 +160,7 @@ public class PopularMap<K, V> implements Map<K, V> {
      * старое значение и новое - одно и тоже), remove (считаем по старому значению).
      */
     public int getValuePopularity(V value) {
-        return 0;
+        return valuePopularity.getOrDefault(value, 0);
     }
 
     /**
@@ -138,6 +168,32 @@ public class PopularMap<K, V> implements Map<K, V> {
      * 2 тугрика
      */
     public Iterator<V> popularIterator() {
-        return null;
+        return new MapIterator();
+    }
+
+    private class MapIterator implements Iterator<V> {
+
+        List<V> list;
+
+        public MapIterator() {
+            list = valuePopularity.entrySet().stream()
+                    .sorted(Entry.comparingByValue())
+                    .map(Map.Entry::getKey)
+                    .map(value -> (V) value)
+                    .collect(Collectors.toList());
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !list.isEmpty();
+        }
+
+        @Override
+        public V next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return list.remove(0);
+        }
     }
 }
