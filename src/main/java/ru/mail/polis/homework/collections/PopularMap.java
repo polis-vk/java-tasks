94c9b4f86e033f6c379082c4a1892d3858728ced
Nats15555 <nats15555@gmail.com>
2022-04-04 01:28:11 +0300

    homework-5 исправления

--- src/main/java/ru/mail/polis/homework/collections/PopularMap.java
+++ src/main/java/ru/mail/polis/homework/collections/PopularMap.java
@@ -1,14 +1,11 @@
 package ru.mail.polis.homework.collections;
 
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
