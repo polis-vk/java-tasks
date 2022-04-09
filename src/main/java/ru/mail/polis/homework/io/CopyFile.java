ad371426c7a6cd7027e78ca99ab149566d2249cd
Nats15555 <nats15555@gmail.com>
2022-04-09 04:47:19 +0300

    homework-6 copyfile && directories

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,17 @@
 package ru.mail.polis.homework.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -9,7 +21,63 @@ public class CopyFile {
      * 3 тугрика
      */
     public static String copyFiles(String pathFrom, String pathTo) {
+        Path from = Paths.get(pathFrom);
+
+        if (!Files.exists(from)) {
+            return null;
+        }
+
+        try {
+            Path to = Paths.get(pathTo);
+
+            Path dir = to;
+            if (!Files.isDirectory(from)) {
+                dir = to.getParent();
+            }
+            createDirectories(dir);
+
+            visitorFiles(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
+    private static void createDirectories(Path dir) throws IOException {
+        if (!Files.exists(dir)) {
+            Files.createDirectories(dir);
+        }
+    }
+
+    private static void visitorFiles(Path from, Path to) throws IOException {
+        Files.walkFileTree(from, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                copyInnerContent(file, to.resolve(from.relativize(file)));
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                createDirectories(to.resolve(from.relativize(dir)));
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private static void copyInnerContent(Path from, Path to) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(from);
+             BufferedWriter writer = Files.newBufferedWriter(to)) {
+            reader.lines().forEach(line -> {
+                try {
+                    writer.write(line + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
 }
