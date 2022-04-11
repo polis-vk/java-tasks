ca980cf37a67a89c0ce9f15367b7237451f9d7be
Porublev Egor <Ameiza_228@list.ru>
2022-04-11 21:51:27 +0300

    HMW 6 Directories.java done

--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,11 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Directories {
 
@@ -13,7 +18,20 @@ public class Directories {
      * 2 балла
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+
+        if (!file.exists()) {
+            return 0;
+        }
+
+        int result = 0;
+        if (!file.isFile()) {
+            for (File temp : Objects.requireNonNull(file.listFiles())) {
+                result += removeWithFile(temp.getPath());
+            }
+        }
+        file.delete();
+        return ++result;
     }
 
     /**
@@ -21,6 +39,29 @@ public class Directories {
      * 2 балла
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path dir = Paths.get(path);
+
+        if (!Files.exists(dir)) {
+            return 0;
+        }
+
+        AtomicInteger result = new AtomicInteger();
+        Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                        result.incrementAndGet();
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFile(Path dir, BasicFileAttributes attrs) throws IOException {
+                        result.incrementAndGet();
+                        Files.delete(dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                }
+        );
+        return result.get();
     }
 }
