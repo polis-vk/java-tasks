6857e6982fc95427fd31e763bb0e2db329731b5a
ballo <ballo.aleksej@gmaiol.com>
2022-04-13 00:34:55 +0300

    directories solved

--- src/main/java/ru/mail/polis/homework/io/blocking/StructureInputStream.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureInputStream.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Вам нужно реализовать StructureInputStream, который умеет читать данные из файла.
@@ -24,7 +25,23 @@ public class StructureInputStream extends FileInputStream {
      * Если структур в файле больше нет, то вернуть null
      */
     public Structure readStructure() throws IOException {
-        return null;
+        if (available() == 0) {
+            return null;
+        }
+        Structure structure = new Structure();
+        structure.setId(readLong());
+        structure.setName(readString());
+        structure.setSubStructures(readSubStructures());
+        structure.setCoeff(readFloat());
+        structure.setFlag1(readFlag());
+        structure.setFlag2(readFlag());
+        structure.setFlag3(readFlag());
+        structure.setFlag4(readFlag());
+        structure.setParam((byte) read());
+
+        structures = Arrays.copyOf(structures, structures.length + 1);
+        structures[structures.length - 1] = structure;
+        return structure;
     }
 
     /**
@@ -33,5 +50,78 @@ public class StructureInputStream extends FileInputStream {
      */
     public Structure[] readStructures() throws IOException {
         return new Structure[0];
+    }
+
+    private long readLong() throws IOException {
+        byte[] bytes = new byte[8];
+        int temp = read(bytes);
+        if (temp != 8) {
+            throw new IOException();
+        }
+        return ((bytes[0] & 0xFFL) << 56)
+                | ((bytes[1] & 0xFFL) << 48)
+                | ((bytes[2] & 0xFFL) << 40)
+                | ((bytes[3] & 0xFFL) << 32)
+                | ((bytes[4] & 0xFFL) << 24)
+                | ((bytes[5] & 0xFFL) << 16)
+                | ((bytes[6] & 0xFFL) << 8)
+                | ((bytes[7] & 0xFFL));
+    }
+
+    private int readInt() throws IOException {
+        byte[] bytes = new byte[4];
+        int temp = read(bytes);
+        if (temp != 4) {
+            throw new IOException();
+        }
+        return (bytes[0] << 24)
+                | (bytes[1] << 16)
+                | (bytes[2] << 8)
+                | (bytes[3]);
+    }
+
+    private double readDouble() throws IOException {
+        return Double.longBitsToDouble(readLong());
+    }
+
+    private float readFloat() throws IOException {
+        return Float.intBitsToFloat(readInt());
+    }
+
+    private String readString() throws IOException {
+        int length = readInt();
+        if (length == -1) {
+            throw new IOException();
+        }
+        byte[] bytes = new byte[length];
+        int temp = read(bytes);
+        if (temp != length) {
+            throw new IOException();
+        }
+        return new String(bytes);
+    }
+
+    private SubStructure[] readSubStructures() throws IOException {
+        int size = readInt();
+        if (size == -1) {
+            throw new IOException();
+        }
+        SubStructure[] subStructures = new SubStructure[size];
+        for (int i = 0; i < size; i++) {
+            subStructures[i] = readSubStructure();
+        }
+        return subStructures;
+    }
+
+    private SubStructure readSubStructure() throws IOException {
+        int id = readInt();
+        String name = readString();
+        boolean flag = readFlag();
+        double score = readDouble();
+        return new SubStructure(id, name, flag, score);
+    }
+
+    private boolean readFlag() throws IOException {
+        return readInt() != 0;
     }
 }
