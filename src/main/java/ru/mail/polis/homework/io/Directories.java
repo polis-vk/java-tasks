370e91b398f5014b4f5184721ecb289bcd853a6b
ballo <ballo.aleksej@gmaiol.com>
2022-04-12 22:40:49 +0300

    directories solved

--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,12 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Directories {
 
@@ -13,7 +19,25 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithFile(String path) {
-        return 0;
+        return removeFile(new File(path));
+    }
+
+    private static int removeFile(File file) {
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        }
+
+        File[] files = file.listFiles();
+        if (files == null) {
+            return 0;
+        }
+        int result = 0;
+        for (File current : files) {
+            result += removeFile(current);
+        }
+        file.delete();
+        return result + 1;
     }
 
     /**
@@ -21,6 +45,25 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path directory = Paths.get(path);
+        int result = 0;
+        if (Files.isRegularFile(directory)) {
+            Files.delete(directory);
+            return 1;
+        }
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(path))) {
+            for (Path file : directoryStream) {
+                if (Files.isDirectory(file)) {
+                    result += removeWithPath(file.toString());
+                } else {
+                    Files.delete(file);
+                    result++;
+                }
+            }
+        } catch (NoSuchFileException e) {
+            return 0;
+        }
+        Files.delete(directory);
+        return result + 1;
     }
 }
