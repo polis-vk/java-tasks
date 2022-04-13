9429143e7e18f9aebce6243741f7cc39d0ea5f64
katemewow <katrinzel0@mail.ru>
2022-04-13 04:00:36 +0300

    hw6 done

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CopyFile {
 
     /**
@@ -8,8 +16,49 @@ public class CopyFile {
      * В тесте для создания нужных файлов для первого запуска надо расскоментировать код в setUp()
      * 3 балла
      */
+    public static final int BUFFER_SIZE = 1024;
+
     public static String copyFiles(String pathFrom, String pathTo) {
-        return null;
+        if (pathFrom == null || pathTo == null) {
+            return null;
+        }
+        Path from = Paths.get(pathFrom);
+        if (Files.notExists(from)) {
+            return null;
+        }
+        Path to = Paths.get(pathTo);
+        try {
+            if (Files.isRegularFile(from)) {
+                Files.createDirectories(to.getParent());
+            } else {
+                Files.createDirectories(to);
+            }
+            copy(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return pathTo;
     }
 
+    private static void copy(Path from, Path to) throws IOException {
+        if (!Files.isDirectory(from)) {
+            byte[] buff = new byte[BUFFER_SIZE];
+            try (InputStream in = Files.newInputStream(from);
+                 OutputStream out = Files.newOutputStream(to)) {
+                int length;
+                while ((length = in.read(buff)) > 0) {
+                    out.write(buff, 0, length);
+                }
+            }
+            return;
+        }
+        if (Files.notExists(to)) {
+            Files.createDirectory(to);
+        }
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(from)) {
+            for (Path path : stream) {
+                copy(path, to.resolve(path.getFileName()));
+            }
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,15 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.List;
 
 public class Directories {
 
@@ -13,7 +22,22 @@ public class Directories {
      * 2 балла
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        if (!file.exists()) {
+            return 0;
+        }
+        int count = 0;
+        if (file.isFile()) {
+            return ifDelete(file, count);
+        }
+        for (File f : Objects.requireNonNull(file.listFiles())) {
+            count += removeWithFile(f.getPath());
+        }
+        return ifDelete(file, count);
+    }
+
+    private static int ifDelete(File file, int count) {
+        return file.delete() ? count + 1 : count;
     }
 
     /**
@@ -21,6 +45,20 @@ public class Directories {
      * 2 балла
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path filePath = Paths.get(path);
+        if (Files.notExists(filePath)) {
+            return 0;
+        }
+        int count = 0;
+        try (Stream<Path> pathStream = Files.walk(filePath)) {
+            List<Path> pathList = pathStream
+                    .sorted(Comparator.reverseOrder())
+                    .collect(Collectors.toList());
+            for (Path curPath : pathList) {
+                Files.delete(curPath);
+                count++;
+            }
+        }
+        return count;
     }
 }
