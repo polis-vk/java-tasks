e4a66022512982b3f6f7cf8a4c10e3dc63fe107f
Vladimir <i.am.polshin@gmail.com>
2022-04-13 20:51:31 +0300

    Done Structure without Serializer

--- src/main/java/ru/mail/polis/homework/io/blocking/StructureInputStream.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureInputStream.java
@@ -1,9 +1,12 @@
 package ru.mail.polis.homework.io.blocking;
 
+import java.util.Arrays;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.nio.ByteBuffer;
 import java.io.IOException;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
 
 /**
  * Вам нужно реализовать StructureInputStream, который умеет читать данные из файла.
@@ -13,9 +16,12 @@ import java.io.IOException;
 public class StructureInputStream extends FileInputStream {
 
     private Structure[] structures;
+    private int structuresCount;
 
     public StructureInputStream(File fileName) throws FileNotFoundException {
         super(fileName);
+        structures = new Structure[0];
+        structuresCount = 0;
     }
 
 
@@ -24,14 +30,123 @@ public class StructureInputStream extends FileInputStream {
      * Если структур в файле больше нет, то вернуть null
      */
     public Structure readStructure() throws IOException {
-        return null;
+        if (available() == 0) {
+            return null;
+        }
+
+        Structure structure = new Structure();
+        structure.setId(readLong());
+        structure.setName(readString());
+        structure.setSubStructures(readSubStructures());
+        structure.setCoeff(readFloat());
+        structure.setFlag1(readBoolean());
+        structure.setFlag2(readBoolean());
+        structure.setFlag3(readBoolean());
+        structure.setFlag4(readBoolean());
+        structure.setParam((byte) read());
+        addStructure(structure);
+        return structure;
     }
 
     /**
      * Метод должен вернуть все структуры, которые есть в файле.
      * Если файл уже прочитан, но возвращается полный массив.
+     *
+     * @return
      */
     public Structure[] readStructures() throws IOException {
-        return new Structure[0];
+        while (available() != 0) {
+            readStructure();
+        }
+
+        if (structures.length != structuresCount) {
+            structures = Arrays.copyOf(structures, structuresCount);
+        }
+        return structures;
+    }
+
+    private void readFully(byte[] b, int length) throws IOException {
+        if (length < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        int n = 0;
+        while (n < length) {
+            int count = read(b, n, length - n);
+            if (count < 0) {
+                throw new EOFException();
+            }
+            n += count;
+        }
+    }
+
+    private boolean readBoolean() throws IOException {
+        return read() == 1;
+    }
+
+    private int readInt() throws IOException {
+        byte[] readBuffer = new byte[4];
+        readFully(readBuffer, 4);
+        return ByteBuffer.wrap(readBuffer).getInt();
+    }
+
+    private float readFloat() throws IOException {
+        byte[] readBuffer = new byte[4];
+        readFully(readBuffer, 4);
+        return ByteBuffer.wrap(readBuffer).getFloat();
+    }
+
+    private long readLong() throws IOException {
+        byte[] readBuffer = new byte[8];
+        readFully(readBuffer, 8);
+        return ByteBuffer.wrap(readBuffer).getLong();
+    }
+
+    private double readDouble() throws IOException {
+        byte[] readBuffer = new byte[8];
+        readFully(readBuffer, 8);
+        return ByteBuffer.wrap(readBuffer).getDouble();
+    }
+
+    private String readString() throws IOException {
+        int length = readInt();
+        if (length == -1) {
+            return null;
+        }
+
+        byte[] readBuffer = new byte[length];
+        readFully(readBuffer, length);
+        return new String(readBuffer);
+    }
+
+    private SubStructure readSubStructure() throws IOException {
+        int id = readInt();
+        String name = readString();
+        if (name == null) {
+            throw new IllegalArgumentException();
+        }
+
+        boolean flag = readBoolean();
+        double score = readDouble();
+        return new SubStructure(id, name, flag, score);
+    }
+
+    private SubStructure[] readSubStructures() throws IOException {
+        int length = readInt();
+        if (length == -1) {
+            return null;
+        }
+
+        SubStructure[] subStructures = new SubStructure[length];
+        for (int i = 0; i < length; i++) {
+            subStructures[i] = readSubStructure();
+        }
+        return subStructures;
+    }
+
+    private void addStructure(Structure structure) {
+        if (structures.length <= structuresCount) {
+            structures = Arrays.copyOf(structures, (structures.length + 1) * 3 / 2);
+        }
+        structures[structuresCount++] = structure;
     }
 }
--- src/main/java/ru/mail/polis/homework/io/blocking/StructureOutputStream.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureOutputStream.java
@@ -1,9 +1,10 @@
 package ru.mail.polis.homework.io.blocking;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 /**
  * Вам нужно реализовать StructureOutputStream, который умеет писать данные в файл.
@@ -20,12 +21,81 @@ public class StructureOutputStream extends FileOutputStream {
      * Метод должен вернуть записать прочитанную структуру.
      */
     public void write(Structure structure) throws IOException {
+        if (structure == null) {
+            return;
+        }
+
+        writeLong(structure.getId());
+        writeString(structure.getName());
+        writeSubStructures(structure.getSubStructures());
+        writeFloat(structure.getCoeff());
+        writeBoolean(structure.isFlag1());
+        writeBoolean(structure.isFlag2());
+        writeBoolean(structure.isFlag3());
+        writeBoolean(structure.isFlag4());
+        write(structure.getParam());
+        flush();
     }
 
     /**
      * Метод должен вернуть записать массив прочитанных структур.
      */
     public void write(Structure[] structures) throws IOException {
+        if (structures == null) {
+            return;
+        }
 
+        for (Structure structure : structures) {
+            write(structure);
+        }
+    }
+
+    private void writeBoolean(boolean b) throws IOException {
+        write(b ? 1 : 0);
+    }
+
+    private void writeInt(int i) throws IOException {
+        write(ByteBuffer.allocate(4).putInt(i).array());
+    }
+
+    private void writeFloat(float f) throws IOException {
+        write(ByteBuffer.allocate(4).putFloat(f).array());
+    }
+
+    private void writeLong(long l) throws IOException {
+        write(ByteBuffer.allocate(8).putLong(l).array());
+    }
+
+    private void writeDouble(double d) throws IOException {
+        write(ByteBuffer.allocate(8).putDouble(d).array());
+    }
+
+    private void writeString(String s) throws IOException {
+        if (s == null) {
+            writeInt(-1);
+            return;
+        }
+
+        writeInt(s.length());
+        write(s.getBytes());
+    }
+
+    private void writeSubStructure(SubStructure subStructure) throws IOException {
+        writeInt(subStructure.getId());
+        writeString(subStructure.getName());
+        writeBoolean(subStructure.isFlag());
+        writeDouble(subStructure.getScore());
+    }
+
+    private void writeSubStructures(SubStructure[] subStructures) throws IOException {
+        if (subStructures == null) {
+            writeInt(-1);
+            return;
+        }
+
+        writeInt(subStructures.length);
+        for (SubStructure subStructure : subStructures) {
+            writeSubStructure(subStructure);
+        }
     }
 }
