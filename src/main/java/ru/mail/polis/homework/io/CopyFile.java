bf692657d2e15b46ab45ff3b0d388a50a129652e
mandarine <l9214453519@gmail.com>
2022-04-14 18:38:23 +0300

    Added stream copy to CopyFile

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.io;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -33,21 +35,39 @@ public class CopyFile {
                             return FileVisitResult.CONTINUE;
                         }
                         Path newDir = target.resolve(source.relativize(dir));
-                        Files.createDirectories(newDir);
+                        if (Files.exists(newDir.getParent())) {
+                            Files.createDirectory(newDir);
+                        } else {
+                            Files.createDirectories(newDir);
+                        }
                         return FileVisitResult.CONTINUE;
                     }
 
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
                         Path newFile = target.resolve(source.relativize(file));
                         try {
-                            Files.copy(file, newFile);
+                            copyContent(file, newFile);
                             return FileVisitResult.CONTINUE;
-                        } catch (IOException e) {
+                        } catch (Exception e) {
                             return FileVisitResult.CONTINUE;
                         }
                     }
                 }
         );
         return null;
+    }
+
+
+    public static void copyContent(Path source, Path target) throws Exception {
+        FileInputStream inputStream = new FileInputStream(source.toFile());
+        FileOutputStream outputStream = new FileOutputStream(target.toFile());
+        byte[] buffer = new byte[1024];
+
+        while (inputStream.available() > 0) {
+            int count = inputStream.read(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        inputStream.close();
+        outputStream.close();
     }
 }
