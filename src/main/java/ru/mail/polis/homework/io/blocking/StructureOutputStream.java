d8fd47c85e2ed0ebfe51bac7bcb9637a82e31179
ballo <ballo.aleksej@gmaiol.com>
2022-04-15 13:11:26 +0300

    structure streams done

--- src/main/java/ru/mail/polis/homework/io/blocking/StructureOutputStream.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureOutputStream.java
@@ -20,12 +20,87 @@ public class StructureOutputStream extends FileOutputStream {
      * Метод должен вернуть записать прочитанную структуру.
      */
     public void write(Structure structure) throws IOException {
+        writeLong(structure.getId());
+        writeString(structure.getName());
+        writeSubStructures(structure.getSubStructures());
+        writeFloat(structure.getCoeff());
+        writeBoolean(structure.isFlag1());
+        writeBoolean(structure.isFlag2());
+        writeBoolean(structure.isFlag3());
+        writeBoolean(structure.isFlag4());
+        write(structure.getParam());
     }
 
     /**
      * Метод должен вернуть записать массив прочитанных структур.
      */
     public void write(Structure[] structures) throws IOException {
+        for (Structure structure : structures) {
+            write(structure);
+        }
+    }
 
+    private void writeLong(long number) throws IOException {
+        byte[] bytes = new byte[] {
+                (byte) (number >>> 56),
+                (byte) (number >>> 48),
+                (byte) (number >>> 40),
+                (byte) (number >>> 32),
+                (byte) (number >>> 24),
+                (byte) (number >>> 16),
+                (byte) (number >>> 8),
+                (byte) number
+        };
+        write(bytes);
+    }
+
+    private void writeInt(int number) throws IOException {
+        byte[] bytes = new byte[] {
+                (byte) (number >>> 24),
+                (byte) (number >>> 16),
+                (byte) (number >>> 8),
+                (byte) number
+        };
+        write(bytes);
+    }
+
+    private void writeDouble(double number) throws IOException {
+        writeLong(Double.doubleToLongBits(number));
+    }
+
+    private void writeFloat(float number) throws IOException {
+        writeInt(Float.floatToIntBits(number));
+    }
+
+    private void writeBoolean(boolean flag) throws IOException {
+        writeInt(flag ? 1 : 0);
+    }
+
+    private void writeString(String string) throws IOException {
+        if (string == null) {
+            writeInt(-1);
+            return;
+        }
+        byte[] bytes = string.getBytes();
+        writeInt(bytes.length);
+        write(bytes);
+    }
+
+    private void writeSubStructure(SubStructure subStructure) throws IOException {
+        writeInt(subStructure.getId());
+        writeString(subStructure.getName());
+        writeBoolean(subStructure.isFlag());
+        writeDouble(subStructure.getScore());
+    }
+
+    private void writeSubStructures(SubStructure[] subStructures) throws IOException {
+        if (subStructures == null) {
+            writeInt(-1);
+            return;
+        }
+        writeInt(subStructures.length);
+        for (SubStructure substructure : subStructures) {
+            writeSubStructure(substructure);
+        }
     }
 }
