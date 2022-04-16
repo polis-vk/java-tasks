4e3bc6199c4367c5126413a8dae4c9f15e846da5
Dasha <dasha-tomlinson21051@mail.ru>
2022-04-16 21:57:37 +0300

    Directories and CopyFile. Tests passed.

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,15 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.FileVisitResult;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -9,6 +19,49 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
+        Path whereFrom = Paths.get(pathFrom);
+        if (Files.notExists(whereFrom)) {
+            return;
+        }
+        Path whereTo = Paths.get(pathTo);
+        try {
+            if (Files.isRegularFile(whereFrom)) {
+                whereTo = whereTo.getParent();
+            }
+            Files.createDirectories(whereTo);
+            doFileCopy(pathFrom, pathTo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    private static void doFileCopy(String pathFrom, String pathTo) throws IOException {
+        Path whereFrom = Paths.get(pathFrom);
+        Path whereTo = Paths.get(pathTo);
+        Files.walkFileTree(Paths.get(pathFrom), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Path targetPath = whereTo.resolve(whereFrom.relativize(dir));
+                if (Files.notExists(targetPath)) {
+                    Files.createDirectories(targetPath);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                InputStream inputStream = Files.newInputStream(file);
+                OutputStream outputStream = Files.newOutputStream(whereTo.resolve(whereFrom.relativize(file)));
+                byte[] buffer = new byte[1024];
+                int blockSize = inputStream.read(buffer);;
+                while (blockSize > 0) {
+                    outputStream.write(buffer, 0, blockSize);
+                    blockSize = inputStream.read(buffer);
+                }
+                inputStream.close();
+                outputStream.close();
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
 }
--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,12 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.NoSuchFileException;
 
 public class Directories {
 
@@ -13,7 +19,18 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File item = new File(path);
+        if (!item.exists()) {
+            return 0;
+        }
+        int count = 0;
+        if (item.isDirectory()) {
+            for (File entry : item.listFiles()) {
+                count += removeWithFile(entry.getPath());
+            }
+        }
+        item.delete();
+        return count + 1;
     }
 
     /**
@@ -21,6 +38,22 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path item = Paths.get(path);
+        int count = 0;
+        if (Files.isDirectory(item)) {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(item)) {
+                for (Path entry : stream) {
+                    count += removeWithPath(entry.toString());
+                }
+            } catch (NoSuchFileException e) {
+                return 0;
+            }
+        }
+        try {
+            Files.delete(item);
+        } catch (NoSuchFileException e) {
+            return 0;
+        }
+        return count + 1;
     }
 }
