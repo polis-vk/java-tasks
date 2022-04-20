3884e5bd2fae57943f922f7206f34a0a39021605
mandarine <l9214453519@gmail.com>
2022-04-20 23:18:14 +0300

    StructureSerializer done

--- src/main/java/ru/mail/polis/homework/io/blocking/StructureSerializer.java
+++ src/main/java/ru/mail/polis/homework/io/blocking/StructureSerializer.java
@@ -1,6 +1,15 @@
 package ru.mail.polis.homework.io.blocking;
 
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,15 +19,15 @@ import java.util.List;
  * с помощью класса Random или руками прописать разные значения переменных).
  * Потом получившийся список записать в один и тот же файл 10 раз (100 раз и более, если у вас это происходит очень быстро).
  * Список и объекты внутри лучше копировать, чтобы у вас не было ссылок на одни и те же объекты.
- *
+ * <p>
  * Далее этот список надо прочитать из файла.
- *
+ * <p>
  * Результатом теста должно быть следующее: размер файла, время записи и время чтения.
  * Время считать через System.currentTimeMillis().
  * В итоговом пулРеквесте должна быть информация об этих значениях для каждого теста. (всего 2 теста,
  * за правильную генерацию данных 1 тугрик, 1 тугрик за каждый тест и 1 тугрик за правильное объяснение результатов)
  * Для тестов создайте классы в соответствующем пакете в папке тестов. Используйте другие тесты как примеры.
- *
+ * <p>
  * В конце теста по чтению данных, не забывайте удалять файлы
  */
 public class StructureSerializer {
@@ -26,11 +35,22 @@ public class StructureSerializer {
     /**
      * 1 тугрик
      * Реализовать простую сериализацию, с помощью специального потока для сериализации объектов
+     *
      * @param structures Список структур для сериализации
-     * @param fileName файл в который "пишем" структуры
+     * @param fileName   файл в который "пишем" структуры
      */
     public void defaultSerialize(List<Structure> structures, String fileName) {
-
+        try {
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            for (Structure structure : structures) {
+                objectOutputStream.writeObject(structure);
+            }
+            objectOutputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -41,19 +61,42 @@ public class StructureSerializer {
      * @return список структур
      */
     public List<Structure> defaultDeserialize(String fileName) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            List<Structure> structures = new ArrayList<>();
+
+            try {
+                while (objectInputStream.available() == 0) {
+                    structures.add((Structure) objectInputStream.readObject());
+                }
+            } catch (EOFException ignore) {
+            }
+            objectInputStream.close();
+            return structures;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return Collections.emptyList();
     }
-
 
 
     /**
      * 1 тугрик
      * Реализовать сериализацию, с помощью StructureOutputStream
+     *
      * @param structures Список структур для сериализации
-     * @param fileName файл в который "пишем" структуры
+     * @param fileName   файл в который "пишем" структуры
      */
     public void serialize(List<Structure> structures, String fileName) {
-
+        try {
+            File file = new File(fileName);
+            StructureOutputStream structureOutputStream = new StructureOutputStream(file);
+            structureOutputStream.write(structures.toArray(new Structure[0]));
+            structureOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -64,8 +107,15 @@ public class StructureSerializer {
      * @return список структур
      */
     public List<Structure> deserialize(String fileName) {
+        try {
+            File file = new File(fileName);
+            StructureInputStream structureInputStream = new StructureInputStream(file);
+            List<Structure> structures = Arrays.asList(structureInputStream.readStructures());
+            structureInputStream.close();
+            return structures;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return Collections.emptyList();
     }
-
-
 }
---
+++ src/test/java/ru/mail/polis/homework/io/blocking/SerializerTest.java
@@ -0,0 +1,97 @@
+package ru.mail.polis.homework.io.blocking;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SerializerTest {
+
+    private ArrayList<Structure> structures;
+
+    @Before
+    public void setUp() throws IOException {
+        Path dir = Paths.get("src", "test", "resources", "blocking");
+        Files.createDirectories(dir);
+
+        Random random = new Random();
+        structures = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Structure structure = new Structure();
+            structure.setId(random.nextLong());
+            structure.setName("Object" + i);
+            structure.setSubStructures(new SubStructure[]{
+                    new SubStructure(random.nextInt(), "sub-oneObject", random.nextBoolean(), Math.random()),
+                    new SubStructure(random.nextInt(), "sub-oneObject-4", random.nextBoolean(), Math.random())});
+            structure.setCoeff(random.nextFloat());
+            structure.setFlag1(random.nextBoolean());
+            structure.setFlag2(random.nextBoolean());
+            structure.setFlag3(random.nextBoolean());
+            structure.setFlag4(random.nextBoolean());
+            structure.setParam((byte) (Math.random() * 125));
+            structures.add(structure);
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        FileUtils.forceDelete(Paths.get("src", "test", "resources", "blocking", "structure.txt").toFile());
+    }
+
+    @Test
+    public void testDefault() throws IOException {
+        Path file = Paths.get("src", "test", "resources", "blocking", "structure.txt");
+        Files.createFile(file);
+
+        System.out.println("Default serializer");
+        StructureSerializer structureSerializer = new StructureSerializer();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 500; i++) {
+            ArrayList<Structure> copy = new ArrayList<>(structures);
+            structureSerializer.defaultSerialize(copy, file.toString());
+        }
+        long endTime = (System.currentTimeMillis() - startTime);
+        System.out.println("Write speed: " + endTime);
+
+        startTime = System.currentTimeMillis();
+        List<Structure> structuresList = structureSerializer.defaultDeserialize(file.toString());
+        endTime = (System.currentTimeMillis() - startTime);
+        System.out.println("Read speed: " + endTime);
+        assertEquals(structures, structuresList);
+    }
+
+    @Test
+    public void testCustom() throws IOException {
+        Path file = Paths.get("src", "test", "resources", "blocking", "structure.txt");
+        Files.createFile(file);
+        StructureSerializer structureSerializer = new StructureSerializer();
+
+        System.out.println("Custom serializer");
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 500; i++) {
+            ArrayList<Structure> copy = new ArrayList<>(structures);
+            structureSerializer.serialize(copy, file.toString());
+        }
+        long endTime = (System.currentTimeMillis() - startTime);
+        System.out.println("Write speed: " + endTime);
+
+        startTime = System.currentTimeMillis();
+        List<Structure> structuresList = structureSerializer.deserialize(file.toString());
+        endTime = (System.currentTimeMillis() - startTime);
+        System.out.println("Read speed: " + endTime);
+        assertEquals(structures, structuresList);
+    }
+}
