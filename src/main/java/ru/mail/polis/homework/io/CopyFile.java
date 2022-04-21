29e0d7a91250678d0f92b111f60a68a242cc9acb
Maria <mari.ponosova.1997@mail.ru>
2022-04-21 17:14:11 +0300

    Solved homework 6: add solution for CopyFile, Directories

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CopyFile {
 
     /**
@@ -9,6 +17,49 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
+        if (pathFrom == null || pathTo == null) {
+            return;
+        }
+        Path from = Paths.get(pathFrom);
+        if (Files.notExists(from)) {
+            return;
+        }
+        Path to = Paths.get(pathTo);
+        try {
+            if (Files.isRegularFile(from)) {
+                Files.createDirectories(to.getParent());
+            } else {
+                Files.createDirectories(to);
+            }
+            copy(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    private static void copy(Path from, Path to) throws IOException {
+        if (Files.isRegularFile(from)) {
+            byte[] buffer = new byte[1024];
+            try (InputStream in = Files.newInputStream(from);
+                 OutputStream out = Files.newOutputStream(to)) {
+                int length;
+                while ((length = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, length);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        if (Files.notExists(to)) {
+            Files.createDirectory(to);
+        }
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(from)) {
+            for (Path file : stream) {
+                copy(file, to.resolve(file.getFileName()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,12 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Directories {
 
@@ -13,7 +19,18 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File itemPath = new File(path);
+        if (!itemPath.exists()) {
+            return 0;
+        }
+        int count = 0;
+        if (itemPath.isDirectory()) {
+            for (File file : itemPath.listFiles()) {
+                count += removeWithFile(file.getPath());
+            }
+        }
+        itemPath.delete();
+        return ++count;
     }
 
     /**
@@ -21,6 +38,26 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path itemPath = Paths.get(path);
+        if (Files.notExists(itemPath)) {
+            return 0;
+        }
+        int count = 0;
+        if (Files.isDirectory(itemPath)) {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(itemPath)) {
+                for (Path file : stream) {
+                    count += removeWithPath(file.toString());
+                }
+            } catch (NoSuchFileException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            Files.delete(itemPath);
+        } catch (NoSuchFileException e) {
+            e.printStackTrace();
+            return 0;
+        }
+        return ++count;
     }
 }
