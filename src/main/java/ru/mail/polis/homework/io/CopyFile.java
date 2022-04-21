6280f290dfbb298c896cd7b440fc728e3d07fa7e
Evgeniy Burovnikov <EvgeniyBurovnikov@yandex.ru>
2022-04-21 13:35:21 +0300

    Optimization of imports

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,7 +1,6 @@
 package ru.mail.polis.homework.io;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
