957b86399033b4880824de71e2f01962ac6ed2ec
Gilfoyal <a0030953@yandex.ru>
2022-04-23 19:26:07 +0300

    Yep

--- src/main/java/ru/mail/polis/homework/exception/RobotsInTheField.java
+++ src/main/java/ru/mail/polis/homework/exception/RobotsInTheField.java
@@ -6,11 +6,11 @@ public class RobotsInTheField {
     public static HashMap<Integer, Robot> realRobots = new HashMap<>();
 
     static {
-        realRobots.put(1, (new Robot(1.0, 1.0, 11)));
-        realRobots.put(2, (new Robot(1.0, 1.0, 22)));
-        realRobots.put(3, (new Robot(1.0, 1.0, 33)));
-        realRobots.put(4, (new Robot(1.0, 1.0, 44)));
-        realRobots.put(5, (new Robot(1.0, 1.0, 55)));
+        realRobots.put(1, (new Robot(1.0, 1.0, 1)));
+        realRobots.put(2, (new Robot(1.0, 1.0, 2)));
+        realRobots.put(3, (new Robot(1.0, 1.0, 3)));
+        realRobots.put(4, (new Robot(1.0, 1.0, 4)));
+        realRobots.put(5, (new Robot(1.0, 1.0, 5)));
     }
 
 }
