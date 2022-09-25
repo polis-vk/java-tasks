59e403995dd0d58fde7c95d1054c04a0f673232f
Ekaterina Zelenkina <katrinzel0@mail.ru>
2022-09-25 13:53:17 +0300

    hw1 done

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -16,6 +16,11 @@ public class DoubleAdvancedTask {
      * Если используете какой-то конкретный способ, напишите какой.
      * Пример: (1, -4, -7, 10) -> "-2.0, 1.0, 5.0"
      */
+
+    /**
+     * Тригонометрическая формула Виета
+     * https://www.wikiwand.com/ru/%D0%A2%D1%80%D0%B8%D0%B3%D0%BE%D0%BD%D0%BE%D0%BC%D0%B5%D1%82%D1%80%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D1%84%D0%BE%D1%80%D0%BC%D1%83%D0%BB%D0%B0_%D0%92%D0%B8%D0%B5%D1%82%D0%B0
+     */
     public static String equation(int a, int b, int c, int d) {
         double root1;
         double root2;
