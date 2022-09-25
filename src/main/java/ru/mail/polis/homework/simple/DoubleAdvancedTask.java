ec058f7d02122fe06ba643b2e12be040efe9054f
Vladimir <i.am.polshin@gmail.com>
2022-09-25 21:29:02 +0300

    Done DoubleAdvancedTask

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -17,10 +17,23 @@ public class DoubleAdvancedTask {
      * Пример: (1, -4, -7, 10) -> "-2.0, 1.0, 5.0"
      */
     public static String equation(int a, int b, int c, int d) {
-        double x1 = 0;
-        double x2 = 0;
-        double x3 = 0;
-        return x1 + ", " + x2 + ", " + x3;
+        // Решал с помощью тригонометрической формулы Виета
+        double A = (double) b / a;
+        double B = (double) c / a;
+        double C = (double) d / a;
+        double Q = (Math.pow(A, 2) - 3 * B) / 9;
+        double R = (2 * Math.pow(A, 3) - 9 * A * B + 27 * C) / 54;
+        double phi = 0;
+        if (Q != 0) {
+            phi = Math.acos(R / Math.sqrt(Math.pow(Q, 3))) / 3;
+        }
+        double x1 = -2 * Math.sqrt(Q) * Math.cos(phi) - A / 3;
+        double x2 = -2 * Math.sqrt(Q) * Math.cos(phi + (2 * Math.PI / 3)) - A / 3;
+        double x3 = -2 * Math.sqrt(Q) * Math.cos(phi - (2 * Math.PI / 3)) - A / 3;
+        double max = Math.max(Math.max(x1, x2), x3);
+        double min = Math.min(Math.min(x1, x2), x3);
+        double mid = x1 + x2 + x3 - min - max;
+        return max + ", " + mid + ", " + min;
     }
 
     /**
@@ -29,7 +42,10 @@ public class DoubleAdvancedTask {
      * (0, 1, 0, 5) -> 4
      */
     public static float length(double a1, double b1, double a2, double b2) {
-        return 0;
+        if (a1 != a2) {
+            return 0;
+        }
+        return (float) (Math.abs(b1 - b2) / Math.sqrt(a1 * a2 + 1));
     }
 
     /**
@@ -44,6 +60,17 @@ public class DoubleAdvancedTask {
                                          int x2, int y2, int z2,
                                          int x3, int y3, int z3,
                                          int x4, int y4) {
-        return 0;
+        // Если вектора лежат в одной плоскости, то определитель матрицы A равен нулю.
+        // Из этого мы получили линейное уравнение относительно одной неизвестной z4
+        double a11 = x4 - x1;
+        double a12 = y4 - y1;
+        double a21 = x2 - x1;
+        double a22 = y2 - y1;
+        double a23 = z2 - z1;
+        double a31 = x3 - x1;
+        double a32 = y3 - y1;
+        double a33 = z3 - z1;
+        return z1 + (-a11 * a22 * a33 - a31 * a12 * a23 + a21 * a12 * a33 + a11 * a23 * a32)
+                / (a21 * a32 - a22 * a31);
     }
 }
