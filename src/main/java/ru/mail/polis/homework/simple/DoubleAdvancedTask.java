b935ddf9ea9415f33a7b23d08bf1b7ed2cfdc333
kiril06344 <kirill12093@mail.ru>
2022-09-27 14:56:43 +0300

    fix mistakes in DoubleAdvancedTask

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -11,6 +11,8 @@ import java.util.Collections;
  */
 public class DoubleAdvancedTask {
 
+  private static final double EPS = 1e-20;
+
   /**
    * Вывести три корня кубического уравнения через запятую: a * x ^ 3 + b * x ^ 2 + c * x + d = 0;
    * Вывод менять не нужно, надо только посчитать x1, x2 и x3, где x1 >= x2 >= x3
@@ -21,44 +23,43 @@ public class DoubleAdvancedTask {
    */
 
   public static double getArch(double x) {
-    return Math.log(x + Math.pow(x * x - 1, 0.5));
+    return Math.log(x + Math.sqrt(x * x - 1));
   }
 
   public static double getCh(double x) {
     return (Math.pow(Math.E, x) + Math.pow(Math.E, -x)) / 2;
   }
 
+  /*
+   Решение реализовано при помощи тригнометрической формулы Виета
+   */
   public static String equation(int a, int b, int c, int d) {
-    //Тригонометрическая формула Виета
-    double x1 = 0;
-    double x2 = 0;
-    double x3 = 0;
+    double[] roots = {0, 0, 0};
 
     double A = (double) b / a;
     double B = (double) c / a;
     double C = (double) d / a;
 
-    double q =  (A * A - 3 * B) / 9;
+    double q = (A * A - 3 * B) / 9;
     double r = (2 * Math.pow(A, 3) - 9 * A * B + 27 * C) / 54;
     double s = Math.pow(q, 3) - Math.pow(r, 2);
 
     if (s > 0) {
       double f = Math.acos(r / Math.pow(q, 1.5)) / 3;
-      x1 = -2 * Math.pow(q, 0.5) * Math.cos(f) - (A) / 3;
-      x2 = -2 *Math.pow(q, 0.5) * Math.cos(f - 2 * Math.PI / 3) - (A) / 3;
-      x3 = -2 * Math.pow(q, 0.5) * Math.cos(f + 2 * Math.PI / 3) - (A) / 3;
-    } else if (s == 0.0) {
-      x1 = -2 * Math.signum(r) * Math.pow(r, (double) 1 / 3) - (A) / 3;
-      x2 = Math.signum(r) * Math.pow(r, (double) 1 / 3) - (A) / 3;
-      x3 = x2;
+      roots[0] = -2 * Math.pow(q, 0.5) * Math.cos(f) - (A) / 3;
+      roots[1] = -2 * Math.pow(q, 0.5) * Math.cos(f - 2 * Math.PI / 3) - (A) / 3;
+      roots[2] = -2 * Math.pow(q, 0.5) * Math.cos(f + 2 * Math.PI / 3) - (A) / 3;
+    } else if (Math.abs(s) <= EPS) {
+      roots[0] = -2 * Math.signum(r) * Math.pow(r, (double) 1 / 3) - (A) / 3;
+      roots[1] = Math.signum(r) * Math.pow(r, (double) 1 / 3) - (A) / 3;
+      roots[2] = roots[1];
     } else {
       double f = getArch(Math.abs(r) / Math.pow(Math.abs(q), 1.5)) / 3;
-      x1 = -2 * Math.signum(r) * Math.pow(Math.abs(q), 0.5) * getCh(f) - (A) / 3;
+      roots[0] = -2 * Math.signum(r) * Math.pow(Math.abs(q), 0.5) * getCh(f) - (A) / 3;
     }
 
-    Double [] answers = {x1, x2, x3};
-    Arrays.sort(answers, Collections.reverseOrder());
-    return answers[0] + ", " + answers[1] + ", " + answers[2];
+    Arrays.sort(roots);
+    return roots[2] + ", " + roots[1] + ", " + roots[0];
   }
 
   /**
@@ -67,7 +68,7 @@ public class DoubleAdvancedTask {
    * (0, 1, 0, 5) -> 4
    */
   public static float length(double a1, double b1, double a2, double b2) {
-    if (a1 != a2) {
+    if (Math.abs(a1 - a2) > EPS) {
       return 0;
     }
     return (float) (Math.abs(b1 - b2) / Math.sqrt(a1 * a2 + 1));
@@ -92,9 +93,7 @@ public class DoubleAdvancedTask {
       this.y = y;
       this.z = z;
     }
-  }
 
-  public static class VectorOperations {
     public static Vector multiplication(Vector a, Vector b) {
       return new Vector(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
     }
@@ -106,7 +105,7 @@ public class DoubleAdvancedTask {
                                        int x4, int y4) {
     Vector m12 = new Vector(x2 - x1, y2 - y1, z2 - z1);
     Vector m13 = new Vector(x3 - x1, y3 - y1, z3 - z1);
-    Vector n = VectorOperations.multiplication(m12, m13);
+    Vector n = Vector.multiplication(m12, m13);
     return (double) (-n.x * (x4 - x1) - n.y * (y4 - y1)) / n.z + z1;
   }
 }
