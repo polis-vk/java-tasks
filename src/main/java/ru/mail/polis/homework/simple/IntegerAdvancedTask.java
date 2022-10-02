78b30a904ef7aad8a6a03a88dd4b7d466316c144
Dmitrij_Tyapuev <dmitrij.typuev@gmail.com>
2022-10-02 12:16:22 +0300

    added progression, snake, kDecimal, minNumber

--- src/main/java/ru/mail/polis/homework/simple/IntegerAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerAdvancedTask.java
@@ -18,7 +18,11 @@ public class IntegerAdvancedTask {
      * Пример: (1, 2, 3) -> 7
      */
     public static long progression(int a, double q, int n) {
-        return 0;
+        long Sum = a;
+        for (int i = 1; i < n; i++) {
+            Sum += a * Math.pow(q, i);
+        }
+        return Sum;
     }
 
     /**
@@ -30,9 +34,30 @@ public class IntegerAdvancedTask {
      * Пример: (10, 3, 5, 5, 20, 11) -> 2
      */
     public static int snake(int up, int right, int down, int left, int grassX, int grassY) {
-        return 0;
-    }
+        int x = 0;
+        int y = 0;
+        double startDistanceX = Math.abs(Math.pow(x - grassX, 2));
+        double startDistanceY = Math.abs(Math.pow(y - grassY, 2));
+        int days = 1;
+        while (x < grassX && y < grassY) {
 
+            x += right;
+            y += up;
+            if (x >= grassX || y >= grassY) {
+                break;
+            }
+            x -= left;
+            y -= down;
+            double newDistanceX = Math.abs(Math.pow(x - grassX, 2));
+            double newDistanceY = Math.abs(Math.pow(y - grassY, 2));
+            if ((newDistanceX >= startDistanceX) && (newDistanceY >= startDistanceY)) {
+                days = Integer.MAX_VALUE;
+                break;
+            }
+            days++;
+        }
+        return days;
+    }
     /**
      * Дано число n в 10-ном формате и номер разряда order.
      * Выведите цифру стоящую на нужном разряде для числа n в 16-ом формате
@@ -40,7 +65,71 @@ public class IntegerAdvancedTask {
      * Пример: (454355, 2) -> D
      */
     public static char kDecimal(int n, int order) {
-        return 0;
+        int counterNumbers = 1;
+        int buff = n;
+        while (buff / 16 != 0) {
+            buff = buff / 16;
+            counterNumbers++;
+        }
+        int[] arrayOfNumbers = new int[counterNumbers];
+        for (int i = 0; i < counterNumbers; i++) {
+            int remainder = n % 16;
+            n = n / 16;
+            arrayOfNumbers[i] = remainder;
+        }
+        char result = ' ';
+        int number = arrayOfNumbers[order - 1];
+        switch (number) {
+            case 0:
+                result = '0';
+                break;
+            case 1:
+                result = '1';
+                break;
+            case 2:
+                result = '2';
+                break;
+            case 3:
+                result = '3';
+                break;
+            case 4:
+                result = '4';
+                break;
+            case 5:
+                result = '5';
+                break;
+            case 6:
+                result = '6';
+                break;
+            case 7:
+                result = '7';
+                break;
+            case 8:
+                result = '8';
+                break;
+            case 9:
+                result = '9';
+                break;
+            case 10:
+                result = 'A';
+                break;
+            case 11:
+                result = 'B';
+                break;
+            case 12:
+                result = 'C';
+                break;
+            case 13:
+                result = 'D';
+                break;
+            case 14:
+                result = 'E';
+                break;
+            case 15:
+                result = 'F';
+                break;
+        }
+        return result;
     }
 
     /**
@@ -51,7 +140,28 @@ public class IntegerAdvancedTask {
      * (6726455) -> 2
      */
     public static byte minNumber(long a) {
-        return 0;
+        int counterNumbers = 1;
+        long buff = a;
+        while (buff / 16 != 0) {
+            buff = buff / 16;
+            counterNumbers++;
+        }
+        byte[] arrayOfNumbers = new byte[counterNumbers];
+        for (byte i = 0; i < counterNumbers; i++) {
+            byte remainder = (byte) (a % 16);
+            a = a / 16;
+            arrayOfNumbers[i] = remainder;
+        }
+        byte max = 16;
+        byte resultIndex = 0;
+        for (byte i = 0; i < counterNumbers; i++) {
+            if (arrayOfNumbers[i] < max) {
+                max = arrayOfNumbers[i];
+                resultIndex = (byte) (i + 1);
+            }
+        }
+
+        return resultIndex;
     }
 
 }
