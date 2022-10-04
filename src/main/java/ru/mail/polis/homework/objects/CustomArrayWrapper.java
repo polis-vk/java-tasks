47a9d604c60176f6dc4394db0c75d182dff5fa8e
Vladimir <i.am.polshin@gmail.com>
2022-10-04 11:49:43 +0300

    Done CustomArrayWrapper

--- src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 /**
@@ -15,6 +16,7 @@ public class CustomArrayWrapper implements Iterable<Integer> {
 
     private final int[] array;          // массив
     private int position;               // следующая позиция куда будет вставлен элемент
+    private int modCount;
 
     public CustomArrayWrapper(int size) {
         this.array = new int[size];
@@ -24,11 +26,13 @@ public class CustomArrayWrapper implements Iterable<Integer> {
         checkIndex(position);
         array[position] = value;
         position++;
+        modCount++;
     }
 
     public void edit(int index, int value) {
         checkIndex(index);
         array[index] = value;
+        modCount++;
     }
 
     public int get(int index) {
@@ -48,7 +52,7 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      */
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new ArrayWrapperIterator();
     }
 
     /**
@@ -58,7 +62,7 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      * @return Iterator for EVEN elements
      */
     public Iterator<Integer> evenIterator() {
-        return null;
+        return new ArrayWrapperIterator(true);
     }
 
     /**
@@ -68,7 +72,7 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      * @return Iterator for ODD elements
      */
     public Iterator<Integer> oddIterator() {
-        return null;
+        return new ArrayWrapperIterator(false);
     }
 
     private void checkIndex(int index) {
@@ -77,4 +81,35 @@ public class CustomArrayWrapper implements Iterable<Integer> {
         }
     }
 
+    class ArrayWrapperIterator implements Iterator<Integer> {
+        private final int expectedModCount;
+        private int currentIndex;
+        private int step;
+
+        public ArrayWrapperIterator() {
+            expectedModCount = modCount;
+            step = 1;
+        }
+
+        public ArrayWrapperIterator(boolean isEven) {
+            this();
+            currentIndex = isEven ? 1 : 0;
+            step = 2;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < array.length;
+        }
+
+        @Override
+        public Integer next() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            currentIndex += step;
+            return array[currentIndex - step];
+        }
+    }
 }
+
