dadb9c91c37fb8e6840a2e4a4814e9127d23d3c6
Egorka <trololo221@bk.ru>
2022-10-06 01:44:01 +0300

    All done

--- src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
@@ -1,20 +1,23 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Вам придется реализовать Iterable класс CustomArrayWrapper вместе с методами которые
- * могут возващать итераторы только по четным/нечетным позициям в массиве. Пример с классического
- * итератора можете взять из лекции. Обратите внимание что подсчет четного или нечетного элемента
+ * могут возвращать итераторы только по четным/нечетным позициям в массиве. Пример с классического
+ * итератора можете взять из лекции. Обратите внимание, что подсчет четного или нечетного элемента
  * идет с человеческой точки зрения.
  * Пример:
- * дан массив [100, 0 ,100, 0, 100]
+ * дан массив [100, 0, 100, 0, 100]
  * тогда все элементы со значением 100 имеют нечетную позицию, а элементы = 0 - четную.
  */
 public class CustomArrayWrapper implements Iterable<Integer> {
 
     private final int[] array;          // массив
-    private int position;               // следующая позиция куда будет вставлен элемент
+    private int position = 0;           // следующая позиция куда будет вставлен элемент
+    private int modCount = 0;
 
     public CustomArrayWrapper(int size) {
         this.array = new int[size];
@@ -24,11 +27,13 @@ public class CustomArrayWrapper implements Iterable<Integer> {
         checkIndex(position);
         array[position] = value;
         position++;
+        modCount++;
     }
 
     public void edit(int index, int value) {
         checkIndex(index);
         array[index] = value;
+        modCount++;
     }
 
     public int get(int index) {
@@ -42,33 +47,97 @@ public class CustomArrayWrapper implements Iterable<Integer> {
 
     /**
      * Реализовать метод:
-     * Возврящает обычный итератор.
+     * Возвращает обычный итератор.
      *
      * @return default Iterator
      */
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>() {
+            int position;
+            final int fixedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return position < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (fixedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (position >= array.length) {
+                    throw new NoSuchElementException();
+                }
+
+                return array[position++];
+            }
+        };
     }
 
     /**
      * Реализовать метод:
-     * Возвращает итератор который проходит только четные элементы.
+     * Возвращает итератор, который проходит только четные элементы.
      *
      * @return Iterator for EVEN elements
      */
     public Iterator<Integer> evenIterator() {
-        return null;
+        return new Iterator<Integer>() {
+            int position = -1;
+            final int fixedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return position + 2 < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (fixedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (position >= array.length) {
+                    throw new NoSuchElementException();
+                }
+
+                position += 2;
+
+                return array[position];
+            }
+        };
     }
 
     /**
      * Реализовать метод:
-     * Возвращает итератор который проходит нечетные элементы
+     * Возвращает итератор, который проходит нечетные элементы
      *
      * @return Iterator for ODD elements
      */
     public Iterator<Integer> oddIterator() {
-        return null;
+        return new Iterator<Integer>() {
+            int position = -2;
+            final int fixedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return position + 2 < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (fixedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (position >= array.length) {
+                    throw new NoSuchElementException();
+                }
+
+                position += 2;
+
+                return array[position];
+            }
+        };
     }
 
     private void checkIndex(int index) {
