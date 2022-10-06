d736a8969a3b0a30f13dbc9e1f253914afa6305c
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-10-06 15:17:34 +0300

    CustomLinkedList is done

--- src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
@@ -1,6 +1,8 @@
 package ru.mail.polis.homework.objects;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
 
 /**
  * 15 тугриков
@@ -9,6 +11,8 @@ import java.util.Iterator;
 public class CustomLinkedList implements Iterable<Integer> {
 
     private Node head;
+    private Node tail;
+    private int size;
 
     /**
      * 1 тугрик
@@ -17,7 +21,7 @@ public class CustomLinkedList implements Iterable<Integer> {
      * @return size
      */
     public int size() {
-        return 0;
+        return size;
     }
 
     /**
@@ -28,7 +32,30 @@ public class CustomLinkedList implements Iterable<Integer> {
      * @param value - data for create Node.
      */
     public void add(int value) {
+        if (head == null) {
+            head = new Node(value);
+            tail = head;
+        } else {
+            Node temp = new Node(value);
+            tail.setNext(temp);
+            tail = temp;
+        }
+        size++;
+    }
 
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(index);
+        }
+    }
+
+    private Node getNode(int index) {
+        checkIndex(index);
+        Node currentNode = head;
+        for (int currentIndex = 0; currentIndex < index; currentIndex++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
     }
 
     /**
@@ -38,7 +65,7 @@ public class CustomLinkedList implements Iterable<Integer> {
      * @param index
      */
     public int get(int index) {
-       return 0;
+        return getNode(index).value;
     }
 
     /**
@@ -48,11 +75,26 @@ public class CustomLinkedList implements Iterable<Integer> {
      * Если был передан невалидный index - надо выкинуть исключение IndexOutOfBoundsException.
      * throw new IndexOutOfBoundsException(i);
      *
-     * @param i - index
+     * @param i     - index
      * @param value - data for create Node.
      */
     public void add(int i, int value) {
-
+        if (i < 0 || i > size) {
+            throw new IndexOutOfBoundsException(i);
+        }
+        Node newNode = new Node(value);
+        if (i == 0) {
+            newNode.setNext(head);
+            head = newNode;
+        } else if (i == size) {
+            tail.setNext(newNode);
+            tail = newNode;
+        } else {
+            Node prevForNew = getNode(i - 1);
+            newNode.setNext(prevForNew.next);
+            prevForNew.setNext(newNode);
+        }
+        size++;
     }
 
     /**
@@ -65,7 +107,17 @@ public class CustomLinkedList implements Iterable<Integer> {
      * @param index - position what element need remove.
      */
     public void removeElement(int index) {
-
+        checkIndex(index);
+        if (index == 0) {
+            head = head.next;
+        } else if (index == size - 1) {
+            Node prevForTail = getNode(size - 2);
+            prevForTail.setNext(null);
+            tail = prevForTail;
+        } else {
+            getNode(index - 1).setNext(getNode(index + 1));
+        }
+        size--;
     }
 
     /**
@@ -73,26 +125,42 @@ public class CustomLinkedList implements Iterable<Integer> {
      * Реализовать метод:
      * Переворачивает все элементы списка.
      * Пример:
-     *  Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
-     *  После исполнения метода последовательность должна быть такой "4 -> 3 -> 2 -> 1 -> null"
+     * Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
+     * После исполнения метода последовательность должна быть такой "4 -> 3 -> 2 -> 1 -> null"
      */
     public void revertList() {
-
+        Node currentNode = head;
+        Node nextNode = currentNode.next;
+        Node nextNextNode = nextNode.next;
+        currentNode.setNext(null);
+        while (nextNextNode != null) {
+            nextNode.setNext(currentNode);
+            currentNode = nextNode;
+            nextNode = nextNextNode;
+            nextNextNode = nextNextNode.next;
+        }
+        nextNode.setNext(currentNode);
+        tail = head;
+        head = nextNode;
     }
 
     /**
      * 1 тугрик
      * Метод выводит всю последовательность хранящуюся в списке начиная с head.
      * Формат вывода:
-     *  - значение каждой Node должно разделяться " -> "
-     *  - последовательность всегда заканчивается на null
-     *  - если в списке нет элементов - верните строку "null"
+     * - значение каждой Node должно разделяться " -> "
+     * - последовательность всегда заканчивается на null
+     * - если в списке нет элементов - верните строку "null"
      *
      * @return - String with description all list
      */
     @Override
     public String toString() {
-        return "1 -> 2 -> 3 -> null";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Node currentNode = head; currentNode != null; currentNode = currentNode.next) {
+            stringBuilder.append(currentNode.value).append(" -> ");
+        }
+        return stringBuilder.append("null").toString();
     }
 
     /**
@@ -103,7 +171,25 @@ public class CustomLinkedList implements Iterable<Integer> {
      */
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>() {
+            Node currentNode = head;
+
+            @Override
+            public boolean hasNext() {
+                return currentNode != null;
+            }
+
+            @Override
+            public Integer next() {
+                if (hasNext()) {
+                    int value = currentNode.value;
+                    currentNode = currentNode.next;
+                    return value;
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
     }
 
     private static class Node {
