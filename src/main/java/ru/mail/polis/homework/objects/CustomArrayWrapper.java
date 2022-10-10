95fa7ee4ed4d8f3b5e88e712c92c4b8dc6ca8d02
Ilgiz2003 <97854970+Ilgiz2003@users.noreply.github.com>
2022-10-10 05:23:31 +0300

    Add tasks

--- src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomArrayWrapper.java
@@ -1,6 +1,8 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Вам придется реализовать Iterable класс CustomArrayWrapper вместе с методами которые
@@ -15,6 +17,7 @@ public class CustomArrayWrapper implements Iterable<Integer> {
 
     private final int[] array;          // массив
     private int position;               // следующая позиция куда будет вставлен элемент
+    private int operations = 0;
 
     public CustomArrayWrapper(int size) {
         this.array = new int[size];
@@ -24,11 +27,13 @@ public class CustomArrayWrapper implements Iterable<Integer> {
         checkIndex(position);
         array[position] = value;
         position++;
+        operations++;
     }
 
     public void edit(int index, int value) {
         checkIndex(index);
         array[index] = value;
+        operations++;
     }
 
     public int get(int index) {
@@ -48,7 +53,28 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      */
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>() {
+            int position = 0;
+            final int operationsInIteration = operations;
+
+            @Override
+            public boolean hasNext() {
+                return position < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (operationsInIteration != operations) {
+                    throw new ConcurrentModificationException();
+                } else if (hasNext()) {
+                    int value = array[position];
+                    position++;
+                    return value;
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
     }
 
     /**
@@ -58,7 +84,28 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      * @return Iterator for EVEN elements
      */
     public Iterator<Integer> evenIterator() {
-        return null;
+        return new Iterator<Integer>() {
+            int position = 1;
+            final int operationsInIteration = operations;
+
+            @Override
+            public boolean hasNext() {
+                return position < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (operationsInIteration != operations) {
+                    throw new ConcurrentModificationException();
+                } else if (hasNext()) {
+                    int value = array[position];
+                    position += 2;
+                    return value;
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
     }
 
     /**
@@ -68,7 +115,28 @@ public class CustomArrayWrapper implements Iterable<Integer> {
      * @return Iterator for ODD elements
      */
     public Iterator<Integer> oddIterator() {
-        return null;
+        return new Iterator<Integer>() {
+            int position = 0;
+            final int operationsInIteration = operations;
+
+            @Override
+            public boolean hasNext() {
+                return position < array.length;
+            }
+
+            @Override
+            public Integer next() {
+                if (operationsInIteration != operations) {
+                    throw new ConcurrentModificationException();
+                } else if (hasNext()) {
+                    int value = array[position];
+                    position += 2;
+                    return value;
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
     }
 
     private void checkIndex(int index) {
@@ -76,5 +144,4 @@ public class CustomArrayWrapper implements Iterable<Integer> {
             throw new IndexOutOfBoundsException();
         }
     }
-
 }
--- src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
+++ src/main/java/ru/mail/polis/homework/objects/CustomLinkedList.java
@@ -1,6 +1,7 @@
 package ru.mail.polis.homework.objects;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * 15 тугриков
@@ -9,6 +10,12 @@ import java.util.Iterator;
 public class CustomLinkedList implements Iterable<Integer> {
 
     private Node head;
+    private int size;
+
+    public CustomLinkedList() {
+        this.head = null;
+        size = 0;
+    }
 
     /**
      * 1 тугрик
@@ -17,7 +24,7 @@ public class CustomLinkedList implements Iterable<Integer> {
      * @return size
      */
     public int size() {
-        return 0;
+        return size;
     }
 
     /**
@@ -28,7 +35,17 @@ public class CustomLinkedList implements Iterable<Integer> {
      * @param value - data for create Node.
      */
     public void add(int value) {
-
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = newNode;
+        }
+        size++;
     }
 
     /**
@@ -38,7 +55,16 @@ public class CustomLinkedList implements Iterable<Integer> {
      * @param index
      */
     public int get(int index) {
-       return 0;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException(index);
+        }
+        int counter = 0;
+        Node currentNode = head;
+        while (counter < index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode.value;
     }
 
     /**
@@ -48,11 +74,28 @@ public class CustomLinkedList implements Iterable<Integer> {
      * Если был передан невалидный index - надо выкинуть исключение IndexOutOfBoundsException.
      * throw new IndexOutOfBoundsException(i);
      *
-     * @param i - index
+     * @param i     - index
      * @param value - data for create Node.
      */
     public void add(int i, int value) {
-
+        if (i > size || i < 0) {
+            throw new IndexOutOfBoundsException(i);
+        }
+        Node currentNode = head;
+        Node newNode = new Node(value);
+        int counter = 0;
+        if (i == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            while (counter < i - 1) {
+                currentNode = currentNode.next;
+                counter++;
+            }
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+        size++;
     }
 
     /**
@@ -65,7 +108,21 @@ public class CustomLinkedList implements Iterable<Integer> {
      * @param index - position what element need remove.
      */
     public void removeElement(int index) {
-
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException(index);
+        }
+        if (index == 0) {
+            head = head.next;
+        } else {
+            int counter = 0;
+            Node currentNode = head;
+            while (counter < index - 1) {
+                currentNode = currentNode.next;
+                counter++;
+            }
+            currentNode.next = currentNode.next.next;
+        }
+        size--;
     }
 
     /**
@@ -73,26 +130,46 @@ public class CustomLinkedList implements Iterable<Integer> {
      * Реализовать метод:
      * Переворачивает все элементы списка.
      * Пример:
-     *  Исходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
-     *  После исполнения метода последовательность должна быть такой "4 -> 3 -> 2 -> 1 -> null"
+     * сходная последовательность списка "1 -> 2 -> 3 -> 4 -> null"
+     * После исполнения метода последовательность должна быть такой "4 -> 3 -> 2 -> 1 -> null"
      */
     public void revertList() {
+        head = reverse(head);
+    }
 
+    Node reverse(Node head) {
+        Node prevNode = null;
+        Node currentNode = head;
+        while (currentNode != null) {
+            Node nextNode = currentNode.next;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+        return prevNode;
     }
 
     /**
      * 1 тугрик
      * Метод выводит всю последовательность хранящуюся в списке начиная с head.
      * Формат вывода:
-     *  - значение каждой Node должно разделяться " -> "
-     *  - последовательность всегда заканчивается на null
-     *  - если в списке нет элементов - верните строку "null"
+     * - значение каждой Node должно разделяться " -> "
+     * - последовательность всегда заканчивается на null
+     * - если в списке нет элементов - верните строку "null"
      *
      * @return - String with description all list
      */
     @Override
     public String toString() {
-        return "1 -> 2 -> 3 -> null";
+        Node currentNode = head;
+        StringBuilder output = new StringBuilder();
+        while (currentNode != null) {
+            output.append(currentNode.value);
+            output.append(" -> ");
+            currentNode = currentNode.next;
+        }
+        output.append("null");
+        return output.toString();
     }
 
     /**
@@ -103,7 +180,26 @@ public class CustomLinkedList implements Iterable<Integer> {
      */
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<>() {
+
+            Node currentNode = head;
+
+            @Override
+            public boolean hasNext() {
+                return currentNode != null;
+            }
+
+            @Override
+            public Integer next() {
+                if (hasNext()) {
+                    int value = currentNode.value;
+                    currentNode = currentNode.next;
+                    return value;
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
     }
 
     private static class Node {
