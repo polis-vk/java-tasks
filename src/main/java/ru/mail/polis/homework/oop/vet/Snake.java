fe19e8a469f2d5717c863a16a2500a2e2e69d207
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-12 12:44:24 +0300

    part3-vet: implements classes

---
+++ src/main/java/ru/mail/polis/homework/oop/vet/Snake.java
@@ -0,0 +1,30 @@
+package ru.mail.polis.homework.oop.vet;
+
+public class Snake extends Animal implements WildAnimal {
+
+    private String organizationName;
+
+    public Snake() {
+        super(0);
+    }
+
+    @Override
+    public String say() {
+        return "Shhhh";
+    }
+
+    @Override
+    public MoveType moveType() {
+        return MoveType.CRAWL;
+    }
+
+    @Override
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    @Override
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+}
