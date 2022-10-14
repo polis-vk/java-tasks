7f7a7ac52024975d953ea129d7958f35b480ccbe
llav3ji2019 <pavelemelyanov03@mail.ru>
2022-10-14 18:37:51 +0300

    adding adapter

--- src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
+++ src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
@@ -4,5 +4,12 @@ package ru.mail.polis.homework.oop.adapter;
  * После имплементации раскоментировать тест в ru.mail.polis.homework.oop.adapter.ProductPriceParsServiceTest
  * для проверки правильной имплементации
  */
-public class JsonCsvDataAdapter {
+public class JsonCsvDataAdapter extends CsvData {
+    public JsonCsvDataAdapter(String text) {
+        super(parseJsonToCsvDataFormat(text));
+    }
+
+    private static String parseJsonToCsvDataFormat(String jsonData) {
+        return jsonData.replace("{", "").replace("}", "").replaceAll("\"", "").replaceAll(",", "").replaceAll(":", ",");
+    }
 }
