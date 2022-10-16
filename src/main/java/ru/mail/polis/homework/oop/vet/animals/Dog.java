eafec40b2aa484000a4bde2f4aed9dd357dc6af3
Denis Strizhkin <strdenis02@gmail.com>
2022-10-16 17:51:29 +0300

    Implemented Animals classes.

---
+++ src/main/java/ru/mail/polis/homework/oop/vet/animals/Dog.java
@@ -0,0 +1,21 @@
+package ru.mail.polis.homework.oop.vet.animals;
+
+import ru.mail.polis.homework.oop.vet.Animal;
+import ru.mail.polis.homework.oop.vet.MoveType;
+import ru.mail.polis.homework.oop.vet.Pet;
+
+public class Dog extends Animal implements Pet {
+    public Dog(int legs) {
+        super(legs);
+    }
+
+    @Override
+    public String say() {
+        return "Wow-wow";
+    }
+
+    @Override
+    public MoveType moveType() {
+        return MoveType.RUN;
+    }
+}
