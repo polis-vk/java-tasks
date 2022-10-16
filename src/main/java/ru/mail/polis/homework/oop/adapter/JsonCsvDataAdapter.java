189436824610721929c4131404dceb30bc2812da
Denis Strizhkin <strdenis02@gmail.com>
2022-10-16 17:55:33 +0300

    Uncommented adapter test.

--- src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
+++ src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
@@ -4,5 +4,8 @@ package ru.mail.polis.homework.oop.adapter;
  * После имплементации раскоментировать тест в ru.mail.polis.homework.oop.adapter.ProductPriceParsServiceTest
  * для проверки правильной имплементации
  */
-public class JsonCsvDataAdapter {
+public class JsonCsvDataAdapter extends CsvData {
+    public JsonCsvDataAdapter(String text) {
+        super(text);
+    }
 }
