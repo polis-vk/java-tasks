55effe798c175fee327777fc896d5663fe0dd374
Almaz Shakirov <47986788+shakirov-aa@users.noreply.github.com>
2022-10-16 23:13:04 +0300

    Added HW-4 (#975)

---
+++ src/main/java/ru/mail/polis/homework/generics/bot/handler/ExampleStateHandler.java
@@ -0,0 +1,19 @@
+package ru.mail.polis.homework.generics.bot.handler;
+
+public class ExampleStateHandler implements StateHandler {
+
+    @Override
+    public void handle() {
+        // do nothing
+    }
+
+    /**
+     * Поправить в соответствии с изменениями в StateHandler
+     *
+     * 1 тугрик
+     */
+    @Override
+    public Class getHandlingStateClass() {
+        return null;
+    }
+}
