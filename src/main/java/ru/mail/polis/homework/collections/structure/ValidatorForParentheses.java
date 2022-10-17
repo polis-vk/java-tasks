dd08abacc7dad5728cbc7682aaf40d7c6bf904eb
StombieIT <stombie@yandex.ru>
2022-10-17 23:45:48 +0300

    validator for parentheses done

--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,5 +1,10 @@
 package ru.mail.polis.homework.collections.structure;
 
+import jdk.internal.joptsimple.internal.Strings;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Задание оценивается в 2 тугрика.
  * Одна из самых популярных задач.
@@ -12,9 +17,49 @@ package ru.mail.polis.homework.collections.structure;
  *
  * Отрабатывать метод должен за О(n)
  */
+
 public class ValidatorForParentheses {
 
     public static boolean validate(String value) {
-        return false;
+        if (value == null) {
+            return false;
+        }
+        Deque<Character> initialBrackets = new LinkedList<>();
+        boolean hasAdded = false;
+        for (int i = 0; i < value.length(); i++) {
+            char symbol = value.charAt(i);
+            switch (value.charAt(i)) {
+                case '(':
+                case '[':
+                case '<':
+                case '{':
+                    initialBrackets.addLast(symbol);
+                    if (!hasAdded) {
+                        hasAdded = true;
+                    }
+                    break;
+                case ')':
+                    if (initialBrackets.isEmpty() || initialBrackets.pollLast() != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (initialBrackets.isEmpty() || initialBrackets.pollLast() != '[') {
+                        return false;
+                    }
+                    break;
+                case '>':
+                    if (initialBrackets.isEmpty() || initialBrackets.pollLast() != '<') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (initialBrackets.isEmpty() || initialBrackets.pollLast() != '{') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return hasAdded && initialBrackets.isEmpty();
     }
 }
