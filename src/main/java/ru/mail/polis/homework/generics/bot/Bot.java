b2234712f362a47e12806b16d857861c1108ba47
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-18 14:36:52 +0300

    part4-generics: done

--- src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
+++ src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
@@ -3,9 +3,13 @@ package ru.mail.polis.homework.generics.bot;
 import ru.mail.polis.homework.generics.bot.handler.StateHandler;
 import ru.mail.polis.homework.generics.bot.state.State;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Bot {
+
+    Map<Class<? extends State>, StateHandler<? extends State>> stateHandlerMap;
 
     /**
      * Конструктор бота, которому на вход подаются хэндлеры состояний.
@@ -13,8 +17,12 @@ public class Bot {
      *
      * 1 тугрик
      */
-    public Bot(List<StateHandler> handlers) {
+    public Bot(List<StateHandler<? extends State>> handlers) {
+        stateHandlerMap = new HashMap<>();
 
+        for (StateHandler<? extends State> stateHandler : handlers) {
+            stateHandlerMap.put(stateHandler.getHandlingStateClass(), stateHandler);
+        }
     }
 
     /**
@@ -23,5 +31,6 @@ public class Bot {
      * 1 тугрик
      */
     public void handleState(State state) {
+        stateHandlerMap.get(state.getClass()).handle();
     }
 }
