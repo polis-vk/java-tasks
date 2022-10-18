e45190fb2a341dd1f7c219aa9080f3379eb3039d
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-18 12:43:54 +0300

    part4-CustomDictionary: done

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,7 +1,12 @@
 package ru.mail.polis.homework.collections.structure;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Задание оценивается в 4 тугрика.
@@ -11,15 +16,35 @@ import java.util.List;
  */
 public class CustomDictionary {
 
+    private final Map<Map<Character, Integer>, Set<String>> values = new HashMap<>();
+    private int size = 0;
+
     /**
      * Сохранить строку в структуру данных
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
      *
-     * Сложность - []
+     * Сложность - [O(n)], где n - длина value.
      */
     public boolean add(String value) {
-        return false;
+        if (value == null || value.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        Map<Character, Integer> letterCountMap = getLetterCountMap(value);
+        if (values.containsKey(letterCountMap)) {
+            if (values.get(letterCountMap).add(value)) {
+                size++;
+                return true;
+            }
+            return false;
+        }
+
+        Set<String> words = new LinkedHashSet<>();
+        words.add(value);
+        values.put(letterCountMap, words);
+        size++;
+        return true;
     }
 
     /**
@@ -27,9 +52,14 @@ public class CustomDictionary {
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
      *
-     * Сложность - []
+     * Сложность - [O(n)], где n - длина value.
      */
     public boolean contains(String value) {
+        Map<Character, Integer> letterCountMap = getLetterCountMap(value);
+
+        if (values.containsKey(letterCountMap)) {
+            return values.get(letterCountMap).contains(value);
+        }
         return false;
     }
 
@@ -38,9 +68,18 @@ public class CustomDictionary {
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
      *
-     * Сложность - []
+     * Сложность - [O(n)], где n - длина value.
      */
     public boolean remove(String value) {
+        Map<Character, Integer> letterCountMap = getLetterCountMap(value);
+
+        if (!values.containsKey(letterCountMap)) {
+            return false;
+        }
+        if (values.get(letterCountMap).remove(value)) {
+            size--;
+            return true;
+        }
         return false;
     }
 
@@ -61,9 +100,17 @@ public class CustomDictionary {
      * @return - список слов которые состоят из тех же букв, что и передаваемая
      * строка.
      *
-     * Сложность - []
+     * Сложность - [O(n + m)], где n - длина value, m - количество подходящий слов.
+     * Думаю, что это стоит пояснить: подходящие слова надо занести в список-результат.
+     * При создании LinkedList'а каждое найденное слово будет в него заноситься, соответственно,
+     * к сложности еще добавляется m (число подходящих слов).
      */
     public List<String> getSimilarWords(String value) {
+        Map<Character, Integer> letterCountMap = getLetterCountMap(value);
+
+        if (values.containsKey(letterCountMap)) {
+            return new LinkedList<>(values.get(letterCountMap));
+        }
         return Collections.emptyList();
     }
 
@@ -71,11 +118,32 @@ public class CustomDictionary {
      * Колл-во хранимых строк.
      * @return - Колл-во хранимых строк.
      *
-     * Сложность - []
+     * Сложность - [O(1)]
      */
     public int size() {
-        return 0;
+        return size;
     }
 
+    /**
+     * Получаем отображение каждого символа строки, на его количество.
+     * При этом регистр символа не учитывается.
+     *
+     * @param value строка, в которой хотим подсчитать количество символов.
+     * @return мапа, отображающая символ на его количество в строке.
+     *
+     * Сложность - [O(n)], где n - длина value.
+     */
+    private Map<Character, Integer> getLetterCountMap(String value) {
+        if (value == null || value.length() == 0) {
+            throw new IllegalArgumentException();
+        }
 
+        Map<Character, Integer> result = new HashMap<>();
+        for (char ch : value.toCharArray()) {
+            ch = Character.toLowerCase(ch);
+            result.merge(ch, 1, Integer::sum);
+        }
+
+        return result;
+    }
 }
