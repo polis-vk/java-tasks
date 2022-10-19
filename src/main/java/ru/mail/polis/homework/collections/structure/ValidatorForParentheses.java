da05cbc5a67e10a70fb5cfd621a9f47eb1f3a5ca
Vladimir <i.am.polshin@gmail.com>
2022-10-19 21:22:01 +0300

    Done ValidatorForParentheses

--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Задание оценивается в 2 тугрика.
  * Одна из самых популярных задач.
@@ -7,14 +10,42 @@ package ru.mail.polis.homework.collections.structure;
  * В строке помимо скобок могут содержаться и другие символы.
  * Скобки могут быть: [],{},<>,()
  * Примеры:
- *      "(-b + (x)^2)/(2+4)" - true
- *      "Понедельники меня угнетают ((" - false
- *
+ * "(-b + (x)^2)/(2+4)" - true
+ * "Понедельники меня угнетают ((" - false
+ * <p>
  * Отрабатывать метод должен за О(n)
  */
 public class ValidatorForParentheses {
+    private static final String brackets = "{}[]()<>";
 
     public static boolean validate(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+
+        boolean isContainingBracket = false;
+        Deque<Character> deque = new LinkedList<>();
+        for (char ch : value.toCharArray()) {
+            if (brackets.indexOf(ch) != -1) {
+                if (isOpeningBracket(ch)) {
+                    deque.push(ch);
+                    isContainingBracket = true;
+                } else if (deque.isEmpty() || !isMatchingBrackets(deque.pop(), ch)) {
+                    return false;
+                }
+            }
+        }
+        return isContainingBracket && deque.isEmpty();
+    }
+
+    public static boolean isOpeningBracket(char ch) {
+        return "{[(<".indexOf(ch) != -1;
+    }
+
+    public static boolean isMatchingBrackets(char openingBracket, char closingBracket) {
+        return '(' == openingBracket && ')' == closingBracket
+                || '[' == openingBracket && ']' == closingBracket
+                || '{' == openingBracket && '}' == closingBracket
+                || '<' == openingBracket && '>' == closingBracket;
     }
 }
