271ea1ca828e7cec4dfc01e7a79047972e2b0a0b
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-19 11:44:37 +0300

    part4-CustomArrayList: initial commit

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
@@ -14,116 +14,137 @@ import java.util.ListIterator;
 public class CustomArrayList<E> implements List<E> {
     @Override
     public int size() {
+        // TODO.
         return 0;
     }
 
     @Override
     public boolean isEmpty() {
+        // TODO.
         return false;
     }
 
     @Override
     public boolean contains(Object o) {
+        // TODO.
         return false;
     }
 
     @Override
     public Iterator<E> iterator() {
+        // TODO.
         return null;
     }
 
     @Override
     public Object[] toArray() {
+        // TODO.
         return new Object[0];
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
+        // TODO.
         return null;
     }
 
     @Override
     public boolean add(E e) {
+        // TODO.
         return false;
     }
 
     @Override
     public boolean remove(Object o) {
+        // TODO.
         return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
+        // TODO.
         return false;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
+        // TODO.
         return false;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
+        // TODO.
         return false;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
+        // TODO.
         return false;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
+        // TODO.
         return false;
     }
 
     @Override
     public void clear() {
-
+        // TODO.
     }
 
     @Override
     public E get(int index) {
+        // TODO.
         return null;
     }
 
     @Override
     public E set(int index, E element) {
+        // TODO.
         return null;
     }
 
     @Override
     public void add(int index, E element) {
-
+        // TODO.
     }
 
     @Override
     public E remove(int index) {
+        // TODO.
         return null;
     }
 
     @Override
     public int indexOf(Object o) {
+        // TODO.
         return 0;
     }
 
     @Override
     public int lastIndexOf(Object o) {
+        // TODO.
         return 0;
     }
 
     @Override
     public ListIterator<E> listIterator() {
+        // TODO.
         return null;
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
+        // TODO.
         return null;
     }
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
+        // TODO.
         return null;
     }
 }
---
+++ src/test/java/ru/mail/polis/homework/collections/structure/CustomArrayListTest.java
@@ -0,0 +1,135 @@
+package ru.mail.polis.homework.collections.structure;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomArrayListTest {
+
+    @Before
+    public void setUp() throws Exception {
+        // TODO.
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // TODO.
+    }
+
+    @Test
+    public void size() {
+        // TODO.
+    }
+
+    @Test
+    public void isEmpty() {
+        // TODO.
+    }
+
+    @Test
+    public void contains() {
+        // TODO.
+    }
+
+    @Test
+    public void iterator() {
+        // TODO.
+    }
+
+    @Test
+    public void toArray() {
+        // TODO.
+    }
+
+    @Test
+    public void testToArray() {
+        // TODO.
+    }
+
+    @Test
+    public void add() {
+        // TODO.
+    }
+
+    @Test
+    public void remove() {
+        // TODO.
+    }
+
+    @Test
+    public void containsAll() {
+        // TODO.
+    }
+
+    @Test
+    public void addAll() {
+        // TODO.
+    }
+
+    @Test
+    public void testAddAll() {
+        // TODO.
+    }
+
+    @Test
+    public void removeAll() {
+        // TODO.
+    }
+
+    @Test
+    public void retainAll() {
+        // TODO.
+    }
+
+    @Test
+    public void clear() {
+        // TODO.
+    }
+
+    @Test
+    public void get() {
+        // TODO.
+    }
+
+    @Test
+    public void set() {
+        // TODO.
+    }
+
+    @Test
+    public void testAdd() {
+        // TODO.
+    }
+
+    @Test
+    public void testRemove() {
+        // TODO.
+    }
+
+    @Test
+    public void indexOf() {
+        // TODO.
+    }
+
+    @Test
+    public void lastIndexOf() {
+        // TODO.
+    }
+
+    @Test
+    public void listIterator() {
+        // TODO.
+    }
+
+    @Test
+    public void testListIterator() {
+        // TODO.
+    }
+
+    @Test
+    public void subList() {
+        // TODO.
+    }
+}
