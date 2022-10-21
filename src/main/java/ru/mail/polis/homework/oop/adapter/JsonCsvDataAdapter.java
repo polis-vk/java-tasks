882e63e93dbf784e362041bc288d9746dcadb13d
Queenore <atcheschev@yandex.ru>
2022-10-22 02:12:24 +0300

    JsonCsvDataAdapter.java done

--- src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
+++ src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
@@ -4,5 +4,21 @@ package ru.mail.polis.homework.oop.adapter;
  * После имплементации раскоментировать тест в ru.mail.polis.homework.oop.adapter.ProductPriceParsServiceTest
  * для проверки правильной имплементации
  */
-public class JsonCsvDataAdapter {
+public class JsonCsvDataAdapter extends CsvData {
+
+    public JsonCsvDataAdapter(String text) {
+        super(generateCsvString(text));
+    }
+
+    private static String generateCsvString(String text) {
+        StringBuilder sb = new StringBuilder();
+        String[] splitCommons = text.replaceAll("[{}]", "").split(",");
+        for (String currStr : splitCommons) {
+            String[] currSplit = currStr.trim().split(":");
+            String firstElem = currSplit[0].replaceAll("^\"|\"$", "");
+            String secondElem = currSplit[1];
+            sb.append(firstElem).append(",").append(secondElem).append("\n");
+        }
+        return sb.toString();
+    }
 }
