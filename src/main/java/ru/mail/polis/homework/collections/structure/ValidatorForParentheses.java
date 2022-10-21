9d335b5c16d65bbd88be1d97ae83b240c32a95ed
Aleksandr Kobyzhev <aleksandr.kobyzhev@spbpu.com>
2022-10-21 13:52:20 +0300

    Add solution in ValidatorForParentheses

--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Stack;
+
 /**
  * Задание оценивается в 2 тугрика.
  * Одна из самых популярных задач.
@@ -15,6 +17,36 @@ package ru.mail.polis.homework.collections.structure;
 public class ValidatorForParentheses {
 
     public static boolean validate(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+
+        Stack<Character> parenthesesStack = new Stack<>();
+        String openParentheses = "[{<(";
+        String closeParentheses = "]}>)";
+        boolean hasParentheses = false;
+
+        for (int i = 0; i < value.length(); i++) {
+            char symbol = value.charAt(i);
+
+            if (openParentheses.indexOf(symbol) >= 0) {
+                parenthesesStack.push(symbol);
+                hasParentheses = true;
+            } else {
+                int symbolIndex = closeParentheses.indexOf(symbol);
+                if (symbolIndex >= 0) {
+                    if (parenthesesStack.isEmpty()) {
+                        return false;
+                    }
+
+                    int bracketIndex = openParentheses.indexOf(parenthesesStack.pop());
+                    if (symbolIndex != bracketIndex) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return parenthesesStack.isEmpty() && hasParentheses;
     }
 }
