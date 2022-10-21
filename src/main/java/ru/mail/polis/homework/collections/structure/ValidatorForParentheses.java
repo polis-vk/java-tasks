a88d2fced0fa87e283070572e21d84dc7d625e57
StombieIT <stombie@yandex.ru>
2022-10-21 23:39:58 +0300

    fixed validator for parentheses

--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,9 +1,9 @@
 package ru.mail.polis.homework.collections.structure;
 
-import jdk.internal.joptsimple.internal.Strings;
-
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * Задание оценивается в 2 тугрика.
@@ -20,46 +20,44 @@ import java.util.LinkedList;
 
 public class ValidatorForParentheses {
 
+    private static final Map<Character, Character> INITIAL_TO_CLOSING_PARENTHESES;
+    private static final Map<Character, Character> CLOSING_TO_INITIAL_PARENTHESES;
+
+    static {
+        INITIAL_TO_CLOSING_PARENTHESES = new HashMap<>();
+
+        INITIAL_TO_CLOSING_PARENTHESES.put('{', '}');
+        INITIAL_TO_CLOSING_PARENTHESES.put('[', ']');
+        INITIAL_TO_CLOSING_PARENTHESES.put('<', '>');
+        INITIAL_TO_CLOSING_PARENTHESES.put('(', ')');
+
+        CLOSING_TO_INITIAL_PARENTHESES = new HashMap<>();
+
+        INITIAL_TO_CLOSING_PARENTHESES.entrySet()
+                .stream()
+                .forEach(entry -> CLOSING_TO_INITIAL_PARENTHESES.put(entry.getValue(), entry.getKey()));
+    }
+
     public static boolean validate(String value) {
         if (value == null) {
             return false;
         }
-        Deque<Character> initialBrackets = new LinkedList<>();
+        Deque<Character> initialParentheses = new LinkedList<>();
         boolean hasAdded = false;
         for (int i = 0; i < value.length(); i++) {
             char symbol = value.charAt(i);
-            switch (value.charAt(i)) {
-                case '(':
-                case '[':
-                case '<':
-                case '{':
-                    initialBrackets.addLast(symbol);
-                    if (!hasAdded) {
-                        hasAdded = true;
-                    }
-                    break;
-                case ')':
-                    if (initialBrackets.isEmpty() || initialBrackets.pollLast() != '(') {
-                        return false;
-                    }
-                    break;
-                case ']':
-                    if (initialBrackets.isEmpty() || initialBrackets.pollLast() != '[') {
-                        return false;
-                    }
-                    break;
-                case '>':
-                    if (initialBrackets.isEmpty() || initialBrackets.pollLast() != '<') {
-                        return false;
-                    }
-                    break;
-                case '}':
-                    if (initialBrackets.isEmpty() || initialBrackets.pollLast() != '{') {
-                        return false;
-                    }
-                    break;
+            if (INITIAL_TO_CLOSING_PARENTHESES.containsKey(symbol)) {
+                initialParentheses.add(symbol);
+                hasAdded = true;
+                continue;
+            }
+            Character initialParenthesisForSymbol = CLOSING_TO_INITIAL_PARENTHESES.get(symbol);
+            if (initialParenthesisForSymbol != null &&
+                (initialParentheses.isEmpty() ||
+                initialParentheses.pollLast() != initialParenthesisForSymbol)) {
+                return false;
             }
         }
-        return hasAdded && initialBrackets.isEmpty();
+        return hasAdded && initialParentheses.isEmpty();
     }
 }
