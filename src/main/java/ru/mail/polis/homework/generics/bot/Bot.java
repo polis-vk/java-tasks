e84652b95a58c2b72e9be558e8c4ed851d233aa7
charlsraven <voron00100867@gmail.com>
2022-10-22 22:02:30 +0300

    hw3

--- src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
+++ src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
@@ -3,18 +3,22 @@ package ru.mail.polis.homework.generics.bot;
 import ru.mail.polis.homework.generics.bot.handler.StateHandler;
 import ru.mail.polis.homework.generics.bot.state.State;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Bot {
-
+    private HashMap<Class<? extends State>, StateHandler<? extends State>> stateHandlerMap;
     /**
      * Конструктор бота, которому на вход подаются хэндлеры состояний.
      * Необходимо как-то сохранить эти хэндлеры так, чтобы потом можно было вызвать нужный хэндлер из метода handleState
      *
      * 1 тугрик
      */
-    public Bot(List<StateHandler> handlers) {
-
+    public Bot(List<StateHandler<? extends State>> handlers) {
+        for (StateHandler<? extends State> handler :
+                handlers) {
+            stateHandlerMap.put(handler.getHandlingStateClass(), handler);
+        }
     }
 
     /**
@@ -23,5 +27,6 @@ public class Bot {
      * 1 тугрик
      */
     public void handleState(State state) {
+        stateHandlerMap.get(state.getClass()).handle();
     }
 }
--- src/main/java/ru/mail/polis/homework/generics/bot/handler/StateHandler.java
+++ src/main/java/ru/mail/polis/homework/generics/bot/handler/StateHandler.java
@@ -1,16 +1,18 @@
 package ru.mail.polis.homework.generics.bot.handler;
 
+import ru.mail.polis.homework.generics.bot.state.State;
+
 /**
  * Интерфейс должен быть типизирован, в качестве типа должен быть обрабатываемый тип State
  *
  * 1 тугрик за типизацию интерфейса и 1 тугрик за типизацию метода
  */
-public interface StateHandler {
+public interface StateHandler<T extends State> {
     void handle();
 
     /**
      * Возвращает класс State, который этот handler умеет обрабатывать.
      * Необходимо типизировать таким образом, чтобы возвращаемый класс был не любым, а наследником State.
      */
-    Class getHandlingStateClass();
+    Class<T> getHandlingStateClass();
 }
