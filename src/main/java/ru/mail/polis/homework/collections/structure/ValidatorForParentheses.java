53b3f0b082950c7df67238b4be8f624618ca10bd
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-10-22 22:49:26 +0300

    ValidatorForParentheses is done.

--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * Задание оценивается в 2 тугрика.
  * Одна из самых популярных задач.
@@ -7,14 +10,42 @@ package ru.mail.polis.homework.collections.structure;
  * В строке помимо скобок могут содержаться и другие символы.
  * Скобки могут быть: [],{},<>,()
  * Примеры:
- *      "(-b + (x)^2)/(2+4)" - true
- *      "Понедельники меня угнетают ((" - false
- *
+ * "(-b + (x)^2)/(2+4)" - true
+ * "Понедельники меня угнетают ((" - false
+ * <p>
  * Отрабатывать метод должен за О(n)
  */
 public class ValidatorForParentheses {
 
     public static boolean validate(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        char currentBracket;
+        boolean thereWasBracket = false;
+        for (int index = 0; index < value.length(); index++) {
+            currentBracket = value.charAt(index);
+            if (!Arrays.asList('(', '[', '{', '<').contains(currentBracket)) {
+                if (Arrays.asList(')', ']', '}', '>').contains(currentBracket)) {
+                    thereWasBracket = true;
+                    if (!stack.isEmpty() && check(stack.peek(), currentBracket)) {
+                        stack.pop();
+                    } else {
+                        stack.push(currentBracket);
+                        break;
+                    }
+                }
+            } else {
+                thereWasBracket = true;
+                stack.push(currentBracket);
+            }
+        }
+        return stack.isEmpty() && thereWasBracket;
+    }
+
+    public static boolean check(Character openingBracket, Character closingBracket) {
+        return (openingBracket == '(' && closingBracket == ')') || (openingBracket == '[' && closingBracket == ']') ||
+                (openingBracket == '{' && closingBracket == '}') || (openingBracket == '<' && closingBracket == '>');
     }
 }
