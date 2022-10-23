540ecdb6f73b79ec39024a05a1593900b9d3864d
Egorka <trololo221@bk.ru>
2022-10-23 19:15:51 +0300

    Implement CustomArrayList

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
@@ -1,9 +1,6 @@
 package ru.mail.polis.homework.collections.structure;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /**
  * Необходимо реализовать свой ArrayList (динамический массив).
@@ -12,120 +9,421 @@ import java.util.ListIterator;
  * Задание оценивается в 10 тугриков
  */
 public class CustomArrayList<E> implements List<E> {
+    private Object[] internalArray;
     private int size;
+    private int modCount;
+
+    public CustomArrayList() {
+        this.internalArray = new Object[0];
+    }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) >= 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            int lastEl = 0;
+            final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return lastEl != size;
+            }
+
+            @Override
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (hasNext()) {
+                    return (E) internalArray[lastEl++];
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return Arrays.copyOf(internalArray, size);
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a.length < size)
+            return (T[]) Arrays.copyOf(internalArray, size, a.getClass());
+        System.arraycopy(internalArray, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+
+        if (size >= internalArray.length) {
+            Object[] tempArr = new Object[(int) (internalArray.length * 1.5 + 1)];
+            System.arraycopy(internalArray, 0, tempArr, 0, internalArray.length);
+            tempArr[size] = e;
+            internalArray = tempArr;
+        } else {
+            internalArray[size] = e;
+        }
+
+        size++;
+        modCount++;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        int indexOfO = 0;
+        if (o == null) {
+            for (; indexOfO < size; indexOfO++) {
+                if (internalArray[indexOfO] == null) {
+                    break;
+                } else if (indexOfO == size - 1 && internalArray[indexOfO] != null) {
+                    return false;
+                }
+            }
+        } else {
+            for (; indexOfO < size; indexOfO++) {
+                if (internalArray[indexOfO].equals(o)) {
+                    break;
+                } else if (indexOfO == size - 1 && !internalArray[indexOfO].equals(o)) {
+                    return false;
+                }
+            }
+        }
+
+        if (indexOfO == size - 1) {
+            internalArray[size--] = null;
+            return true;
+        }
+
+        System.arraycopy(internalArray, indexOfO + 1, internalArray, indexOfO, size - indexOfO);
+        size--;
+        modCount++;
+
+        return true;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        Object[] arrayFromC = c.toArray();
+        boolean result;
+
+        for (int i = 0; i < arrayFromC.length; i++) {
+            result = this.contains(arrayFromC[i]);
+            if (!result) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        Object[] arrayOfNewElements = c.toArray();
+
+        if (internalArray.length <= size + arrayOfNewElements.length) {
+            Object[] newInternalArray = new Object[internalArray.length + arrayOfNewElements.length + 1];
+            System.arraycopy(internalArray, 0, newInternalArray, 0, size);
+            System.arraycopy(arrayOfNewElements, 0, newInternalArray, size, arrayOfNewElements.length);
+            internalArray = newInternalArray;
+        } else {
+            System.arraycopy(arrayOfNewElements, 0, internalArray, size, arrayOfNewElements.length);
+        }
+
+        size += arrayOfNewElements.length;
+        modCount += arrayOfNewElements.length;
+        return true;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        Object[] arrayOfNewElements = c.toArray();
+
+        if (internalArray.length <= size + arrayOfNewElements.length) {
+            Object[] newInternalArray = new Object[internalArray.length + arrayOfNewElements.length + 1];
+            System.arraycopy(internalArray, 0, newInternalArray, 0, index);
+            System.arraycopy(arrayOfNewElements, 0, newInternalArray, index, arrayOfNewElements.length);
+            System.arraycopy(internalArray, index, newInternalArray, arrayOfNewElements.length + index, internalArray.length - index);
+            internalArray = newInternalArray;
+        } else {
+            System.arraycopy(arrayOfNewElements, 0, internalArray, size, arrayOfNewElements.length);
+        }
+
+        size += arrayOfNewElements.length;
+        modCount += arrayOfNewElements.length;
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        Object[] arrFromC = c.toArray();
+
+        for (Object o : arrFromC) {
+            this.remove(o);
+        }
+
+        modCount++;
+
+        return true;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(internalArray[i])) {
+                this.remove(internalArray[i]);
+                i--;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public void clear() {
-
+        this.size = 0;
+        internalArray = new Object[0];
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return (E) internalArray[index];
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+
+        if (index >= size || index < 0) {
+            throw new NoSuchElementException();
+        }
+
+        E result = (E) internalArray[index];
+        internalArray[index] = element;
+
+        return result;
     }
 
     @Override
     public void add(int index, E element) {
+        Object[] newInternalArray;
+        if (size >= internalArray.length) {
+            newInternalArray = new Object[(int) (internalArray.length * 1.5 + 1)];
+        } else {
+            newInternalArray = new Object[internalArray.length];
+        }
+        System.arraycopy(internalArray, 0, newInternalArray, 0, index);
+        newInternalArray[index] = element;
+        System.arraycopy(internalArray, index, newInternalArray, index + 1, size - index);
+        internalArray = newInternalArray;
 
+        size++;
+        modCount++;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        E result = (E) internalArray[index];
+        System.arraycopy(internalArray, index + 1, internalArray, index, size - index);
+        internalArray[size--] = null;
+        modCount++;
+
+        return result;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        int i = 0;
+        if (o == null) {
+            for(; i < size; i++) {
+                if (internalArray[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for(; i < size; i++) {
+                if (internalArray[i].equals(o)) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        int i = size - 1;
+        if (o == null) {
+            for(; i >= 0; i--) {
+                if (internalArray[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for(; i >= 0; i--) {
+                if (internalArray[i].equals(o)) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public ListIterator<E> listIterator() {
-        return null;
+        return new ListItr(0);
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
-        return null;
+        return new ListItr(index);
+    }
+
+    private class ListItr implements ListIterator<E> {
+        int pointer;
+        int lastReturned = -1;
+        int expectedModCount = modCount;
+
+        ListItr(int index) {
+            this.pointer = index;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return pointer != size;
+        }
+
+        @Override
+        public E next() {
+            if (hasNext()) {
+                lastReturned++;
+                return (E) internalArray[pointer++];
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return pointer != 0;
+        }
+
+        @Override
+        public E previous() {
+            int i = pointer - 1;
+            if (i < 0)
+                throw new NoSuchElementException();
+            Object[] internalArray = CustomArrayList.this.internalArray;
+            if (i >= internalArray.length)
+                throw new ConcurrentModificationException();
+            pointer = i;
+            return (E) internalArray[lastReturned = i];
+        }
+
+        @Override
+        public int nextIndex() {
+            return pointer;
+        }
+
+        @Override
+        public int previousIndex() {
+            return pointer - 1;
+        }
+
+        @Override
+        public void remove() {
+            if (lastReturned < 0) {
+                throw new IllegalStateException();
+            }
+
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+
+            try {
+                CustomArrayList.this.remove(lastReturned);
+                pointer = lastReturned;
+                lastReturned = -1;
+                expectedModCount = modCount;
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+        }
+
+        @Override
+        public void set(E e) {
+            if (lastReturned < 0)
+                throw new IllegalStateException();
+
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+
+            try {
+                CustomArrayList.this.set(lastReturned, e);
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+        }
+
+        public void add(E e) {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+
+            try {
+                int i = pointer;
+                CustomArrayList.this.add(i, e);
+                pointer = i + 1;
+                lastReturned = -1;
+                expectedModCount = modCount;
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+        }
     }
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        return null;
+
+        if (fromIndex < 0 || toIndex > size || fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        CustomArrayList<E> sublist = new CustomArrayList<>();
+
+        for (int i = fromIndex; i < toIndex; i++) {
+            sublist.add((E) internalArray[i]);
+        }
+
+        return sublist;
     }
 }
