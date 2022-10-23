461758cbfdc372d0bebc416c0ffaac6a28e3f305
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-23 19:15:49 +0300

    part4-CustomArrayList: fixed add() in iterators

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
@@ -344,6 +344,7 @@ public class CustomArrayList<E> implements List<E> {
             public void add(E e) {
                 checkForMod();
                 CustomArrayList.this.add(curIndex, e);
+                curIndex++;
                 fixedModCount = modCount;
             }
 
