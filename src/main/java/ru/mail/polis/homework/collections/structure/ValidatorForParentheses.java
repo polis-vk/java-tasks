1086de37642c62f558779da5ed44ef27de4adf1d
sold666 <sold666@github.com>
2022-10-23 17:59:23 +0300

    add ValidatorForParentheses.java (all test passed)

--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Stack;
+
 /**
  * Задание оценивается в 2 тугрика.
  * Одна из самых популярных задач.
@@ -15,6 +17,74 @@ package ru.mail.polis.homework.collections.structure;
 public class ValidatorForParentheses {
 
     public static boolean validate(String value) {
-        return false;
+        Stack<Integer> stack = new Stack<>();
+        char a;
+        int i, k;
+        boolean flag = true;
+        if (value == null || value.equals("")) {
+            return false;
+        } else if (!(value.contains("{") || value.contains("}") || value.contains("[") || value.contains("]")
+                || value.contains("<") || value.contains(">") || value.contains("(") || value.contains(")"))) {
+            return false;
+        }
+        for (i = 0; i < value.length(); i++) {
+            a = value.charAt(i);
+            switch (a) {
+                case '(':
+                    stack.push(1);
+                    break;
+                case '[':
+                    stack.push(2);
+                    break;
+                case '{':
+                    stack.push(3);
+                    break;
+                case '<':
+                    stack.push(4);
+                    break;
+                case ')':
+                    if (stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    k = stack.peek();
+                    stack.pop();
+                    if (k != 1) flag = false;
+                    break;
+                case ']':
+                    if (stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    k = stack.peek();
+                    stack.pop();
+                    if (k != 2) flag = false;
+                    break;
+                case '}':
+                    if (stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    k = stack.peek();
+                    stack.pop();
+                    if (k != 3) flag = false;
+                    break;
+                case '>':
+                    if (stack.isEmpty()) {
+                        flag = false;
+                        break;
+                    }
+                    k = stack.peek();
+                    stack.pop();
+                    if (k != 4) flag = false;
+                    break;
+                default:
+                    break;
+            }
+            if (!flag) {
+                break;
+            }
+        }
+        return flag && stack.isEmpty();
     }
 }
