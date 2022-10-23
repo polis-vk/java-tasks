2cc2ff63fd82180a0c16300f9324acbbcc3a89f3
Kirill06344 <kirill12093@mail.ru>
2022-10-23 15:08:38 +0300

    add last part - generics

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
@@ -4,14 +4,16 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 /**
  * Необходимо реализовать свой ArrayList (динамический массив).
  * При изменении размера массива помните про метод System.arraycopy()
- *
+ * <p>
  * Задание оценивается в 10 тугриков
  */
 public class CustomArrayList<E> implements List<E> {
@@ -36,18 +38,34 @@ public class CustomArrayList<E> implements List<E> {
 
     @Override
     public boolean contains(Object o) {
-        return indexOf(0) >= 0;
+        return indexOf(o) >= 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+
+            private int inx = 0;
+
+            @Override
+            public boolean hasNext() {
+                return inx < size;
+            }
+
+            @Override
+            public E next() {
+                if (hasNext()) {
+                    return arr[inx++];
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
     }
 
     @Override
     public Object[] toArray() {
-        Object[] tmp = arr;
-        return tmp;
+        return Arrays.copyOf(arr, size);
     }
 
     @Override
@@ -64,42 +82,82 @@ public class CustomArrayList<E> implements List<E> {
 
     @Override
     public boolean add(E e) {
-        return false;
+        if (size == arr.length) {
+            resize(arr.length * 2);
+        }
+        arr[size++] = e;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
+        for (int i = 0; i < size; ++i) {
+            if (arr[i].equals(o)) {
+                System.arraycopy(arr, i + 1, arr, i, --size - i);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        Object[] tmp = c.toArray();
+        for (int i = 0; i < tmp.length; ++i) {
+            if (!contains(tmp[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        return addAll(size, c);
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        if (index < 0 || index > size) {
+            return false;
+        }
+        Object[] tmp = c.toArray();
+        if (tmp.length == 0) {
+            return false;
+        }
+        if (arr.length - size < tmp.length) {
+            resize(arr.length + tmp.length);
+        }
+        System.arraycopy(arr, index, arr, index + tmp.length, size - index);
+        System.arraycopy(tmp, 0, arr, index, tmp.length);
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        Object[] tmp = c.toArray();
+        for (int i = 0; i < tmp.length; ++i) {
+            remove(tmp[i]);
+        }
+        return true;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        boolean changed = false;
+        for (int i = size - 1; i >= 0; --i) {
+            Object o = get(i);
+            if (!c.contains(o)) {
+                remove(i);
+                changed = true;
+            }
+        }
+        return changed;
     }
 
     @Override
     public void clear() {
-
+        size = 0;
     }
 
     @Override
@@ -116,12 +174,25 @@ public class CustomArrayList<E> implements List<E> {
 
     @Override
     public void add(int index, E element) {
-
+        if (!checkIndex(index)) {
+            return;
+        }
+        if (size == arr.length) {
+            resize(arr.length * 2);
+        }
+        System.arraycopy(arr, index, arr, index + 1, size++ - index);
+        arr[index] = element;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        if (!checkIndex(index)) {
+            return null;
+        }
+        E tmp = arr[index];
+        System.arraycopy(arr, index + 1, arr, index, size - index - 1);
+        size--;
+        return tmp;
     }
 
     @Override
@@ -145,21 +216,98 @@ public class CustomArrayList<E> implements List<E> {
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for (int i = size - 1; i >= 0; --i) {
+            if (arr[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public ListIterator<E> listIterator() {
-        return null;
+        return new CustomListIterator<>(0);
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
-        return null;
+        return new CustomListIterator<>(index);
+
+    }
+
+    private class CustomListIterator<T> implements ListIterator<T> {
+
+        private int index;
+
+        public CustomListIterator(int index) {
+            this.index = index;
+        }
+        @Override
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        @Override
+        public T next() {
+            if (hasNext()) {
+                return (T) arr[index++];
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return index >= 0;
+        }
+
+        @Override
+        public T previous() {
+            if (hasPrevious()) {
+                return (T) arr[index--];
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        @Override
+        public int nextIndex() {
+            return (index == size) ? index : index + 1;
+        }
+
+        @Override
+        public int previousIndex() {
+            return (index == 0) ? index : index - 1;
+        }
+
+        @Override
+        public void remove() {
+            CustomArrayList.this.remove(index);
+        }
+
+        @Override
+        public void set(T e) {
+            CustomArrayList.this.set(index, (E) e);
+        }
+
+        @Override
+        public void add(T e) {
+            CustomArrayList.this.add(index, (E) e);
+            next();
+        }
     }
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        return null;
+        return Collections.emptyList();
     }
+
+    private void resize(int newCapacity) {
+        arr = Arrays.copyOf(arr, newCapacity);
+    }
+
+    private boolean checkIndex(int index) {
+        return index >= 0 && index < size;
+    }
+
 }
--- src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
+++ src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
@@ -3,9 +3,14 @@ package ru.mail.polis.homework.generics.bot;
 import ru.mail.polis.homework.generics.bot.handler.StateHandler;
 import ru.mail.polis.homework.generics.bot.state.State;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Bot {
+
+    private Map<Class< ? extends State>, StateHandler< ? extends State>> storage;
 
     /**
      * Конструктор бота, которому на вход подаются хэндлеры состояний.
@@ -13,8 +18,11 @@ public class Bot {
      *
      * 1 тугрик
      */
-    public Bot(List<StateHandler> handlers) {
-
+    public Bot(List<StateHandler< ? extends State>> handlers) {
+        storage = new HashMap<>();
+        for (StateHandler<? extends State> sh : handlers) {
+            storage.put(sh.getHandlingStateClass(), sh);
+        }
     }
 
     /**
@@ -23,5 +31,6 @@ public class Bot {
      * 1 тугрик
      */
     public void handleState(State state) {
+        storage.get(state.getClass()).handle();
     }
 }
