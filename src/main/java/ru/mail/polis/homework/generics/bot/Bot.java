35349d9310ec0224c924c6b1eed67a54865a0f1d
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-10-23 13:53:32 +0300

    Generics are done

--- src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
+++ src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
@@ -3,25 +3,31 @@ package ru.mail.polis.homework.generics.bot;
 import ru.mail.polis.homework.generics.bot.handler.StateHandler;
 import ru.mail.polis.homework.generics.bot.state.State;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Bot {
+    private final Map<Class<? extends State>, StateHandler<? extends State>> map = new HashMap<>();
 
     /**
      * Конструктор бота, которому на вход подаются хэндлеры состояний.
      * Необходимо как-то сохранить эти хэндлеры так, чтобы потом можно было вызвать нужный хэндлер из метода handleState
-     *
+     * <p>
      * 1 тугрик
      */
-    public Bot(List<StateHandler> handlers) {
-
+    public Bot(List<StateHandler<? extends State>> handlers) {
+        for (StateHandler<? extends State> stateHandler : handlers) {
+            map.put(stateHandler.getHandlingStateClass(), stateHandler);
+        }
     }
 
     /**
      * Вызывает хэндлер, предназначенный для переданного State
-     *
+     * <p>
      * 1 тугрик
      */
     public void handleState(State state) {
+        map.get(state.getClass()).handle();
     }
 }
