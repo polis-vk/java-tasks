842bc3947e3e0b85d1f7f1c89d12293aba3e9cc6
Queenore <atcheschev@yandex.ru>
2022-10-26 02:05:48 +0300

    CustomDictionary.java done

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,7 +1,6 @@
 package ru.mail.polis.homework.collections.structure;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Задание оценивается в 4 тугрика.
@@ -11,37 +10,59 @@ import java.util.List;
  */
 public class CustomDictionary {
 
+    private final HashMap<String, String> map = new HashMap<>();
+
     /**
      * Сохранить строку в структуру данных
+     *
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - O(N)
      */
     public boolean add(String value) {
-        return false;
+        if (value == null || value.equals("")) {
+            throw new IllegalArgumentException();
+        } else if (map.containsKey(value)) { // T(N) = O(1)
+            return false;
+        }
+        String sortLowerCaseValue = sortedLowerCaseStr(value);
+        map.put(value, sortLowerCaseValue); // T(N) = O(N)  <-- (худший случай при перезаписи элементов)
+        return true;
+    }
+
+    private String sortedLowerCaseStr(String value) {
+        char[] chars = value.toLowerCase(Locale.ROOT).toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 
     /**
      * Проверяем, хранится ли такая строка уже у нас
+     *
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - O(1)
      */
     public boolean contains(String value) {
-        return false;
+        return map.containsKey(value);
     }
 
     /**
      * Удаляем сохраненную строку если она есть
+     *
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - O(1)
      */
     public boolean remove(String value) {
-        return false;
+        if (map.size() == 0 || !map.containsKey(value)) { // T(N) = O(1)
+            return false;
+        }
+        map.remove(value); // T(N) = O(1)
+        return true;
     }
 
     /**
@@ -51,7 +72,7 @@ public class CustomDictionary {
      * сохраняем строки ["aaa", "aBa", "baa", "aaB"]
      * При поиске по строке "AAb" нам должен вернуться следующий
      * список: ["aBa","baa","aaB"]
-     *
+     * <p>
      * сохраняем строки ["aaa", "aAa", "a"]
      * поиск "aaaa"
      * результат: []
@@ -60,21 +81,29 @@ public class CustomDictionary {
      *
      * @return - список слов которые состоят из тех же букв, что и передаваемая
      * строка.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - O(N^2)
      */
     public List<String> getSimilarWords(String value) {
-        return Collections.emptyList();
+        ArrayList<String> list = new ArrayList<>();
+        String inputStr = sortedLowerCaseStr(value);
+        for (Map.Entry<String, String> entry: map.entrySet()) { // T(N) = O(N)
+            if (entry.getValue().equals(inputStr)) { // T(N) = O(N)
+                list.add(entry.getKey()); // T(N) = O(1)
+            }
+        }
+        return list;
     }
 
     /**
      * Колл-во хранимых строк.
-     * @return - Колл-во хранимых строк.
      *
-     * Сложность - []
+     * @return - Колл-во хранимых строк.
+     * <p>
+     * Сложность - O(1)
      */
     public int size() {
-        return 0;
+        return map.size();
     }
 
 
