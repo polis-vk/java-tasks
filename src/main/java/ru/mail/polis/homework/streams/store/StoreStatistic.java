af21eb1083a156f17032ef71a748c4cc218a943b
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-25 23:01:12 +0300

    part5-StoreStatistic: removed extra import

--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
