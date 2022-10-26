a50345272767b904ee5ae2a6a9a3dafef9625c60
Egorka <trololo221@bk.ru>
2022-10-26 14:51:43 +0300

    Add solve for CustomDictionary

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,7 +1,6 @@
 package ru.mail.polis.homework.collections.structure;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Задание оценивается в 4 тугрика.
@@ -11,15 +10,23 @@ import java.util.List;
  */
 public class CustomDictionary {
 
+    List<String> store = new ArrayList<>();
+
     /**
      * Сохранить строку в структуру данных
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
      *
-     * Сложность - []
+     * Сложность - [O(1)]
      */
     public boolean add(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (store.contains(value)) {
+            return false;
+        }
+        return store.add(value);
     }
 
     /**
@@ -27,10 +34,10 @@ public class CustomDictionary {
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
      *
-     * Сложность - []
+     * Сложность - [O(n)]
      */
     public boolean contains(String value) {
-        return false;
+        return store.contains(value);
     }
 
     /**
@@ -38,10 +45,10 @@ public class CustomDictionary {
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
      *
-     * Сложность - []
+     * Сложность - [O(n)]
      */
     public boolean remove(String value) {
-        return false;
+        return store.remove(value);
     }
 
     /**
@@ -61,20 +68,68 @@ public class CustomDictionary {
      * @return - список слов которые состоят из тех же букв, что и передаваемая
      * строка.
      *
-     * Сложность - []
+     * Сложность - [O(n * k), где k - количество букв в слове, n - размер массива]
      */
     public List<String> getSimilarWords(String value) {
-        return Collections.emptyList();
+
+        Character[] letters = new Character[value.length()];
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < value.length(); i++) {
+            if (value.toLowerCase(Locale.ROOT).charAt(i) >= 97 && value.toLowerCase(Locale.ROOT).charAt(i) <= 122) {
+                letters[i] = value.toLowerCase(Locale.ROOT).charAt(i);
+            }
+        }
+
+        // Counting unique letters in value
+        int[] countOfUniqueLetterInValue = new int[26];
+        int shift = 97; // Position of 'a' in ascii
+
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] == null) {
+                continue;
+            }
+            countOfUniqueLetterInValue[letters[i] - shift]++;
+        }
+
+        for (String element : store) {
+            if (value.length() != element.length()) {
+                continue;
+            }
+
+            int[] countOfUniqueLetterInElement = new int[26];
+            Character[] lettersInElement = new Character[element.length()];
+
+            for (int i = 0; i < value.length(); i++) {
+                if (element.toLowerCase(Locale.ROOT).charAt(i) >= 97 && element.toLowerCase(Locale.ROOT).charAt(i) <= 122) {
+                    lettersInElement[i] = element.toLowerCase(Locale.ROOT).charAt(i);
+                }
+            }
+
+
+            for (int i = 0; i < element.length(); i++) {
+                if (lettersInElement[i] == null) {
+                    continue;
+                }
+                countOfUniqueLetterInElement[lettersInElement[i] - shift]++;
+            }
+
+            if (Arrays.equals(countOfUniqueLetterInElement, countOfUniqueLetterInValue)) {
+                result.add(element);
+            }
+        }
+
+        return result;
     }
 
     /**
      * Колл-во хранимых строк.
      * @return - Колл-во хранимых строк.
      *
-     * Сложность - []
+     * Сложность - [O(1)]
      */
     public int size() {
-        return 0;
+        return store.size();
     }
 
 
