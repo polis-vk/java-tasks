1b66572770802fe7f3b3c269f9b8e8c5c883cde9
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-10-26 20:05:59 +0300

    CustomDictionary is done

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomDictionary.java
@@ -1,7 +1,12 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Задание оценивается в 4 тугрика.
@@ -10,38 +15,70 @@ import java.util.List;
  * Напишите какая сложность операций у вас получилась для каждого метода.
  */
 public class CustomDictionary {
+    private final HashMap<String, Set<String>> map = new HashMap<>();
+    private int size;
 
     /**
      * Сохранить строку в структуру данных
+     *
      * @param value - передаваемая строка
      * @return - успешно сохранили строку или нет.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(n*Log[n]), где n - длина массива]
      */
     public boolean add(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (contains(value)) {
+            return false;
+        }
+        String sortedLowerCaseValue = getSortedLowerCaseString(value);
+        if (map.get(sortedLowerCaseValue) != null) {
+            map.get(sortedLowerCaseValue).add(value);
+        } else {
+            Set<String> set = new HashSet<>();
+            set.add(value);
+            map.put(sortedLowerCaseValue, set);
+        }
+        size++;
+        return true;
     }
 
     /**
      * Проверяем, хранится ли такая строка уже у нас
+     *
      * @param value - передаваемая строка
      * @return - есть такая строка или нет в нашей структуре
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(n*Log[n]), где n - длина массива]
      */
     public boolean contains(String value) {
-        return false;
+        String sortedLowerCaseValue = getSortedLowerCaseString(value);
+        return map.containsKey(sortedLowerCaseValue) && map.get(sortedLowerCaseValue).contains(value);
     }
 
     /**
      * Удаляем сохраненную строку если она есть
+     *
      * @param value - какую строку мы хотим удалить
      * @return - true если удалили, false - если такой строки нет
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(n*Log[n]), где n - длина массива]
      */
     public boolean remove(String value) {
-        return false;
+        if (!contains(value)) {
+            return false;
+        }
+        String sortedLowerCaseValue = getSortedLowerCaseString(value);
+        if (map.get(sortedLowerCaseValue).size() == 1) {
+            map.remove(sortedLowerCaseValue);
+            size--;
+            return true;
+        } else {
+            size--;
+            return map.get(sortedLowerCaseValue).remove(value);
+        }
     }
 
     /**
@@ -51,7 +88,7 @@ public class CustomDictionary {
      * сохраняем строки ["aaa", "aBa", "baa", "aaB"]
      * При поиске по строке "AAb" нам должен вернуться следующий
      * список: ["aBa","baa","aaB"]
-     *
+     * <p>
      * сохраняем строки ["aaa", "aAa", "a"]
      * поиск "aaaa"
      * результат: []
@@ -60,22 +97,30 @@ public class CustomDictionary {
      *
      * @return - список слов которые состоят из тех же букв, что и передаваемая
      * строка.
-     *
-     * Сложность - []
+     * <p>
+     * Сложность - [O(n*Log[n]), где n - длина массива]
      */
     public List<String> getSimilarWords(String value) {
-        return Collections.emptyList();
+        String sortedLowerCaseValue = getSortedLowerCaseString(value);
+        Set<String> set = map.get(sortedLowerCaseValue);
+        return new ArrayList<>(set != null ? set : Collections.emptyList());
     }
 
     /**
      * Колл-во хранимых строк.
-     * @return - Колл-во хранимых строк.
      *
-     * Сложность - []
+     * @return - Колл-во хранимых строк.
+     * <p>
+     * Сложность - [O(1)]
      */
     public int size() {
-        return 0;
+        return size;
     }
 
+    private String getSortedLowerCaseString(String value) {
+        char[] chars = value.toLowerCase().toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
 
 }
