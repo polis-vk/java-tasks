852f92cdd24712fbe5ca6845f0f449557f6abb68
StombieIT <stombie@yandex.ru>
2022-10-26 14:46:58 +0300

    library statistic done

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,8 +1,10 @@
 package ru.mail.polis.homework.streams.lib;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 /**
  * Класс для работы со статистикой по библиотеке.
  * Оценка 5-ть баллов
@@ -18,7 +20,28 @@ public class LibraryStatistic {
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return null;
+        return library.getArchive()
+                .stream()
+                .filter(archivedData -> {
+                    LocalDateTime takePlus14Days = archivedData.getTake()
+                            .toLocalDateTime()
+                            .plusDays(14);
+                    LocalDateTime returned = archivedData.getReturned().toLocalDateTime();
+                    return returned.isEqual(takePlus14Days) ||
+                            returned.isAfter(takePlus14Days) ||
+                            archivedData.getBook().getGenre() == genre;
+                })
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() >= 5)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue()
+                                .stream()
+                                .mapToInt(archivedData -> archivedData.getBook().getPage())
+                                .sum()
+                ));
     }
 
     /**
@@ -29,7 +52,19 @@ public class LibraryStatistic {
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        return null;
+        return library.getArchive()
+                .stream()
+                .filter(archivedData -> user.equals(archivedData.getUser()) &&
+                        user.getBook().getGenre() != archivedData.getBook().getGenre())
+                .collect(Collectors.groupingBy(
+                        archivedData -> archivedData.getBook().getGenre(),
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream()
+                .max((entry1, entry2) -> (int) (entry1.getValue() - entry2.getValue()))
+                .orElseThrow(NoSuchElementException::new)
+                .getKey();
     }
 
     /**
@@ -39,7 +74,29 @@ public class LibraryStatistic {
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        return null;
+        return library.getArchive()
+                .stream()
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet()
+                .stream()
+                .filter(entry -> {
+                    Long booksCount = entry.getValue().stream().count();
+                    Long expiredBooksCount = entry.getValue()
+                            .stream()
+                            .filter(archivedData -> {
+                                LocalDateTime takePlus30Days = archivedData.getTake()
+                                        .toLocalDateTime()
+                                        .plusDays(30);
+                                // TODO: null considered
+                                LocalDateTime returned = archivedData.getReturned()
+                                        .toLocalDateTime();
+                                return returned.isAfter(takePlus30Days);
+                            })
+                            .count();
+                    return expiredBooksCount > (booksCount >> 1);
+                })
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -49,7 +106,10 @@ public class LibraryStatistic {
      * @return - список книг
      */
     public List<Book> booksWithMoreCountPages(Library library, int countPage) {
-        return null;
+        return library.getBooks()
+                .stream()
+                .filter(book -> book.getPage() >= countPage)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -58,6 +118,22 @@ public class LibraryStatistic {
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-        return null;
+        return library.getArchive()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        archivedData -> archivedData.getBook().getGenre(),
+                        Collectors.groupingBy(
+                                archivedData -> archivedData.getBook().getAuthor(),
+                                Collectors.counting())))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue()
+                                .entrySet()
+                                .stream()
+                                .max((entry1, entry2) -> (int) (entry1.getValue() - entry2.getValue()))
+                                .orElseThrow(NoSuchElementException::new)
+                                .getKey()));
     }
 }
