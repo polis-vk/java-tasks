2c7cb2bc11ac7860a1b3bcfba755f09c1e529cd8
StombieIT <stombie@yandex.ru>
2022-10-27 23:40:47 +0300

    store statistic some methods implemented

--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -1,8 +1,10 @@
 package ru.mail.polis.homework.streams.store;
 
 import java.sql.Timestamp;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
@@ -20,7 +22,13 @@ public class StoreStatistic {
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return 0L;
+        return orders.stream()
+                .filter(order -> {
+                    Timestamp orderTime = order.getTime();
+                    // maybe should consider item doesn't exist in order ?
+                    return orderTime.after(from) && orderTime.before(to);
+                })
+                .collect(Collectors.summingLong(order -> order.getItemCount().get(typeItem)));
     }
 
     /**
@@ -39,7 +47,18 @@ public class StoreStatistic {
      * @return - товар
      */
     public Item mostPopularItem(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .flatMap(order -> order.getItemCount().entrySet().stream())
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getKey,
+                        // maybe should summarize?
+                        Collectors.counting()
+                ))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparingLong(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 
     /**
@@ -48,6 +67,21 @@ public class StoreStatistic {
      * @return map - заказ / общая сумма заказа
      */
     public Map<Order, Long> sum5biggerOrders(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .sorted(Comparator.comparingLong(order -> -order.getItemCount()
+                        .entrySet()
+                        .stream()
+                        .mapToLong(Map.Entry::getValue)
+                        .sum()))
+                .limit(5)
+                .collect(Collectors.toMap(
+                        order -> order,
+                        order -> order.getItemCount()
+                                .entrySet()
+                                .stream()
+                                .mapToLong(entry -> entry.getKey().getPrice() * entry.getValue())
+                                .sum()
+                ));
+
     }
 }
