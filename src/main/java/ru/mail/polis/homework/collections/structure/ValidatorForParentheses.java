f2075aef7a9508f466df91baf1af3b4b6639b04e
NIKITry <nikitabandurin2002@gmail.com>
2022-10-27 21:11:44 +0300

    validator is done

--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,5 +1,9 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Задание оценивается в 2 тугрика.
  * Одна из самых популярных задач.
@@ -7,14 +11,46 @@ package ru.mail.polis.homework.collections.structure;
  * В строке помимо скобок могут содержаться и другие символы.
  * Скобки могут быть: [],{},<>,()
  * Примеры:
- *      "(-b + (x)^2)/(2+4)" - true
- *      "Понедельники меня угнетают ((" - false
- *
+ * "(-b + (x)^2)/(2+4)" - true
+ * "Понедельники меня угнетают ((" - false
+ * <p>
  * Отрабатывать метод должен за О(n)
  */
 public class ValidatorForParentheses {
+    private static final Character[] PARENTHESES = {'{', '}', '[', ']', '<', '>', '(', ')'};
 
     public static boolean validate(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+
+        final List<Character> listOfParentheses = Arrays.asList(PARENTHESES);
+        Stack<Character> stack = new Stack<>();
+        boolean bracketExists = false;
+        for (int i = 0; i < value.length(); i++) {
+            if (!listOfParentheses.contains(value.charAt(i))) {
+                continue;
+            }
+            bracketExists = true;
+            if (i != 0 && !stack.isEmpty() && isClosedBracket(value.charAt(i), stack.peek())) {
+                stack.pop();
+                continue;
+            }
+            stack.push(value.charAt(i));
+        }
+        return bracketExists && stack.empty();
+    }
+
+    private static boolean isClosedBracket(char current, char opening) {
+        if (current == '}' && opening == '{') {
+            return true;
+        }
+        if (current == ')' && opening == '(') {
+            return true;
+        }
+        if (current == '>' && opening == '<') {
+            return true;
+        }
+        return current == ']' && opening == '[';
     }
 }
