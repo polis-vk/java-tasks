18674b4e4d66747dcc22375581865f08b927a835
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2022-10-27 16:50:08 +0300

    1/2 LibraryStatistic

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,7 +1,12 @@
 package ru.mail.polis.homework.streams.lib;
 
+import java.sql.Timestamp;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по библиотеке.
@@ -13,38 +18,65 @@ public class LibraryStatistic {
      * Вернуть "специалистов" в литературном жанре с кол-вом прочитанных страниц.
      * Специалист жанра считается пользователь который прочел как минимум 5 книг в этом жанре,
      * при этом читал каждую из них не менее 14 дней.
+     *
      * @param library - данные библиотеки
-     * @param genre - жанр
+     * @param genre   - жанр
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return null;
+        return library.getArchive().stream()
+                .filter(e -> (e.getBook().getGenre() == genre))
+                .filter(e -> (getDays(e.getTake(), e.getReturned()) >= 14))
+                .collect(Collectors.groupingBy(ArchivedData::getUser, Collectors.counting()))
+                .entrySet().stream()
+                .filter(s -> (s.getValue() >= 5))
+                .map(Map.Entry::getKey).collect(Collectors.toMap(s -> s, User::getReadedPages));
     }
 
     /**
      * Вернуть любимый жанр пользователя. Тот что чаще всего встречается. Не учитывать тот что пользователь читает в данный момент.
      * Если есть несколько одинаковых по весам жанров - брать в расчет то, что пользователь читает в данный момент.
+     *
      * @param library - данные библиотеки
-     * @param user - пользователь
+     * @param user    - пользователь
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        return null;
+        Map<Genre, Long> mapGenreCount = library.getArchive().stream()
+                .filter(e -> e.getUser().equals(user))
+                .collect(Collectors.groupingBy(e -> (e.getBook().getGenre()), Collectors.counting()));
+        long max = mapGenreCount.values().stream()
+                .max(Comparator.comparingLong(s -> s)).get();
+        Set<Genre> favoriteGenres = mapGenreCount.entrySet().stream()
+                .filter(k -> (k.getValue().equals(max)))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+        if (favoriteGenres.size() == 1 || (!favoriteGenres.contains(user.getBook().getGenre()))) {
+            return favoriteGenres.stream().findAny().get();
+        }
+        return user.getBook().getGenre();
     }
 
     /**
      * Вернуть список пользователей которые больше половины книг держали на руках более 30-ти дней. Брать в расчет и книги которые сейчас
      * пользователи держат у себя (ArchivedData.returned == null)
+     *
      * @param library - данные библиотеки
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        return null;
+        return library.getArchive().stream()
+                .collect(Collectors.toMap(ArchivedData::getUser,
+                        s -> getDays(s.getTake(), s.getReturned())))
+                .entrySet().stream()
+                .map(s -> ((s.getValue() > 30) ? s.setValue((long) 1) : s.setValue((long) -1)))
+                .collect(Collectors.toMap(ArchivedData::getUser, s -> Collectors.summarizingLong(s.ge)))
     }
 
     /**
      * Вернуть список книг у которых страниц равно или больше чем переданное значение
-     * @param library - данные библиотеки
+     *
+     * @param library   - данные библиотеки
      * @param countPage - кол-во страниц
      * @return - список книг
      */
@@ -54,10 +86,19 @@ public class LibraryStatistic {
 
     /**
      * Вернуть самого популярного автора в каждом жанре. Если кол-во весов у авторов одинаково брать по алфавиту.
+     *
      * @param library - данные библиотеки
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
         return null;
+    }
+
+    private long getDays(Timestamp timestamp1, Timestamp timestamp2) {
+        if (timestamp2 == null) {
+            timestamp2 = new Timestamp(System.currentTimeMillis());
+        }
+        return ChronoUnit.DAYS.between(timestamp1.toInstant(),
+                timestamp2.toInstant());
     }
 }
