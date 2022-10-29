b3f93169106b1882fb0a6bb688fb155cbe4ace17
Coo3n <Khlamov_ilya@mail.ru>
2022-10-30 01:21:44 +0300

    del Main

---
+++ src/main/java/ru/mail/polis/homework/simple/Main.java
@@ -0,0 +1,12 @@
+package ru.mail.polis.homework.simple;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+
+    static String hello() {
+        return "Hello world! I am а first program";
+    }
+}
