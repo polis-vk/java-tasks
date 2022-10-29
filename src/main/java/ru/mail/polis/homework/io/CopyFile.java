00663204b46b8c86a9e5971f99aa0f45c4fe8ebf
Vladislav Pochernin <vspochernin@gmail.com>
2022-10-29 15:45:39 +0300

    part6-CopyFile: done;

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,15 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -9,6 +19,45 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
+        if (pathFrom == null || pathTo == null) {
+            return;
+        }
+
+        Path from = Paths.get(pathFrom);
+        if (Files.notExists(from)) {
+            return;
+        }
+        Path to = Paths.get(pathTo);
+
+        try {
+            Files.createDirectories(to.resolve(from.relativize(to)).getParent());
+            Files.walkFileTree(from, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    Files.createDirectory(to.resolve(from.relativize(dir)));
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    copyFile(file, to.resolve(from.relativize(file)));
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copyFile(Path from, Path to) throws IOException {
+        try (InputStream input = Files.newInputStream(from);
+             OutputStream output = Files.newOutputStream(to)) {
+            byte[] buffer = new byte[1024];
+            int recorded;
+            while ((recorded = input.read(buffer)) > 0) {
+                output.write(buffer, 0, recorded);
+            }
+        }
     }
 
 }
