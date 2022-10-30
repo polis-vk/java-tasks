c446ffcc949120709ec06328dab444c2d35ada83
GenryEden <a926788@gmail.com>
2022-10-30 17:04:11 +0300

    solved all

--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -1,8 +1,11 @@
 package ru.mail.polis.homework.streams.store;
 
 import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
@@ -13,41 +16,90 @@ public class StoreStatistic {
     /**
      * Вернуть сколько было продано определенного товара за переданный промежуток времени
      *
-     * @param orders - список заказов
+     * @param orders   - список заказов
      * @param typeItem - вид товара
-     * @param from - с какого момента вести подсчет
-     * @param to - по какой момент вести подсчет
+     * @param from     - с какого момента вести подсчет
+     * @param to       - по какой момент вести подсчет
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return 0L;
+        return orders.stream().filter(
+                order -> order.getTime().getTime() < to.getTime() && order.getTime().getTime() > from.getTime()
+        ).reduce(
+                0,
+                (first, second) -> first + second.getItemCount().getOrDefault(typeItem, 0),
+                Integer::sum
+        );
     }
 
     /**
      * Вернуть статистику по каждому дню сколько какого товара было проданно
+     *
      * @param orders - список заказов
      * @return - map, где ключ - начало дня (00:00:00 000 UTC) для которого собрана статистика
      * значение - map товар/кол-во
      */
     public Map<Timestamp, Map<Item, Integer>> statisticItemsByDay(List<Order> orders) {
-        return null;
+        long dayInMillis = 24 * 60 * 60 * 1_000;
+        return orders.stream().collect(
+                Collectors.groupingBy(
+                        order -> new Timestamp(order.getTime().getTime() % dayInMillis),
+                        Collectors.toSet()
+                )
+        ).entrySet().stream().collect(
+                Collectors.toMap(
+                        Map.Entry::getKey,
+                        orderListEntry -> orderListEntry.getValue().stream().reduce(
+                                (Map<Item, Integer>) new HashMap<Item, Integer>(),
+                                (first, second) -> mergeMaps(first, second.getItemCount()),
+                                this::mergeMaps
+                        )
+                )
+        );
+    }
+
+    public <K> Map<K, Integer> mergeMaps(Map<K, Integer> mergeInto, Map<K, Integer> toMerge) {
+        for (K key : toMerge.keySet()) {
+            mergeInto.put(key, mergeInto.getOrDefault(key, 0) + toMerge.get(key));
+        }
+        return mergeInto;
     }
 
     /**
      * Вернуть самый популярный товар
+     *
      * @param orders - список заказов
      * @return - товар
      */
     public Item mostPopularItem(List<Order> orders) {
-        return null;
+        return Collections.max(
+                orders.stream().reduce(
+                        (Map<Item, Integer>) new HashMap<Item, Integer>(),
+                        (first, second) -> mergeMaps(first, second.getItemCount()),
+                        this::mergeMaps
+                ).entrySet(),
+                Map.Entry.comparingByValue()
+        ).getKey();
     }
 
     /**
      * Вернуть сумму для 5 самых больших по кол-ву товаров заказу.
+     *
      * @param orders - список заказов
      * @return map - заказ / общая сумма заказа
      */
     public Map<Order, Long> sum5biggerOrders(List<Order> orders) {
-        return null;
+        return orders.stream().sorted(
+                (first, second) -> second.getItemCount().values().stream().mapToInt(i -> i).sum() -
+                        first.getItemCount().values().stream().mapToInt(i -> i).sum()
+        ).limit(5).collect(
+                Collectors.toMap(
+                        order -> order,
+                        order -> order.getItemCount().entrySet().stream()
+                                .mapToLong(
+                                        itemNumEntry -> itemNumEntry.getValue() * itemNumEntry.getKey().getPrice()
+                                ).sum()
+                )
+        );
     }
 }
