bb84ec044aa0f247ba8311844158b104186185f9
Vladimir <i.am.polshin@gmail.com>
2022-10-30 15:03:41 +0300

    CopyFile: all done

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,15 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -9,6 +19,48 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
+        Path from = Paths.get(pathFrom);
+        if (Files.notExists(from)) {
+            return;
+        }
+
+        try {
+            Path to = Paths.get(pathTo);
+            Path dir = !Files.isDirectory(to) ? to.getParent() : to;
+            Files.createDirectories(dir);
+            copyInternalFiles(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
+    private static void copyInternalFiles(Path pathFrom, Path pathTo) throws IOException {
+        Files.walkFileTree(pathFrom, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Files.createDirectories(pathTo.resolve(pathFrom.relativize(dir)));
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                copy(file, pathTo.resolve(pathFrom.relativize(file)));
+                return FileVisitResult.CONTINUE;
+            }
+
+            private void copy(Path pathFrom, Path pathTo) throws IOException {
+                try (InputStream input = Files.newInputStream(pathFrom)) {
+                    try (OutputStream output = Files.newOutputStream(pathTo)) {
+                        byte[] buffer = new byte[1024];
+                        int bytesNumber;
+                        while ((bytesNumber = input.read(buffer)) > 0) {
+                            output.write(buffer, 0, bytesNumber);
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
+
