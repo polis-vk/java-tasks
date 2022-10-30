8f5c9e77e9a00251035324eff7e20f766b8ea278
Vladimir <i.am.polshin@gmail.com>
2022-10-30 15:56:26 +0300

    Directories: all done

--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,14 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Directories {
 
@@ -13,7 +21,22 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File currPath = new File(path);
+        if (!currPath.exists()) {
+            return 0;
+        }
+
+        int deletedCount = 0;
+        if (currPath.isDirectory()) {
+            File[] innerFiles = currPath.listFiles();
+            if (innerFiles != null) {
+                for (File file : innerFiles) {
+                    deletedCount += removeWithFile(file.toString());
+                }
+            }
+        }
+        currPath.delete();
+        return ++deletedCount;
     }
 
     /**
@@ -21,6 +44,28 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path currPath = Paths.get(path);
+        if (Files.notExists(currPath)) {
+            return 0;
+        }
+
+        AtomicInteger deletedCount = new AtomicInteger();
+        Files.walkFileTree(currPath, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                deletedCount.incrementAndGet();
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                deletedCount.incrementAndGet();
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
+        return deletedCount.get();
     }
 }
