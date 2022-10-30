672aa3b689619883afdb3f8d7fcfa52f78cf713b
Coo3n <Khlamov_ilya@mail.ru>
2022-10-30 23:28:37 +0300

    fix merge

--- src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/CustomArrayList.java
@@ -1,129 +1,355 @@
 package ru.mail.polis.homework.collections.structure;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Необходимо реализовать свой ArrayList (динамический массив).
  * При изменении размера массива помните про метод System.arraycopy()
- *
+ * <p>
  * Задание оценивается в 10 тугриков
  */
 public class CustomArrayList<E> implements List<E> {
+    private E[] array;
+    private int size;
+    private int modCnt;
+
+    private int initialCapacityArray = 10;
+
+    public CustomArrayList() {
+        array = (E[]) new Object[initialCapacityArray];
+    }
+
+    public CustomArrayList(int size) {
+        initialCapacityArray = size;
+        array = (E[]) new Object[initialCapacityArray];
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return indexOf(o) != -1;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private int finalModCount = modCnt;
+            private int position;
+
+            @Override
+            public boolean hasNext() {
+                return position < size;
+            }
+
+            @Override
+            public E next() {
+                if (modCnt != finalModCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+
+                return array[position++];
+            }
+        };
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        Object[] masObjects = new Object[size];
+        System.arraycopy(array, 0, masObjects, 0, size);
+        return masObjects;
     }
 
     @Override
     public <T> T[] toArray(T[] a) {
-        return null;
+        if (a.length < size) {
+            return (T[]) Arrays.copyOf(array, size, a.getClass());
+        }
+
+        T[] newArray = a;
+        System.arraycopy(array, 0, newArray, 0, newArray.length);
+        return newArray;
     }
 
     @Override
     public boolean add(E e) {
-        return false;
+        increasingArraySize();
+        array[size++] = e;
+        modCnt++;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        return false;
+        int index = indexOf(o);
+
+        if (index == -1) {
+            return false;
+        }
+
+        remove(index);
+        return true;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        return false;
+        for (Object element : c) {
+            if (!contains(element)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        return false;
+        addAll(size, c);
+        return true;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        return false;
+        if (index != size) {
+            checkIndex(index);
+        }
+
+        if (c.isEmpty()) {
+            return false;
+        }
+
+        if (size + c.size() > array.length) {
+            array = Arrays.copyOf(array, size + c.size());
+        }
+
+        System.arraycopy(array, index, array, index + c.size(), size - index);
+        System.arraycopy(c.toArray(), 0, array, index, c.size());
+
+        size += c.size();
+        modCnt++;
+        return true;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        if (c.isEmpty()) {
+            return false;
+        }
+
+        for (Object element : c) {
+            if (!remove(element)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        return false;
+        if (c.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(array[i])) {
+                remove(i--);
+            }
+        }
+
+        return true;
     }
 
     @Override
     public void clear() {
-
+        modCnt++;
+        Arrays.fill(array, null);
+        size = 0;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        checkIndex(index);
+        return array[index];
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        checkIndex(index);
+        modCnt++;
+        E currentElement = array[index];
+        array[index] = element;
+        return array[index];
     }
 
     @Override
     public void add(int index, E element) {
+        checkIndex(index);
+        increasingArraySize();
 
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = element;
+        modCnt++;
+        size++;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        checkIndex(index);
+        E removeValue = array[index];
+
+        System.arraycopy(array, index + 1, array, index, size - index);
+
+        size--;
+        modCnt++;
+
+        return removeValue;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        int index = (int) Arrays.stream(array)
+                .limit(size)
+                .takeWhile(element -> {
+                    if (element == null && o == null) {
+                        return false;
+                    }
+
+                    return !element.equals(o);
+                }).count();
+
+        return (index != size) ? index : -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
+        int iterator = size - 1;
+
+        for (int i = size - 1; i >= 0; i++) {
+            if (array[i] == null && o == null) {
+                return iterator;
+            }
+
+            if (array[i].equals(o)) {
+                return iterator;
+            }
+
+            iterator--;
+        }
+
         return 0;
     }
 
+
     @Override
     public ListIterator<E> listIterator() {
-        return null;
+        return new CustomListIterator(0);
     }
 
     @Override
     public ListIterator<E> listIterator(int index) {
-        return null;
+        return new CustomListIterator(index);
     }
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        return null;
+        checkIndex(fromIndex);
+        checkIndex(toIndex);
+
+        if (fromIndex > toIndex) {
+            throw new IllegalArgumentException();
+        }
+
+        CustomArrayList<E> subList = new CustomArrayList<>(toIndex - fromIndex + 1);
+        System.arraycopy(array, fromIndex, subList.array, 0, toIndex - fromIndex + 1);
+        subList.size = toIndex - fromIndex + 1;
+        return subList;
+    }
+
+    public class CustomListIterator implements ListIterator<E> {
+        int currentPosition;
+
+        public CustomListIterator(int index) {
+            currentPosition = index;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentPosition < size;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            return array[++currentPosition];
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return currentPosition >= 0;
+        }
+
+        @Override
+        public E previous() {
+            if (!hasPrevious()) {
+                throw new NoSuchElementException();
+            }
+
+            return array[--currentPosition];
+        }
+
+        @Override
+        public int nextIndex() {
+            return (hasNext()) ? currentPosition + 1 : currentPosition;
+        }
+
+        @Override
+        public int previousIndex() {
+            return (hasPrevious()) ? currentPosition - 1 : currentPosition;
+        }
+
+        @Override
+        public void remove() {
+            CustomArrayList.this.remove(currentPosition);
+        }
+
+        @Override
+        public void set(E e) {
+            CustomArrayList.this.set(currentPosition, e);
+        }
+
+        @Override
+        public void add(E e) {
+            CustomArrayList.this.add(currentPosition, e);
+        }
+    }
+
+    private void increasingArraySize() {
+        if (size == initialCapacityArray) {
+            initialCapacityArray = (int) (array.length * 1.5);
+            array = Arrays.copyOf(array, initialCapacityArray);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index > size - 1) {
+            throw new IndexOutOfBoundsException(index);
+        }
     }
 }
--- src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
+++ src/main/java/ru/mail/polis/homework/collections/structure/ValidatorForParentheses.java
@@ -1,5 +1,10 @@
 package ru.mail.polis.homework.collections.structure;
 
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 /**
  * Задание оценивается в 2 тугрика.
  * Одна из самых популярных задач.
@@ -7,14 +12,39 @@ package ru.mail.polis.homework.collections.structure;
  * В строке помимо скобок могут содержаться и другие символы.
  * Скобки могут быть: [],{},<>,()
  * Примеры:
- *      "(-b + (x)^2)/(2+4)" - true
- *      "Понедельники меня угнетают ((" - false
- *
+ * "(-b + (x)^2)/(2+4)" - true
+ * "Понедельники меня угнетают ((" - false
+ * <p>
  * Отрабатывать метод должен за О(n)
  */
 public class ValidatorForParentheses {
 
     public static boolean validate(String value) {
-        return false;
+        if (value == null || value.isEmpty()) {
+            return false;
+        }
+
+        Map<Character, Character> matcher = new HashMap<>();
+        matcher.put(')', '(');
+        matcher.put(']', '[');
+        matcher.put('}', '{');
+        matcher.put('>', '<');
+
+        Deque<Character> closedBrackets = new LinkedList<>();
+        boolean containsBracket = false;
+
+        for (char symbol : value.toCharArray()) {
+            if (matcher.containsValue(symbol)) {
+                closedBrackets.push(symbol);
+                containsBracket = true;
+                continue;
+            }
+
+            if (matcher.containsKey(symbol) && (closedBrackets.isEmpty() || !closedBrackets.pop().equals(matcher.get(symbol)))) {
+                return false;
+            }
+        }
+
+        return containsBracket && closedBrackets.isEmpty();
     }
 }
