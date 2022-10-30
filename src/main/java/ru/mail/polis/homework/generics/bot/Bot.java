ff6debff6f126df7f80dbd1d2d7767412a34c642
Coo3n <Khlamov_ilya@mail.ru>
2022-10-30 19:29:26 +0300

    task4

--- src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
+++ src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
@@ -1,12 +1,16 @@
 package ru.mail.polis.homework.generics.bot;
 
 import ru.mail.polis.homework.generics.bot.handler.StateHandler;
+import ru.mail.polis.homework.generics.bot.state.ExampleState;
 import ru.mail.polis.homework.generics.bot.state.State;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Handler;
 
 public class Bot {
-
+    private Map<Class<? extends State>,StateHandler<? extends State>> map = new HashMap();
     /**
      * Конструктор бота, которому на вход подаются хэндлеры состояний.
      * Необходимо как-то сохранить эти хэндлеры так, чтобы потом можно было вызвать нужный хэндлер из метода handleState
@@ -14,7 +18,9 @@ public class Bot {
      * 1 тугрик
      */
     public Bot(List<StateHandler> handlers) {
-
+        for (StateHandler stateHandler: handlers) {
+            map.put(stateHandler.getHandlingStateClass(),stateHandler);
+        }
     }
 
     /**
@@ -23,5 +29,6 @@ public class Bot {
      * 1 тугрик
      */
     public void handleState(State state) {
+        map.get(state).handle();
     }
 }
--- src/main/java/ru/mail/polis/homework/generics/bot/handler/ExampleStateHandler.java
+++ src/main/java/ru/mail/polis/homework/generics/bot/handler/ExampleStateHandler.java
@@ -1,19 +1,22 @@
 package ru.mail.polis.homework.generics.bot.handler;
+import ru.mail.polis.homework.generics.bot.state.ExampleState;
 
-public class ExampleStateHandler implements StateHandler {
+public class ExampleStateHandler implements StateHandler<ExampleState> {
 
     @Override
     public void handle() {
-        // do nothing
+
     }
+
 
     /**
      * Поправить в соответствии с изменениями в StateHandler
      *
      * 1 тугрик
      */
+
     @Override
-    public Class getHandlingStateClass() {
-        return null;
+    public Class<ExampleState> getHandlingStateClass() {
+        return ExampleState.class;
     }
 }
