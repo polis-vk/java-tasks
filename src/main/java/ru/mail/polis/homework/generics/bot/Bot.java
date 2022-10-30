8e51858aa63d6a4d65cd86e7f7bb47273e8a2182
Artem Cherepanov <owvaoamc7@gmail.com>
2022-10-30 23:25:25 +0300

    Add StateHandler, ExampleStateHandler, Bot

--- src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
+++ src/main/java/ru/mail/polis/homework/generics/bot/Bot.java
@@ -3,9 +3,12 @@ package ru.mail.polis.homework.generics.bot;
 import ru.mail.polis.homework.generics.bot.handler.StateHandler;
 import ru.mail.polis.homework.generics.bot.state.State;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Bot {
+    private final Map<Class<? extends State>, StateHandler<? extends State>> botStateHandlers = new HashMap<>();
 
     /**
      * Конструктор бота, которому на вход подаются хэндлеры состояний.
@@ -13,8 +16,10 @@ public class Bot {
      *
      * 1 тугрик
      */
-    public Bot(List<StateHandler> handlers) {
-
+    public Bot(List<StateHandler<? extends State>> handlers) {
+        for (StateHandler<? extends State> handler : handlers) {
+            botStateHandlers.put(handler.getHandlingStateClass(), handler);
+        }
     }
 
     /**
@@ -23,5 +28,6 @@ public class Bot {
      * 1 тугрик
      */
     public void handleState(State state) {
+        botStateHandlers.get(state.getClass()).handle();
     }
 }
