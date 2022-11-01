319639492105368cc705e66b59e29fcf2a717137
Vladimir <i.am.polshin@gmail.com>
2022-11-01 17:43:10 +0300

    MoveType: refactored

--- src/main/java/ru/mail/polis/homework/io/objects/MoveType.java
+++ src/main/java/ru/mail/polis/homework/io/objects/MoveType.java
@@ -1,28 +1,9 @@
 package ru.mail.polis.homework.io.objects;
 
 public enum MoveType {
-//    RUN("run"),
-//    SWIM("swim"),
-//    FLY("fly"),
-//    JUMP("jump"),
-//    CRAWL("crawl");
-
     RUN,
     SWIM,
     FLY,
     JUMP,
-    CRAWL;
-
-//    private final String type;
-//
-//    MoveType(String type) {
-//        this.type = type;
-//    }
-
-    @Override
-    public String toString() {
-        return "MoveType{" +
-                "type='" + this +
-                '}';
-    }
+    CRAWL
 }
