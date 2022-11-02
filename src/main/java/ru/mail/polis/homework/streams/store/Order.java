c399fe36364ca9d43f9cc44e837fa739d224242e
nikita.derevyanko <gidros07@gmail.com>
2022-11-02 03:29:02 +0300

    add tests for StoreStatistic
    (cherry picked from commit 1c4eecdd3352abba15458888906f209101af4331)

--- src/main/java/ru/mail/polis/homework/streams/store/Order.java
+++ src/main/java/ru/mail/polis/homework/streams/store/Order.java
@@ -2,6 +2,7 @@ package ru.mail.polis.homework.streams.store;
 
 import java.sql.Timestamp;
 import java.util.Map;
+import java.util.Objects;
 
 public class Order {
     private final Timestamp time;
@@ -18,5 +19,18 @@ public class Order {
 
     public Map<Item, Integer> getItemCount() {
         return itemCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(time, order.time) && Objects.equals(itemCount, order.itemCount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(time, itemCount);
     }
 }
