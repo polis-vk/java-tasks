997de37528f443480a4ad0cfa987c13b0cda8cfe
charlsraven <voron00100867@gmail.com>
2022-11-02 16:33:09 +0300

    Update StoreStatistic.java

--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -1,8 +1,10 @@
 package ru.mail.polis.homework.streams.store;
 
 import java.sql.Timestamp;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
@@ -13,41 +15,72 @@ public class StoreStatistic {
     /**
      * Вернуть сколько было продано определенного товара за переданный промежуток времени
      *
-     * @param orders - список заказов
+     * @param orders   - список заказов
      * @param typeItem - вид товара
-     * @param from - с какого момента вести подсчет
-     * @param to - по какой момент вести подсчет
+     * @param from     - с какого момента вести подсчет
+     * @param to       - по какой момент вести подсчет
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return 0L;
+        return orders.stream()
+                .filter(order -> (from.before(order.getTime()) || from.equals(order.getTime()))
+                        && (to.after(order.getTime()) || to.equals(order.getTime()))
+                        && order.getItemCount().containsKey(typeItem))
+                .mapToLong(order -> order.getItemCount().get(typeItem))
+                .sum();
     }
 
     /**
      * Вернуть статистику по каждому дню сколько какого товара было проданно
+     *
      * @param orders - список заказов
      * @return - map, где ключ - начало дня (00:00:00 000 UTC) для которого собрана статистика
      * значение - map товар/кол-во
      */
     public Map<Timestamp, Map<Item, Integer>> statisticItemsByDay(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .collect(Collectors.toMap(order -> Timestamp.valueOf(order.getTime().toString().substring(0, 10).concat(" 00:00:00")),
+                        Order::getItemCount, (old, current) -> {
+                            old.forEach((key, value) -> current.merge(key, value, Integer::sum));
+                            return current;
+                        }));
     }
 
     /**
      * Вернуть самый популярный товар
+     *
      * @param orders - список заказов
      * @return - товар
      */
     public Item mostPopularItem(List<Order> orders) {
-        return null;
+        var item = orders.stream()
+                .flatMap(order -> order.getItemCount().entrySet().stream())
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue(Comparator.naturalOrder()));
+        return item.map(Map.Entry::getKey).orElse(null);
     }
 
     /**
      * Вернуть сумму для 5 самых больших по кол-ву товаров заказу.
+     *
      * @param orders - список заказов
      * @return map - заказ / общая сумма заказа
      */
     public Map<Order, Long> sum5biggerOrders(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .map(order -> Map.entry(order, order.getItemCount()
+                        .values()
+                        .stream()
+                        .mapToLong(Integer::longValue)
+                        .sum()))
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).limit(5).map(Map.Entry::getKey)
+                .collect(Collectors.toMap(order -> order,
+                        order -> order.getItemCount()
+                                .entrySet()
+                                .stream()
+                                .mapToLong(entry -> entry.getKey().getPrice() * entry.getValue())
+                                .sum()));
     }
 }
