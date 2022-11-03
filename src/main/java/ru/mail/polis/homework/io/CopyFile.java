c6f679e32dde013588433788e7f88a7159362a4a
Ekaterina Zelenkina <katrinzel0@mail.ru>
2022-11-03 18:16:13 +0300

    CopyFile done

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,6 +1,15 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CopyFile {
+    private static final int BUFFER_SIZE = 1024;
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
@@ -9,6 +18,45 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
+        if (pathFrom == null || pathTo == null) {
+            return;
+        }
+        Path from = Paths.get(pathFrom);
+        if (Files.notExists(from)) {
+            return;
+        }
+        Path to = Paths.get(pathTo);
+        try {
+            if (Files.isRegularFile(from)) {
+                Files.createDirectories(to.getParent());
+            } else if (Files.exists(from)) {
+                Files.createDirectory(to.getParent());
+            }
+            copy(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    private static void copy(Path from, Path to) throws IOException {
+        if (!Files.isDirectory(from)) {
+            byte[] buff = new byte[BUFFER_SIZE];
+            try (InputStream in = Files.newInputStream(from);
+                 OutputStream out = Files.newOutputStream(to)) {
+                int length;
+                while ((length = in.read(buff)) > 0) {
+                    out.write(buff, 0, length);
+                }
+            }
+            return;
+        }
+        if (Files.notExists(to)) {
+            Files.createDirectory(to);
+        }
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(from)) {
+            for (Path path : stream) {
+                copy(path, to.resolve(path.getFileName()));
+            }
+        }
+    }
 }
