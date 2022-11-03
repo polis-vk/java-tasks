8d2bab1efc98dc10249f57b98b4ec8af13098086
Ekaterina Zelenkina <katrinzel0@mail.ru>
2022-11-03 14:31:47 +0300

    LibraryStatistic done

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,13 +1,19 @@
 package ru.mail.polis.homework.streams.lib;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по библиотеке.
  * Оценка 5-ть баллов
  */
 public class LibraryStatistic {
+    private static final int READING_DAYS_FOR_EXPERT = 14;
+    private static final int COUNT_OF_BOOKS_FOR_EXPERT = 5;
+    private static final int MONTH = 30;
+    private static final int TRANSFER_COEFFICIENT = 60 * 60 * 24 * 100;
 
     /**
      * Вернуть "специалистов" в литературном жанре с кол-вом прочитанных страниц.
@@ -18,7 +24,14 @@ public class LibraryStatistic {
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return null;
+        return library.getArchive().stream()
+                .filter(archive -> archive.getReturned() != null)
+                .filter(archive -> archive.getBook().getGenre().equals(genre))
+                .filter(arch -> ((arch.getReturned().getTime() - arch.getTake().getTime()) / (TRANSFER_COEFFICIENT)) > READING_DAYS_FOR_EXPERT)
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(archiveMap -> archiveMap.getValue().size() >= COUNT_OF_BOOKS_FOR_EXPERT)
+                .collect(Collectors.toMap(Map.Entry::getKey, value -> value.getValue().size()));
     }
 
     /**
@@ -29,7 +42,11 @@ public class LibraryStatistic {
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        return null;
+        return library.getArchive().stream()
+                .filter(archivedData -> archivedData.getUser().equals(user))
+                .collect(Collectors.groupingBy(archivedData -> archivedData.getBook().getGenre(), Collectors.counting()))
+                .entrySet().stream()
+                .max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(null);
     }
 
     /**
@@ -39,7 +56,10 @@ public class LibraryStatistic {
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        return null;
+        return library.getArchive().stream()
+                .filter(LibraryStatistic::isMoreThanMonth)
+                .map(ArchivedData::getUser)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -49,7 +69,9 @@ public class LibraryStatistic {
      * @return - список книг
      */
     public List<Book> booksWithMoreCountPages(Library library, int countPage) {
-        return null;
+        return library.getBooks().stream()
+                .filter(book -> book.getPage() >= countPage)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -58,6 +80,18 @@ public class LibraryStatistic {
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-        return null;
+        return library.getArchive().stream()
+                .map(ArchivedData::getBook)
+                .collect(Collectors.groupingBy(Book::getGenre, Collectors.groupingBy(Book::getAuthor, Collectors.counting())))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, genreMap -> genreMap.getValue().entrySet().stream()
+                        .max(Comparator.comparingLong(Map.Entry<String, Long>::getValue)
+                                .thenComparing(Map.Entry.comparingByKey())).map(Map.Entry::getKey).orElse("")));
+    }
+
+    private static boolean isMoreThanMonth(ArchivedData a) {
+        return (a.getReturned() == null
+                && ((System.currentTimeMillis() - a.getTake().getTime()) / (TRANSFER_COEFFICIENT)) > MONTH)
+                || ((a.getReturned().getTime() - a.getTake().getTime()) / (TRANSFER_COEFFICIENT)) > MONTH;
     }
 }
