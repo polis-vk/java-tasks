66b39a8eaf12060597c4abf3c7de4e6923343b49
llav3ji2019 <pavelemelyanov03@mail.ru>
2022-11-04 02:44:31 +0300

    fixing hm 5

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -4,7 +4,6 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -29,8 +28,7 @@ public class LibraryStatistic {
         return library.getArchive().stream()
                 .filter(archivedData -> archivedData.getBook().getGenre().equals(genre) && bookTimeHolding(archivedData) >= timeLimit)
                 .collect(Collectors.groupingBy(ArchivedData::getUser, Collectors.mapping(ArchivedData::getBook, Collectors.counting())))
-                .entrySet()
-                .stream()
+                .entrySet().stream()
                 .filter(pairOfUserAndBooks -> pairOfUserAndBooks.getValue() >= 5)
                 .collect(Collectors.toMap(Map.Entry::getKey, pairOfUserAndBooks -> pairOfUserAndBooks.getKey().getReadedPages()));
     }
@@ -44,30 +42,21 @@ public class LibraryStatistic {
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        Stream<ArchivedData> neededUser = library.getArchive()
-                .stream()
-                .filter(archivedData -> archivedData.getUser().equals(user) && archivedData.getReturned() == null);
-        return library.getArchive()
-                .stream()
+        return library.getArchive().stream()
                 .filter(archivedData -> archivedData.getUser().equals(user) && archivedData.getReturned() != null)
                 .collect(Collectors.groupingBy(archivedData -> archivedData.getBook().getGenre(), Collectors.counting()))
-                .entrySet()
-                .stream()
+                .entrySet().stream()
                 .max((pairWithGenreAndAmount1, pairWithGenreAndAmount2) -> {
-                    int difference = pairWithGenreAndAmount2.getValue().compareTo(pairWithGenreAndAmount1.getValue());
-                    if (difference != 0) {
-                        return difference;
+                    if (!pairWithGenreAndAmount2.getValue().equals(pairWithGenreAndAmount1.getValue())) {
+                        return pairWithGenreAndAmount2.getValue().compareTo(pairWithGenreAndAmount1.getValue());
                     }
-                    Long amountOfAllBooksInFirstGenre = neededUser
-                            .filter(archivedData -> archivedData.getBook().getGenre().equals(pairWithGenreAndAmount1.getKey()))
-                            .count();
-                    Long amountOfAllBooksInSecondGenre = neededUser
-                            .filter(archivedData -> archivedData.getBook().getGenre().equals(pairWithGenreAndAmount2.getKey()))
-                            .count();
+                    Stream<ArchivedData> neededUser = library.getArchive().stream()
+                            .filter(archivedData -> archivedData.getUser().equals(user) && archivedData.getReturned() == null);
+                    Long amountOfAllBooksInFirstGenre = amountOfAllBooks(pairWithGenreAndAmount1.getKey(), neededUser);
+                    Long amountOfAllBooksInSecondGenre = amountOfAllBooks(pairWithGenreAndAmount2.getKey(), neededUser);
                     return amountOfAllBooksInSecondGenre.compareTo(amountOfAllBooksInFirstGenre);
                 })
-                .map(Map.Entry::getKey)
-                .orElse(null);
+                .map(Map.Entry::getKey).orElse(null);
     }
 
     /**
@@ -79,19 +68,15 @@ public class LibraryStatistic {
      */
     public List<User> unreliableUsers(Library library) {
         final long timeLimit = TIME_PER_DAY * 30;
-        Map<User, Set<Book>> usersWithBooks = library.getArchive()
-                .stream()
-                .collect(Collectors.groupingBy(ArchivedData::getUser, Collectors.mapping(ArchivedData::getBook, Collectors.toSet())));
         return library.getArchive().stream()
                 .filter(archivedData -> bookTimeHolding(archivedData) > timeLimit)
                 .collect(Collectors.groupingBy(ArchivedData::getUser, Collectors.mapping(ArchivedData::getBook, Collectors.toSet())))
-                .entrySet()
-                .stream()
+                .entrySet().stream()
                 .filter(userListEntry -> {
                     int amountBooksOfUserWith30DaysHolding = userListEntry.getValue().size() * 2;
-                    int amountOfAllBooksForAUser = usersWithBooks
-                            .entrySet()
-                            .stream()
+                    int amountOfAllBooksForAUser = library.getArchive().stream()
+                            .collect(Collectors.groupingBy(ArchivedData::getUser, Collectors.mapping(ArchivedData::getBook, Collectors.toSet())))
+                            .entrySet().stream()
                             .filter(userListWithBooksEntry -> userListWithBooksEntry.getKey().equals(userListEntry.getKey()))
                             .flatMap(userListWithBooksEntry -> userListWithBooksEntry.getValue().stream())
                             .collect(Collectors.toSet()).size();
@@ -121,22 +106,18 @@ public class LibraryStatistic {
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-        return library.getBooks()
-                .stream()
+        return library.getBooks().stream()
                 .collect(Collectors.groupingBy(Book::getGenre, Collectors.groupingBy(Book::getAuthor, Collectors.counting())))
-                .entrySet()
-                .stream()
+                .entrySet().stream()
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         genreAndMapEntry -> genreAndMapEntry.getValue()
-                                .entrySet()
-                                .stream()
+                                .entrySet().stream()
                                 .max((genreAndMapEntry1, genreAndMapEntry2) -> {
                                     int difference = genreAndMapEntry2.getValue().compareTo(genreAndMapEntry1.getValue());
                                     return difference != 0 ? difference : genreAndMapEntry2.getKey().compareTo(genreAndMapEntry1.getKey());
                                 })
-                                .map(Map.Entry::getKey)
-                                .orElse("")
+                                .map(Map.Entry::getKey).orElse("")
                 ));
     }
 
@@ -144,5 +125,9 @@ public class LibraryStatistic {
         long currentTime = Timestamp.from(Instant.now()).getTime();
         long startTime = data.getTake().getTime();
         return data.getReturned() != null ? data.getReturned().getTime() - startTime : currentTime - startTime;
+    }
+
+    private static long amountOfAllBooks(Genre genre, Stream<ArchivedData> neededUser) {
+        return neededUser.filter(archivedData -> archivedData.getBook().getGenre().equals(genre)).count();
     }
 }
--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -11,8 +11,6 @@ import java.util.stream.Collectors;
  * Оценка 5-ть баллов
  */
 public class StoreStatistic {
-    private static final long TIME_PER_DAY = 24 * 60 * 60 * 1000;
-
     /**
      * Вернуть сколько было продано определенного товара за переданный промежуток времени
      *
@@ -23,14 +21,12 @@ public class StoreStatistic {
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return orders
-                .stream()
+        return orders.stream()
                 .filter(order -> {
                     Timestamp currentTime = order.getTime();
                     return (currentTime.before(to) || currentTime.equals(to)) && (currentTime.after(from) || currentTime.equals(from));
                 })
-                .map(order -> order.getItemCount().getOrDefault(typeItem, 0))
-                .mapToLong(productAmount -> (long) productAmount)
+                .mapToLong(order -> (long) order.getItemCount().getOrDefault(typeItem, 0))
                 .sum();
     }
 
@@ -42,10 +38,9 @@ public class StoreStatistic {
      * значение - map товар/кол-во
      */
     public Map<Timestamp, Map<Item, Integer>> statisticItemsByDay(List<Order> orders) {
-        return orders
-                .stream()
+        return orders.stream()
                 .collect(Collectors.toMap(
-                        order -> new Timestamp(order.getTime().getTime() % TIME_PER_DAY),
+                        order -> convertToDateWithStartingTime(order.getTime()),
                         Order::getItemCount,
                         (mapWithItemAndAmount1, mapWithItemAndAmount2) -> {
                             mapWithItemAndAmount1.forEach((item, amount) -> mapWithItemAndAmount2.merge(item, amount, Integer::sum));
@@ -67,12 +62,9 @@ public class StoreStatistic {
                         Map.Entry::getKey,
                         Map.Entry::getValue,
                         Integer::sum
-                ))
-                .entrySet()
-                .stream()
+                )).entrySet().stream()
                 .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .orElse(null);
+                .map(Map.Entry::getKey).orElse(null);
     }
 
     /**
@@ -82,27 +74,25 @@ public class StoreStatistic {
      * @return map - заказ / общая сумма заказа
      */
     public Map<Order, Long> sum5biggerOrders(List<Order> orders) {
-        return orders
-                .stream()
+        return orders.stream()
                 .collect(Collectors.toMap(
                         order -> order,
-                        order -> order.getItemCount().values()
-                                .stream()
-                                .mapToInt(i -> i)
+                        order -> order.getItemCount().values().stream()
+                                .mapToInt(itemsAmount -> itemsAmount)
                                 .sum()
-                ))
-                .entrySet()
-                .stream()
+                )).entrySet().stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .limit(5)
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
                         pairOfOrderAndAmount -> pairOfOrderAndAmount.getKey()
-                                .getItemCount()
-                                .entrySet()
-                                .stream()
+                                .getItemCount().entrySet().stream()
                                 .mapToLong(entry -> entry.getKey().getPrice() * entry.getValue())
                                 .sum()
                 ));
+    }
+
+    private static Timestamp convertToDateWithStartingTime(Timestamp data) {
+        return Timestamp.valueOf(data.toString().split(" ")[0] + " 00:00:00");
     }
 }
