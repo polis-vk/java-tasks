9085e5a7b8276ce6a36f563c014aebe30f45e6b7
Artem Bugakov <0bugakovartem@gmail.com>
2022-11-04 15:24:16 +0300

    Add LibraryStatistic

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,7 +1,11 @@
 package ru.mail.polis.homework.streams.lib;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по библиотеке.
@@ -18,7 +22,24 @@ public class LibraryStatistic {
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return null;
+        return library.getArchive().stream()
+                .filter(archivedData -> {
+                    long takeTime = archivedData.getTake().getTime();
+                    Timestamp returnedTime = archivedData.getReturned();
+
+                    if (returnedTime == null) {
+                        return false;
+                    } else {
+                        return countHoldDays(takeTime, returnedTime.getTime()) >= 14;
+                    }
+                })
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue().size() >= 5)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getKey().getReadedPages()
+                ));
     }
 
     /**
@@ -29,7 +50,17 @@ public class LibraryStatistic {
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        return null;
+        return library.getArchive().stream()
+                .filter(archivedData -> archivedData.getUser().equals(user)
+                        && user.getBook().getGenre() != archivedData.getBook().getGenre())
+                .collect(Collectors.groupingBy(
+                        archivedData -> archivedData.getBook().getGenre(),
+                        Collectors.counting()
+                ))
+                .entrySet().stream()
+                .max((o1, o2) -> (int) (o1.getValue() - o2.getValue()))
+                .orElseThrow(NoSuchElementException::new)
+                .getKey();
     }
 
     /**
@@ -39,7 +70,25 @@ public class LibraryStatistic {
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        return null;
+        return library.getArchive().stream()
+                .collect(Collectors.groupingBy(
+                        ArchivedData::getUser,
+                        Collectors.toList()
+                )).entrySet().stream()
+                .filter(entry -> entry.getValue().stream()
+                        .filter(archivedData -> {
+                            long takeTime = archivedData.getTake().getTime();
+                            Timestamp returnedTime = archivedData.getReturned();
+
+                            if (returnedTime == null) {
+                                return false;
+                            } else {
+                                return countHoldDays(takeTime, returnedTime.getTime()) > 30;
+                            }
+                        })
+                        .count() > (entry.getValue().size() / 2))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -49,7 +98,9 @@ public class LibraryStatistic {
      * @return - список книг
      */
     public List<Book> booksWithMoreCountPages(Library library, int countPage) {
-        return null;
+        return library.getBooks().stream()
+                .filter(book -> book.getPage() >= countPage)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -58,6 +109,29 @@ public class LibraryStatistic {
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-        return null;
+        return library.getBooks().stream()
+                .collect(Collectors.groupingBy(
+                        Book::getGenre,
+                        Collectors.groupingBy(
+                                Book::getAuthor,
+                                Collectors.counting()
+                        )
+                ))
+                .entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue()
+                                .entrySet()
+                                .stream()
+                                .max((o1, o2) -> (int) (o1.getValue() - o2.getValue()))
+                                .orElseThrow(NoSuchElementException::new)
+                                .getKey()));
     }
+
+    private static int countHoldDays(long takeTime, long returnedTime) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(returnedTime - takeTime);
+        return calendar.get(Calendar.DATE) - 1;
+    }
+
 }
