ebe601689deaa43a9ffa26738ee62f76dce0f329
Jaroslav Chernyshev <31724693+Jar-Cher@users.noreply.github.com>
2022-11-05 22:48:37 +0300

    + Animal

--- src/main/java/ru/mail/polis/homework/io/objects/Animal.java
+++ src/main/java/ru/mail/polis/homework/io/objects/Animal.java
@@ -1,11 +1,28 @@
 package ru.mail.polis.homework.io.objects;
 
 
+import java.io.Serializable;
+
 /**
- * Класс должен содержать несколько полей с примитивами (минимум 2 булеана и еще что-то), строками, энамами и некоторыми сапомисными объектами (не энам).
+ * Класс должен содержать несколько полей с примитивами (минимум 2 булеана и еще что-то), строками, энамами и некоторыми самописными объектами (не энам).
  * Всего должно быть минимум 6 полей с разными типами.
  * Во всех полях, кроме примитивов должны поддерживаться null-ы
  * 1 тугрик
  */
-public class Animal {
+public class Animal implements Serializable {
+    private int legs;
+    private boolean isDomesticated;
+    private boolean isWarmBlooded;
+    private String name;
+    private Diet diet;
+    private Person discoveredBy;
+}
+
+class Person implements Serializable {
+}
+
+enum Diet {
+    CARNIVORE,
+    OMNIVORE,
+    HERBIVORE
 }
