475292ea548a6514d84d96397eece80b7395ad1f
nikita.derevyanko <gidros07@gmail.com>
2022-11-05 17:21:43 +0300

    fix statisticItemsByDay test

--- src/test/java/ru/mail/polis/homework/streams/store/StoreStatisticTest.java
+++ src/test/java/ru/mail/polis/homework/streams/store/StoreStatisticTest.java
@@ -3,10 +3,13 @@ package ru.mail.polis.homework.streams.store;
 import org.junit.Test;
 
 import java.sql.Timestamp;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -34,17 +37,17 @@ public class StoreStatisticTest {
     }
 
     @Test
-    public void statisticItemsByDay() throws ParseException {
-        Date date1 = formatter.parse("2015-09-05");
-        Date date2 = formatter.parse("2015-09-15");
-        Date date3 = formatter.parse("2015-09-30");
+    public void statisticItemsByDay() {
+        LocalDateTime date1 = LocalDateTime.of(2015, Month.SEPTEMBER, 5, 0, 0, 0);
+        LocalDateTime date2 = LocalDateTime.of(2015, Month.SEPTEMBER, 15, 0, 0, 0);
+        LocalDateTime date3 = LocalDateTime.of(2015, Month.SEPTEMBER, 30, 0, 0, 0);
 
-        Order order_day_1_1 = generateByItemList(new Timestamp(date1.getTime()), Stream.of(banana, banana, chery, apple).collect(Collectors.toList()));
-        Order order_day_1_2 = generateByItemList(new Timestamp(date1.getTime() + 10000L), Stream.of(chery, chery, chery, apple).collect(Collectors.toList()));
-        Order order_day_2_1 = generateByItemList(new Timestamp(date2.getTime()), Stream.of(apple, apple, chery, apple).collect(Collectors.toList()));
-        Order order_day_2_2 = generateByItemList(new Timestamp(date2.getTime() + TimeUnit.DAYS.toMillis(1) - 10000L), Stream.of(chery, banana, banana, banana).collect(Collectors.toList()));
-        Order order_day_3_1 = generateByItemList(new Timestamp(date3.getTime()), Stream.of(chery, apple, chery, apple).collect(Collectors.toList()));
-        Order order_day_3_2 = generateByItemList(new Timestamp(date3.getTime() + TimeUnit.DAYS.toMillis(1) / 2), Stream.of(chery, banana, apple).collect(Collectors.toList()));
+        Order order_day_1_1 = generateByItemList(Timestamp.valueOf(date1), Stream.of(banana, banana, chery, apple).collect(Collectors.toList()));
+        Order order_day_1_2 = generateByItemList(Timestamp.valueOf(date1.plusSeconds(10)), Stream.of(chery, chery, chery, apple).collect(Collectors.toList()));
+        Order order_day_2_1 = generateByItemList(Timestamp.valueOf(date2), Stream.of(apple, apple, chery, apple).collect(Collectors.toList()));
+        Order order_day_2_2 = generateByItemList(Timestamp.valueOf(date2.plusDays(1).minusSeconds(10)), Stream.of(chery, banana, banana, banana).collect(Collectors.toList()));
+        Order order_day_3_1 = generateByItemList(Timestamp.valueOf(date3), Stream.of(chery, apple, chery, apple).collect(Collectors.toList()));
+        Order order_day_3_2 = generateByItemList(Timestamp.valueOf(date3.plusHours(12)), Stream.of(chery, banana, apple).collect(Collectors.toList()));
 
         List<Order> orders = Stream.of(order_day_1_1, order_day_2_1, order_day_3_1, order_day_1_2, order_day_2_2, order_day_3_2).collect(Collectors.toList());
 
@@ -53,28 +56,29 @@ public class StoreStatisticTest {
 
         assertEquals(3, results.size());
         //Test first day statistic
-        assertEquals(Integer.valueOf(2), results.get(new Timestamp(date1.getTime())).get(banana));
-        assertEquals(Integer.valueOf(4), results.get(new Timestamp(date1.getTime())).get(chery));
-        assertEquals(Integer.valueOf(2), results.get(new Timestamp(date1.getTime())).get(apple));
+        assertEquals(Integer.valueOf(2), results.get(Timestamp.valueOf(date1)).get(banana));
+        assertEquals(Integer.valueOf(4), results.get(Timestamp.valueOf(date1)).get(chery));
+        assertEquals(Integer.valueOf(2), results.get(Timestamp.valueOf(date1)).get(apple));
 
         //Test second day statistic
-        assertEquals(Integer.valueOf(3), results.get(new Timestamp(date2.getTime())).get(banana));
-        assertEquals(Integer.valueOf(2), results.get(new Timestamp(date2.getTime())).get(chery));
-        assertEquals(Integer.valueOf(3), results.get(new Timestamp(date2.getTime())).get(apple));
+        assertEquals(Integer.valueOf(3), results.get(Timestamp.valueOf(date2)).get(banana));
+        assertEquals(Integer.valueOf(2), results.get(Timestamp.valueOf(date2)).get(chery));
+        assertEquals(Integer.valueOf(3), results.get(Timestamp.valueOf(date2)).get(apple));
 
         //Test third day statistic
-        assertEquals(Integer.valueOf(1), results.get(new Timestamp(date3.getTime())).get(banana));
-        assertEquals(Integer.valueOf(3), results.get(new Timestamp(date3.getTime())).get(chery));
-        assertEquals(Integer.valueOf(3), results.get(new Timestamp(date3.getTime())).get(apple));
+        assertEquals(Integer.valueOf(1), results.get(Timestamp.valueOf(date3)).get(banana));
+        assertEquals(Integer.valueOf(3), results.get(Timestamp.valueOf(date3)).get(chery));
+        assertEquals(Integer.valueOf(3), results.get(Timestamp.valueOf(date3)).get(apple));
     }
 
     @Test
-    public void mostPopularItem() throws ParseException {
-        Date date = formatter.parse("2015-09-05");
-        Order order1 = generateByItemList(new Timestamp(date.getTime()), Stream.of(chery, chery, chery, banana).collect(Collectors.toList()));
-        Order order2 = generateByItemList(new Timestamp(date.getTime()), Stream.of(banana, banana, apple, apple).collect(Collectors.toList()));
-        Order order3 = generateByItemList(new Timestamp(date.getTime()), Stream.of(apple, banana, apple, chery).collect(Collectors.toList()));
-        Order decisiveOrder = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(chery));
+    public void mostPopularItem() {
+        LocalDateTime date = LocalDateTime.of(2015, Month.SEPTEMBER, 5, 0, 0, 0);
+        ;
+        Order order1 = generateByItemList(Timestamp.valueOf(date), Stream.of(chery, chery, chery, banana).collect(Collectors.toList()));
+        Order order2 = generateByItemList(Timestamp.valueOf(date), Stream.of(banana, banana, apple, apple).collect(Collectors.toList()));
+        Order order3 = generateByItemList(Timestamp.valueOf(date), Stream.of(apple, banana, apple, chery).collect(Collectors.toList()));
+        Order decisiveOrder = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(chery));
 
         List<Order> orders = Stream.of(order1, order2, order3, decisiveOrder).collect(Collectors.toList());
 
@@ -85,11 +89,12 @@ public class StoreStatisticTest {
     }
 
     @Test
-    public void mostPopularItemWithSamePopularItems() throws ParseException {
-        Date date = formatter.parse("2015-09-05");
-        Order order1 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(banana));
-        Order order2 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(chery));
-        Order order3 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(apple));
+    public void mostPopularItemWithSamePopularItems() {
+        LocalDateTime date = LocalDateTime.of(2015, Month.SEPTEMBER, 5, 0, 0, 0);
+
+        Order order1 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(banana));
+        Order order2 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(chery));
+        Order order3 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(apple));
 
         List<Order> orders = Stream.of(order1, order2, order3).collect(Collectors.toList());
 
@@ -100,16 +105,16 @@ public class StoreStatisticTest {
     }
 
     @Test
-    public void sum5biggerOrders() throws ParseException {
-        Date date = formatter.parse("2015-09-05");
-        Order order1 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(banana));
-        Order order2 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(chery));
-        Order order3 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(apple));
-        Order order4 = generateByItemList(new Timestamp(date.getTime()), Stream.of(banana, apple).collect(Collectors.toList()));//80+40=120
-        Order order5 = generateByItemList(new Timestamp(date.getTime()), Stream.of(apple, chery).collect(Collectors.toList()));//40+120=160
-        Order order6 = generateByItemList(new Timestamp(date.getTime()), Stream.of(banana, chery).collect(Collectors.toList()));//80+120=200
-        Order order7 = generateByItemList(new Timestamp(date.getTime()), Stream.of(banana, chery, chery, banana).collect(Collectors.toList()));//400
-        Order order8 = generateByItemList(new Timestamp(date.getTime()), Stream.of(chery, apple, chery, apple).collect(Collectors.toList()));//320
+    public void sum5biggerOrders() {
+        LocalDateTime date = LocalDateTime.of(2015, Month.SEPTEMBER, 5, 0, 0, 0);
+        Order order1 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(banana));
+        Order order2 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(chery));
+        Order order3 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(apple));
+        Order order4 = generateByItemList(Timestamp.valueOf(date), Stream.of(banana, apple).collect(Collectors.toList()));//80+40=120
+        Order order5 = generateByItemList(Timestamp.valueOf(date), Stream.of(apple, chery).collect(Collectors.toList()));//40+120=160
+        Order order6 = generateByItemList(Timestamp.valueOf(date), Stream.of(banana, chery).collect(Collectors.toList()));//80+120=200
+        Order order7 = generateByItemList(Timestamp.valueOf(date), Stream.of(banana, chery, chery, banana).collect(Collectors.toList()));//400
+        Order order8 = generateByItemList(Timestamp.valueOf(date), Stream.of(chery, apple, chery, apple).collect(Collectors.toList()));//320
 
         List<Order> orders = Stream.of(order1, order2, order3, order4, order5, order6, order7, order8).collect(Collectors.toList());
 
@@ -128,11 +133,11 @@ public class StoreStatisticTest {
     }
 
     @Test
-    public void sum5biggerOrdersIfCountOrdersLess5() throws ParseException {
-        Date date = formatter.parse("2015-09-05");
-        Order order1 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(banana));
-        Order order2 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(chery));
-        Order order3 = generateByItemList(new Timestamp(date.getTime()), Collections.singletonList(apple));
+    public void sum5biggerOrdersIfCountOrdersLess5() {
+        LocalDateTime date = LocalDateTime.of(2015, Month.SEPTEMBER, 5, 0, 0, 0);
+        Order order1 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(banana));
+        Order order2 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(chery));
+        Order order3 = generateByItemList(Timestamp.valueOf(date), Collections.singletonList(apple));
 
         List<Order> orders = Stream.of(order1, order2, order3).collect(Collectors.toList());
 
