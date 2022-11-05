573a9c7ba1766fe0d0189f14339ebba3269f1b3c
Denis Strizhkin <strdenis02@gmail.com>
2022-11-05 23:38:05 +0300

    Finished CopyFile & Directories.

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -8,7 +16,30 @@ public class CopyFile {
      * В тесте для создания нужных файлов для первого запуска надо раскомментировать код в setUp()
      * 3 тугрика
      */
-    public static void copyFiles(String pathFrom, String pathTo) {
-    }
+    public static void copyFiles(String pathFromStr, String pathToStr) {
+        Path pathFrom = Paths.get(pathFromStr);
+        if (Files.notExists(pathFrom)) {
+            return;
+        }
+        Path pathTo = Paths.get(pathToStr);
 
+        try {
+            Files.createDirectories(pathTo.getParent());
+            Files.walkFileTree(pathFrom, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path pathDir, BasicFileAttributes attrs) throws IOException {
+                    Files.createDirectory(pathTo.resolve(pathFrom.relativize(pathDir)));
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path pathFile, BasicFileAttributes attrs) throws IOException {
+                    Files.copy(pathFile, pathTo.resolve(pathFrom.relativize(pathFile)));
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,17 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Directories {
 
@@ -12,15 +23,59 @@ public class Directories {
      * Написать двумя способами. С использованием File
      * 2 тугрика
      */
-    public static int removeWithFile(String path) {
-        return 0;
+    public static int removeWithFile(String pathStr) {
+        File file = new File(pathStr);
+        if (!file.exists()) {
+            return 0;
+        }
+
+        int count = 0;
+        Deque<File> filesToVisit = new ArrayDeque<>();
+        Deque<File> dirsToDelete = new ArrayDeque<>();
+
+        filesToVisit.offerLast(file);
+        while (filesToVisit.size() > 0) {
+            File curFile = filesToVisit.pollFirst();
+            if (curFile.isDirectory()) {
+                Arrays.stream(curFile.listFiles()).forEach(filesToVisit::offerLast);
+                dirsToDelete.offerFirst(curFile);
+            } else {
+                curFile.delete();
+            }
+            count++;
+        }
+        dirsToDelete.stream().forEach(File::delete);
+
+        return count;
     }
 
     /**
      * С использованием Path
      * 2 тугрика
      */
-    public static int removeWithPath(String path) throws IOException {
-        return 0;
+    public static int removeWithPath(String pathStr) throws IOException {
+        Path path = Paths.get(pathStr);
+        if (Files.notExists(path)) {
+            return 0;
+        }
+
+        AtomicInteger atomicCount = new AtomicInteger();
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult postVisitDirectory(Path pathDir, IOException e) throws IOException {
+                Files.delete(pathDir);
+                atomicCount.incrementAndGet();
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path pathFile, BasicFileAttributes attrs) throws IOException {
+                Files.delete(pathFile);
+                atomicCount.incrementAndGet();
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        return atomicCount.get();
     }
 }
