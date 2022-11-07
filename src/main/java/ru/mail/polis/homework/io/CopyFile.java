2fa9b8c1fb4e17ac40ab4eee4e5de7160daef441
dasu <kirill12093@mail.ru>
2022-11-07 20:26:43 +0300

    first commit

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,12 @@
 package ru.mail.polis.homework.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CopyFile {
 
     /**
@@ -8,7 +15,21 @@ public class CopyFile {
      * В тесте для создания нужных файлов для первого запуска надо раскомментировать код в setUp()
      * 3 тугрика
      */
-    public static void copyFiles(String pathFrom, String pathTo) {
+    public static void copyFiles(String pathFrom, String pathTo) throws IOException {
+        Path newFile = Paths.get(pathTo);
+        Files.createDirectories(newFile.getParent());
+        Files.createFile(newFile);
+        copyFile(Paths.get(pathFrom), newFile);
     }
+
+    private static void copyFile(Path from, Path to) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(to);
+             BufferedReader reader = Files.newBufferedReader(from)) {
+            while (reader.ready()) {
+                writer.write(reader.readLine());
+            }
+        }
+    }
+
 
 }
