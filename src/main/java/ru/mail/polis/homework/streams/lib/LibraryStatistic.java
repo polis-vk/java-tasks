b040a5a13a02dec31dc203d6499cf37aef4d7562
Vladimir <i.am.polshin@gmail.com>
2022-11-07 12:15:13 +0300

    LibraryStatistic: fixed specialistInGenre(), unreliableUsers()

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -17,8 +17,9 @@ public class LibraryStatistic {
      * Вернуть "специалистов" в литературном жанре с кол-вом прочитанных страниц.
      * Специалист жанра считается пользователь который прочел как минимум 5 книг в этом жанре,
      * при этом читал каждую из них не менее 14 дней.
+     *
      * @param library - данные библиотеки
-     * @param genre - жанр
+     * @param genre   - жанр
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
@@ -26,19 +27,28 @@ public class LibraryStatistic {
                 .filter(archive -> genre.equals(archive.getBook().getGenre()))
                 .collect(Collectors.groupingBy(ArchivedData::getUser))
                 .entrySet().stream()
-                .filter(lib -> lib.getValue().stream()
+                .filter(userEntry -> userEntry.getValue().stream()
                         .allMatch(e -> countDays(e) >= SPECIALIST_READING_DAYS))
-                .filter(lib -> lib.getValue().size() >= SPECIALIST_MIN_BOOKS_COUNT)
-                .collect(Collectors.toMap(Map.Entry::getKey, lib -> lib.getValue().stream()
+                .filter(userEntry -> userEntry.getValue().size() >= SPECIALIST_MIN_BOOKS_COUNT)
+                .collect(Collectors.toMap(Map.Entry::getKey, userEntry -> userEntry.getValue().stream()
                         .map(ArchivedData::getBook)
-                        .mapToInt(Book::getPage).sum()));
+                        .mapToInt(Book::getPage).sum() + library.getUsers().stream()
+                        .filter(user -> user.equals(userEntry.getKey()))
+                        .filter(user -> user.getBook() != null)
+                        .filter(user -> genre.equals(user.getBook().getGenre()))
+                        .filter(user -> library.getArchive().stream()
+                                .anyMatch(archive -> !archive.getBook().equals(user.getBook())))
+                        .map(User::getReadedPages)
+                        .reduce(0, Integer::sum))
+                );
     }
 
     /**
      * Вернуть любимый жанр пользователя. Тот что чаще всего встречается. Не учитывать тот что пользователь читает в данный момент.
      * Если есть несколько одинаковых по весам жанров - брать в расчет то, что пользователь читает в данный момент.
+     *
      * @param library - данные библиотеки
-     * @param user - пользователь
+     * @param user    - пользователь
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
@@ -59,6 +69,7 @@ public class LibraryStatistic {
     /**
      * Вернуть список пользователей которые больше половины книг держали на руках более 30-ти дней. Брать в расчет и книги которые сейчас
      * пользователи держат у себя (ArchivedData.returned == null)
+     *
      * @param library - данные библиотеки
      * @return - список ненадежных пользователей
      */
@@ -66,15 +77,16 @@ public class LibraryStatistic {
         return library.getArchive().stream()
                 .collect(Collectors.groupingBy(ArchivedData::getUser))
                 .entrySet().stream()
-                .filter(archive -> archive.getValue().size() / 2 <=
-                        archive.getValue().stream().filter(e -> countDays(e) >= UNRELIABLE_USER_READING_DAYS).count())
+                .filter(archive -> archive.getValue().size() / 2 <
+                        archive.getValue().stream().filter(e -> countDays(e) > UNRELIABLE_USER_READING_DAYS).count())
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
     }
 
     /**
      * Вернуть список книг у которых страниц равно или больше чем переданное значение
-     * @param library - данные библиотеки
+     *
+     * @param library   - данные библиотеки
      * @param countPage - кол-во страниц
      * @return - список книг
      */
@@ -86,6 +98,7 @@ public class LibraryStatistic {
 
     /**
      * Вернуть самого популярного автора в каждом жанре. Если кол-во весов у авторов одинаково брать по алфавиту.
+     *
      * @param library - данные библиотеки
      * @return - map жанр / самый популярный автор
      */
@@ -101,11 +114,11 @@ public class LibraryStatistic {
                         .orElse("")));
     }
 
-    private static int countDays(ArchivedData archive) {
+    private static long countDays(ArchivedData archive) {
         if (archive.getReturned() == null) {
-            return (int) (System.currentTimeMillis() - archive.getTake().getTime()) / (1000 * 60 * 60 * 24);
+            return (System.currentTimeMillis() - archive.getTake().getTime()) / (1000 * 60 * 60 * 24);
         }
-        return (int) (archive.getReturned().getTime() - archive.getTake().getTime()) / (1000 * 60 * 60 * 24);
+        return (archive.getReturned().getTime() - archive.getTake().getTime()) / (1000 * 60 * 60 * 24);
     }
 }
 
