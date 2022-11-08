1d917e7a46b8fc4ba5f6626bf3e397652f9e3308
Алиса <sealisaa@yandex.ru>
2022-11-08 14:44:22 +0300

    HW6 CopyFile done

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,6 +1,16 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CopyFile {
+
+    private static final int BUFFER_SIZE = 1024;
 
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
@@ -9,6 +19,54 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
+        if (pathFrom == null || pathTo == null) {
+            return;
+        }
+        Path from = Paths.get(pathFrom);
+        if (Files.notExists(from)) {
+            return;
+        }
+        Path to = Paths.get(pathTo);
+        try {
+            if (Files.isRegularFile(from)) {
+                Files.createDirectories(to.getParent());
+            } else {
+                Files.createDirectories(to);
+            }
+            copy(from, to);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    private static void copy(Path from, Path to) throws IOException {
+        if (Files.isRegularFile(from)) {
+            copyFile(from, to);
+            return;
+        }
+        if (Files.notExists(to)) {
+            Files.createDirectory(to);
+        }
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(from)) {
+            for (Path file : stream) {
+                copy(file, to.resolve(file.getFileName()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copyFile(Path from, Path to) {
+        try (InputStream in = Files.newInputStream(from)) {
+            try (OutputStream out = Files.newOutputStream(to)) {
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int length;
+                while ((length = in.read(buffer)) > 0) {
+                    out.write(buffer, 0, length);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
