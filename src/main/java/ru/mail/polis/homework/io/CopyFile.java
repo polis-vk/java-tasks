8b6cf2e2f2e3e64a5a9fad4273c77bf7997551c9
Egorka <trololo221@bk.ru>
2022-11-10 16:32:20 +0300

    Add solve for CopyFile

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,11 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -9,6 +15,55 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
+        if (pathFrom == null || pathTo == null) {
+            return;
+        }
+
+        Path fromDir = Path.of(pathFrom);
+        Path toDir = Path.of(pathTo);
+        try {
+            if (Files.notExists(fromDir)) {
+                throw new NoSuchFileException("Path does not exist");
+            }
+
+            if (Files.isDirectory(fromDir)) {
+                Files.createDirectories(toDir);
+                Files.walkFileTree(fromDir, new SimpleFileVisitor<>() {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                        Files.createDirectories(toDir.resolve(fromDir.relativize(dir)));
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                        copy(file, toDir.resolve(fromDir.relativize(file)));
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            } else {
+                Files.createDirectories(toDir.getParent());
+                copy(fromDir, toDir);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void copy(Path from, Path to) {
+        final int SIZE_OF_BUFFER = 1024;
+        final byte[] BUFFER = new byte[SIZE_OF_BUFFER];
+
+        try (InputStream fis = Files.newInputStream(from);
+             OutputStream fos = Files.newOutputStream(to)) {
+            int input = fis.read(BUFFER);
+            while (input != -1) {
+                fos.write(input);
+                input = fis.read(BUFFER);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
