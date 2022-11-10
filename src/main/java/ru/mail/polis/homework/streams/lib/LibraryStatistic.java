206aef9a490747e7605340be7882f1609b288dcf
osokindm <osokin34@gmail.com>
2022-11-10 21:22:50 +0300

    LibraryStatistic fix, tests passed

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,6 +1,8 @@
 package ru.mail.polis.homework.streams.lib;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -19,18 +21,27 @@ public class LibraryStatistic {
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return library.getUsers().stream()
-                .filter(user -> library.getArchive().stream().filter(archivedData -> archivedData.getBook().getGenre().equals(genre)
-                        && archivedData.getUser().equals(user)
-                        && enoughDays(archivedData, 14)).count() >= 5)
-                .collect(Collectors.toMap(user -> user, User::getReadedPages));
+
+        return library.getArchive().stream()
+                .filter(archivedData -> archivedData.getBook().getGenre().equals(genre) &&
+                        enoughDays(archivedData, 14))
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue().size() >= 5)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().stream()
+                                .mapToInt(archivedData -> archivedData.getBook().getPage())
+                                .sum() + (entry.getKey().getBook().getGenre().equals(genre) ?
+                                entry.getKey().getReadedPages() : 0)
+                ));
     }
 
     private boolean enoughDays(ArchivedData archivedData, int days) {
         if (archivedData.getReturned() == null) {
-            return System.currentTimeMillis() - archivedData.getTake().getTime() >= days * (1000 * 60 * 60 * 24);
+            return (System.currentTimeMillis() - archivedData.getTake().getTime()) / (1000 * 60 * 60 * 24) >= days;
         }
-        return archivedData.getReturned().getTime() - archivedData.getTake().getTime() >= days * (1000 * 60 * 60 * 24);
+        return (archivedData.getReturned().getTime() - archivedData.getTake().getTime()) / (1000 * 60 * 60 * 24) >= days;
     }
 
     /**
@@ -44,15 +55,19 @@ public class LibraryStatistic {
     public Genre loveGenre(Library library, User user) {
 
         Map<Genre, Long> genreFrequency = library.getArchive().stream()
-                .filter(archivedData -> archivedData.getUser().equals(user) && archivedData.getReturned() == null)
+                .filter(archivedData -> archivedData.getUser().equals(user) && archivedData.getReturned() != null)
                 .collect(Collectors.groupingBy(archivedData -> archivedData.getBook().getGenre(), Collectors.counting()));
+
+        if (genreFrequency.isEmpty()) {
+            return null;
+        }
 
         List<Genre> topGenres = genreFrequency.entrySet().stream()
                 .filter(x -> x.getValue().equals(
                         genreFrequency.values().stream().max(Long::compareTo).get())) // maxFrequency
                 .map(Map.Entry::getKey).collect(Collectors.toList());
 
-        if (topGenres.contains(user.getBook().getGenre())) {
+        if (user.getBook() != null && topGenres.contains(user.getBook().getGenre())) {
             return user.getBook().getGenre();
         }
 
@@ -88,7 +103,7 @@ public class LibraryStatistic {
     public List<Book> booksWithMoreCountPages(Library library, int countPage) {
 
         return library.getBooks().stream()
-                .filter(book -> book.getPage() > countPage)
+                .filter(book -> book.getPage() >= countPage)
                 .collect(Collectors.toList());
     }
 
@@ -99,12 +114,16 @@ public class LibraryStatistic {
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-
-        return library.getArchive().stream()
-                .collect(Collectors.groupingBy(archivedData -> archivedData.getBook().getGenre(),
-                        Collectors.groupingBy(archivedData -> archivedData.getBook().getAuthor(), Collectors.counting())))
-                .entrySet().stream().collect(
-                        Collectors.toMap(Map.Entry::getKey,
-                                entry -> entry.getValue().entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey()));
+        return Arrays.stream(Genre.values()).collect(Collectors.toMap(genre -> genre,
+                genre ->
+                    library.getArchive().stream()
+                            .map(ArchivedData::getBook)
+                            .filter(book -> book.getGenre().equals(genre))
+                            .collect(Collectors.groupingBy(Book::getAuthor, Collectors.counting()))
+                            .entrySet().stream()
+                            .sorted(Map.Entry.comparingByKey())
+                            .max(Map.Entry.comparingByValue()).map(Map.Entry::getKey)
+                            .orElse("Author not determined")
+               ));
     }
 }
