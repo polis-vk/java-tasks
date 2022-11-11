9b0bb914e68a13dde5fa0e3d8ab8ab12afcc944a
artemokky <ferrumsir@gmail.com>
2022-11-11 21:36:17 +0300

    added homework 5

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,63 +1,137 @@
 package ru.mail.polis.homework.streams.lib;
 
-import java.util.List;
-import java.util.Map;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по библиотеке.
  * Оценка 5-ть баллов
  */
 public class LibraryStatistic {
+    private final static int EXPERT_DAY_NORM = 14;
+    private final static int EXPERT_BOOK_NORM = 5;
 
+    private final static int RELIABILITY_DAYS_NORM = 30;
     /**
      * Вернуть "специалистов" в литературном жанре с кол-вом прочитанных страниц.
      * Специалист жанра считается пользователь который прочел как минимум 5 книг в этом жанре,
      * при этом читал каждую из них не менее 14 дней.
+     *
      * @param library - данные библиотеки
-     * @param genre - жанр
+     * @param genre   - жанр
      * @return - map пользователь / кол-во прочитанных страниц
      */
+
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return null;
+        return library.getArchive().stream()
+                .filter(archivedData -> archivedData.getBook().getGenre() == genre)
+                .filter(archivedData -> {
+                    LocalDateTime twoWeeksAfterTake = archivedData.getTake().toLocalDateTime().plusDays(EXPERT_DAY_NORM);
+                    return archivedData.getReturned().toLocalDateTime().isEqual(twoWeeksAfterTake) ||
+                            archivedData.getReturned().toLocalDateTime().isAfter(twoWeeksAfterTake);
+                })
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(books -> books.getValue().size() >= EXPERT_BOOK_NORM)
+                .collect(Collectors.toMap(Map.Entry::getKey, archivedData ->
+                        (archivedData.getKey().getBook().getGenre() == genre ?
+                                archivedData.getKey().getReadedPages() : 0)
+                                + archivedData.getValue().stream()
+                                .map(book -> book.getBook().getPage())
+                                .mapToInt(pages -> pages).sum()));
     }
 
     /**
      * Вернуть любимый жанр пользователя. Тот что чаще всего встречается. Не учитывать тот что пользователь читает в данный момент.
      * Если есть несколько одинаковых по весам жанров - брать в расчет то, что пользователь читает в данный момент.
+     *
      * @param library - данные библиотеки
-     * @param user - пользователь
+     * @param user    - пользователь
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        return null;
+        return library.getArchive().stream()
+                .filter(archivedData -> archivedData.getUser().equals(user))
+                .collect(Collectors.groupingBy(archivedData -> archivedData.getBook().getGenre(), Collectors.counting()))
+                .entrySet().stream()
+                .max((genre1, genre2) -> {
+                    if (genre1.getValue() - genre2.getValue() != 0) {
+                        return (int) (genre1.getValue() - genre2.getValue());
+                    }
+
+                    Genre notReturnedGenre = library.getUsers().stream()
+                            .filter(reader -> reader == user)
+                            .findFirst().get().getBook().getGenre();
+
+                    if (notReturnedGenre.equals(genre1.getKey())) {
+                        return 1;
+                    }
+                    return -1;
+                }).get().getKey();
     }
 
     /**
      * Вернуть список пользователей которые больше половины книг держали на руках более 30-ти дней. Брать в расчет и книги которые сейчас
      * пользователи держат у себя (ArchivedData.returned == null)
+     *
      * @param library - данные библиотеки
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        return null;
+        return library.getArchive().stream()
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(userListEntry -> userListEntry.getValue().stream()
+                        .filter(archivedData -> archivedData.getReturned() != null
+                                && archivedData.getReturned().toLocalDateTime()
+                                .isAfter(archivedData.getTake().toLocalDateTime().plusDays(RELIABILITY_DAYS_NORM))
+                                || archivedData.getReturned() == null
+                                && LocalDateTime.now()
+                                .isAfter(archivedData.getTake().toLocalDateTime().plusDays(RELIABILITY_DAYS_NORM)))
+                        .count() > userListEntry.getValue().size() / 2)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     /**
      * Вернуть список книг у которых страниц равно или больше чем переданное значение
-     * @param library - данные библиотеки
+     *
+     * @param library   - данные библиотеки
      * @param countPage - кол-во страниц
      * @return - список книг
      */
     public List<Book> booksWithMoreCountPages(Library library, int countPage) {
-        return null;
+        return library.getBooks().stream()
+                .filter(book -> book.getPage() >= countPage)
+                .collect(Collectors.toList());
     }
 
     /**
      * Вернуть самого популярного автора в каждом жанре. Если кол-во весов у авторов одинаково брать по алфавиту.
+     *
      * @param library - данные библиотеки
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-        return null;
+        Map<Genre, String> mapOfPopularity = library.getArchive().stream()
+                .collect(Collectors.groupingBy(archivedData -> archivedData.getBook().getGenre(),
+                        Collectors.groupingBy(archivedData -> archivedData.getBook().getAuthor(), Collectors.counting())))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, genreMapEntry -> genreMapEntry.getValue().entrySet().stream()
+                        .max(Map.Entry.<String,Long>comparingByValue().reversed().thenComparing(Map.Entry::getKey))
+                        .map(Map.Entry::getKey)
+                        .get()
+                ));
+
+        for (Genre genre : Genre.values()) {
+            if (!mapOfPopularity.containsKey(genre)) {
+                mapOfPopularity.put(genre, "Author not determined");
+            }
+        }
+
+        return mapOfPopularity;
     }
 }
--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -1,8 +1,10 @@
 package ru.mail.polis.homework.streams.store;
 
 import java.sql.Timestamp;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
@@ -13,41 +15,78 @@ public class StoreStatistic {
     /**
      * Вернуть сколько было продано определенного товара за переданный промежуток времени
      *
-     * @param orders - список заказов
+     * @param orders   - список заказов
      * @param typeItem - вид товара
-     * @param from - с какого момента вести подсчет
-     * @param to - по какой момент вести подсчет
+     * @param from     - с какого момента вести подсчет
+     * @param to       - по какой момент вести подсчет
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return 0L;
+        return orders.stream()
+                .filter(order -> order.getTime().compareTo(from) >= 0)
+                .filter(order -> order.getTime().compareTo(to) <= 0)
+                .filter(order -> order.getItemCount().containsKey(typeItem))
+                .mapToLong(order -> order.getItemCount().get(typeItem)).sum();
     }
 
     /**
      * Вернуть статистику по каждому дню сколько какого товара было проданно
+     *
      * @param orders - список заказов
      * @return - map, где ключ - начало дня (00:00:00 000 UTC) для которого собрана статистика
      * значение - map товар/кол-во
      */
     public Map<Timestamp, Map<Item, Integer>> statisticItemsByDay(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .collect(Collectors.toMap(order ->
+                                Timestamp.valueOf(order.getTime().toLocalDateTime().toLocalDate().atStartOfDay()),
+                        Order::getItemCount,
+                        (order1, order2) -> {
+                            order2.forEach((item, count) -> order1.merge(item, count, Integer::sum));
+                            return order1;
+                        }
+                ));
     }
 
     /**
      * Вернуть самый популярный товар
+     *
      * @param orders - список заказов
      * @return - товар
      */
     public Item mostPopularItem(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .flatMap(order -> order.getItemCount().entrySet().stream())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        Integer::sum
+                ))
+                .entrySet().stream().max(Map.Entry.comparingByValue())
+                .get().getKey();
     }
 
     /**
      * Вернуть сумму для 5 самых больших по кол-ву товаров заказу.
+     *
      * @param orders - список заказов
      * @return map - заказ / общая сумма заказа
      */
     public Map<Order, Long> sum5biggerOrders(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .sorted(Comparator.comparingInt(order -> order.getItemCount()
+                        .values()
+                        .stream()
+                        .mapToInt(count -> -count)
+                        .sum()))
+                .limit(5)
+                .collect(Collectors.toMap(
+                        order -> order,
+                        order -> order.getItemCount()
+                                .entrySet()
+                                .stream()
+                                .mapToLong(entry -> entry.getKey().getPrice() * entry.getValue())
+                                .sum()
+                ));
     }
 }
