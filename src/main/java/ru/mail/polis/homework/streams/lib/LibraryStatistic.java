01f374dbeab8f83ea8f0dba8496cf97e2abc9b39
Ilya Viktorov <viktorov.ia@edu.spbstu.ru>
2022-11-11 14:16:06 +0300

    all tasks

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,7 +1,10 @@
 package ru.mail.polis.homework.streams.lib;
 
+import java.sql.Timestamp;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по библиотеке.
@@ -18,7 +21,32 @@ public class LibraryStatistic {
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return null;
+        return library.getArchive().stream()
+                .filter(data -> data.getBook().getGenre().equals(genre) &&
+                        compareDates(data.getTake(), data.getReturned(), 14) >= 0)
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(pair -> pair.getValue().size() >= 5)
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        pair -> {
+                            int pages = pair.getValue().stream()
+                                    .mapToInt(data -> data.getBook().getPage())
+                                    .sum();
+                            if (pair.getKey().getBook().getGenre().equals(genre)) {
+                                pages += pair.getKey().getReadedPages();
+                            }
+                            return pages;
+                        }));
+    }
+
+    private int compareDates(Timestamp from, Timestamp to, int days) {
+        long actualDays;
+        if (to != null) {
+            actualDays = (to.getTime() - from.getTime()) / (24 * 60 * 60 * 1000);
+        } else {
+            actualDays = (System.currentTimeMillis() - from.getTime()) / (24 * 60 * 60 * 1000);
+        }
+        return actualDays > days ? 1 : actualDays == days ? 0 : -1;
     }
 
     /**
@@ -29,7 +57,20 @@ public class LibraryStatistic {
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        return null;
+        Map<Genre, Long> map = library.getArchive().stream()
+                .filter(data -> data.getUser().equals(user) && data.getReturned() != null)
+                .collect(Collectors.groupingBy(data -> data.getBook().getGenre(), Collectors.counting()));
+        long maxCountOfGenre = Collections.max(map.values());
+        List<Genre> list = map.entrySet().stream()
+                .filter(pair -> pair.getValue() == maxCountOfGenre)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        if (list.size() == 1) {
+            return list.get(0);
+        }
+        // Исхожу из того, что если есть два одинаковых по наибольшим весам жанра,
+        // то есть книга, которую читает пользователь, и которая относится к одному из этих жанров
+        return user.getBook().getGenre();
     }
 
     /**
@@ -39,7 +80,15 @@ public class LibraryStatistic {
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        return null;
+        return library.getArchive().stream()
+                .collect(Collectors.groupingBy(ArchivedData::getUser,
+                        Collectors.partitioningBy(
+                                data -> compareDates(data.getTake(), data.getReturned(), 30) > 0)))
+                .entrySet().stream()
+                .filter(map -> (map.getValue().get(true).size() + map.getValue().get(false).size())
+                        / map.getValue().get(true).size() < 2)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -49,7 +98,9 @@ public class LibraryStatistic {
      * @return - список книг
      */
     public List<Book> booksWithMoreCountPages(Library library, int countPage) {
-        return null;
+        return library.getBooks().stream()
+                .filter(book -> book.getPage() >= countPage)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -58,6 +109,19 @@ public class LibraryStatistic {
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-        return null;
+        Map<Genre, String> map = library.getArchive().stream()
+                .map(ArchivedData::getBook)
+                .collect(Collectors.groupingBy(Book::getGenre,
+                        Collectors.groupingBy(Book::getAuthor, Collectors.counting())))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, pair -> pair.getValue().entrySet().stream()
+                        .min(Map.Entry.<String, Long>comparingByKey().thenComparing(Map.Entry.comparingByValue()))
+                        .get().getKey()));
+        for (Genre genre : Genre.values()) {
+            if (!map.containsKey(genre)) {
+                map.put(genre, "Author not determined");
+            }
+        }
+        return map;
     }
 }
--- src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/store/StoreStatistic.java
@@ -1,8 +1,10 @@
 package ru.mail.polis.homework.streams.store;
 
 import java.sql.Timestamp;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Класс для работы со статистикой по заказам магазина.
@@ -20,7 +22,12 @@ public class StoreStatistic {
      * @return - кол-во проданного товара
      */
     public long proceedsByItems(List<Order> orders, Item typeItem, Timestamp from, Timestamp to) {
-        return 0L;
+        return orders.stream()
+                .filter(order -> order.getTime().compareTo(from) >= 0 && order.getTime().compareTo(to) <= 0)
+                .flatMap(order -> order.getItemCount().entrySet().stream())
+                .filter(pair -> pair.getKey().equals(typeItem))
+                .mapToLong(Map.Entry::getValue)
+                .sum();
     }
 
     /**
@@ -30,7 +37,15 @@ public class StoreStatistic {
      * значение - map товар/кол-во
      */
     public Map<Timestamp, Map<Item, Integer>> statisticItemsByDay(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .map(order -> new Order(
+                        Timestamp.valueOf(order.getTime().toLocalDateTime().toLocalDate().atStartOfDay()),
+                        order.getItemCount()))
+                .collect(Collectors.toMap(Order::getTime, Order::getItemCount,
+                        (map1, map2) -> {
+                            map1.forEach((k, v) -> map2.merge(k, v, Integer::sum));
+                            return map2;
+                        }));
     }
 
     /**
@@ -39,7 +54,10 @@ public class StoreStatistic {
      * @return - товар
      */
     public Item mostPopularItem(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .flatMap(order -> order.getItemCount().entrySet().stream())
+                .max(Map.Entry.comparingByValue())
+                .get().getKey();
     }
 
     /**
@@ -48,6 +66,17 @@ public class StoreStatistic {
      * @return map - заказ / общая сумма заказа
      */
     public Map<Order, Long> sum5biggerOrders(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .sorted(Comparator.comparing(
+                        order -> order.getItemCount().values().stream()
+                                .mapToInt(Integer::intValue)
+                                .sum(),
+                        Comparator.reverseOrder()))
+                .limit(5)
+                .collect(Collectors.toMap(order -> order,
+                        order -> order.getItemCount().entrySet().stream()
+                                .map(pair -> pair.getKey().getPrice() * pair.getValue())
+                                .mapToLong(Long::longValue)
+                                .sum()));
     }
 }
