926dbf43ed137fcb13a44f6e6130286b847fafa0
beatHunteRcode <nikitasherepa@gmail.com>
2022-11-12 16:23:23 +0300

    done LibraryStatistic.java

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -1,8 +1,16 @@
 package ru.mail.polis.homework.streams.lib;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+
 
 
 /**
@@ -10,6 +18,10 @@ import java.util.stream.Collectors;
  * Оценка 5-ть баллов
  */
 public class LibraryStatistic {
+
+    private final int MIN_READ_DAYS = 14;
+    private final int MIN_READ_BOOKS = 5;
+    private final int MAX_TAKEN_DAYS = 30;
 
     /**
      * Вернуть "специалистов" в литературном жанре с кол-вом прочитанных страниц.
@@ -20,7 +32,21 @@ public class LibraryStatistic {
      * @return - map пользователь / кол-во прочитанных страниц
      */
     public Map<User, Integer> specialistInGenre(Library library, Genre genre) {
-        return null;
+        return library.getArchive().stream()
+                .filter(archivedData -> archivedData.getBook().getGenre().name().equals(genre.name()))
+                .filter(archivedData -> archivedData.getReturned() != null &&
+                        TimeUnit.MILLISECONDS.toDays(
+                                archivedData.getReturned().getTime() - archivedData.getTake().getTime()
+                        ) >= MIN_READ_DAYS)
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream().filter(userEntry -> userEntry.getValue().size() >= MIN_READ_BOOKS)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().stream().
+                                mapToInt(archivedData -> archivedData.getBook().getPage()).sum() +
+                                (entry.getKey().getBook().getGenre().name().equals(genre.name()) ?
+                                        entry.getKey().getReadedPages() : 0))
+                );
     }
 
     /**
@@ -31,7 +57,24 @@ public class LibraryStatistic {
      * @return - жанр
      */
     public Genre loveGenre(Library library, User user) {
-        return null;
+        return library.getArchive().stream()
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(entry -> entry.getKey().equals(user))
+                .map(entry -> entry.getValue().stream()
+                        .collect(Collectors.groupingBy(archivedData -> archivedData.getBook().getGenre())))
+                .collect(Collectors.toList()).get(0).entrySet().stream()
+                .max((entry1, entry2) -> {
+                    if (entry1.getValue().size() == entry2.getValue().size()) {
+                        if (entry1.getKey().equals(user.getBook().getGenre())) {
+                            return 1;
+                        }
+                        else if (entry2.getKey().equals(user.getBook().getGenre())) {
+                            return -1;
+                        }
+                    }
+                    return Integer.compare(entry1.getValue().size(), entry2.getValue().size());
+                }).get().getKey();
     }
 
     /**
@@ -41,7 +84,21 @@ public class LibraryStatistic {
      * @return - список ненадежных пользователей
      */
     public List<User> unreliableUsers(Library library) {
-        return null;
+        return library.getArchive().stream()
+                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(entry ->
+                        entry.getValue().stream().filter(archivedData -> {
+                            if (archivedData.getReturned() == null) {
+                                return (TimeUnit.MILLISECONDS.toDays(Timestamp.valueOf(LocalDateTime.now()).getTime() -
+                                        TimeUnit.MILLISECONDS.toDays(archivedData.getTake().getTime()))) > MAX_TAKEN_DAYS;
+                            }
+                            else {
+                                return (TimeUnit.MILLISECONDS.toDays(archivedData.getReturned().getTime()) -
+                                        TimeUnit.MILLISECONDS.toDays(archivedData.getTake().getTime())) > MAX_TAKEN_DAYS;
+                            }
+                                }).count() > (entry.getValue().size() / 2))
+                .map(Map.Entry::getKey).collect(Collectors.toList());
     }
 
     /**
