1b3e5594ca9904b1a629a2daea02e6012386c502
Ekaterina Zelenkina <katrinzel0@mail.ru>
2022-11-12 16:34:53 +0300

    LibraryStatistic fix

--- src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
+++ src/main/java/ru/mail/polis/homework/streams/lib/LibraryStatistic.java
@@ -3,7 +3,9 @@ package ru.mail.polis.homework.streams.lib;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import static java.util.stream.Collectors.*;
 
 /**
  * Класс для работы со статистикой по библиотеке.
@@ -13,7 +15,6 @@ public class LibraryStatistic {
     private static final int READING_DAYS_FOR_EXPERT = 14;
     private static final int COUNT_OF_BOOKS_FOR_EXPERT = 5;
     private static final int MONTH = 30;
-    private static final int TRANSFER_COEFFICIENT = 60 * 60 * 24 * 100;
 
     /**
      * Вернуть "специалистов" в литературном жанре с кол-вом прочитанных страниц.
@@ -27,11 +28,13 @@ public class LibraryStatistic {
         return library.getArchive().stream()
                 .filter(archive -> archive.getReturned() != null)
                 .filter(archive -> archive.getBook().getGenre().equals(genre))
-                .filter(arch -> ((arch.getReturned().getTime() - arch.getTake().getTime()) / (TRANSFER_COEFFICIENT)) > READING_DAYS_FOR_EXPERT)
-                .collect(Collectors.groupingBy(ArchivedData::getUser))
+                .filter(arch -> TimeUnit.MILLISECONDS
+                        .toDays(arch.getReturned().getTime() - arch.getTake().getTime()) >= READING_DAYS_FOR_EXPERT)
+                .collect(Collectors.groupingBy(ArchivedData::getUser,
+                        Collectors.mapping(archive -> archive.getBook().getPage(), Collectors.toList())))
                 .entrySet().stream()
                 .filter(archiveMap -> archiveMap.getValue().size() >= COUNT_OF_BOOKS_FOR_EXPERT)
-                .collect(Collectors.toMap(Map.Entry::getKey, value -> value.getValue().size()));
+                .collect(Collectors.toMap(Map.Entry::getKey, archiveMap -> getReadPages(genre, archiveMap)));
     }
 
     /**
@@ -44,9 +47,22 @@ public class LibraryStatistic {
     public Genre loveGenre(Library library, User user) {
         return library.getArchive().stream()
                 .filter(archivedData -> archivedData.getUser().equals(user))
-                .collect(Collectors.groupingBy(archivedData -> archivedData.getBook().getGenre(), Collectors.counting()))
+                .collect(groupingBy(archivedData -> archivedData.getBook().getGenre(), Collectors.counting()))
                 .entrySet().stream()
-                .max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse(null);
+                .min((o1, o2) -> {
+                    int compare = Long.compare(o2.getValue(), o1.getValue());
+                    if (compare != 0) {
+                        return compare;
+                    }
+                    if (o1.getKey().equals(user.getBook().getGenre())) {
+                        return -1;
+                    }
+                    if (o2.getKey().equals(user.getBook().getGenre())) {
+                        return 1;
+                    }
+                    return compare;
+                })
+                .map(Map.Entry::getKey).orElse(null);
     }
 
     /**
@@ -57,9 +73,11 @@ public class LibraryStatistic {
      */
     public List<User> unreliableUsers(Library library) {
         return library.getArchive().stream()
-                .filter(LibraryStatistic::isMoreThanMonth)
-                .map(ArchivedData::getUser)
-                .collect(Collectors.toList());
+                .collect(groupingBy(ArchivedData::getUser))
+                .entrySet().stream()
+                .filter(arch -> arch.getValue().stream()
+                        .filter(LibraryStatistic::isMoreThanMonth).count() > arch.getValue().size() / 2)
+                .map(Map.Entry::getKey).collect(toList());
     }
 
     /**
@@ -71,7 +89,7 @@ public class LibraryStatistic {
     public List<Book> booksWithMoreCountPages(Library library, int countPage) {
         return library.getBooks().stream()
                 .filter(book -> book.getPage() >= countPage)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
     /**
@@ -80,18 +98,33 @@ public class LibraryStatistic {
      * @return - map жанр / самый популярный автор
      */
     public Map<Genre, String> mostPopularAuthorInGenre(Library library) {
-        return library.getArchive().stream()
-                .map(ArchivedData::getBook)
-                .collect(Collectors.groupingBy(Book::getGenre, Collectors.groupingBy(Book::getAuthor, Collectors.counting())))
+        return library.getBooks().stream()
+                .collect(groupingBy(Book::getGenre, groupingBy(Book::getAuthor, Collectors.counting())))
                 .entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, genreMap -> genreMap.getValue().entrySet().stream()
-                        .max(Comparator.comparingLong(Map.Entry<String, Long>::getValue)
-                                .thenComparing(Map.Entry.comparingByKey())).map(Map.Entry::getKey).orElse("")));
+                .collect(Collectors.toMap(Map.Entry::getKey, genreMap -> genreMap.getValue()
+                        .entrySet().stream()
+                        .max(Map.Entry.<String, Long>comparingByValue()
+                                .thenComparing(Map.Entry.comparingByKey(Comparator.reverseOrder())))
+                        .filter(entryMap -> library.getArchive().stream()
+                                .anyMatch(archivedData -> archivedData.getBook().getAuthor()
+                                        .equals(entryMap.getKey()))).map(Map.Entry::getKey)
+                        .orElse("Author not determined")));
     }
 
     private static boolean isMoreThanMonth(ArchivedData a) {
-        return (a.getReturned() == null
-                && ((System.currentTimeMillis() - a.getTake().getTime()) / (TRANSFER_COEFFICIENT)) > MONTH)
-                || ((a.getReturned().getTime() - a.getTake().getTime()) / (TRANSFER_COEFFICIENT)) > MONTH;
+        if (a.getReturned() == null) {
+            return TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis() - a.getTake().getTime()) >= MONTH;
+        }
+        return TimeUnit.MILLISECONDS.toDays(a.getReturned().getTime() - a.getTake().getTime()) >= MONTH;
     }
+
+    private int getReadPages(Genre genre, Map.Entry<User, List<Integer>> archiveMap) {
+        int readPages = archiveMap.getValue().stream()
+                .mapToInt(integer -> integer).sum();
+        if (archiveMap.getKey().getBook().getGenre() == genre) {
+            readPages += archiveMap.getKey().getReadedPages();
+        }
+        return readPages;
+    }
+
 }
