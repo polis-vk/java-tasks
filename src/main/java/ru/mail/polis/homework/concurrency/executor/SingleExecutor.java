83e8f2e668f2e1df62307d29046e2d02aa326c64
Vladimir <i.am.polshin@gmail.com>
2022-11-19 23:37:29 +0300

    SingleExecutor: all done

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,24 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.LinkedList;
 import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
+    private final MyThread thread = new MyThread();
+    private final LinkedList<Runnable> tasks = new LinkedList<>();
+    private boolean terminated = false;
 
+    public SingleExecutor() {
+        thread.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +26,10 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (terminated) {
+            throw new RejectedExecutionException();
+        }
+        tasks.add(command);
     }
 
     /**
@@ -25,6 +37,7 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        terminated = true;
     }
 
     /**
@@ -32,6 +45,20 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
+        shutdown();
+        thread.interrupt();
+    }
 
+    private class MyThread extends Thread {
+        @Override
+        public void run() {
+            while (!isInterrupted() && (!terminated || !tasks.isEmpty())) {
+                Runnable task = tasks.poll();
+                if (task != null) {
+                    task.run();
+                }
+            }
+        }
     }
 }
+
