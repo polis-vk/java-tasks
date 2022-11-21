ac0e0f455c7e9a3a379104c408deb12ad8368540
Vladislav Pochernin <vspochernin@gmail.com>
2022-11-21 09:41:39 +0300

    part8: implement SingleExecutor

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,39 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
 
+    private final Thread thread;
+    private final AtomicBoolean isShutdown;
+    private final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+
+    public SingleExecutor() {
+        isShutdown = new AtomicBoolean(false);
+
+        thread = new Thread(() -> {
+            while (true) {
+                try {
+                    queue.take().run();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thread.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +41,14 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (isShutdown.get()) {
+            throw new RejectedExecutionException();
+        }
+        try {
+            queue.put(command);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -25,6 +56,7 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        isShutdown.compareAndSet(false, true);
     }
 
     /**
@@ -32,6 +64,7 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
-
+        thread.interrupt();
+        isShutdown.compareAndSet(false, true);
     }
 }
