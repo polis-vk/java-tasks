fc11019d20b5a3027fcc059459e7eb2efdd5f199
Alexander-1703 <gladyshev.aa@edu.spbstu.ru>
2022-11-21 21:05:18 +0300

    single and simple executors

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,22 +1,38 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
  * Ленивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 тугриков
  */
 public class SimpleExecutor implements Executor {
 
-    public SimpleExecutor(int maxThreadCount) {
+    private final BlockingQueue<Runnable> workersTasks = new LinkedBlockingQueue<>();
+    private final ReentrantLock executeLock = new ReentrantLock();
+    private final ReentrantLock shutDownNowLock = new ReentrantLock();
+    private final AtomicInteger freeWorkers = new AtomicInteger(0);
+    private final List<Thread> workers;
+    private final int maxThreadCount;
+    private boolean isShutdown;
 
+    public SimpleExecutor(int maxThreadCount) {
+        this.maxThreadCount = maxThreadCount;
+        this.workers = new ArrayList<>(maxThreadCount);
     }
 
     /**
@@ -25,7 +41,18 @@ public class SimpleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (isShutdown) {
+            throw new RejectedExecutionException();
+        }
 
+        executeLock.lock();
+        if (workers.size() < maxThreadCount && freeWorkers.get() == 0) {
+            Thread worker = new Thread(new Worker());
+            workers.add(worker);
+            worker.start();
+        }
+        workersTasks.add(command);
+        executeLock.unlock();
     }
 
     /**
@@ -33,6 +60,7 @@ public class SimpleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        isShutdown = true;
     }
 
     /**
@@ -40,13 +68,35 @@ public class SimpleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdownNow() {
-
+        shutDownNowLock.lock();
+        isShutdown = true;
+        for (Thread worker : workers) {
+            worker.interrupt();
+        }
+        shutDownNowLock.unlock();
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        return workers.size();
     }
+
+    private class Worker implements Runnable {
+        @Override
+        public void run() {
+            while (!isShutdown) {
+                try {
+                    freeWorkers.incrementAndGet();
+                    Runnable task = workersTasks.take();
+                    freeWorkers.decrementAndGet();
+                    task.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,26 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
 
+    private final BlockingQueue<Runnable> workerTasks = new LinkedBlockingQueue<>();
+    private boolean isShutDown;
+
+    public SingleExecutor() {
+        Thread worker = new Thread(new Worker());
+        worker.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +28,10 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (isShutDown) {
+            throw new RejectedExecutionException();
+        }
+        workerTasks.add(command);
     }
 
     /**
@@ -25,6 +39,7 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        isShutDown = true;
     }
 
     /**
@@ -32,6 +47,21 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
-
+        isShutDown = true;
+        Thread.currentThread().interrupt();
     }
+
+    private class Worker implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    workerTasks.take().run();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
