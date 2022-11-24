8dd043df40faa1b2cc571d20573fba1de9eaa070
beatHunteRcode <nikitasherepa@gmail.com>
2022-11-24 18:00:24 +0300

    homework 7 done

--- src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
+++ src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
@@ -1,14 +1,13 @@
 package ru.mail.polis.homework.reflection;
 
-import ru.mail.polis.homework.reflection.objects.easy.Easy;
-
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Необходимо реализовать метод reflectiveToString, который для произвольного объекта
@@ -52,8 +51,68 @@ import java.util.stream.Stream;
  */
 public class ReflectionToStringHelper {
 
+    private static List<Field> fields = new ArrayList<>();
+
     public static String reflectiveToString(Object object) {
-        // TODO: implement
-        return null;
+        if (object == null) {
+            return "null";
+        }
+        StringBuilder sb = new StringBuilder();
+        fields = filterAndSortFields(object.getClass().getDeclaredFields());
+        checkForSuperclass(object.getClass());
+        sb.append("{");
+        if (fields.size() > 0) {
+            try {
+                for (Field field : fields) {
+                    sb.append(field.getName()).append(": ");
+                    field.setAccessible(true);
+                    Object value = field.get(object);
+                    if (value != null) {
+                        if (value.getClass().isArray()) {
+                            sb.append(Arrays.toString(convertToObjectArray(value)));
+                        } else {
+                            sb.append(field.get(object));
+                        }
+                    } else {
+                        sb.append("null");
+                    }
+                    sb.append(", ");
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } finally {
+                sb.replace(sb.length() - 2, sb.length(), "");
+            }
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+    private static List<Field> filterAndSortFields(Field[] fields) {
+        return Arrays.stream(fields).filter(field -> !Modifier.isStatic(field.getModifiers())
+                        && !field.isAnnotationPresent(SkipField.class))
+                .sorted(Comparator.comparing(Field::getName)).collect(Collectors.toList());
+    }
+
+    private static void checkForSuperclass(Class clazz) {
+        if (clazz.getSuperclass() != Object.class) {
+            List<Field> superClassFields = filterAndSortFields(clazz.getSuperclass().getDeclaredFields());
+            fields.addAll(superClassFields);
+            checkForSuperclass(clazz.getSuperclass());
+        }
+    }
+
+    private static Object[] convertToObjectArray(Object obj) {
+        Class arrType = obj.getClass().getComponentType();
+        if (arrType.isPrimitive()) {
+            List<Object> primitiveArr = new ArrayList<>();
+            int length = Array.getLength(obj);
+            for (int i = 0; i < length; i++) {
+                primitiveArr.add(Array.get(obj, i));
+            }
+            return primitiveArr.toArray();
+        } else {
+            return (Object[]) obj;
+        }
     }
 }
