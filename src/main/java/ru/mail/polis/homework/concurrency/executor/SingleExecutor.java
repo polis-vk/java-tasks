f54f5713d5833ec78e9bb77cf7a3bfb1ed068590
radkoff26 <radko.slava@mail.ru>
2022-11-24 17:46:00 +0300

    homework 8: SingleExecutor is done

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,24 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.Queue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
+    private final Worker worker;
+    private final Queue<Runnable> queue = new LinkedBlockingQueue<>();
 
+    public SingleExecutor() {
+        this.worker = new Worker();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +26,16 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (!worker.isWorking()) {
+            throw new RejectedExecutionException();
+        }
+        if (command == null) {
+            throw new NullPointerException();
+        }
+        if (!worker.isAlive()) {
+            worker.start();
+        }
+        queue.add(command);
     }
 
     /**
@@ -25,6 +43,9 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        if (worker.isWorking()) {
+            worker.shutdown();
+        }
     }
 
     /**
@@ -32,6 +53,33 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
+        if (worker.isWorking()) {
+            worker.shutdown();
+            worker.interrupt();
+        }
+    }
 
+    private class Worker extends Thread {
+        private boolean isWorking = true;
+
+        @Override
+        public void run() {
+            while (isWorking || !queue.isEmpty()) {
+                if (!queue.isEmpty()) {
+                    if (isInterrupted()) {
+                        return;
+                    }
+                    queue.poll().run();
+                }
+            }
+        }
+
+        public void shutdown() {
+            isWorking = false;
+        }
+
+        private boolean isWorking() {
+            return isWorking;
+        }
     }
 }
