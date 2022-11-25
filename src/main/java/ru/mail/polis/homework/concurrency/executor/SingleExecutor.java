a27a7a77c68c41e66c0dd4bc010465d63e8877dd
osokindm <osokin34@gmail.com>
2022-11-25 22:45:14 +0300

    SingleExecutor is ready

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,31 @@
 package ru.mail.polis.homework.concurrency.executor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
 
+    private static final int RUNNING = 0;
+    private static final int SHUTDOWN = 1;
+    private static final int STOP = 2;
+    LinkedBlockingQueue<Runnable> workQueue;
+    private int currentState;
+    private final Thread thread;
+
+    public SingleExecutor() {
+        workQueue = new LinkedBlockingQueue<>();
+        currentState = RUNNING;
+        thread = new Thread(new TaskWorker());
+        thread.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +33,11 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (currentState == RUNNING) {
+            workQueue.offer(command);
+        } else {
+            throw new RejectedExecutionException();
+        }
     }
 
     /**
@@ -25,6 +45,7 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        currentState = SHUTDOWN;
     }
 
     /**
@@ -32,6 +53,21 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
+        if (thread != null && !thread.isInterrupted()) {
+            thread.interrupt();
+        }
+        currentState = STOP;
+    }
 
+    private final class TaskWorker implements Runnable {
+        @Override
+        public void run() {
+            while (currentState == RUNNING || !thread.isInterrupted()) {
+                Runnable nextTask = workQueue.poll();
+                if (nextTask != null) {
+                    nextTask.run();
+                }
+            }
+        }
     }
 }
