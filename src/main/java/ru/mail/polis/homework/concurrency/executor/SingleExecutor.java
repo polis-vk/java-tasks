4e5e017ddf276541cf38bca9b36f4879b210962a
Queenore <atcheschev@yandex.ru>
2022-11-26 17:05:50 +0300

    SingleExecutor.java done

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,32 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.Executor;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
 
+    private final Thread singleThread;
+    private final ConcurrentLinkedDeque<Runnable> commandDeque = new ConcurrentLinkedDeque<>();
+    private volatile boolean isShutDown = false;
+
+    public SingleExecutor() {
+        this.singleThread = new Thread(() -> {
+            while (true) {
+                if (!commandDeque.isEmpty()) {
+                    commandDeque.pollLast().run();
+                }
+            }
+        });
+        singleThread.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +34,10 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (isShutDown) {
+            throw new RejectedExecutionException();
+        }
+        commandDeque.addFirst(command);
     }
 
     /**
@@ -25,6 +45,7 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        isShutDown = true;
     }
 
     /**
@@ -32,6 +53,7 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
-
+        isShutDown = true;
+        singleThread.interrupt();
     }
 }
