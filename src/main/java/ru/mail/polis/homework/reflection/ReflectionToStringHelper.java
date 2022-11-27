0f32dd924655b9fe425977462210d840484459bc
Egorka <trololo221@bk.ru>
2022-11-27 16:12:48 +0300

    delete arrayList

--- src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
+++ src/main/java/ru/mail/polis/homework/reflection/ReflectionToStringHelper.java
@@ -8,40 +8,40 @@ import java.util.*;
 /**
  * Необходимо реализовать метод reflectiveToString, который для произвольного объекта
  * возвращает его строковое описание в формате:
- *
+ * <p>
  * {field_1: value_1, field_2: value_2, ..., field_n: value_n}
- *
+ * <p>
  * где field_i - имя поля
- *     value_i - его строковое представление (String.valueOf),
- *               за исключением массивов, для которых value формируется как:
- *               [element_1, element_2, ..., element_m]
- *                 где element_i - строковое представление элемента (String.valueOf)
- *                 элементы должны идти в том же порядке, что и в массиве.
- *
+ * value_i - его строковое представление (String.valueOf),
+ * за исключением массивов, для которых value формируется как:
+ * [element_1, element_2, ..., element_m]
+ * где element_i - строковое представление элемента (String.valueOf)
+ * элементы должны идти в том же порядке, что и в массиве.
+ * <p>
  * Все null'ы следует представлять строкой "null".
- *
+ * <p>
  * Порядок полей
  * Сначала следует перечислить в алфавитном порядке поля, объявленные непосредственно в классе объекта,
  * потом в алфавитном порядке поля объявленные в родительском классе и так далее по иерархии наследования.
  * Примеры можно посмотреть в тестах.
- *
+ * <p>
  * Какие поля выводить
  * Необходимо включать только нестатические поля. Также нужно пропускать поля, помеченные аннотацией @SkipField
- *
+ * <p>
  * Упрощения
  * Чтобы не усложнять задание, предполагаем, что нет циклических ссылок, inner классов, и transient полей
- *
+ * <p>
  * Реализация
  * В пакете ru.mail.polis.homework.reflection можно редактировать только этот файл
  * или добавлять новые (не рекомендуется, т.к. решение вполне умещается тут в несколько методов).
  * Редактировать остальные файлы нельзя.
- *
+ * <p>
  * Баллы
  * В задании 3 уровня сложности, для каждого свой набор тестов:
- *   Easy - простой класс, нет наследования, массивов, статических полей, аннотации SkipField (4 балла)
- *   Easy + Medium - нет наследования, массивов, но есть статические поля и поля с аннотацией SkipField (6 баллов)
- *   Easy + Medium + Hard - нужно реализовать все требования задания (10 баллов)
- *
+ * Easy - простой класс, нет наследования, массивов, статических полей, аннотации SkipField (4 балла)
+ * Easy + Medium - нет наследования, массивов, но есть статические поля и поля с аннотацией SkipField (6 баллов)
+ * Easy + Medium + Hard - нужно реализовать все требования задания (10 баллов)
+ * <p>
  * Итого, по заданию можно набрать 10 баллов
  * Баллы могут снижаться за неэффективный или неаккуратный код
  */
@@ -54,38 +54,57 @@ public class ReflectionToStringHelper {
             return NULL_RESULT;
         }
 
-        List<String> result = new ArrayList<>();
+        StringBuilder resultString = new StringBuilder("{");
         try {
             List<Field> allFields = getAllFields(object);
             boolean isStaticFld;
             boolean isSkip;
             boolean isArray;
-            StringBuilder currentElement = new StringBuilder();
+            boolean isAccess;
+            Field currentFld;
 
-            for (Field fld : allFields) {
-                fld.setAccessible(true);
-                isStaticFld = Modifier.isStatic(fld.getModifiers());
-                isSkip = fld.isAnnotationPresent(SkipField.class);
-                isArray = fld.getType().isArray();
+            for (int i = 0; i < allFields.size(); i++) {
+                currentFld = allFields.get(i);
+                isStaticFld = Modifier.isStatic(currentFld.getModifiers());
+                isSkip = currentFld.isAnnotationPresent(SkipField.class);
+                isArray = currentFld.getType().isArray();
 
-                if (!isStaticFld && !isSkip) {
-                    currentElement.append(fld.getName()).append(": ");
-                    if (fld.get(object) == null) {
-                        result.add(currentElement.append("null").toString());
-                    } else if (isArray) {
-                        result.add(currentElement.append(arrayToString(fld.get(object))).toString());
-                    } else {
-                        result.add(currentElement.append(fld.get(object)).toString());
-                    }
+                if (isStaticFld || isSkip) {
+                    continue;
                 }
-                fld.setAccessible(false);
-                currentElement.setLength(0);
+
+                isAccess = currentFld.canAccess(object);
+                if (!isAccess) {
+                    currentFld.setAccessible(true);
+                }
+
+                resultString.append(currentFld.getName()).append(": ");
+
+                if (currentFld.get(object) == null) {
+                    resultString.append("null");
+                } else if (isArray) {
+                    resultString.append(arrayToString(currentFld.get(object)));
+                } else {
+                    resultString.append(currentFld.get(object));
+                }
+
+                if (i != allFields.size() - 1) {
+                    resultString.append(", ");
+                }
+
+                if (!isAccess) {
+                    currentFld.setAccessible(false);
+                }
             }
-        }
-        catch (Throwable e) {
+        } catch (Throwable e) {
             System.err.println(e);
         }
-        return new StringBuilder(String.join(", ", result)).insert(0, "{").append("}").toString();
+
+        if (resultString.charAt(resultString.length() - 1) == ' ') {
+            resultString.delete(resultString.length() - 2, resultString.length());
+        }
+
+        return resultString.append("}").toString();
     }
 
     private static List<Field> getAllFields(Object object) {
@@ -113,11 +132,14 @@ public class ReflectionToStringHelper {
             return "[]";
         }
 
-        List<String> result = new ArrayList<>();
+        StringBuilder result = new StringBuilder("[");
 
         for (int i = 0; i < arrayLength; i++) {
-            result.add(String.valueOf(Array.get(array, i)));
+            result.append(Array.get(array, i));
+            if (i != arrayLength - 1) {
+                result.append(", ");
+            }
         }
-        return new StringBuilder(String.join(", ", result)).insert(0, "[").append("]").toString();
+        return result.append("]").toString();
     }
 }
