e82cd393970aa0260aad97acf611251f838b9c90
Coo3n <Khlamov_ilya@mail.ru>
2022-11-28 01:49:53 +0300

    add PR-8

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,31 +1,74 @@
 package ru.mail.polis.homework.concurrency.executor;
 
-import java.util.concurrent.Executor;
+import java.util.ArrayList;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
  * Ленивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 баллов
  */
 public class SimpleExecutor implements Executor {
+    private BlockingQueue<Runnable> tasksQueue;
+    private ArrayList<Thread> threadsArray;
+    private AtomicInteger cntFreeThreads;
+    private volatile boolean isShutdownThreads;
+    private int maxThreadCount;
 
     public SimpleExecutor(int maxThreadCount) {
-
+        tasksQueue = new LinkedBlockingQueue<>();
+        threadsArray = new ArrayList<>(maxThreadCount);
+        cntFreeThreads = new AtomicInteger(0);
+        isShutdownThreads = false;
+        this.maxThreadCount = maxThreadCount;
     }
 
     /**
      * Ставит задачу в очередь на исполнение, если надо -- создает новый поток.
      * 8 баллов
      */
+
     @Override
     public void execute(Runnable command) {
+        if (command == null) {
+            return;
+        }
 
+        if (isShutdownThreads) {
+            throw new RejectedExecutionException();
+        }
+
+        synchronized (threadsArray) {
+            if (threadsArray.size() < maxThreadCount && cntFreeThreads.compareAndSet(0, 0)) {
+                Thread newThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (!isShutdownThreads || !tasksQueue.isEmpty()) {
+                            try {
+                                cntFreeThreads.incrementAndGet();
+                                Runnable currentTask = tasksQueue.take();
+                                cntFreeThreads.decrementAndGet();
+                                currentTask.run();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+
+                newThread.start();
+                threadsArray.add(newThread);
+            }
+        }
+
+        tasksQueue.offer(command);
     }
 
     /**
@@ -33,6 +76,7 @@ public class SimpleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        isShutdownThreads = true;
     }
 
     /**
@@ -40,13 +84,18 @@ public class SimpleExecutor implements Executor {
      * 1 балла за метод
      */
     public void shutdownNow() {
-
+        isShutdownThreads = true;
+        synchronized (threadsArray) {
+            for (Thread currentThread : threadsArray) {
+                currentThread.interrupt();
+            }
+        }
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        return threadsArray.size();
     }
 }
--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,35 @@
 package ru.mail.polis.homework.concurrency.executor;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.*;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 баллов
  */
 public class SingleExecutor implements Executor {
+    private BlockingQueue<Runnable> tasks;
+    private final Thread worker;
+    private volatile boolean isShutdownThreads;
 
+    public SingleExecutor() {
+        tasks = new LinkedBlockingQueue<>();
+        isShutdownThreads = false;
+
+        this.worker = new Thread(() -> {
+            while (!isShutdownThreads || !tasks.isEmpty()) {
+                try {
+                    tasks.take().run();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        worker.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +37,15 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (command == null) {
+            return;
+        }
+
+        if (isShutdownThreads) {
+            throw new RejectedExecutionException();
+        }
+
+        tasks.add(command);
     }
 
     /**
@@ -25,6 +53,7 @@ public class SingleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
+        isShutdownThreads = true;
     }
 
     /**
@@ -32,6 +61,7 @@ public class SingleExecutor implements Executor {
      * 2 балла за метод
      */
     public void shutdownNow() {
-
+        isShutdownThreads = true;
+        worker.interrupt();
     }
 }
