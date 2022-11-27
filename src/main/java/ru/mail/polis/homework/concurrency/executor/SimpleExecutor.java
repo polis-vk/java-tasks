5901c5f20c898bfce573f0b3fce575caac429bd1
Queenore <atcheschev@yandex.ru>
2022-11-27 15:50:22 +0300

    SimpleExecutor.java 50/50

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,22 +1,57 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
  * Ленивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
+ * <p>
  * Max 10 тугриков
  */
 public class SimpleExecutor implements Executor {
 
-    public SimpleExecutor(int maxThreadCount) {
+    private final int maxThreadCount;
+    private boolean isShutDown = false;
+    private boolean isShutDownNow = false;
+    private final AtomicInteger readyThreadsCount = new AtomicInteger(0);
+    private final ConcurrentLinkedDeque<Runnable> commandDeque = new ConcurrentLinkedDeque<>();
+    private final CopyOnWriteArrayList<SimpleThread> threadPool = new CopyOnWriteArrayList<>();
 
+    public SimpleExecutor(int maxThreadCount) {
+        this.maxThreadCount = maxThreadCount;
+    }
+
+    private class SimpleThread extends Thread {
+        public SimpleThread() {
+            readyThreadsCount.incrementAndGet();
+            start();
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                if (!commandDeque.isEmpty()) {
+                    readyThreadsCount.decrementAndGet();
+                    try {
+                        commandDeque.pollLast().run();
+                    } catch (NullPointerException ignored) {
+                    }
+                    readyThreadsCount.incrementAndGet();
+                }
+                if (isShutDownNow) {
+                    this.interrupt();
+                }
+            }
+        }
     }
 
     /**
@@ -25,7 +60,15 @@ public class SimpleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
-
+        if (isShutDown || isShutDownNow) {
+            throw new RejectedExecutionException();
+        }
+        commandDeque.addFirst(command);
+        synchronized (this) {
+            if (readyThreadsCount.get() == 0 && getLiveThreadsCount() < maxThreadCount) {
+                threadPool.add(new SimpleThread());
+            }
+        }
     }
 
     /**
@@ -33,6 +76,7 @@ public class SimpleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        isShutDown = true;
     }
 
     /**
@@ -40,13 +84,13 @@ public class SimpleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdownNow() {
-
+        isShutDownNow = true;
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        return threadPool.size();
     }
 }
