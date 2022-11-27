5ce0b78fbd315afcf1da59c3c4903edddab12b1a
AlexAndrovSPBSTU <AleksandrovAlexey0929@yandex.ru>
2022-11-27 22:35:57 +0300

    SingleExecutor is done

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,23 +1,51 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
+    private final Thread thread;
+    private final BlockingQueue<Runnable> commands;
+    private volatile boolean isShutDown;
 
-
+    public SingleExecutor() {
+        this.isShutDown = false;
+        this.commands = new LinkedBlockingQueue<>();
+        this.thread = new Thread(() -> {
+            while (!isShutDown || !commands.isEmpty()) {
+                try {
+                    commands.take().run();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread.start();
+    }
     /**
      * Метод ставит задачу в очередь на исполнение.
      * 3 тугрика за метод
      */
     @Override
     public void execute(Runnable command) {
+        if (!isShutDown) {
+            try {
+                commands.put(command);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            throw new RejectedExecutionException();
+        }
     }
 
     /**
@@ -25,6 +53,7 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        isShutDown = true;
     }
 
     /**
@@ -32,6 +61,7 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
-
+        isShutDown = true;
+        thread.interrupt();
     }
 }
