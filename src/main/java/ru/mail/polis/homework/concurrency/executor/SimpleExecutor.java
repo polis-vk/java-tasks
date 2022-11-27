c16c2eff0320d0cf8e48b0eea0b0d74afc800eed
Dmitrij_Tyapuev <dmitrij.typuev@gmail.com>
2022-11-27 20:08:10 +0300

    Homework 8 done

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,6 +1,11 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.Vector;
 import java.util.concurrent.Executor;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
@@ -11,42 +16,84 @@ import java.util.concurrent.Executor;
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
  *
- * Max 10 баллов
+ * Max 10 тугриков
  */
 public class SimpleExecutor implements Executor {
+    private final AtomicInteger vacantThreadCount;
+    private final LinkedBlockingQueue<Runnable> queue;
+    private final Vector<TreadOfTreadPool> threadPool;
+    private final int maxThreadCount;
+    private volatile boolean isNotShutDown;
 
     public SimpleExecutor(int maxThreadCount) {
-
+        this.maxThreadCount = maxThreadCount;
+        queue = new LinkedBlockingQueue<>();
+        threadPool = new Vector<>(maxThreadCount);
+        isNotShutDown = true;
+        vacantThreadCount = new AtomicInteger(0);
     }
 
     /**
      * Ставит задачу в очередь на исполнение, если надо -- создает новый поток.
-     * 8 баллов
+     * 8 тугриков
      */
     @Override
     public void execute(Runnable command) {
+        if (!isNotShutDown) {
+            throw new RejectedExecutionException();
+        }
+        queue.add(command);
+        synchronized (vacantThreadCount) {
+            if (vacantThreadCount.get() == 0 && threadPool.size() < maxThreadCount) {
+                threadPool.add(new TreadOfTreadPool());
+            }
+        }
+    }
 
+    private class TreadOfTreadPool extends Thread {
+        TreadOfTreadPool() {
+            this.start();
+        }
+
+        @Override
+        public void run() {
+            Runnable command;
+            while (isNotShutDown || !queue.isEmpty()) {
+                vacantThreadCount.incrementAndGet();
+                try {
+                    command = queue.take();
+                    vacantThreadCount.decrementAndGet();
+                    command.run();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     /**
      * Дает текущим задачам выполниться. Добавление новых - бросает RejectedExecutionException
-     * 1 балл за метод
+     * 1 тугрик за метод
      */
     public void shutdown() {
+        isNotShutDown = false;
     }
 
     /**
      * Прерывает текущие задачи. При добавлении новых - бросает RejectedExecutionException
-     * 1 балла за метод
+     * 1 тугрик за метод
      */
     public void shutdownNow() {
-
+        shutdown();
+        for (TreadOfTreadPool tread : threadPool) {
+            tread.interrupt();
+        }
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        return threadPool.size();
     }
 }
--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,37 +1,68 @@
 package ru.mail.polis.homework.concurrency.executor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
  *
  * Задачи должны выполняться в порядке FIFO
  *
- * Max 6 баллов
+ * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
-
-
-    /**
-     * Метод ставит задачу в очередь на исполнение.
-     * 3 балла за метод
-     */
-    @Override
-    public void execute(Runnable command) {
+    private final LinkedBlockingQueue<Runnable> queue;
+    private volatile boolean isNotShutDown;
+    private final EternalTread tread;
+    SingleExecutor() {
+        queue = new LinkedBlockingQueue<>();
+        isNotShutDown = true;
+        tread = new EternalTread();
+        tread.start();
     }
 
     /**
+     * Метод ставит задачу в очередь на исполнение.
+     * 3 тугрика за метод
+     */
+    @Override
+    public void execute(Runnable command) {
+        if (!isNotShutDown) {
+            throw new RejectedExecutionException();
+        }
+        queue.add(command);
+    }
+    private class EternalTread extends Thread {
+        @Override
+        public void run() {
+            Runnable command;
+            while (isNotShutDown || !queue.isEmpty()) {
+                try {
+                    command = queue.take();
+                    command.run();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+
+    /**
      * Дает текущим задачам выполниться. Добавление новых - бросает RejectedExecutionException
-     * 1 балл за метод
+     * 1 тугрик за метод
      */
     public void shutdown() {
+        isNotShutDown = false;
     }
 
     /**
      * Прерывает текущие задачи. При добавлении новых - бросает RejectedExecutionException
-     * 2 балла за метод
+     * 2 тугрика за метод
      */
     public void shutdownNow() {
-
+        shutdown();
+        tread.interrupt();
     }
 }
