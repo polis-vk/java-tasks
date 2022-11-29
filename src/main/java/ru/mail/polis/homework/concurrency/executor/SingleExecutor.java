fbc6b07f6f4a4719873f58d1cbea3b8fd6df8da6
Kirill06344 <kirill12093@mail.ru>
2022-11-29 14:24:22 +0300

    add SingleExecutor

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,16 +1,29 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
- *
+ * <p>
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
+    private final Thread thread;
+    private final BlockingQueue<Runnable> taskQueue;
+    private volatile boolean isShutdown;
 
+
+    public SingleExecutor() {
+        taskQueue = new LinkedBlockingQueue<>();
+        thread = new SingleThread();
+        isShutdown = false;
+        thread.start();
+    }
 
     /**
      * Метод ставит задачу в очередь на исполнение.
@@ -18,6 +31,15 @@ public class SingleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (isShutdown) {
+            throw new RejectedExecutionException();
+        }
+
+        try {
+            taskQueue.put(command);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -25,6 +47,7 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        isShutdown = true;
     }
 
     /**
@@ -32,6 +55,20 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
+        shutdown();
+        thread.interrupt();
+    }
 
+    private class SingleThread extends Thread {
+        @Override
+        public void run() {
+            while (!taskQueue.isEmpty() || !isShutdown) {
+                try {
+                    taskQueue.take().run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
