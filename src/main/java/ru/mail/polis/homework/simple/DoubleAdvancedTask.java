d2a82eb8a995b788ab51aca12b830df1cf48ff48
Дягай Александра <dyagaay@gmail.com>
2022-11-29 18:25:56 +0300

    Исправлены некоторые замечания

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -1,4 +1,5 @@
 package ru.mail.polis.homework.simple;
+import java.util.Arrays;
 
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
@@ -17,10 +18,45 @@ public class DoubleAdvancedTask {
      * Пример: (1, -4, -7, 10) -> "-2.0, 1.0, 5.0"
      */
     public static String equation(int a, int b, int c, int d) {
-        double x1 = 0;
-        double x2 = 0;
-        double x3 = 0;
-        return x1 + ", " + x2 + ", " + x3;
+        //Способ: Тригонометрическая формула Виета
+        //Решение: https://math.fandom.com/ru/wiki/Тригонометрическая_формула_Виета
+        double[] roots = new double[3];
+
+        double A = (double) b / a;
+        double B = (double) c / a;
+        double C = (double) d / a;
+
+        double Q = (Math.pow(A, 2) - 3 * B) / 9;
+        double R = (2 * Math.pow(A, 3) - 9 * A * B + 27 * C) / 54;
+        double S = Math.pow(Q, 3) - Math.pow(R, 2);
+
+        if (S > 0) {
+            double f = Math.acos(R / Math.sqrt(Math.pow(Q, 3))) / 3;
+            roots[0] = -2 * Math.sqrt(Q) * Math.cos(f) - A / 3;
+            roots[1] = -2 * Math.sqrt(Q) * Math.cos(f + 2 * Math.PI / 3) - A / 3;
+            roots[2] = -2 * Math.sqrt(Q) * Math.cos(f - 2 * Math.PI / 3) - A / 3;
+        } else if (S < 0) {
+            double f = getArch(Math.abs(R / Math.sqrt(Math.abs(Math.pow(Q, 3)))));
+            double T = Math.signum(R) * Math.sqrt(Math.abs(Q)) * getCh(f);
+            roots[0] = -2 * T - A / 3;
+        } else {
+            double T = Math.signum(R) * Math.sqrt(Math.abs(Q));
+            roots[0] = -2 * T - A / 3;
+            roots[1] = T - A / 3;
+        }
+
+        Arrays.sort(roots);
+        return roots[2] + ", " + roots[1] + ", " + roots[0];
+    }
+
+    //Рассчитывает ареакосинус
+    private static double getArch(double z) {
+        return Math.log(z + Math.sqrt(Math.pow(z, 2) - 1));
+    }
+
+    //Рассчитывает гиперболический косинус
+    private static double getCh(double z) {
+        return (Math.pow(Math.E, z) + Math.pow(Math.E, -z)) / 2;
     }
 
     /**
@@ -29,7 +65,10 @@ public class DoubleAdvancedTask {
      * (0, 1, 0, 5) -> 4
      */
     public static float length(double a1, double b1, double a2, double b2) {
-        return 0;
+        if (Double.compare(a1, a2) != 0) {
+            return 0;
+        }
+        return (float) (Math.abs(b1 - b2) / Math.sqrt(a1 * a2 + 1));
     }
 
     /**
@@ -44,6 +83,22 @@ public class DoubleAdvancedTask {
                                          int x2, int y2, int z2,
                                          int x3, int y3, int z3,
                                          int x4, int y4) {
-        return 0;
+        //Задача решается через условие принадлежности четырех точек одной плоскости
+        //Координаты первого вектора
+        double xv1 = x2 - x1;
+        double yv1 = y2 - y1;
+        double zv1 = z2 - z1;
+
+        //Координаты второго вектора
+        double xv2 = x3 - x1;
+        double yv2 = y3 - y1;
+        double zv2 = z3 - z1;
+
+        //Координаты x и y третьего вектора
+        double xv3 = x4 - x1;
+        double yv3 = y4 - y1;
+
+        return z1 + (-xv3 * yv1 * zv2 - xv2 * yv3 * zv1 +
+                xv1 * yv3 * zv2 + xv3 * zv1 * yv2) / (xv1 * yv2 - yv1 * xv2);
     }
 }
