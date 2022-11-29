c5e826bde2dea08a19d0f16478f6704af4f68ef9
CHEYSOFF <slobberyblanket@gmail.com>
2022-11-29 21:55:18 +0300

    DoubleAdvancedTask works

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -14,6 +14,7 @@ public class DoubleAdvancedTask {
 //    public static void main(String[]args){
 //        System.out.println(equation(1, -4, -7, 10));
 //    }
+
     /**
      * Вывести три корня кубического уравнения через запятую: a * x ^ 3 + b * x ^ 2 + c * x + d = 0;
      * Вывод менять не нужно, надо только посчитать x1, x2 и x3, где x1 >= x2 >= x3
@@ -36,9 +37,9 @@ public class DoubleAdvancedTask {
         final double s = Math.pow(q, 3) - Math.pow(r, 2);
         if (s > 0) {
             double phi = Math.acos(r / (Math.sqrt(q * q * q))) / 3;
-            results[0] = - 2 * Math.sqrt(q) * Math.cos(phi) - a_new / 3;
-            results[1] = - 2 * Math.sqrt(q) * Math.cos(phi + 2 * Math.PI / 3) - a_new / 3;
-            results[2] = - 2 * Math.sqrt(q) * Math.cos(phi - 2 * Math.PI / 3) - a_new / 3;
+            results[0] = -2 * Math.sqrt(q) * Math.cos(phi) - a_new / 3;
+            results[1] = -2 * Math.sqrt(q) * Math.cos(phi + 2 * Math.PI / 3) - a_new / 3;
+            results[2] = -2 * Math.sqrt(q) * Math.cos(phi - 2 * Math.PI / 3) - a_new / 3;
         } else if (s < 0) {
             int sign = Integer.signum((int) r);
             double temp = Math.sqrt(Math.pow(r, 2) - Math.pow(q, 3));
@@ -58,13 +59,13 @@ public class DoubleAdvancedTask {
      * (0, 1, 0, 5) -> 4
      */
     public static float length(double a1, double b1, double a2, double b2) {
-        if(a1 != a2) {
+        if (a1 != a2) {
             return 0;
         }
         double x1 = 0;
         double y1 = b1;
-        float ans = (float) ((-y1 * 1 + b2) / Math.sqrt(a2 * a2 + 1));
-        if(ans > 0) {
+        float ans = (float) ((x1 * a2 - y1 * 1 + b2) / Math.sqrt(a2 * a2 + 1));
+        if (ans > 0) {
             return ans;
         }
         return -ans;
@@ -77,11 +78,26 @@ public class DoubleAdvancedTask {
      * 1, 1, 1,
      * 10, 100, 1,
      * 235, -5) -> 1
+     * <p>
+     * Ax1 + By1 + Cz1 + D = 0
+     * Ax2 + By2 + Cz2 + D = 0
+     * Ax3 + By3 + Cz3 + D = 0
      */
     public static double surfaceFunction(int x1, int y1, int z1,
                                          int x2, int y2, int z2,
                                          int x3, int y3, int z3,
                                          int x4, int y4) {
-        return 0;
+        int matrix[][] = {
+                {x2 - x1, y2 - y1, z2 - z1},
+                {x3 - x1, y3 - y1, z3 - z1}
+        };
+
+        int a = matrix[0][1] * matrix[1][2] - matrix[0][2] * matrix[1][1];
+        int b = - (matrix[0][0] * matrix[1][2] - matrix[0][2] * matrix[1][0]);
+        int c = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+        int d = -(a * x1 + b * y1 + c * z1);
+
+        return (double) -(a * x4 + b * y4 + d) / c;
+
     }
 }
