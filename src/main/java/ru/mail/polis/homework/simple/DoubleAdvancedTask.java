5b99759e226e20a2b292275e30906bf9dfcd58ef
CHEYSOFF <slobberyblanket@gmail.com>
2022-11-29 17:10:58 +0300

    equation and length working

--- src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleAdvancedTask.java
@@ -1,13 +1,19 @@
 package ru.mail.polis.homework.simple;
 
+import java.util.Arrays;
+
 /**
  * Возможно вам понадобится класс Math с его методами. Например, чтобы вычислить квадратный корень, достаточно написать
  * Math.sqrt(1.44)
  * Чтобы увидеть все методы класса Math, достаточно написать Math. и среда вам сама покажет возможные методы.
  * Для просмотра подробной документации по выбранному методу нажмите Ctrl + q
  */
-public class DoubleAdvancedTask {
 
+
+public class DoubleAdvancedTask {
+//    public static void main(String[]args){
+//        System.out.println(equation(1, -4, -7, 10));
+//    }
     /**
      * Вывести три корня кубического уравнения через запятую: a * x ^ 3 + b * x ^ 2 + c * x + d = 0;
      * Вывод менять не нужно, надо только посчитать x1, x2 и x3, где x1 >= x2 >= x3
@@ -16,11 +22,34 @@ public class DoubleAdvancedTask {
      * Если используете какой-то конкретный способ, напишите какой.
      * Пример: (1, -4, -7, 10) -> "-2.0, 1.0, 5.0"
      */
+
+//    Подстановка Виета
     public static String equation(int a, int b, int c, int d) {
-        double x1 = 0;
-        double x2 = 0;
-        double x3 = 0;
-        return x1 + ", " + x2 + ", " + x3;
+        double[] results = {0, 0, 0};
+
+        double a_new = (double) b / a;
+        double b_new = (double) c / a;
+        double c_new = (double) d / a;
+
+        final double q = (Math.pow(a_new, 2) - 3 * b_new) / 9;
+        final double r = ((2 * Math.pow(a_new, 3)) - (9 * a_new * b_new) + (27 * c_new)) / 54;
+        final double s = Math.pow(q, 3) - Math.pow(r, 2);
+        if (s > 0) {
+            double phi = Math.acos(r / (Math.sqrt(q * q * q))) / 3;
+            results[0] = - 2 * Math.sqrt(q) * Math.cos(phi) - a_new / 3;
+            results[1] = - 2 * Math.sqrt(q) * Math.cos(phi + 2 * Math.PI / 3) - a_new / 3;
+            results[2] = - 2 * Math.sqrt(q) * Math.cos(phi - 2 * Math.PI / 3) - a_new / 3;
+        } else if (s < 0) {
+            int sign = Integer.signum((int) r);
+            double temp = Math.sqrt(Math.pow(r, 2) - Math.pow(q, 3));
+            double a_tm = -1 * sign * Math.cbrt(Math.abs(r) + temp);
+            double b_tm = q / a_tm;
+            results[0] = (a_tm + b_tm) - a_new / 3;
+        }
+
+        Arrays.sort(results);
+
+        return results[2] + ", " + results[1] + ", " + results[0];
     }
 
     /**
@@ -29,7 +58,16 @@ public class DoubleAdvancedTask {
      * (0, 1, 0, 5) -> 4
      */
     public static float length(double a1, double b1, double a2, double b2) {
-        return 0;
+        if(a1 != a2) {
+            return 0;
+        }
+        double x1 = 0;
+        double y1 = b1;
+        float ans = (float) ((-y1 * 1 + b2) / Math.sqrt(a2 * a2 + 1));
+        if(ans > 0) {
+            return ans;
+        }
+        return -ans;
     }
 
     /**
