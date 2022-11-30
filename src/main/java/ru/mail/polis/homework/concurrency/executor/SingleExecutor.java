4118cd650c91959fc7cf409e4c81af913c94ba78
Ilya Viktorov <viktorov.ia@edu.spbstu.ru>
2022-12-01 00:20:26 +0300

    task SingleExecutor

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SingleExecutor.java
@@ -1,6 +1,9 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * Нужно сделать свой executor с одним вечным потоком. Пока не вызовут shutdown или shutdownNow
@@ -10,14 +13,26 @@ import java.util.concurrent.Executor;
  * Max 6 тугриков
  */
 public class SingleExecutor implements Executor {
+    private final BlockingQueue<Runnable> taskQueue = new LinkedBlockingQueue<>();
+    private final Thread thread = new SingleThread();
+    private volatile boolean closeQueue;
 
-
+    public SingleExecutor() {
+        thread.start();
+    }
     /**
      * Метод ставит задачу в очередь на исполнение.
      * 3 тугрика за метод
      */
     @Override
     public void execute(Runnable command) {
+        if (command == null) {
+            throw new IllegalArgumentException();
+        }
+        if (closeQueue) {
+            throw new RejectedExecutionException();
+        }
+        taskQueue.add(command);
     }
 
     /**
@@ -25,6 +40,7 @@ public class SingleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        closeQueue = true;
     }
 
     /**
@@ -32,6 +48,21 @@ public class SingleExecutor implements Executor {
      * 2 тугрика за метод
      */
     public void shutdownNow() {
+        shutdown();
+        thread.interrupt();
+    }
 
+    private class SingleThread extends Thread {
+
+        @Override
+        public void run() {
+            try {
+                while (!closeQueue || !taskQueue.isEmpty()) {
+                    taskQueue.take().run();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
