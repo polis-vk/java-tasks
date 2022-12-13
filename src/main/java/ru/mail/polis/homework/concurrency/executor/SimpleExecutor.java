245d4ad7d6c2a3c0baf3c194a1cfa06ecced0683
dasu <kirill12093@mail.ru>
2022-12-14 00:34:51 +0300

    add first version SimpleExecutor without blocking

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -1,6 +1,13 @@
 package ru.mail.polis.homework.concurrency.executor;
 
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
@@ -15,8 +22,16 @@ import java.util.concurrent.Executor;
  */
 public class SimpleExecutor implements Executor {
 
-    public SimpleExecutor(int maxThreadCount) {
+    BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
 
+    List<Thread> threads;
+    private final AtomicBoolean isActive = new AtomicBoolean(true);
+    private AtomicInteger freeThreadCount = new AtomicInteger(0);
+    private final int maxThreadCount;
+
+    public SimpleExecutor(int maxThreadCount) {
+        this.maxThreadCount = maxThreadCount;
+        threads = new CopyOnWriteArrayList<>();
     }
 
     /**
@@ -25,7 +40,17 @@ public class SimpleExecutor implements Executor {
      */
     @Override
     public void execute(Runnable command) {
+        if (!isActive.get()) {
+            throw new RejectedExecutionException();
+        }
 
+        if (freeThreadCount.intValue() == 0 && threads.size() < maxThreadCount) {
+            Thread thread = new SimpleThread();
+            threads.add(thread);
+            thread.start();
+        }
+
+        queue.add(command);
     }
 
     /**
@@ -33,6 +58,7 @@ public class SimpleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdown() {
+        isActive.set(false);
     }
 
     /**
@@ -40,13 +66,33 @@ public class SimpleExecutor implements Executor {
      * 1 тугрик за метод
      */
     public void shutdownNow() {
-
+        isActive.set(false);
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
     }
 
     /**
      * Должен возвращать количество созданных потоков.
      */
     public int getLiveThreadsCount() {
-        return 0;
+        return threads.size();
+    }
+
+    private class SimpleThread extends Thread {
+       @Override
+       public void run(){
+            freeThreadCount.incrementAndGet();
+            while (!Thread.currentThread().isInterrupted() && (!queue.isEmpty() || isActive.get())) {
+                try {
+                    Runnable task = queue.take();
+                    freeThreadCount.decrementAndGet();
+                    task.run();
+                    freeThreadCount.incrementAndGet();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+       }
     }
 }
