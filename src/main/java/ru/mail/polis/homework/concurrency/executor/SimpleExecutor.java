b79e60788d84ef240d4d10b64d9e86855cdb5804
Coo3n <Khlamov_ilya@mail.ru>
2022-12-19 13:57:51 +0300

    new version lab9

--- src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
+++ src/main/java/ru/mail/polis/homework/concurrency/executor/SimpleExecutor.java
@@ -5,24 +5,24 @@ import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Нужно сделать свой executor с ленивой инициализацией потоков до какого-то заданного предела.
  * Ленивая инициализация означает, что если вам приходит раз в 5 секунд задача, которую вы выполняете 2 секунды,
  * то вы создаете только один поток. Если приходит сразу 2 задачи - то два потока.  То есть, если приходит задача
  * и есть свободный запущенный поток - он берет задачу, если такого нет, то создается новый поток.
- *
+ * <p>
  * Задачи должны выполняться в порядке FIFO
  * Потоки после завершения выполнения задачи НЕ умирают, а ждут.
- *
- * Max 10 баллов
+ * <p>
+ * Max 10 тугриков
  */
 public class SimpleExecutor implements Executor {
     private final BlockingQueue<Runnable> tasksQueue = new LinkedBlockingQueue<>();
     private final List<Thread> threadsArray = new CopyOnWriteArrayList<>();
-    private final AtomicReference<ExecutorState> executorState = new AtomicReference<>(new ExecutorState(0, 0, false));
+    private volatile ExecutorState executorState = new ExecutorState(0, 0, false);
     private final int maxThreadCount;
+
     public SimpleExecutor(int maxThreadCount) {
         if (maxThreadCount <= 0) {
             throw new IllegalArgumentException();
@@ -42,12 +42,12 @@ public class SimpleExecutor implements Executor {
             return;
         }
 
-        if (executorState.get().isShutdownThreads.get()) {
+        if (executorState.isShutdownThreads.get()) {
             throw new RejectedExecutionException();
         }
 
-        if (executorState.get().sizeThreadList.get() < maxThreadCount && executorState.get().cntFreeThreads.compareAndSet(0, executorState.get().cntFreeThreads.get())) {
-            int currentSizeThreadList = executorState.get().sizeThreadList.incrementAndGet();
+        if (executorState.sizeThreadList.get() < maxThreadCount && executorState.cntFreeThreads.compareAndSet(0, executorState.cntFreeThreads.get())) {
+            int currentSizeThreadList = executorState.sizeThreadList.incrementAndGet();
             if (currentSizeThreadList <= maxThreadCount) {
                 Thread newThread = new SimpleThread();
                 newThread.start();
@@ -63,7 +63,7 @@ public class SimpleExecutor implements Executor {
      * 1 балл за метод
      */
     public void shutdown() {
-        executorState.set(new ExecutorState(executorState.get().cntFreeThreads.get(), executorState.get().sizeThreadList.get(), true));
+        executorState = new ExecutorState(executorState.cntFreeThreads.get(), executorState.sizeThreadList.get(), true);
     }
 
     /**
@@ -72,15 +72,15 @@ public class SimpleExecutor implements Executor {
      */
     public void shutdownNow() {
         shutdown();
-        int currentThreadListSize = executorState.get().sizeThreadList.get();
+        int currentThreadListSize = executorState.sizeThreadList.get();
 
-        while (true) {
-            for (Thread currentThread : threadsArray) {
-                currentThread.interrupt();
-            }
+        for (Thread currentThread : threadsArray) {
+            currentThread.interrupt();
+        }
 
-            if (currentThreadListSize == executorState.get().sizeThreadList.get()) {
-                break;
+        if (currentThreadListSize != executorState.sizeThreadList.get()) {
+            for (int i = currentThreadListSize; i < executorState.sizeThreadList.get(); i++) {
+                threadsArray.get(i).interrupt();
             }
         }
     }
@@ -95,12 +95,12 @@ public class SimpleExecutor implements Executor {
     class SimpleThread extends Thread {
         @Override
         public void run() {
-            while ((executorState.get().isShutdownThreads.compareAndSet(false, executorState.get().isShutdownThreads.get()) || !tasksQueue.isEmpty())
+            while ((executorState.isShutdownThreads.compareAndSet(false, executorState.isShutdownThreads.get()) || !tasksQueue.isEmpty())
                     && !Thread.currentThread().isInterrupted()) {
                 try {
-                    executorState.get().cntFreeThreads.incrementAndGet();
+                    executorState.cntFreeThreads.incrementAndGet();
                     Runnable currentTask = tasksQueue.take();
-                    executorState.get().cntFreeThreads.decrementAndGet();
+                    executorState.cntFreeThreads.decrementAndGet();
                     currentTask.run();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -109,7 +109,7 @@ public class SimpleExecutor implements Executor {
         }
     }
 
-    class ExecutorState {
+    static class ExecutorState {
         private final AtomicInteger cntFreeThreads;
         private final AtomicInteger sizeThreadList;
         private final AtomicBoolean isShutdownThreads;
