bf6ec800ae3bcefc3e38b157e11ed1be98f13723
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-03-04 22:18:52 +0300

    Выполнить задание IntegerTask

--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -14,7 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -24,7 +28,15 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        int counter = 0;
+        if (bottom >= top && top < height)
+            return Integer.MAX_VALUE;
+        do {
+            height -= top;
+            height += bottom;
+            counter++;
+        } while (height > bottom);
+        return counter;
     }
 
     /**
@@ -32,7 +44,11 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+        int value = 0;
+        for (int i = 0; i < order - 1; i++) {
+            n = n / 10;
+        }
+        return Math.abs(n % 10);
     }
 
 
@@ -41,6 +57,10 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        if (n < 2) {
+            return 1;
+        } else {
+            return n * factorial((byte) (n - 1));
+        }
     }
 }
