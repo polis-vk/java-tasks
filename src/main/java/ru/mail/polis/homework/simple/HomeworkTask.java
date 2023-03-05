52a558d4c7e1a06d97f8cd57020792aacceae77f
Rudenman <rudenko.do@edu.spbstu.ru>
2023-03-05 05:52:12 +0300

    add not full homeworkTask

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.homework.simple;
 
+import java.awt.*;
 import java.util.function.ToDoubleFunction;
 
 public class HomeworkTask {
@@ -18,7 +19,26 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
+        byte numberOfdigits = 1; //количество цифр в числе
+
+        byte number = (byte) Math.abs(a % 10); //очередная цифра числа
+        byte order = 1; //номер очередной цифры справа налево
+
+        byte max = number; //наибольшая цифра
+        byte maxOrder = 1; //номер максимальной цифры
+
+        a /= 10;
+        while (a != 0) {
+            numberOfdigits++;
+            order++;
+            number = (byte) Math.abs(a % 10);
+            if (number >= max) {
+                max = number;
+                maxOrder = order;
+            }
+            a /= 10;
+        }
+        return (byte) (numberOfdigits + 1 - maxOrder);
     }
 
 
@@ -27,7 +47,10 @@ public class HomeworkTask {
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        //Уравнение прямой вида: y = ax + b
+        double a = (double) (y2 - y1) / (x2 - x1);
+        double b = y1 - a * x1;
+        return a * x3 + b;
     }
 
     /**
@@ -35,7 +58,25 @@ public class HomeworkTask {
      * четырехуголька ABCD.
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
-    }
+        //Уравнение прямой вида: y = ax + b
+        double a = (double) (y2 - y1) / (x2 - x1);
+        double b = y1 - a * x1;
 
+        double s1, s2;
+        s1 = Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2.0;
+        if (Double.compare(Math.abs(a * x3 + b - y3), 0.0) * Double.compare(Math.abs(a * x4 + b - y4), 0.0) != 1) {
+            s2 = Math.abs((x2 - x1) * (y4 - y1) - (x4 - x1) * (y2 - y1)) / 2.0;
+            return s1 + s2;
+        }
+
+        a = (double) (y3 - y2) / (x3 - x2);
+        b = y2 - a * x2;
+        if (Double.compare(Math.abs(a * x1 + b - y1), 0.0) * Double.compare(Math.abs(a * x4 + b - y4), 0.0) != 1) {
+            s2 = Math.abs((x2 - x3) * (y4 - y3) - (x4 - x3) * (y2 - y3)) / 2.0;
+            return s1 + s2;
+        }
+
+        s2 = Math.abs((x1 - x3) * (y4 - y3) - (x4 - x3) * (y1 - y3)) / 2.0;
+        return s1 + s2;
+    }
 }
