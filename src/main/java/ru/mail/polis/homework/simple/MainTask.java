be892cc7db0057c0dea48f8e4b00a49915df8334
YanaSpirchina <spirchina04@gmail.com>
2023-03-05 18:17:20 +0300

    Изменила метод в MainTask

--- src/main/java/ru/mail/polis/homework/simple/MainTask.java
+++ src/main/java/ru/mail/polis/homework/simple/MainTask.java
@@ -12,7 +12,7 @@ public class MainTask {
 //        System.out.println("It is work.");
 //    }
 
-//    public static void main(String[] args) {
-//        MainTask.main("");
-//    }
+    public static void main(String[] args) {
+        System.out.println("It is work.");
+    }
 }
