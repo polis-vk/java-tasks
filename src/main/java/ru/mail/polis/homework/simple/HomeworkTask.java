7a63d3414b71b5dd4f85f9b4b665c226abaa74c9
Chupina Anna <chupina.ayu@edu.spbstu.ru>
2023-03-05 11:54:46 +0300

    complited HomeworkTask

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -9,16 +9,33 @@ public class HomeworkTask {
      * Для того, что бы получить значение по Y для точки X, надо просто написать function.applyAsDouble(t)
      * Считаем, что функция определена на всем пространстве от a до b
      */
-    public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        return 0;
+    public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta)
+    {
+        double integral = 0.0;
+        for (double i = a; i < b; i += delta) {
+            integral += function.applyAsDouble(i) * delta;
+        }
+        return integral;
     }
 
     /**
      * Вывести номер максимальной цифры. Счет начинается слева направо,
      * выводим номер первой максимальной цифры (если их несколько)
      */
-    public static byte maxNumber(long a) {
-        return 0;
+    public static byte maxNumber(long a)
+    {
+        byte maxDecimal = 0;
+        byte maxNumber = 0;
+        byte i = 0;
+        while (a != 0) {
+            if (Math.abs(a) % 10 >= maxDecimal) {
+                maxDecimal = (byte)(Math.abs(a) % 10);
+                maxNumber = i;
+            }
+            a = Math.abs(a) / 10;
+            i++;
+        }
+        return (byte)( i - maxNumber );
     }
 
 
@@ -26,16 +43,28 @@ public class HomeworkTask {
      * Даны две точки в пространстве (x1, y1) и (x2, y2). Вам нужно найти Y координату третьей точки (x3, y3),
      * которая находится на той же прямой что и первые две.
      */
-    public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+    public static double lineFunction(int x1, int y1, int x2, int y2, int x3)
+    {
+        // y = kx + b
+        double k, b, y3;
+        k = (double)(y2 - y1) / (x2 - x1);
+        b = y1 - k * x1;
+        y3 = k * x3 + b;
+        return y3;
     }
 
     /**
      * Даны 4 точки в пространстве A(x1, y1), B(x2, y2), C(x3, y3), D(x4, y4). Найдите площадь выпуклого
      * четырехуголька ABCD.
      */
-    public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+    public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+    {
+        //площадь как сумма двух треугольников
+        // S = 1/2 module((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1))
+        double S1, S2;
+        S1 = 0.5 * Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1));
+        S2 = 0.5 * Math.abs((x3 - x1) * (y4 - y1) - (x4 - x1) * (y3 - y1));
+        return S1 + S2;
     }
 
 }
