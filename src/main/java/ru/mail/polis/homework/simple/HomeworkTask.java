73ed3b06c129f8b6a95c4dd8d2c2a0f740b906b5
Artem Sheverdyaev <artem-sheverdyaev@mail.ru>
2023-03-07 03:05:08 +0300

    Sheverdyaev Artem

--- src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
+++ src/main/java/ru/mail/polis/homework/simple/HomeworkTask.java
@@ -10,7 +10,11 @@ public class HomeworkTask {
      * Считаем, что функция определена на всем пространстве от a до b
      */
     public static double calcIntegral(double a, double b, ToDoubleFunction<Double> function, double delta) {
-        return 0;
+        double result = 0;
+        for(double i = a; i <= b; i += delta){
+            result += function.applyAsDouble(i)*delta;
+        }
+        return result;
     }
 
     /**
@@ -18,7 +22,26 @@ public class HomeworkTask {
      * выводим номер первой максимальной цифры (если их несколько)
      */
     public static byte maxNumber(long a) {
-        return 0;
+        byte[] reverseNumbers = new byte[19];
+        byte sizeOfArray = 0;
+        do{
+            byte remains ;
+            remains = (byte) Math.abs(a % 10);
+            reverseNumbers[sizeOfArray] = remains;
+            a /= 10;
+            ++sizeOfArray;
+        }while(a!=0);
+
+        byte maxDigitNumber = 0;
+        byte maxDigit = reverseNumbers[0];
+        for(byte j = 1; j < sizeOfArray; ++j) {
+            if (reverseNumbers[j] >= maxDigit) {
+                maxDigitNumber = j;
+                maxDigit = reverseNumbers[j];
+            }
+        }
+
+        return (byte) Math.abs(maxDigitNumber - sizeOfArray);
     }
 
 
@@ -27,7 +50,9 @@ public class HomeworkTask {
      * которая находится на той же прямой что и первые две.
      */
     public static double lineFunction(int x1, int y1, int x2, int y2, int x3) {
-        return 0;
+        double k = (double)(y2 - y1)/(x2-x1);
+        double b = y1 - k*x1;
+        return k*x3 + b;
     }
 
     /**
@@ -35,7 +60,7 @@ public class HomeworkTask {
      * четырехуголька ABCD.
      */
     public static double square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        return 0;
+        return 0.5*Math.abs(x1*y2 + x2*y3 + x3*y4 + x4*y1 - x2*y1 - x3*y2 - x4*y3 - x1*y4);
     }
 
 }
--- src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
+++ src/main/java/ru/mail/polis/homework/simple/IntegerTask.java
@@ -14,7 +14,11 @@ public class IntegerTask {
      * Пример: (5) -> 15
      */
     public static int sum(int n) {
-        return 0;
+        int result = 0;
+        for(int i = 1; i <= n; ++i){
+            result +=i;
+        }
+        return result;
     }
 
     /**
@@ -24,7 +28,28 @@ public class IntegerTask {
      * Пример: (10, 3, 2) -> 8
      */
     public static int snake(int height, int top, int bottom) {
-        return 0;
+        int countOfDays = 0;
+        int riseInOneDay = top - bottom;
+        if(top >= height){
+            return 1;
+        }
+        if(riseInOneDay <= 0){
+            return Integer.MAX_VALUE;
+        }
+        else {
+            int position = 0;
+            while (position < height){
+                position += top;
+                ++countOfDays;
+                if(position >= height){
+                    return countOfDays;
+                }
+                else{
+                    position -= bottom;
+                }
+            }
+            return countOfDays;
+        }
     }
 
     /**
@@ -32,7 +57,12 @@ public class IntegerTask {
      * Пример: (454355, 3) -> 3
      */
     public static int kDecimal(int n, int order) {
-        return 0;
+
+        for(int i = 0; i < order - 1; ++i){
+            n /= 10;
+        }
+
+        return Math.abs(n % 10);
     }
 
 
@@ -41,6 +71,10 @@ public class IntegerTask {
      * Пример: (5) -> 120
      */
     public static long factorial(byte n) {
-        return 0;
+        long result = 1;
+        for(int i = 1; i <= n; ++i){
+            result *= i;
+        }
+        return result;
     }
 }
