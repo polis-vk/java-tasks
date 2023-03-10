e9d62d34169cebcc10e7d2162f7272512148b62c
Boris <Boris_siobko@mail.ru>
2023-03-10 20:18:29 +0300

    done StringTasks

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -16,6 +16,51 @@ public class StringTasks {
      * 6 тугриков
      */
     public static Number valueOf(String str) {
-        return null;
+
+        if (str == null || str.length() == 0 || str.charAt(str.length() - 1) == '-' || str.charAt(str.length() - 1) == 'e') {
+            return null;
+        }
+
+        StringBuilder returnValue = new StringBuilder();
+        int dotAmount = 0;
+        int eAmount = 0;
+
+        for (int i = 0; i < str.length() - 1; ++i) {
+
+            if (str.charAt(i) != '-' && str.charAt(i) != 'e' && str.charAt(i) != '.' && !Character.isDigit(str.charAt(i))) {
+                continue;
+            }
+            if (str.charAt(i) == '.') {
+                ++dotAmount;
+                if (dotAmount > 1){
+                    return null;
+                }
+            }
+            if (str.charAt(i) == 'e') {
+                ++eAmount;
+                if (eAmount > 1){
+                    return null;
+                }
+            }
+            returnValue.append(str.charAt(i));
+        }
+        if (Character.isDigit(str.charAt(str.length() - 1))) {
+            returnValue.append(str.charAt(str.length() - 1));
+        }
+
+        for (int i = 1; i < returnValue.length() - 1; ++i) {
+            if (returnValue.charAt(i) == '-' && (returnValue.charAt(i - 1) != 'e' || !Character.isDigit(returnValue.charAt(i + 1)))) {
+                return null;
+            }
+        }
+
+        if (returnValue.indexOf(".") != -1 || returnValue.indexOf("e") != -1){
+            return Double.valueOf(returnValue.toString());
+        }
+        if (Long.valueOf(returnValue.toString()) > Integer.MAX_VALUE
+                || Long.valueOf(returnValue.toString()) < Integer.MIN_VALUE) {
+            return Long.valueOf(returnValue.toString());
+        }
+        return Integer.valueOf(returnValue.toString());
     }
 }
