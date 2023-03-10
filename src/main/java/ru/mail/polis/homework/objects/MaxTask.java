0baf3a795dd854b43fe1d18b18b8a930dd387a93
Ivann <nemchinov.im@edu.spbstu.ru>
2023-03-10 21:07:04 +0300

    MaxTask

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -12,7 +12,34 @@ public class MaxTask {
      * 4 тугрика
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
-    }
 
+        if (array == null || array.length < count) {
+            return null;
+        }
+        if (count == 0) {
+            return new int[0];
+        }
+
+        int[] result = new int[count];
+        for(int i = 0; i < count; i++) {
+            result[i] = Integer.MIN_VALUE;
+        }
+
+        for (int element : array) {
+            if (result[count - 1] > element) {
+                continue;
+            }
+            result[count - 1] = element;
+            for (int i = count - 1; i > 0; i--) {
+                if (result[i - 1] < element) {
+                    result[i] = result[i - 1];
+                    result[i - 1] = element;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
 }
--- src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
+++ src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 public class RepeatingCharacters {
 
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
+
         if(str == null || str.length() == 0) {
             return null;
         }
