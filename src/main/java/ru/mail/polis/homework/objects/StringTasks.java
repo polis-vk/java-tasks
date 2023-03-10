fea1c5a0c55f0dc2b682e3098964c6a93bb17c6c
Ivann <nemchinov.im@edu.spbstu.ru>
2023-03-10 20:02:51 +0300

    StringTasks

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -16,6 +16,65 @@ public class StringTasks {
      * 6 тугриков
      */
     public static Number valueOf(String str) {
-        return null;
+
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+
+        StringBuilder result = new StringBuilder();
+        byte countMinus = 0;
+        byte countPoint = 0;
+        byte countExp = 0;
+
+        for (int i = 0; i < str.length(); ++i) {
+            char symbol = str.charAt(i);
+
+            switch (symbol) {
+                case '-':
+                    if (countMinus == 2) {
+                        return null;
+                    }
+                    if (countMinus == 1 && (result.charAt(0) != '-' || result.charAt(result.length() - 1) != 'e')) {
+                        return null;
+                    }
+                    if (countMinus == 0 && (result.length() != 0 && result.charAt(result.length() - 1) != 'e')) {
+                        return null;
+                    }
+                    countMinus++;
+                    break;
+                case '.':
+                    if (countPoint == 1) {
+                        return null;
+                    }
+                    countPoint++;
+                    break;
+                case 'e':
+                    if (countExp == 1) {
+                        return null;
+                    }
+                    countExp++;
+                    break;
+                default:
+                    if (!Character.isDigit(symbol)) {
+                        continue;
+                    }
+            }
+            result.append(symbol);
+        }
+
+        if (countMinus == 2 && result.charAt(result.length() - 1) == '-') {
+            return null;
+        }
+        if (countExp == 1 && result.charAt(result.length() - 1) == 'e') {
+            return null;
+        }
+
+        if (countPoint > 0 || countExp > 0) {
+            return (Number) Double.valueOf(result.toString());
+        }
+        if (Long.valueOf(result.toString()) > Integer.MAX_VALUE || Long.valueOf(result.toString()) < Integer.MIN_VALUE) {
+            return Long.valueOf(result.toString());
+        }
+        return Integer.valueOf(result.toString());
     }
 }
