29a46c1bd61f4a22db0c950969ea7002a69170c9
Flusova Sofia <flusova.sa@edu.spbstu.ru>
2023-03-11 00:58:55 +0300

    initial commit HW2

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -16,6 +16,52 @@ public class StringTasks {
      * 6 тугриков
      */
     public static Number valueOf(String str) {
-        return null;
+        if (str == null || str == "") {
+            return null;
+        }
+        StringBuilder number = new StringBuilder();
+        int dotsCnt = 0;
+        int expCnt = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            char curSym = str.charAt(i);
+            char nextSym = str.charAt(i + 1);
+            if (!(curSym == '-' ||  curSym == '.' || curSym == 'e' || Character.isDigit(curSym))) {
+                continue;
+            }
+            if (curSym == '-' && nextSym == '-') {
+                return null;
+            }
+            if (curSym == '.') {
+                dotsCnt++;
+            }
+            if (curSym == 'e') {
+                expCnt++;
+            }
+            number.append(curSym);
+        }
+        if (str.charAt(str.length() - 1) == '-' || str.charAt(str.length() - 1) == 'e') {
+            return null;
+        }
+        if (Character.isDigit(str.charAt(str.length() - 1))) {
+            number.append(str.charAt(str.length() - 1));
+        }
+        if (dotsCnt > 1 || expCnt > 1) {
+            return null;
+        }
+        for (int i = 1; i < number.length() - 1; i++) {
+            char prevSym = number.charAt(i - 1);
+            char curSym = number.charAt(i);
+            char nextSym = number.charAt(i + 1);
+            if (curSym == '-' && ( prevSym != 'e' || !Character.isDigit(nextSym) )) {
+                return null;
+            }
+        }
+        if (number.indexOf("e") != -1 || number.indexOf(".") != -1) {
+            return Double.valueOf(number.toString());
+        } else if (Long.valueOf(number.toString()) > Integer.MAX_VALUE
+                   || Long.valueOf(number.toString()) < Integer.MIN_VALUE) {
+            return Long.valueOf(number.toString());
+        }
+        return Integer.valueOf(number.toString());
     }
 }
