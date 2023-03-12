0acde27364bdf82ba103d9e611b6d2fc080c572b
jbisss <pozolotusiko@mail.ru>
2023-03-13 02:00:21 +0300

    hw_2 MaxTask

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -11,8 +13,42 @@ public class MaxTask {
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
      * 4 тугрика
      */
-    public static int[] getMaxArray(int[] array, int count) {
-        return null;
+    // проверяем по индексу, есть ли очередное число в списке максимальных
+    public static boolean isInRestricted(int index, int[] restrictedIndexesArray, int size) {
+        for (int i = 0; i <= size; i++) {
+            if (restrictedIndexesArray[i] == index) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    public static int[] getMaxArray(int[] array, int count) {
+        if (array == null || count > array.length) {
+            return null;
+        }
+        int[] result = new int[count];
+        int restrictedIndexesSize = 0;
+        // массив с индексами чисел, которые уже были выбраны в качестве МАКСИМАЛЬНОГО
+        int[] restrictedIndexes = new int[array.length];
+        Arrays.fill(restrictedIndexes, -1);
+        int max = Integer.MIN_VALUE;
+        int maxIndex = 0;
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] > max && !isInRestricted(j, restrictedIndexes, restrictedIndexesSize)) {
+                    max = array[j];
+                    maxIndex = j;
+                }
+            }
+            // заносим полученный максимум в результирующий массив и дополняем массив индексов максимальных чисел
+            restrictedIndexes[restrictedIndexesSize] = maxIndex;
+            result[restrictedIndexesSize] = max;
+            restrictedIndexesSize++;
+            // устанавливаем значения по-умолчанию для служебных переменных на следующий шаг
+            max = Integer.MIN_VALUE;
+            maxIndex = 0;
+        }
+        return result;
+    }
 }
