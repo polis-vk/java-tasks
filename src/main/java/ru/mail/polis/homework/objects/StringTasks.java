42d6958c1686171b0fa2a90bc8df23835b248913
jbisss <pozolotusiko@mail.ru>
2023-03-13 02:45:49 +0300

    hw_2 StringTasks

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -16,6 +16,35 @@ public class StringTasks {
      * 6 тугриков
      */
     public static Number valueOf(String str) {
-        return null;
+        if (str == null || str.equals("")) {
+            return null;
+        }
+        int length = str.length();
+        boolean flag = false;
+        for (int i = 0; i < length; i++) {
+            // флаг для проверки - кастить ли к Double (если . или 'e' - ДА)
+            if (str.charAt(i) == '.' || str.charAt(i) == 'e') {
+                flag = true;
+            }
+            if (!(Character.isDigit(str.charAt(i)) || str.charAt(i) == '-' || str.charAt(i) == '.' || str.charAt(i) == 'e')) {
+                str = str.substring(0, i) + str.substring(i + 1);
+                i--;
+                length--;
+            }
+        }
+        try {
+            if (flag) {
+                return Double.parseDouble(str);
+            } else {
+                long result = Long.parseLong(str);
+                if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+                    return result;
+                } else {
+                    return (int) result;
+                }
+            }
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
