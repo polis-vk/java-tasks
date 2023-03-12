a94068226ae71487bd20b718086fb1cd0969002f
Yana Spirchina <spirchina04@gmail.com>
2023-03-12 15:34:53 +0300

    MaxTask.java,RepeatingCharacters.java,StringTasks.java. All tests were passed

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -12,7 +12,48 @@ public class MaxTask {
      * 4 тугрика
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
-    }
+        if (array == null || array.length < count) {
+            return null;
+        }
+        if (array.length == 0 || count == 0) {
+            return new int[0];
+        }
 
+        int[] newArray = new int[count];
+        int length = array.length;
+        int[] arrayOfIndex = new int[count];
+        int maxValue = Integer.MIN_VALUE;
+
+        for (int i = 0; i < count; i++) {
+            arrayOfIndex[i] = -1;
+        }
+
+        int index = 0;
+        boolean isAlreadyAdd = false;  //Добавление в arrayOfIndex
+
+        for (int j = 0; j < count; j++) {
+            for (int i = 0; i < length; i++) {
+                if (maxValue <= array[i]) {
+                    for (int k : arrayOfIndex) {
+                        if (k == -1) {
+                            break;
+                        } else if (i == k) {
+                            isAlreadyAdd = true;
+                            break;
+                        }
+                    }
+                    if (!isAlreadyAdd) {
+                        maxValue = array[i];
+                        index = i;
+                    }
+                }
+                isAlreadyAdd = false;
+            }
+            arrayOfIndex[j] = index;
+            newArray[j] = maxValue;
+            maxValue = Integer.MIN_VALUE;
+        }
+
+        return newArray;
+    }
 }
