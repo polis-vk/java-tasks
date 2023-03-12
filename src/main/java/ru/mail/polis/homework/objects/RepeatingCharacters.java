0559229b458acf685b6cbcb61fb0d218f902bf8b
jbisss <pozolotusiko@mail.ru>
2023-03-13 01:38:41 +0300

    hw_2 RepeatingCharacters

--- src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
+++ src/main/java/ru/mail/polis/homework/objects/RepeatingCharacters.java
@@ -12,9 +12,39 @@ import java.util.Objects;
  * 4 тугрика
  */
 public class RepeatingCharacters {
-
     public static Pair<Character, Integer> getMaxRepeatingCharacters(String str) {
-        return new Pair<>('s', 4);
+        if (str == null || str.equals("")) {
+            return null;
+        }
+        int count = 0; // счётчик элементов в массивах
+        int[] arrayRepeats = new int[str.length()]; // массив количества повторений каждого символа
+        char[] arrayElems = new char[str.length()]; // массив символов
+        char currentElem = str.charAt(0); // текущий элемент
+        int countRepeats = 0; // количество повторений текущего элемента
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == currentElem) {
+                countRepeats++;
+            } else {
+                // если встречаем новый элемент - заносим в массивы данные о предыдущем элементе и обновляем currentElem
+                arrayElems[count] = currentElem;
+                arrayRepeats[count] = countRepeats;
+                countRepeats = 1;
+                currentElem = str.charAt(i);
+                count++;
+            }
+        }
+        arrayElems[count] = currentElem;
+        arrayRepeats[count] = countRepeats;
+        int maxRepeats = 0;
+        char maxElem = '\n';
+        // выбираем максимальный
+        for (int i = 0; i <= count; i++) {
+            if (arrayRepeats[i] > maxRepeats) {
+                maxRepeats = arrayRepeats[i];
+                maxElem = arrayElems[i];
+            }
+        }
+        return new Pair<>(maxElem, maxRepeats);
     }
 
     public static class Pair<T, V> {
