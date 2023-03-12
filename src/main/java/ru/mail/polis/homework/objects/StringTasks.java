e635d3b6cd940499da431cd6d5de29dd9fdd3760
Chupina Anna <chupina.ayu@edu.spbstu.ru>
2023-03-12 20:32:08 +0300

    StringTask complited

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -16,6 +16,46 @@ public class StringTasks {
      * 6 тугриков
      */
     public static Number valueOf(String str) {
-        return null;
+        if (str == null || str.isEmpty()) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        boolean isExp = false;
+        boolean isDot = false;
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sb.append(c);
+            } else if (c == '.') {
+                if (isDot) {
+                    return null;
+                }
+                isDot = true;
+                sb.append(c);
+            } else if (c == 'e') {
+                if (isExp) {
+                    return null;
+                }
+                isExp = true;
+                sb.append(c);
+            } else if (c == '-') {
+                if (sb.length() != 0 && sb.charAt(sb.length() - 1) != 'e') {
+                    return null;
+                }
+                sb.append(c);
+            }
+        }
+        String num = sb.toString();
+        if (!Character.isDigit(num.charAt(num.length() - 1))) {
+            return null;
+        }
+        if (isDot || isExp) {
+            return Double.parseDouble(num);
+        }
+        try {
+            return Integer.parseInt(num);
+        } catch (NumberFormatException e) {
+            return Long.valueOf(num);
+        }
     }
+
 }
