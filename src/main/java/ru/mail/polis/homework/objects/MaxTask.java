70684b5f582760e625dd78b712e7468920a8ddbb
Chupina Anna <chupina.ayu@edu.spbstu.ru>
2023-03-15 16:00:37 +0300

    merge error fixed

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -9,35 +9,10 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-<<<<<<< HEAD
-=======
      * 4 тугрика
->>>>>>> master
      */
     public static int[] getMaxArray(int[] array, int count) {
-        if (array == null || array.length < count) {
-            return null;
-        }
-        if (count == 0) {
-            return new int[0];
-        }
-        int[] nMaxNums = new int[count];
-        java.util.Arrays.fill(nMaxNums, Integer.MIN_VALUE);
-        for (int num : array) {
-            if (nMaxNums[count - 1] > num) {
-                continue;
-            }
-            nMaxNums[count - 1] = num;
-            for (int i = count - 2; i >= 0; i--) {
-                if (nMaxNums[i] < num) {
-                    nMaxNums[i + 1] = nMaxNums[i];
-                    nMaxNums[i] = num;
-                } else {
-                    break;
-                }
-            }
-        }
-        return nMaxNums;
+        return null;
     }
 
 }
