7b0731b57024cc4040127e0ace5442462ef1059b
Chupina Anna <chupina.ayu@edu.spbstu.ru>
2023-03-15 18:24:23 +0300

    redone MaxTask

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,6 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
 public class MaxTask {
 
     /**
@@ -12,7 +13,31 @@ public class MaxTask {
      * 4 тугрика
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        if (array == null || array.length < count) {
+            return null;
+        }
+        if (count == 0) {
+            return new int[0];
+        }
+        int result[] = Arrays.copyOf(array, count);
+        Arrays.sort(result);
+        int insertPos;
+        for (int i = count; i < array.length; i++) {
+            if (array[i] < result[0]) {
+                continue;
+            }
+            insertPos = Arrays.binarySearch(result, array[i]);
+            insertPos = insertPos >= 0 ? insertPos : -insertPos - 2;
+            System.arraycopy(result, 1, result, 0, insertPos);
+            result[insertPos] = array[i];
+        }
+        int temp;
+        for (int i = 0; i < count / 2; i++) {
+            temp = result[i];
+            result[i] = result[count - i - 1];
+            result[count - i - 1] = temp;
+        }
+        return result;
     }
 
 }
