66b048787dc701f9136a84a50a0f18a3853aa878
Igr1x <varnavskii.igrix@mail.ru>
2023-03-17 14:56:32 +0300

    Commit #1
    1.Tasks with the most repetitive characters and arrays are made.

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -9,10 +11,48 @@ public class MaxTask {
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
-     *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        if (array == null || array.length < count)
+        {
+            return null;
+        }
+        int copyLength = array.length;
+        int[] arrayCopy = Arrays.copyOf(array, copyLength);
+        int[] max = new int[count];
+        int indexMaxValue = -1;
+        int []lastMaxValue = new int[count];
+        for (int i = 0; i < lastMaxValue.length; i++)
+        {
+            lastMaxValue[i] = -1;
+        }
+        for (int j = 0; j < count; j++) {
+            int i = 0;
+            int tmpMax = Integer.MIN_VALUE;
+            while(i < arrayCopy.length)
+            {
+                boolean flag = false;
+                for (int k = 0; k < lastMaxValue.length; k++) {
+                    if (i == lastMaxValue[k]) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    i++;
+                    continue;
+                }
+                if (tmpMax < arrayCopy[i])
+                {
+                    tmpMax = arrayCopy[i];
+                    indexMaxValue = i;
+                }
+                i++;
+            }
+            max[j] = tmpMax;
+            lastMaxValue[j] = indexMaxValue;
+        }
+        return max;
     }
-
 }
