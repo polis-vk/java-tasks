976affdc0ac64fe813a6a688b55a375c734ccf22
Juliakuznetsovaaaa <jyliakuz@gmail.com>
2023-03-19 19:01:51 +0300

    убрала ненужный импорт

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,7 +1,5 @@
 package ru.mail.polis.homework.objects;
 
-import java.util.Arrays;
-
 public class MaxTask {
 
     /**
@@ -10,7 +8,8 @@ public class MaxTask {
      * Если массив null или его длина меньше count, то вернуть null
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
-     * НЕЛЬЗЯ СОРТNРОВАТЬ массив array и его копии
+     * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
+     * 4 тугрика
      */
     public static int[] getMaxArray(int[] array, int count) {
         if (array == null || array.length < count) {
