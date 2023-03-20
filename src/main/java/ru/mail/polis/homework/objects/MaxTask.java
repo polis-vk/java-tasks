0128511f6166d654065c9bb52c70fe5419e2b7b6
whoscared <coklook1@gmail.com>
2023-03-20 23:36:39 +0300

    Senatorova Sofya Homework 2 MaxTask исправлены ошибки => Сложность О(N*logN)

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,5 +1,8 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MaxTask {
 
     /**
@@ -11,32 +14,103 @@ public class MaxTask {
      * НЕЛЬЗЯ СОРТИРОВАТЬ массив array и его копии
      * 4 тугрика
      */
+
+    // O(N*logN) -> N - обход массива, logN - сортировка heap
     public static int[] getMaxArray(int[] array, int count) {
 
         if (array == null || array.length < count) {
             return null;
         }
 
+        MyHeapMax heap = new MyHeapMax(array.length);
+
+        for (int cur : array) {
+            heap.insert(cur);
+        }
+
         int[] result = new int[count];
-        boolean[] takeElement = new boolean[array.length];
-        int indexResult = 0;
-        int indexTakeElement = 0;
-
-        while (indexResult < count) {
-            int curMax = Integer.MIN_VALUE;
-            for (int i = 0; i < array.length; i++) {
-                if (takeElement[i]) {
-                    continue;
-                }
-                indexTakeElement = array[i] > curMax ? i : indexTakeElement;
-                curMax = Math.max(array[i], curMax);
-            }
-
-            takeElement[indexTakeElement] = true;
-            result[indexResult] = curMax;
-            indexResult++;
+        for (int i = 0; i < result.length; i++) {
+            result[i] = heap.extract();
         }
 
         return result;
     }
+
+    public static class MyHeapMax {
+        private final int[] elements;
+        private int current;
+
+        public MyHeapMax(int size) {
+            elements = new int[size];
+            current = -1;
+        }
+
+        public void insert(int x) {
+            if (current == -1) {
+                elements[++current] = x;
+                return;
+            }
+            elements[++current] = x;
+            up();
+        }
+
+        public Integer extract() {
+            if (current == -1) {
+                return null;
+            }
+            int result = elements[0];
+            elements[0] = elements[current];
+            elements[current] = 0;
+            current--;
+            down();
+            return result;
+        }
+
+        private void up() {
+            int index = current;
+            while (((index - 1) / 2 >= 0) && elements[(index - 1) / 2] < elements[index]) {
+                int temp = elements[(index - 1) / 2];
+                elements[(index - 1) / 2] = elements[index];
+                elements[index] = temp;
+                index = (index - 1) / 2;
+            }
+        }
+
+        private void down() {
+            int index = 0;
+
+            while ((2 * index + 2 <= current)) {
+                int first = 2 * index + 1;
+                int second = 2 * index + 2;
+                if (elements[second] > elements[index]
+                        && elements[second] > elements[first]) {
+
+                    int temp = elements[second];
+                    elements[second] = elements[index];
+                    elements[index] = temp;
+
+                    index = second;
+
+                } else if (elements[first] > elements[index]) {
+
+                    int temp = elements[first];
+                    elements[first] = elements[index];
+                    elements[index] = temp;
+
+                    index = first;
+
+                } else {
+                    break;
+                }
+            }
+
+            int first = 2 * index + 1;
+            if ((first <= current) && (elements[2 * index + 1] > elements[index])) {
+                int temp = elements[first];
+                elements[first] = elements[index];
+                elements[index] = temp;
+            }
+        }
+    }
 }
+
