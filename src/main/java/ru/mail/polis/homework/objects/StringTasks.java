68c952e16ba959acd2b3a13d1a346b5cbda81a05
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-03-20 14:37:43 +0300

    Сделать задание StringTasks

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -15,6 +15,65 @@ public class StringTasks {
      * У класса Character есть полезные методы, например Character.isDigit()
      */
     public static Number valueOf(String str) {
-        return null;
+        if (str == null || str.equals(""))
+            return null;
+        Character value;
+
+        for (int i = 0; i < str.length(); i++) {
+            value = str.charAt(i);
+            if (value != '-' && value != '.' && (value > '9' || value < '0') && value != 'e') {
+                str = str.replace(value.toString(), "");
+                i = 0;
+            }
+        }
+        for (int i = 0; i < str.length() - 1; i++) {
+            value = str.charAt(i);
+            if (value == '-' && str.charAt(i + 1) == '-')
+                return null;
+
+            if (value != 'e' && str.charAt(i + 1) == '-')
+                return null;
+            if (str.charAt(str.length() - 1) == 'e')
+                return null;
+        }
+
+        return turn_into_number(str);
+    }
+
+    private static Number turn_into_number(String str) {
+        if (str.contains("e")) {
+            return Double.valueOf(str);
+        }
+        int length = str.length();
+        double number = 0;
+        boolean is_negative = false;
+        boolean decimal = false;
+        int decimalPoint = 0;
+        for (int i = 0; i < length; i++) {
+            if (str.charAt(i) == '-') {
+                is_negative = true;
+                continue;
+            }
+            if (str.charAt(i) == '.') {
+                decimal = true;
+                decimalPoint = i;
+                number /= 100;
+                continue;
+            }
+            if (decimal) {
+                number += (str.charAt(i) - '0') * Math.pow(0.1, i - decimalPoint);
+            } else
+                number += (str.charAt(i) - '0') * Math.pow(10, length - i - 1);
+
+        }
+        if (is_negative)
+            number = -1 * number;
+
+        if (!str.contains(".") && !str.contains("e")) {
+            if ((number >= -2147483648 && number <= 0) || (number > 0 && number <= 2147483647))
+                return (int) number;
+            return (long) number;
+        }
+        return number;
     }
 }
