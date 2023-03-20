a341b208964ef3a92441f4845697c863e6689e01
nikaNGC224 <veronika_rassokhina@mail.ru>
2023-03-20 17:14:02 +0300

    Update MaxTask.java

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -26,18 +26,67 @@ public class MaxTask {
 
         Arrays.fill(maxArray, Integer.MIN_VALUE);
 
-        for (int value : array) {
-            for (int j = 0; j < count; ++j) {
-                if (value >= maxArray[j]) {
-                    for (int k = count - 2; k >= j; --k) {
-                        maxArray[k + 1] = maxArray[k];
+        maxArray[0] = array[0];
+        int iMin = 0;
+        int iMax = 0;
+        for (int i = 1; i < array.length; ++i) {
+            if (array[i] >= maxArray[iMax]) {
+                if (iMin <= iMax) {
+                    iMax = iMax == 0 ? count - 1 : iMax - 1;
+                    if (iMax == iMin) {
+                        iMin = iMin == 0 ? count - 1 : iMin - 1;
                     }
-                    maxArray[j] = value;
-                    break;
+                } else {
+                    iMax = iMax + 1 == count ? 0 : iMax + 1;
+                    if (iMax == iMin) {
+                        iMin = iMin + 1 == count ? 0 : iMin + 1;
+                    }
+                }
+                maxArray[iMax] = array[i];
+            } else {
+                int iMinNext = iMin + 1 == count ? 0 : iMin + 1;
+                if (array[i] > maxArray[iMin]) {
+                    if (maxArray[iMinNext] == Integer.MIN_VALUE) {
+                        maxArray[iMinNext] = maxArray[iMin];
+                        maxArray[iMin] = array[i];
+                        iMin = iMinNext;
+                    } else {
+                        maxArray[iMin] = array[i];
+                        int iPrevious = iMin == 0 ? count - 1 : iMin - 1;
+                        if (maxArray[iMin] > maxArray[iPrevious]) {
+                            iMin = iPrevious;
+                        }
+                    }
+
+                } else {
+                    if (maxArray[iMinNext] == Integer.MIN_VALUE) {
+                        iMin = iMinNext;
+                        maxArray[iMin] = array[i];
+                    }
                 }
             }
         }
+        bubbleSort(maxArray);
         return maxArray;
+    }
+
+    public static void bubbleSort(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; ++i) {
+                if (array[i] < array[i + 1]) {
+                    swap(array, i, i + 1);
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] array, int i1, int i2) {
+        int temp = array[i1];
+        array[i1] = array[i2];
+        array[i2] = temp;
     }
 
 }
