3be1ae7fbf6b8034d5ecfc9859e7d17915f4b5c6
Kirill Kashirin <kirykaxxx@gmail.com>
2023-03-21 14:35:10 +0300

    Add StringTasks realisation

--- src/main/java/ru/mail/polis/homework/objects/StringTasks.java
+++ src/main/java/ru/mail/polis/homework/objects/StringTasks.java
@@ -1,5 +1,7 @@
 package ru.mail.polis.homework.objects;
 
+import java.util.Objects;
+
 public class StringTasks {
 
     /**
@@ -16,6 +18,60 @@ public class StringTasks {
      * 6 тугриков
      */
     public static Number valueOf(String str) {
-        return null;
+        Number result = null;
+        if (Objects.nonNull(str) && !str.isEmpty()) {
+            char prevSymbol = ' ';
+            boolean hasDot = false;
+            boolean hasEpsilon = false;
+            boolean isNegative = false;
+            StringBuilder numberBuilder = new StringBuilder();
+            for (int currSymbolIndex = 0; currSymbolIndex < str.length(); currSymbolIndex++) {
+                char currSymbol = str.charAt(currSymbolIndex);
+                if (isValidNumberSymbol(currSymbol)) {
+                    if ((currSymbol == '-') && (prevSymbol != 'e')) {
+                        if (isNegative || (numberBuilder.length() != 0)) {
+                            return result;
+                        }
+                        isNegative = true;
+                    } else if (currSymbol == '.') {
+                        if (hasDot || hasEpsilon) {
+                            return result;
+                        }
+                        hasDot = true;
+                    } else if (currSymbol == 'e') {
+                        if (hasEpsilon) {
+                            return result;
+                        }
+                        hasEpsilon = true;
+                    }
+                    prevSymbol = currSymbol;
+                    numberBuilder.append(currSymbol);
+                }
+            }
+            if (Character.isDigit(prevSymbol) || ((prevSymbol == '.') && (numberBuilder.length() > 1))) {
+                String resultNumberString = numberBuilder.toString();
+                if (hasDot || hasEpsilon) {
+                    result = Double.parseDouble(resultNumberString);
+                } else {
+                    result = Long.valueOf(resultNumberString);
+                    if ((result.longValue() <= Integer.MAX_VALUE) && (result.longValue() >= Integer.MIN_VALUE)) {
+                        result = Integer.valueOf(resultNumberString);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    private static boolean isValidNumberSymbol(char symbol) {
+        boolean result = Character.isDigit(symbol);
+        switch (symbol) {
+            case '-':
+            case '.':
+            case 'e':
+                result = true;
+                break;
+        }
+        return result;
     }
 }
