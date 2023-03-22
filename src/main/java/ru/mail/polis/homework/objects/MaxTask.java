7195945dc16c57db0d46c8643bfdaf1c61086611
whoscared <coklook1@gmail.com>
2023-03-22 19:58:11 +0300

    Senatorova Sofya Homework 2 StringTask убрала лишнюю вложенность

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -1,6 +1,7 @@
 package ru.mail.polis.homework.objects;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class MaxTask {
@@ -15,33 +16,56 @@ public class MaxTask {
      * 4 тугрика
      */
 
-    // O(N*logN) -> N - обход массива, logN - сортировка heap
+
     public static int[] getMaxArray(int[] array, int count) {
 
         if (array == null || array.length < count) {
             return null;
         }
-
-        MyHeapMax heap = new MyHeapMax(array.length);
-
-        for (int cur : array) {
-            heap.insert(cur);
+        if (count == 0) {
+            return new int[0];
         }
 
-        int[] result = new int[count];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = heap.extract();
+        int[] result = Arrays.copyOf(array, count);
+        Arrays.sort(result);
+
+        for (int i = count; i < array.length; i++) {
+            if (result[0] > array[i]) {
+                continue;
+            }
+            int index = Arrays.binarySearch(result, array[i]);
+            if (index < 0) {
+                index *= (-1);
+                index = Math.min(index - 2, result.length - 1);
+            } else {
+                index -= 1;
+            }
+            if (index < 0) {
+                continue;
+            }
+            for (int j = 1; j <= index; j++) {
+                result[j - 1] = result[j];
+            }
+            result[index] = array[i];
+        }
+
+        for (int i = 0; i < (result.length + 1) / 2; i++) {
+            int temp = result[i];
+            result[i] = result[result.length - 1 - i];
+            result[result.length - 1 - i] = temp;
         }
 
         return result;
     }
 
     public static class MyHeapMax {
+        private final int size;
         private final int[] elements;
         private int current;
 
         public MyHeapMax(int size) {
-            elements = new int[size];
+            this.size = size;
+            elements = new int[this.size + 1];
             current = -1;
         }
 
@@ -52,12 +76,16 @@ public class MaxTask {
             }
             elements[++current] = x;
             up();
+            current -= current == size ? 1 : 0;
+//            if (current == size - 1) {
+//                elements[current + 1] = x;
+//            } else {
+//                elements[++current] = x;
+//            }
+//            up();
         }
 
-        public Integer extract() {
-            if (current == -1) {
-                return null;
-            }
+        public int extract() {
             int result = elements[0];
             elements[0] = elements[current];
             elements[current] = 0;
@@ -66,9 +94,13 @@ public class MaxTask {
             return result;
         }
 
+        public int getMin() {
+            return elements[current];
+        }
+
         private void up() {
             int index = current;
-            while (((index - 1) / 2 >= 0) && elements[(index - 1) / 2] < elements[index]) {
+            while ((index - 1) / 2 >= 0 && elements[(index - 1) / 2] < elements[index]) {
                 int temp = elements[(index - 1) / 2];
                 elements[(index - 1) / 2] = elements[index];
                 elements[index] = temp;
@@ -79,7 +111,7 @@ public class MaxTask {
         private void down() {
             int index = 0;
 
-            while ((2 * index + 2 <= current)) {
+            while (2 * index + 2 <= current) {
                 int first = 2 * index + 1;
                 int second = 2 * index + 2;
                 if (elements[second] > elements[index]
@@ -105,7 +137,7 @@ public class MaxTask {
             }
 
             int first = 2 * index + 1;
-            if ((first <= current) && (elements[2 * index + 1] > elements[index])) {
+            if (first <= current && (elements[2 * index + 1] > elements[index])) {
                 int temp = elements[first];
                 elements[first] = elements[index];
                 elements[index] = temp;
