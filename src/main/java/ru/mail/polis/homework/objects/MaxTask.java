f50b1ffcc7737e95d802cecbdcdf5a29103d33ed
makniker <102602253+makniker@users.noreply.github.com>
2023-03-22 19:54:04 +0300

    fixed MaxTast(N + N * logK + N)

--- src/main/java/ru/mail/polis/homework/objects/MaxTask.java
+++ src/main/java/ru/mail/polis/homework/objects/MaxTask.java
@@ -14,7 +14,11 @@ public class MaxTask {
      * 4 тугрика
      */
     public static int[] getMaxArray(int[] array, int count) {
-        if (array == null || array.length < count) {
+        if (array == null) {
+            return null;
+        }
+        int arrLength = array.length;
+        if (arrLength < count) {
             return null;
         }
         if (count == 0) {
@@ -24,26 +28,23 @@ public class MaxTask {
         Arrays.sort(result);
         int min = result[0];
         int pos;
-        for (int i = count; i < array.length; i++) {
-            if (array[i] < min) {
+        for (int i = count; i < arrLength; i++) {
+            if (array[i] <= min) {
                 continue;
-            }//{1, 3, 10, 11, 22, 0}//3, 1
+            }
             pos = Arrays.binarySearch(result, array[i]);
             if (pos < 0) {
-                pos = - pos - 1;
-                System.arraycopy(result, pos - 1, result, pos - 2, result.length - pos + 1);
-                result[pos - 1] = array[i];
-            } else {
-                System.arraycopy(result, pos - 1, result, pos - 2, result.length - pos + 1);
+                pos = -pos - 1;
             }
-            min = array[0];
+            System.arraycopy(result, 1, result, 0, pos - 1);
+            result[pos - 1] = array[i];
+            min = result[0];
         }
-        for (int i = 0; i < result.length / 2; i++) {
+        for (int i = 0; i < count / 2; i++) {
             int temp = result[i];
-            result[i] = result[result.length - (i + 1)];
-            result[result.length - (i + 1)] = temp;
+            result[i] = result[count - (i + 1)];
+            result[count - (i + 1)] = temp;
         }
-
         return result;
     }
 }
