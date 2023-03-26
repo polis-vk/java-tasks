3395c865aedce4ac2fcbda7bc559e85460d11715
Flusova Sofia <flusova.sa@edu.spbstu.ru>
2023-03-26 19:34:35 +0300

    priority field & constructor were added in enum class

--- src/main/java/ru/mail/polis/homework/analyzer/FilterType.java
+++ src/main/java/ru/mail/polis/homework/analyzer/FilterType.java
@@ -4,9 +4,12 @@ package ru.mail.polis.homework.analyzer;
  * Типы фильтров (2 тугрика)
  */
 public enum FilterType {
-    GOOD,
-    SPAM,
-    TOO_LONG,
-    NEGATIVE_TEXT,
-    CAPITAL_CHAR
+    GOOD(1),
+    SPAM(2),
+    TOO_LONG(3),
+    NEGATIVE_TEXT(4),
+    CAPITAL_CHAR(5);
+    private final int priority;
+
+    FilterType(int priority) { this.priority = priority; }
 }
