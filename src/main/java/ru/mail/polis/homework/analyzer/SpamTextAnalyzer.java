111ac4efd974f700b9e41e63a0f872e0bb5baa26
jyliakuz <127231883+jyliakuz@users.noreply.github.com>
2023-04-01 12:27:44 +0300

    Delete SpamTextAnalyzer.java

--- src/main/java/ru/mail/polis/homework/analyzer/SpamTextAnalyzer.java
+++
@@ -1,8 +0,0 @@
-package ru.mail.polis.homework.analyzer;
-
-public class SpamTextAnalyzer extends ForbiddenTextAnalyzer{
-    public SpamTextAnalyzer(String[] spam) {
-        forbiddenText = spam;
-        filterType = FilterType.SPAM;
-    }
-}
