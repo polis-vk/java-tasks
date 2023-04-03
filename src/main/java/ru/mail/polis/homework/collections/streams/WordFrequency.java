bd8d25fc4b4921d036c1a74f158a0f815b325c5d
Nikita <nvd2002@mail.ru>
2023-04-03 22:03:58 +0300

    wordFrequency v1.0

--- src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
@@ -1,6 +1,9 @@
 package ru.mail.polis.homework.collections.streams;
 
-import java.util.List;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -21,7 +24,14 @@ public class WordFrequency {
      * Задачу можно решить без единого условного оператора, только с помощью стримов.
      */
     public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
+        return lines.flatMap(str -> Arrays.stream(str.split("[ .,!:-?;]")))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Entry.<String, Long>comparingByValue().reversed())
+                .map(Entry::getKey)
+                .limit(10)
+                .collect(Collectors.toList());
     }
 
 
