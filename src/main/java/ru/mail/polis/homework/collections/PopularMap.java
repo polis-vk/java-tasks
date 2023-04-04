e5264f410d1c3ed2bb98aa7f3731f12b38434ac3
Kirill Kashirin <kirykaxxx@gmail.com>
2023-04-04 23:47:19 +0300

    Remove redundant imports

--- src/main/java/ru/mail/polis/homework/collections/PopularMap.java
+++ src/main/java/ru/mail/polis/homework/collections/PopularMap.java
@@ -2,14 +2,11 @@ package ru.mail.polis.homework.collections;
 
 
 import java.util.AbstractMap;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.List;
 import java.util.Set;
 
 
