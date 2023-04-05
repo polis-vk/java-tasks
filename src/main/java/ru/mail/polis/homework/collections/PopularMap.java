243070d1633a55841fd1ce57a591947e2b82c01a
Alexander Ponomarev <ponomarev7.aa@edu.spbstu.ru>
2023-04-06 01:22:38 +0300

    Remove unused headers

--- src/main/java/ru/mail/polis/homework/collections/PopularMap.java
+++ src/main/java/ru/mail/polis/homework/collections/PopularMap.java
@@ -1,10 +1,8 @@
 package ru.mail.polis.homework.collections;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
