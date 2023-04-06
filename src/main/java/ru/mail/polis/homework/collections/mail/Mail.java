b65ff49d17c108deeb747998179c069f44d7fdae
Juliakuznetsovaaaa <jyliakuz@gmail.com>
2023-04-06 14:30:34 +0300

    домашнее задание 5 Кузнецова

--- src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
@@ -3,5 +3,26 @@ package ru.mail.polis.homework.collections.mail;
 /**
  * 1 тугрик
  */
-public class Mail {
+public class Mail<T> {
+    private final String sender;
+    private final String addressee;
+    private final T messages;
+
+    public Mail(String sender, String addressee, T message) {
+        this.sender = sender;
+        this.addressee = addressee;
+        this.messages = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getAddressee() {
+        return addressee;
+    }
+
+    public T getMessages() {
+        return messages;
+    }
 }
--- src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
@@ -1,6 +1,10 @@
 package ru.mail.polis.homework.collections.mail;
 
 
+import ru.mail.polis.homework.collections.PopularMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -9,27 +13,35 @@ import java.util.function.Consumer;
  * Нужно создать сервис, который умеет обрабатывать письма и зарплату.
  * Письма состоят из получателя, отправителя, текста сообщения
  * Зарплата состоит из получателя, отправителя и суммы.
- *
- * В реализации нигде не должно быть классов Object и коллекций без типа. Используйте дженерики.
+ * <p>
+ * В реализации нигде не должно быть классов Object и коллекций без типа. Nспользуйте дженерики.
  * Всего 7 тугриков за пакет mail
  */
-public class MailService implements Consumer {
+public class MailService<T extends Mail<T>> implements Consumer<T> {
+    private final PopularMap<String, String> Persons = new PopularMap<>();
+    private final Map<String, List<T>> personMail = new HashMap<>();
 
     /**
      * С помощью этого метода почтовый сервис обрабатывает письма и зарплаты
      * 1 тугрик
      */
     @Override
-    public void accept(Object o) {
-
+    public void accept(T mail) {
+        String sender = mail.getSender();
+        String addressee = mail.getAddressee();
+        if (sender != null) {
+            Persons.put(sender, mail.getAddressee());
+        }
+        personMail.putIfAbsent(addressee, new ArrayList<>());
+        personMail.get(addressee).add(mail);
     }
 
     /**
      * Метод возвращает мапу получатель -> все объекты которые пришли к этому получателю через данный почтовый сервис
      * 1 тугрик
      */
-    public Map<String, List> getMailBox() {
-        return null;
+    public Map<String, List<T>> getMailBox() {
+        return personMail;
     }
 
     /**
@@ -37,7 +49,7 @@ public class MailService implements Consumer {
      * 1 тугрик
      */
     public String getPopularSender() {
-        return null;
+        return Persons.getPopularKey();
     }
 
     /**
@@ -45,14 +57,15 @@ public class MailService implements Consumer {
      * 1 тугрик
      */
     public String getPopularRecipient() {
-        return null;
+        return Persons.getPopularValue();
     }
 
     /**
      * Метод должен заставить обработать service все mails.
      * 1 тугрик
      */
-    public static void process(MailService service, List mails) {
-
+    public static <T extends Mail<T>> void process(MailService service, List mails) {
+        mails.forEach(service);
     }
+
 }
