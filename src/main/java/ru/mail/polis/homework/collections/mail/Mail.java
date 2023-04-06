c39558dd09731a035db4cee1e8299b77739cbe70
Chupina Anna <chupina.ayu@edu.spbstu.ru>
2023-04-06 12:21:47 +0300

    done MailService

--- src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
@@ -3,5 +3,27 @@ package ru.mail.polis.homework.collections.mail;
 /**
  * 1 тугрик
  */
-public class Mail {
+public class Mail<T> {
+
+    private final String recipient;
+    private final String sender;
+    private final T content;
+
+    public Mail(String recipient, String sender, T content) {
+        this.recipient = recipient;
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public T getContent() {
+        return content;
+    }
 }
--- src/main/java/ru/mail/polis/homework/collections/mail/Salary.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/Salary.java
@@ -3,5 +3,9 @@ package ru.mail.polis.homework.collections.mail;
 /**
  * 1 тугрик
  */
-public class Salary {
+public class Salary extends Mail<Integer> {
+
+    public Salary(String sender, String recipient, Integer salary) {
+        super(sender, recipient, salary);
+    }
 }
