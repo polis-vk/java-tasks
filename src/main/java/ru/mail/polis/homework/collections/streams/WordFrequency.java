b035c37652f71a655cf069bd96a7d60982910921
jbisss <pozolotusiko@mail.ru>
2023-04-09 23:30:42 +0300

    WordFrequency task

--- src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
@@ -1,6 +1,10 @@
 package ru.mail.polis.homework.collections.streams;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -16,13 +20,20 @@ import java.util.stream.Stream;
  * 5 тугриков
  */
 public class WordFrequency {
-
     /**
      * Задачу можно решить без единого условного оператора, только с помощью стримов.
      */
     public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
+        List<String> words = lines
+                .flatMap(line -> Arrays.stream(line.split("[ (.,!:-?;)]+")))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.<String, Long>comparingByKey().reversed()))
+                .map(Map.Entry::getKey)
+                .limit(10)
+                .collect(Collectors.toList());
+        return words;
     }
-
-
 }
