dfa5fc6859d213423ca82c4d5395508633c79bb4
evgenijkonopkin <konopkin11@gmail.com>
2023-04-11 22:44:06 +0300

    HW 5 Конопкин Евгений

--- src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
@@ -3,5 +3,26 @@ package ru.mail.polis.homework.collections.mail;
 /**
  * 1 тугрик
  */
-public class Mail {
+public class Mail<T> {
+    private final String recipient;
+    private final String sender;
+    private final T message;
+
+    public Mail(String recipient, String sender, T message) {
+        this.recipient = recipient;
+        this.sender = sender;
+        this.message = message;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public T getMessage() {
+        return message;
+    }
 }
--- src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
@@ -1,9 +1,13 @@
 package ru.mail.polis.homework.collections.mail;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+
+import ru.mail.polis.homework.collections.PopularMap;
 
 /**
  * Нужно создать сервис, который умеет обрабатывать письма и зарплату.
@@ -13,31 +17,34 @@ import java.util.function.Consumer;
  * В реализации нигде не должно быть классов Object и коллекций без типа. Используйте дженерики.
  * Всего 7 тугриков за пакет mail
  */
-public class MailService implements Consumer {
+public class MailService<T extends Mail<?>> implements Consumer<T> {
 
+    private final PopularMap<String, String> senders = new PopularMap<>();
+    private final Map<String, List<T>> mailBox = new HashMap<>();
     /**
      * С помощью этого метода почтовый сервис обрабатывает письма и зарплаты
      * 1 тугрик
      */
     @Override
-    public void accept(Object o) {
-
+    public void accept(T mail) {
+        senders.put(mail.getSender(), mail.getRecipient());
+        mailBox.computeIfAbsent(mail.getSender(), recipient -> new ArrayList<>())
+                .add(mail);
     }
 
     /**
      * Метод возвращает мапу получатель -> все объекты которые пришли к этому получателю через данный почтовый сервис
      * 1 тугрик
      */
-    public Map<String, List> getMailBox() {
-        return null;
+    public Map<String, List<T>> getMailBox() {
+        return mailBox;
     }
-
     /**
      * Возвращает самого популярного отправителя
      * 1 тугрик
      */
     public String getPopularSender() {
-        return null;
+        return senders.getPopularKey();
     }
 
     /**
@@ -45,14 +52,14 @@ public class MailService implements Consumer {
      * 1 тугрик
      */
     public String getPopularRecipient() {
-        return null;
+        return senders.getPopularValue();
     }
 
     /**
      * Метод должен заставить обработать service все mails.
      * 1 тугрик
      */
-    public static void process(MailService service, List mails) {
-
+    public static <T extends Mail<?>> void process(MailService<T> service, List<T> mails) {
+        mails.forEach(service);
     }
 }
