02ebbcfa9fd1f8ec5ca69122497bcfb1509e3495
Ivann <nemchinov.im@edu.spbstu.ru>
2023-04-11 15:53:27 +0300

    5 hw first v

--- src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
@@ -1,35 +1,44 @@
 package ru.mail.polis.homework.collections.mail;
 
+import ru.mail.polis.homework.collections.PopularMap;
 
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Нужно создать сервис, который умеет обрабатывать письма и зарплату.
  * Письма состоят из получателя, отправителя, текста сообщения
  * Зарплата состоит из получателя, отправителя и суммы.
- *
+ * <p>
  * В реализации нигде не должно быть классов Object и коллекций без типа. Используйте дженерики.
  * Всего 7 тугриков за пакет mail
  */
-public class MailService implements Consumer {
+public class MailService<T extends Mail<?>> implements Consumer<T> {
+
+    private final PopularMap<String, String> sendersAndRecipientsMap = new PopularMap<>();
+    private final Map<String, List<T>> mailBox = new HashMap<>();
 
     /**
      * С помощью этого метода почтовый сервис обрабатывает письма и зарплаты
      * 1 тугрик
      */
     @Override
-    public void accept(Object o) {
-
+    public void accept(T mail) {
+        String sender = mail.getSender();
+        sendersAndRecipientsMap.put(sender, mail.getRecipient());
+        mailBox.computeIfAbsent(sender, recipient -> new ArrayList<>())
+                .add(mail);
     }
 
     /**
      * Метод возвращает мапу получатель -> все объекты которые пришли к этому получателю через данный почтовый сервис
      * 1 тугрик
      */
-    public Map<String, List> getMailBox() {
-        return null;
+    public Map<String, List<T>> getMailBox() {
+        return mailBox;
     }
 
     /**
@@ -37,7 +46,7 @@ public class MailService implements Consumer {
      * 1 тугрик
      */
     public String getPopularSender() {
-        return null;
+        return sendersAndRecipientsMap.getPopularKey();
     }
 
     /**
@@ -45,14 +54,14 @@ public class MailService implements Consumer {
      * 1 тугрик
      */
     public String getPopularRecipient() {
-        return null;
+        return sendersAndRecipientsMap.getPopularValue();
     }
 
     /**
      * Метод должен заставить обработать service все mails.
      * 1 тугрик
      */
-    public static void process(MailService service, List mails) {
-
+    public static <T extends Mail<?>> void process(MailService<T> service, List<T> mails) {
+        mails.forEach(service);
     }
 }
--- src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
@@ -1,6 +1,10 @@
 package ru.mail.polis.homework.collections.streams;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -21,7 +25,16 @@ public class WordFrequency {
      * Задачу можно решить без единого условного оператора, только с помощью стримов.
      */
     public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
+        return lines
+                .flatMap(line -> Arrays.stream(line.split("[!?.,-;: ]")))
+                .filter(line -> !line.isEmpty())
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
+                .map(Map.Entry::getKey)
+                .limit(10)
+                .collect(Collectors.toList());
     }
 
 
