97014b9051000e5c84cf19c0a8a81e3e37dc42bd
Artem Sheverdyaev <artem-sheverdyaev@mail.ru>
2023-04-13 15:48:43 +0300

    Did Mail package

--- src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
@@ -3,5 +3,27 @@ package ru.mail.polis.homework.collections.mail;
 /**
  * 1 тугрик
  */
-public class Mail {
+public class Mail<T> {
+    private final T content;
+    private final String recipient;
+    private final String sender;
+
+
+    public Mail(String sender, String recipient, T content) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public T getContent() {
+        return content;
+    }
 }
