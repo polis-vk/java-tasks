73828f0a76fd56df39161604ec148689b028530d
Igr1x <varnavskii.igrix@mail.ru>
2023-04-13 13:24:00 +0300

    Commit #1

--- src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/Mail.java
@@ -3,5 +3,26 @@ package ru.mail.polis.homework.collections.mail;
 /**
  * 1 тугрик
  */
-public class Mail {
+public class Mail<T> {
+    private final String sender;
+    private final String recipient;
+    private final T content;
+
+    public Mail(String sender, String recipient, T content) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public T getContent() {
+        return content;
+    }
 }
--- src/main/java/ru/mail/polis/homework/collections/mail/MailMessage.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/MailMessage.java
@@ -3,6 +3,10 @@ package ru.mail.polis.homework.collections.mail;
 /**
  * 1 тугрик
  */
-public class MailMessage {
+public class MailMessage extends Mail<String> {
+
+    public MailMessage(String sender, String recipient, String content) {
+        super(sender, recipient, content);
+    }
 
 }
