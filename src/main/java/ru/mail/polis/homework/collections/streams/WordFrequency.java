db447e2ea54026e8df138624f8e8498572b75121
Artem Sheverdyaev <artem-sheverdyaev@mail.ru>
2023-04-13 15:19:47 +0300

    Did WordFrequency

--- src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
@@ -1,6 +1,13 @@
 package ru.mail.polis.homework.collections.streams;
 
+import ru.mail.polis.homework.io.blocking.StructureOutputStream;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -20,8 +27,16 @@ public class WordFrequency {
     /**
      * Задачу можно решить без единого условного оператора, только с помощью стримов.
      */
-    public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
+    public static List<String> wordFrequency(Stream<String> lines){
+        return lines.flatMap(str -> Arrays.stream(str.split("[.,!:-?; ]")))
+                .filter(Predicate.not(String::isEmpty))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
 
