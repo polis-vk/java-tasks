0d15602b3606b62fb3be9d7354ec87a546bb649c
xayblln <askarxay@yandex.ru>
2023-04-17 18:37:13 +0300

    copyFile

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,14 +1,47 @@
 package ru.mail.polis.homework.io;
 
-public class CopyFile {
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
+public class CopyFile {
     /**
      * Реализовать копирование папки из pathFrom в pathTo. Скопировать надо все внутренности
      * Файлы копировать ручками через стримы. Используем новый API
      * В тесте для создания нужных файлов для первого запуска надо раскомментировать код в setUp()
      * 3 тугрика
      */
-    public static void copyFiles(String pathFrom, String pathTo) {
+    public static void copyFiles(String pathFrom, String pathTo) throws IOException {
+        Path copyFrom = Paths.get(pathFrom);
+        if (Files.notExists(copyFrom)) {
+            return;
+        }
+        Path copyTo = Paths.get(pathTo);
+        if (Files.notExists(copyTo)) {
+            Files.createDirectories(Files.isDirectory(copyTo) ? copyTo : copyTo.getParent());
+        }
+        Files.walkFileTree(copyFrom, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                Files.createDirectories(copyTo.resolve(copyFrom.relativize(dir)));
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                copyOneFile(file, copyTo.resolve(copyFrom.relativize(file)));
+                return FileVisitResult.CONTINUE;
+            }
+        });
     }
 
+    private static void copyOneFile(Path copyFrom, Path copyTo) throws IOException {
+        try (BufferedInputStream inStream = new BufferedInputStream(Files.newInputStream(copyFrom)); BufferedOutputStream outStream = new BufferedOutputStream(Files.newOutputStream(copyTo))) {
+            byte[] bytes = new byte[inStream.available()];
+            int length;
+            while ((length = inStream.read(bytes)) != -1) {
+                outStream.write(bytes, 0, length);
+            }
+        }
+    }
 }
