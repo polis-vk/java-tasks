a3aa602d7334cb96268add5999502c33e710df84
Artem Shevtsov <shevtsov.ae@edu.spbstu.ru>
2023-04-21 02:47:55 +0300

    Задание 6(без робота)

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,13 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class CopyFile {
 
     /**
@@ -8,7 +16,50 @@ public class CopyFile {
      * В тесте для создания нужных файлов для первого запуска надо раскомментировать код в setUp()
      * 3 тугрика
      */
-    public static void copyFiles(String pathFrom, String pathTo) {
+    public static void copyFiles(String pathFrom, String pathTo) throws IOException {
+        Path from = Paths.get(pathFrom);
+        Path to = Paths.get(pathTo);
+        if (!Files.exists(from)) {
+            return;
+        }
+        if (Files.isRegularFile(from)) {
+            Files.createDirectories(to.getParent());
+        }
+        try {
+            copyDirectory(from, to);
+        } catch (IOException E) {
+            E.printStackTrace();
+        }
     }
 
+    private static void copyDirectory(Path source, Path target) throws IOException {
+        if (Files.isRegularFile(source)) {
+            Files.createFile(target);
+            copyFile(source, target);
+        } else {
+            if (Files.notExists(target)) {
+                Files.createDirectories(target);
+            }
+            try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(source)) {
+                for (Path path : dirStream) {
+                    copyDirectory(path, target.resolve(path.getFileName()));
+                }
+            }
+        }
+    }
+
+    private static void copyFile(Path source, Path target) {
+        try (InputStream inputStream = Files.newInputStream(source);
+             OutputStream outputStream = Files.newOutputStream(target)) {
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,9 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Directories {
 
@@ -12,8 +15,23 @@ public class Directories {
      * Написать двумя способами. С использованием File
      * 2 тугрика
      */
+
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        if (!file.exists()) {
+            return 0;
+        }
+        if (file.isFile()) {
+            file.delete();
+            return 1;
+        }
+        int counter = 0;
+        for (File f : file.listFiles()) {
+            counter += removeWithFile(f.getPath());
+        }
+        file.delete();
+        return counter + 1;
+
     }
 
     /**
@@ -21,6 +39,29 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path directory = Paths.get(path);
+        if (!Files.exists(directory)) {
+            return 0;
+        }
+        final int[] counter = new int[]{0};
+        SimpleFileVisitor<Path> fileVisitor = new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (attrs.isRegularFile()) {
+                    Files.delete(file);
+                    counter[0]++;
+                }
+                return FileVisitResult.CONTINUE;
+            }
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                counter[0]++;
+                return FileVisitResult.CONTINUE;
+            }
+        };
+        Files.walkFileTree(directory, fileVisitor);
+        return counter[0];
     }
 }
+
