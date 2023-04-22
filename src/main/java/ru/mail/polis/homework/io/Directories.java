bcdfe26d8e7055fc853ee1dedb442e56ba62e9b8
NadirCianid <den4ik.strelok@gmail.com>
2023-04-22 10:59:50 +0300

    реализовал задачу с удалением файлов/директорий

--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,9 +1,16 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 public class Directories {
-
 
     /**
      * Реализовать рекурсивное удаление всех файлов и директорий из директории по заданному пути.
@@ -13,7 +20,19 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithFile(String path) {
-        return 0;
+        AtomicInteger deletedAmount = new AtomicInteger();
+        File fileForDeleting = new File(path);
+        if (fileForDeleting.exists()) {
+            if (fileForDeleting.isDirectory()) {
+                File[] content = fileForDeleting.listFiles();
+                Arrays.stream(content).forEach(file -> deletedAmount.addAndGet(removeWithFile(file.getPath())));
+            }
+
+            deletedAmount.getAndIncrement();
+            fileForDeleting.delete();
+        }
+
+        return deletedAmount.get();
     }
 
     /**
@@ -21,6 +40,28 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        AtomicInteger deletedAmount = new AtomicInteger();
+
+        Path pathForDeleting = Paths.get(path);
+        if (Files.isDirectory(pathForDeleting)) {
+            try (Stream<Path> content = Files.list(pathForDeleting)) {
+                content.forEach(filePath ->
+                {
+                    try {
+                        deletedAmount.addAndGet(removeWithPath(filePath.toString()));
+                    } catch (IOException e) {
+                        System.out.println("Input|Output exception");
+                    }
+                });
+            }
+        }
+
+        try {
+            Files.delete(pathForDeleting);
+            deletedAmount.getAndIncrement();
+        } catch (NoSuchFileException exception) {
+            System.out.println("No such file");
+        }
+        return deletedAmount.get();
     }
 }
