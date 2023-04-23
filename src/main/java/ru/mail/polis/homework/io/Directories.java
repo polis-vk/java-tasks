ebd050ae67be6bf962ff63bb8a4c6c68f51917bc
polainait <polaina.mezentseva@mail.ru>
2023-04-23 12:27:55 +0300

    Done class Directories

--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,10 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Directories {
 
@@ -13,7 +17,18 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithFile(String path) {
-        return 0;
+        File file = new File(path);
+        if (!file.exists()) {
+            return 0;
+        }
+        int result = 0;
+        if (file.isDirectory()) {
+            for (File temp : file.listFiles()) {
+                result += removeWithFile(temp.toString());
+            }
+        }
+        file.delete();
+        return ++result;
     }
 
     /**
@@ -21,6 +36,26 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+        Path dir = Paths.get(path);
+        if (!Files.exists(dir)) {
+            return 0;
+        }
+        AtomicInteger result = new AtomicInteger();
+        Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                result.incrementAndGet();
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException e) throws IOException {
+                Files.delete(dir);
+                result.incrementAndGet();
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return result.get();
     }
 }
