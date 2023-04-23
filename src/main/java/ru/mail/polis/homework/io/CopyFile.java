169eee7c09a7f628554f16a606eaf9addd132383
NadirCianid <den4ik.strelok@gmail.com>
2023-04-23 20:02:46 +0300

    реализовал задачу с копированием

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,11 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class CopyFile {
 
     /**
@@ -9,6 +15,25 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
-    }
+        Path dest = Paths.get(pathTo);
+        Path source = Paths.get(pathFrom);
 
+        if (Files.exists(source)) {
+            try {
+                if (Files.isDirectory(source)) {
+                    Files.createDirectories(dest);
+                    try (Stream<Path> contentInStream = Files.list(source)) {
+                        contentInStream.forEach(path -> {
+                            copyFiles(path.toString(), dest.resolve(source.relativize(path)).toString()); //копирование файлов из источника в целевую папку по составному пути
+                        });
+                    }
+                } else {
+                    Files.createDirectories(dest.getParent());
+                    Files.copy(source, dest);
+                }
+            } catch (IOException e) {
+                System.out.println("IOException");
+            }
+        }
+    }
 }
