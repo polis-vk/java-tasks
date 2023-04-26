8149f4ee1f05414c39a74eb6fccaebc6b40fafa5
whoscared <coklook1@gmail.com>
2023-04-26 04:17:17 +0300

    Исправила ошибки Directories

--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,7 +1,10 @@
 package ru.mail.polis.homework.io;
 
-import java.io.*;
-import java.nio.file.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public class Directories {
