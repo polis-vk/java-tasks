07c427d580e2fc9a763393ccaddf182ab81f5659
polainait <polaina.mezentseva@mail.ru>
2023-04-27 17:19:21 +0300

    Made single imports

--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -2,7 +2,11 @@ package ru.mail.polis.homework.io;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.atomic.AtomicInteger;
 
