d52a066dc2670f6acaf47ed7c4f4e0db98b774c1
Ivann <nemchinov.im@edu.spbstu.ru>
2023-04-27 10:30:11 +0300

    done hw6

--- src/main/java/ru/mail/polis/homework/io/CopyFile.java
+++ src/main/java/ru/mail/polis/homework/io/CopyFile.java
@@ -1,5 +1,11 @@
 package ru.mail.polis.homework.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class CopyFile {
 
     /**
@@ -9,6 +15,51 @@ public class CopyFile {
      * 3 тугрика
      */
     public static void copyFiles(String pathFrom, String pathTo) {
+        Path srcDirectory = Paths.get(pathFrom).getParent();
+        Path destDirectory = Paths.get(pathTo).getParent();
+        if (Files.notExists(srcDirectory)) {
+            return;
+        }
+        if (Files.notExists(destDirectory)) {
+            try {
+                Files.createDirectories(destDirectory);
+            } catch (IOException e) {
+                System.err.println("ERROR : " + e.getMessage());
+            }
+        }
+        try {
+            Files.walkFileTree(srcDirectory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    try {
+                        Files.createDirectory(destDirectory.resolve(srcDirectory.relativize(dir)));
+                    } catch (FileAlreadyExistsException e) {
+                        System.out.println("Error: " + e);
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Path copiedFile = Files.createFile(srcDirectory.resolve(srcDirectory.relativize(file)));
+                    copyFileContent(file, copiedFile);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            System.err.println("ERROR : " + e.getMessage());
+        }
     }
 
+    public static void copyFileContent(Path fileFrom, Path fileTo) throws IOException {
+        try (InputStream is = Files.newInputStream(fileFrom)) {
+            try (OutputStream outputStream = Files.newOutputStream(fileTo)) {
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = is.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+            }
+        }
+    }
 }
--- src/main/java/ru/mail/polis/homework/io/Directories.java
+++ src/main/java/ru/mail/polis/homework/io/Directories.java
@@ -1,6 +1,12 @@
 package ru.mail.polis.homework.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Directories {
 
@@ -13,7 +19,21 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithFile(String path) {
-        return 0;
+
+        int count = 0;
+        File directory = new File(path);
+        if (!directory.exists()) {
+            return count;
+        }
+
+        if (directory.isDirectory()) {
+            for (File file : Objects.requireNonNull(directory.listFiles(Objects::nonNull))) {
+                count += removeWithFile(file.toString());
+            }
+        }
+
+        directory.delete();
+        return ++count;
     }
 
     /**
@@ -21,6 +41,22 @@ public class Directories {
      * 2 тугрика
      */
     public static int removeWithPath(String path) throws IOException {
-        return 0;
+
+        int count = 0;
+        Path directory = Paths.get(path);
+        if (Files.notExists(directory)) {
+            return count;
+        }
+
+        if (Files.isDirectory(directory)) {
+            try (Stream<Path> pathStream = Files.list(directory)) {
+                List<Path> directoryPaths = pathStream.collect(Collectors.toList());
+                for (Path directoryPath : directoryPaths) {
+                    count += removeWithPath(directoryPath.toString());
+                }
+            }
+        }
+        Files.delete(directory);
+        return ++count;
     }
 }
