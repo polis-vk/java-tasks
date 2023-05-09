672f237a0faa27b6cc3c06d2c975ffec956f9526
Boris <Boris_siobko@mail.ru>
2023-05-09 20:50:54 +0300

    done Book && Library

--- src/main/java/ru/mail/polis/homework/retake/first/collection/Book.java
+++ src/main/java/ru/mail/polis/homework/retake/first/collection/Book.java
@@ -4,4 +4,19 @@ package ru.mail.polis.homework.retake.first.collection;
  * Реализовать класс книги
  */
 public class Book {
+    private final String author;
+    private final Integer year;
+
+    Book(String author, Integer year) {
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
--- src/main/java/ru/mail/polis/homework/retake/first/collection/Library.java
+++ src/main/java/ru/mail/polis/homework/retake/first/collection/Library.java
@@ -1,86 +1,99 @@
 package ru.mail.polis.homework.retake.first.collection;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * Реализовать класс библиотеки, которая в себе хранит книги.
  * Книги в библиотеку можно добавлять и удалять из нее.
- *
- *
+ * <p>
+ * <p>
  * Для тех, кто хочет получить 3
  * Упор делается на время работы программы. Память не важна
- *
+ * <p>
  * Для тех, кто хочет получить 4
  * Упор делается на память, время работы не важно
- *
+ * <p>
  * Для тех, кто хочет получить 5
  * Надо найти баланс, память важнее, но за временем тоже надо следить
  */
 public class Library implements Iterable<Book> {
+    ArrayList<Book> books;
 
     /**
      * Добавляем книгу в библиотеку
      */
     public boolean addBook(Book book) {
-        return false;
+        return books.add(book);
     }
 
     /**
      * Удаляем книгу из библиотеки
      */
     public boolean removeBook(Book book) {
-        return false;
+        return books.remove(book);
     }
 
     /**
      * Получаем список книг заданного автора
      */
     public List<Book> getBooksByAuthor(String author) {
-        return Collections.emptyList();
+        ArrayList<Book> result = new ArrayList<>();
+
+        for (Book book : books) {
+            if (Objects.equals(book.getAuthor(), author)) {
+                result.add(book);
+            }
+        }
+        return result;
     }
 
     /**
      * Получаем список книг написанных в определенный год
      */
     public List<Book> getBooksByDate(int year) {
-        return Collections.emptyList();
+        ArrayList<Book> result = new ArrayList<>();
+
+        for (Book book : books) {
+            if (Objects.equals(book.getYear(), year)) {
+                result.add(book);
+            }
+        }
+        return result;
     }
 
     /**
      * Получаем книгу, которую последней добавили в библиотеку
      */
     public Book getLastBook() {
-        return null;
+        return books.get(books.size() - 1);
     }
 
     /**
      * ЗАДАНИЕ ТОЛЬКО ДЛЯ ТЕХ, КТО ХОЧЕТ ПОЛУЧИТЬ 3 или 4
-     *
+     * <p>
      * Возвращается итератор, который бегает по всем книгам, удовлетворяющим предикату
      */
     public Iterator<Book> iterator(Predicate<Book> predicate) {
-        return null;
+        return books.stream().filter(predicate).iterator();
     }
 
     /**
-     *
      * Возвращается итератор, который бегает по всем книгам в порядке добавления
      */
     @Override
     public Iterator<Book> iterator() {
-        return null;
+        return books.iterator();
     }
 
     /**
      * ЗАДАНИЕ ТОЛЬКО ДЛЯ ТЕХ, КТО ХОЧЕТ ПОЛУЧИТЬ 5
-     *
+     * <p>
      * Возвращается итератор, который бегает по всем парам (индекс, книга), удовлетворяющим предикату
      */
     public Iterator<Book> iterator(BiPredicate<Integer, Book> predicate) {
-        return null;
+        return books.stream().filter(book -> predicate.test(books.indexOf(book), book)).iterator();
     }
 }
