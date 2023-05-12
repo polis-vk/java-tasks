f8fef0792f5aa18f0ad6cb696a89997b57a35a5b
Flusova Sofia <flusova.sa@edu.spbstu.ru>
2023-05-12 20:47:11 +0300

    initial commit HWextra

--- src/main/java/ru/mail/polis/homework/retake/first/collection/Book.java
+++ src/main/java/ru/mail/polis/homework/retake/first/collection/Book.java
@@ -4,4 +4,48 @@ package ru.mail.polis.homework.retake.first.collection;
  * Реализовать класс книги
  */
 public class Book {
+    private String title;
+    private String author;
+    private int year;
+    private int pages;
+    public Book(String title, String author, int year, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    public String printBook() {
+        return "Title: " + title + ", Author: " + author + ", Pages: " + pages;
+    }
+
 }
--- src/main/java/ru/mail/polis/homework/retake/first/collection/Library.java
+++ src/main/java/ru/mail/polis/homework/retake/first/collection/Library.java
@@ -1,19 +1,21 @@
 package ru.mail.polis.homework.retake.first.collection;
 
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+import java.util.Vector;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Реализовать класс библиотеки, которая в себе хранит книги.
  * Книги в библиотеку можно добавлять и удалять из нее.
- *
+ *</p>
  *
  * Для тех, кто хочет получить 3
  * Упор делается на время работы программы. Память не важна
- *
+ *</p>
  * Для тех, кто хочет получить 4
  * Упор делается на память, время работы не важно
  *
@@ -21,40 +23,44 @@ import java.util.function.Predicate;
  * Надо найти баланс, память важнее, но за временем тоже надо следить
  */
 public class Library implements Iterable<Book> {
-
+    private Vector<Book> storage_;
     /**
      * Добавляем книгу в библиотеку
      */
     public boolean addBook(Book book) {
-        return false;
+        return storage_.add(book);
     }
 
     /**
      * Удаляем книгу из библиотеки
      */
     public boolean removeBook(Book book) {
-        return false;
+        return storage_.remove(book);
     }
 
     /**
      * Получаем список книг заданного автора
      */
     public List<Book> getBooksByAuthor(String author) {
-        return Collections.emptyList();
+        return storage_.stream()
+                .filter(book -> Objects.equals(book.getAuthor(), author))
+                .collect(Collectors.toList());
     }
 
     /**
      * Получаем список книг написанных в определенный год
      */
     public List<Book> getBooksByDate(int year) {
-        return Collections.emptyList();
+        return storage_.stream()
+                .filter(book -> Objects.equals(book.getYear(), year))
+                .collect(Collectors.toList());
     }
 
     /**
      * Получаем книгу, которую последней добавили в библиотеку
      */
     public Book getLastBook() {
-        return null;
+        return storage_.lastElement();
     }
 
     /**
@@ -63,7 +69,13 @@ public class Library implements Iterable<Book> {
      * Возвращается итератор, который бегает по всем книгам, удовлетворяющим предикату
      */
     public Iterator<Book> iterator(Predicate<Book> predicate) {
-        return null;
+        Vector<Book> filteredBooks = new Vector<>();
+        for (Book book : storage_) {
+            if (predicate.test(book)) {
+                filteredBooks.add(book);
+            }
+        }
+        return filteredBooks.iterator();
     }
 
     /**
@@ -72,7 +84,7 @@ public class Library implements Iterable<Book> {
      */
     @Override
     public Iterator<Book> iterator() {
-        return null;
+        return storage_.iterator();
     }
 
     /**
@@ -81,6 +93,8 @@ public class Library implements Iterable<Book> {
      * Возвращается итератор, который бегает по всем парам (индекс, книга), удовлетворяющим предикату
      */
     public Iterator<Book> iterator(BiPredicate<Integer, Book> predicate) {
-        return null;
+        return storage_.stream()
+                .filter(book -> predicate.test(storage_.indexOf(book), book))
+                .iterator();
     }
 }
