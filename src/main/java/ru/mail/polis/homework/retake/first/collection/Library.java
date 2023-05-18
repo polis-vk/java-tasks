e6841a290637a25ddf9753097ec3416c238a7edb
Ivann <nemchinov.im@edu.spbstu.ru>
2023-05-18 12:43:10 +0300

    small corrections

--- src/main/java/ru/mail/polis/homework/retake/first/collection/Library.java
+++ src/main/java/ru/mail/polis/homework/retake/first/collection/Library.java
@@ -10,18 +10,19 @@ import java.util.stream.IntStream;
 /**
  * Реализовать класс библиотеки, которая в себе хранит книги.
  * Книги в библиотеку можно добавлять и удалять из нее.
- *
- *
+ * <p>
+ * <p>
  * Для тех, кто хочет получить 3
  * Упор делается на время работы программы. Память не важна
- *
+ * <p>
  * Для тех, кто хочет получить 4
  * Упор делается на память, время работы не важно
- *
+ * <p>
  * Для тех, кто хочет получить 5
  * Надо найти баланс, память важнее, но за временем тоже надо следить
  */
 public class Library implements Iterable<Book> {
+
     private final List<Book> books;
     private final List<Pair<String, List<Book>>> booksSortedByAuthor;
     private final List<Pair<Integer, List<Book>>> booksSortedByYear;
@@ -42,21 +43,18 @@ public class Library implements Iterable<Book> {
 
         books.add(book);
 
-        int addIndexByAuthor = binSearchAuthor(book.getAuthor());
+        int addIndexByAuthor = binarySearchInPairList(booksSortedByAuthor, book.getAuthor());
         if (!checkForEqualityByIndex(booksSortedByAuthor, book.getAuthor(), addIndexByAuthor)) {
             booksSortedByAuthor.add(addIndexByAuthor, new Pair<>(book.getAuthor(), new ArrayList<>()));
-            booksSortedByAuthor.get(addIndexByAuthor).second.add(book);
-        } else {
-            booksSortedByAuthor.get(addIndexByAuthor).second.add(book);
         }
+        booksSortedByAuthor.get(addIndexByAuthor).second.add(book);
 
-        int addIndexByYear = binSearchYear(book.getYear());
+        int addIndexByYear = binarySearchInPairList(booksSortedByYear, book.getYear());
         if (!checkForEqualityByIndex(booksSortedByYear, book.getYear(), addIndexByYear)) {
             booksSortedByYear.add(addIndexByYear, new Pair<>(book.getYear(), new ArrayList<>()));
-            booksSortedByYear.get(addIndexByYear).second.add(book);
-        } else {
-            booksSortedByYear.get(addIndexByYear).second.add(book);
         }
+        booksSortedByYear.get(addIndexByYear).second.add(book);
+
         return true;
     }
 
@@ -71,17 +69,18 @@ public class Library implements Iterable<Book> {
             return false;
         }
 
-        int indexOfAuthorsBooks = binSearchAuthor(book.getAuthor());
+        int indexOfAuthorsBooks = binarySearchInPairList(booksSortedByAuthor, book.getAuthor());
         booksSortedByAuthor.get(indexOfAuthorsBooks).second.remove(book);
         if (booksSortedByAuthor.get(indexOfAuthorsBooks).second.isEmpty()) {
             booksSortedByAuthor.remove(indexOfAuthorsBooks);
         }
 
-        int indexOfYearsBooks = binSearchYear(book.getYear());
+        int indexOfYearsBooks = binarySearchInPairList(booksSortedByYear, book.getYear());
         booksSortedByYear.get(indexOfYearsBooks).second.remove(book);
         if (booksSortedByYear.get(indexOfYearsBooks).second.isEmpty()) {
             booksSortedByYear.remove(indexOfYearsBooks);
         }
+
         return true;
     }
 
@@ -89,7 +88,7 @@ public class Library implements Iterable<Book> {
      * Получаем список книг заданного автора
      */
     public List<Book> getBooksByAuthor(String author) {
-        int indexOfAuthor = binSearchAuthor(author);
+        int indexOfAuthor = binarySearchInPairList(booksSortedByAuthor, author);
         if (!checkForEqualityByIndex(booksSortedByAuthor, author, indexOfAuthor)) {
             return new ArrayList<>();
         }
@@ -100,7 +99,7 @@ public class Library implements Iterable<Book> {
      * Получаем список книг написанных в определенный год
      */
     public List<Book> getBooksByDate(int year) {
-        int indexOfYear = binSearchYear(year);
+        int indexOfYear = binarySearchInPairList(booksSortedByYear, year);
         if (!checkForEqualityByIndex(booksSortedByYear, year, indexOfYear)) {
             return new ArrayList<>();
         }
@@ -119,7 +118,7 @@ public class Library implements Iterable<Book> {
 
     /**
      * ЗАДАНИЕ ТОЛЬКО ДЛЯ ТЕХ, КТО ХОЧЕТ ПОЛУЧИТЬ 3 или 4
-     *
+     * <p>
      * Возвращается итератор, который бегает по всем книгам, удовлетворяющим предикату
      */
     public Iterator<Book> iterator(Predicate<Book> predicate) {
@@ -136,70 +135,42 @@ public class Library implements Iterable<Book> {
 
     /**
      * ЗАДАНИЕ ТОЛЬКО ДЛЯ ТЕХ, КТО ХОЧЕТ ПОЛУЧИТЬ 5
-     *
+     * <p>
      * Возвращается итератор, который бегает по всем парам (индекс, книга), удовлетворяющим предикату
      */
     public Iterator<Book> iterator(BiPredicate<Integer, Book> predicate) {
         return IntStream.range(0, books.size())
-                .boxed()
                 .filter(index -> predicate.test(index, books.get(index)))
-                .map(books::get)
+                .mapToObj(books::get)
                 .iterator();
     }
 
-    private int binSearchYear(int element) {
-        if (booksSortedByYear.isEmpty()) {
+    private <T extends Comparable<T>> int binarySearchInPairList(List<Pair<T, List<Book>>> list, T element) {
+        if (list.isEmpty()) {
             return 0;
         }
 
         int leftIndex = 0;
-        int rightIndex = booksSortedByYear.size() - 1;
-        int middleIndex = 0;
+        int rightIndex = list.size() - 1;
+        int middleIndex;
 
         while (leftIndex <= rightIndex) {
             middleIndex = (leftIndex + rightIndex) / 2;
 
-            if (element == booksSortedByYear.get(middleIndex).first)
+            if (element.equals(list.get(middleIndex).first)) {
                 return middleIndex;
-            else if (element < booksSortedByYear.get(middleIndex).first)
+            }
+            if (element.compareTo(list.get(middleIndex).first) < 0) {
                 rightIndex = middleIndex - 1;
-            else
+            } else {
                 leftIndex = middleIndex + 1;
+            }
         }
 
-        if (element > booksSortedByYear.get(middleIndex).first) {
-            ++middleIndex;
-        }
-        return middleIndex;
+        return leftIndex;
     }
 
-    private int binSearchAuthor(String element) {
-        if (booksSortedByAuthor.isEmpty()) {
-            return 0;
-        }
-
-        int leftIndex = 0;
-        int rightIndex = booksSortedByAuthor.size() - 1;
-        int middleIndex = 0;
-
-        while (leftIndex <= rightIndex) {
-            middleIndex = (leftIndex + rightIndex) / 2;
-
-            if (element.equals(booksSortedByAuthor.get(middleIndex).first))
-                return middleIndex;
-            else if (element.compareTo(booksSortedByAuthor.get(middleIndex).first) < 0)
-                rightIndex = middleIndex - 1;
-            else
-                leftIndex = middleIndex + 1;
-        }
-
-        if (element.compareTo(booksSortedByAuthor.get(middleIndex).first) > 0) {
-            ++middleIndex;
-        }
-        return middleIndex;
-    }
-
-    private <K> boolean checkForEqualityByIndex(List<Pair<K, List<Book>>> list, K key, int index) {
+    private <T> boolean checkForEqualityByIndex(List<Pair<T, List<Book>>> list, T key, int index) {
         if (list.isEmpty()) {
             return false;
         }
