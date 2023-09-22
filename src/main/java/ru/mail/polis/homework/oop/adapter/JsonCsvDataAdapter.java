829d16870bd478c1e56b930eb83456bb1b013137
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2023-09-22 07:36:19 +0300

    Add Adapter

--- src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
+++ src/main/java/ru/mail/polis/homework/oop/adapter/JsonCsvDataAdapter.java
@@ -4,5 +4,25 @@ package ru.mail.polis.homework.oop.adapter;
  * После имплементации раскоментировать тест в ru.mail.polis.homework.oop.adapter.ProductPriceParsServiceTest
  * для проверки правильной имплементации
  */
-public class JsonCsvDataAdapter {
+public class JsonCsvDataAdapter extends CsvData{
+    public JsonCsvDataAdapter(String text) {
+        super(text);
+
+    }
+
+    public static String transform(String text)
+    {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < text.length(); ++i)
+        {
+            if(text.charAt(i) != ',' && text.charAt(i) != '"' && text.charAt(i) != '{'
+            && text.charAt(i) != '}')
+            {
+                if(text.charAt(i) == ':') builder.append(',');
+                else builder.append(text.charAt(i));
+            }
+        }
+        return builder.toString();
+    }
+
 }
